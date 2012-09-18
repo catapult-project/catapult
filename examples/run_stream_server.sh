c109f14bb2d1cb79eefde5c7b07ddd5d58209bb6
Nat Duca <nduca@chromium.org>
2012-09-18 01:17:30 +0000

    Fix licenses

--- examples/run_stream_server.sh
+++ examples/run_stream_server.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 BASEDIR=$(dirname $0)
 SERVERPATH=$BASEDIR/stream_server/standalone.py
 SERVERROOT=$BASEDIR/../
--- examples/stream_server/handlers/stream_wsh.py
+++ examples/stream_server/handlers/stream_wsh.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 import time
 
 def web_socket_do_extra_handshake(request):
