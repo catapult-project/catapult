c4c5035f387069ecc55f5a568583a076743fb92c
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-23 09:20:10 +0000

    Telemetry: adding memory tests & solving problems on the go.
    BUG=158323
    
    Review URL: https://codereview.chromium.org/11416134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169322 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ telemetry/telemetry/click_element_interaction.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import time
+
+from telemetry import page_interaction
+from telemetry import util
+
+class ClickElementInteraction(page_interaction.PageInteraction):
+  def __init__(self, attributes=None):
+    super(ClickElementInteraction, self).__init__(attributes)
+
+  def PerformInteraction(self, page, tab):
+    def DoClick():
+      assert hasattr(self, 'selector') or hasattr(self, 'text')
+      if hasattr(self, 'selector'):
+        code = 'document.querySelector(\'' + self.selector + '\').click();'
+        tab.runtime.Execute(code)
+      else:
+        click_element = """
+            function clickElement(element, text) {
+              if (element.innerHTML == text) {
+                element.click();
+                return true;
+              }
+              for (var i in element.childNodes) {
+                if (clickElement(element.childNodes[i], text))
+                  return true;
+              }
+              return false;
+            }"""
+        tab.runtime.Execute(click_element)
+        code = 'clickElement(document, "' + self.text + '");'
+        if not tab.runtime.Evaluate(code):
+          raise page_interaction.PageInteractionFailed(
+              'Cannot find element with text ' + self.text)
+
+    if hasattr(self, 'wait_for_navigate'):
+      tab.page.PerformActionAndWaitForNavigate(DoClick)
+    elif hasattr(self, 'wait_for_href_change'):
+      old_url = tab.runtime.Evaluate('document.location.href')
+      DoClick()
+      util.WaitFor(lambda: tab.runtime.Evaluate(
+          'document.location.href') != old_url, 60)
+    elif hasattr(self, 'wait_seconds'):
+      time.sleep(self.wait_seconds)
+      DoClick()
+    else:
+      DoClick()
+
+    tab.WaitForDocumentReadyStateToBeComplete()
