ff29ea16762ffc96c9f7f2d913be38efd7ba1b6e
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 11:09:33 +0000

    Telemetry: fixes InspectorTimeline.testGotTimeline
    This test was flaky due to image caching.
    Simplify it and just ensure rAF is triggered and accounted in the timeline.
    
    BUG=170271
    TEST=tools/telemetry/run_tests --browser=system testGotTimeline
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/11968014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177387 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/inspector_timeline_unittest.py
+++ telemetry/telemetry/inspector_timeline_unittest.py
@@ -112,20 +112,14 @@ class InspectorTimelineTabTest(tab_test_case.TabTestCase):
     util.WaitFor(_IsDone, 5)
 
   def testGotTimeline(self):
-    self._StartServer()
-    image_url = self._browser.http_server.UrlOf('image.png')
     with InspectorTimeline.Recorder(self._tab):
       self._tab.runtime.Execute(
 """
 var done = false;
-var i = document.createElement('img');
-i.src = '%s';
-document.body.appendChild(i);
 window.webkitRequestAnimationFrame(function() { done = true; });
-""" % image_url)
+""")
       self._WaitForAnimationFrame()
 
-    r = self._tab.timeline_model.GetAllOfName('DecodeImage')
+    r = self._tab.timeline_model.GetAllOfName('FireAnimationFrame')
     self.assertTrue(len(r) > 0)
-    self.assertEquals(r[0].args['data']['imageType'], 'PNG')
     self.assertTrue(r[0].duration_ms > 0)
