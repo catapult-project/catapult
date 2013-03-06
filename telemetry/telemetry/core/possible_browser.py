c4726dca1a207eccc7712ed49d8520277b7423f1
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 23:33:41 +0000

    Make browser_type a property of PossibleBrowser.
    BUG=NONE
    TEST=run_tests
    NOTRY=true
    Review URL: https://codereview.chromium.org/12479003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186537 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/possible_browser.py
+++ telemetry/telemetry/core/possible_browser.py
@@ -8,11 +8,15 @@ class PossibleBrowser(object):
   """
 
   def __init__(self, browser_type, options):
-    self.browser_type = browser_type
+    self._browser_type = browser_type
     self._options = options
 
   def __repr__(self):
     return 'PossibleBrowser(browser_type=%s)' % self.browser_type
+
+  @property
+  def browser_type(self):
+    return self._browser_type
 
   @property
   def options(self):
