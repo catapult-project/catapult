56e799eb50ca931ad3a3189d8d40f8223dc91d3f
tonyg@google.com <tonyg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 00:22:01 +0000

    [Telemetry] Memory stats should be in bytes.
    ps on mac was reporting them in kb. The strip() was unnessary because split()
    is greedy on spaces.
    
    BUG=196411
    TEST=tools/perf/run_multipage_benchmarks --browser=system page_cycler tools/perf/page_sets/page_cycler/moz.json
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/12888004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189171 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/chrome/mac_platform_backend.py
+++ telemetry/telemetry/core/chrome/mac_platform_backend.py
@@ -32,9 +32,9 @@ class MacPlatformBackend(platform_backend.PlatformBackend):
     vm_stat = subprocess.Popen(['vm_stat'],
                                stdout=subprocess.PIPE).communicate()[0]
     for stat in vm_stat.splitlines():
-      key_value = stat.split(':')
-      if key_value[0] == 'Pages active':
-        pages_active = int(key_value[1].strip()[:-1])  # Strip trailing '.'
+      key, value = stat.split(':')
+      if key == 'Pages active':
+        pages_active = int(value.strip()[:-1])  # Strip trailing '.'
         return pages_active * resource.getpagesize() / 1024
     return 0
 
@@ -42,19 +42,20 @@ class MacPlatformBackend(platform_backend.PlatformBackend):
     pid_rss_vsz_list = subprocess.Popen(['ps', '-e', '-o', 'pid=,rss=,vsz='],
                                         stdout=subprocess.PIPE).communicate()[0]
     for pid_rss_vsz in pid_rss_vsz_list.splitlines():
-      pid_rss_vsz = pid_rss_vsz.split()
-      if int(pid_rss_vsz[0]) == pid:
-        return {'VM': int(pid_rss_vsz[2].strip()),
-                'WorkingSetSize': int(pid_rss_vsz[1].strip())}
+      curr_pid, rss, vsz = pid_rss_vsz.split()
+      if int(curr_pid) == pid:
+        return {'VM': 1024 * int(vsz),
+                'WorkingSetSize': 1024 * int(rss)}
     return {}
 
   def GetChildPids(self, pid):
     """Retunds a list of child pids of |pid|."""
     child_pids = []
-    pid_ppid_list = subprocess.Popen(['ps', '-e', '-o', 'pid=', '-o', 'ppid='],
+    pid_ppid_list = subprocess.Popen(['ps', '-e', '-o', 'pid=,ppid='],
                                      stdout=subprocess.PIPE).communicate()[0]
     for pid_ppid in pid_ppid_list.splitlines():
-      pid_ppid = pid_ppid.split()
-      if int(pid_ppid[1].strip()) == pid:
-        child_pids.append(int(pid_ppid[0].strip()))
+      curr_pid, curr_ppid = pid_ppid.split()
+      if int(curr_ppid) == pid:
+        child_pids.append(int(curr_pid))
+        child_pids.extend(self.GetChildPids(int(curr_pid)))
     return child_pids
