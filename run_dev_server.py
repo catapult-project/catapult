f8394d50ea7b8d76ffa2b942baf3738835b09ce0
dan sinclair <dj2@everburning.com>
2013-04-01 23:50:56 +0000

    Don't rebuild deps and about_tracing on every request.
    Currently, both deps and about_tracing will get regenerated for
    every JS test file included in tests.html. This causes a full reload
    of the tests page to take ~9-10 seconds.
    
    With this patch, we'll rebuild the deps and about_tracing files at most
    once every 5 seconds. This is fast enough that all of the JS files in
    tests.html get loaded and try to load their deps requirement. This
    takes the loading down to ~4-5 seconds for a full reload.
    
    Review URL: https://codereview.appspot.com/8187043

--- run_dev_server.py
+++ run_dev_server.py
@@ -5,6 +5,7 @@
 import optparse
 import os
 import sys
+import time
 
 import SimpleHTTPServer
 import BaseHTTPServer
@@ -12,20 +13,22 @@ import BaseHTTPServer
 from build import calcdeps
 
 DEFAULT_PORT = 8003
+DEPS_CHECK_DELAY = 5
 
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
   def do_GET(self):
     if self.path == '/src/deps.js':
-      self.log_message('Regenerating deps')
-      calcdeps.regenerate_deps()
+      current_time = time.time()
+      if self.server.next_deps_check < current_time:
+        self.log_message('Regenerating deps')
+        self.server.next_deps_check = current_time + DEPS_CHECK_DELAY
+        calcdeps.regenerate_deps()
     return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 class Server(BaseHTTPServer.HTTPServer):
-  def handle_one_request(self):
-    try:
-      HTTPServer.handle_one_request(self)
-    except KeyboardInterrupt:
-      sys.exit(255)
+  def __init__(self, *args, **kwargs):
+    BaseHTTPServer.HTTPServer.__init__(self, *args, **kwargs)
+    self.next_deps_check = -1
 
 def Main(args):
   parser = optparse.OptionParser()
