692f9c008852277fd74f13285078bf65e826d6eb
Nat Duca <nduca@chromium.org>
2013-05-06 15:10:42 +0000

    Add vertical mode to DragHandle

--- src/ui/drag_handle.js
+++ src/ui/drag_handle.js
@@ -22,22 +22,55 @@ base.exportTo('ui', function() {
     __proto__: HTMLDivElement.prototype,
 
     decorate: function() {
-      this.lastMousePosY = 0;
+      this.lastMousePos = 0;
       this.onMouseMove_ = this.onMouseMove_.bind(this);
       this.onMouseUp_ = this.onMouseUp_.bind(this);
       this.addEventListener('mousedown', this.onMouseDown_);
       this.target = undefined;
+      this.horizontal = true;
+    },
+
+    get horizontal() {
+      return this.horizontal_;
+    },
+
+    set horizontal(h) {
+      this.horizontal_ = h;
+      if (this.horizontal_)
+        this.className = 'horizontal-drag-handle';
+      else
+        this.className = 'vertical-drag-handle';
+    },
+
+    get vertical() {
+      return !this.horizontal_;
+    },
+
+    set vertical(v) {
+      this.horizontal = !v;
     },
 
     onMouseMove_: function(e) {
       // Compute the difference in height position.
-      var dy = this.lastMousePosY - e.clientY;
+      var curMousePos = this.horizontal_ ? e.clientY : e.clientX;
+      var delta = this.lastMousePos - curMousePos;
+      var targetKey = this.horizontal_ ? 'height' : 'width';
+
       // If style is not set, start off with computed height.
-      if (!this.target.style.height)
-        this.target.style.height = window.getComputedStyle(this.target).height;
-      // Apply new height to the container.
-      this.target.style.height = parseInt(this.target.style.height) + dy + 'px';
-      this.lastMousePosY = e.clientY;
+      if (!this.target.style[targetKey])
+        this.target.style[targetKey] = window.getComputedStyle(this.target)[targetKey];
+
+      // Apply new size to the container.
+      if (this.target == this.nextSibling) {
+        this.target.style[targetKey] = parseInt(this.target.style[targetKey]) + delta + 'px';
+      } else {
+        if (this.target != this.previousSibling)
+          throw Error('Must be next sibling');
+        this.target.style[targetKey] = parseInt(this.target.style[targetKey]) - delta + 'px';
+      }
+
+
+      this.lastMousePos = curMousePos;
       e.preventDefault();
       return true;
     },
@@ -45,7 +78,7 @@ base.exportTo('ui', function() {
     onMouseDown_: function(e) {
       if (!this.target)
         return;
-      this.lastMousePosY = e.clientY;
+      this.lastMousePos = this.horizontal_ ? e.clientY : e.clientX;
       document.addEventListener('mousemove', this.onMouseMove_);
       document.addEventListener('mouseup', this.onMouseUp_);
       e.preventDefault();
