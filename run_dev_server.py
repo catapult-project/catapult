e95c588cfc51263d9ad990db3daa458321eb14dd
Nat Duca <nduca@chromium.org>
2013-05-14 04:36:57 +0000

    Obtain test list from devserver instead of hard coding it in tests.html

--- run_dev_server.py
+++ run_dev_server.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import json
 import optparse
 import os
 import sys
@@ -14,23 +15,63 @@ import BaseHTTPServer
 DEFAULT_PORT = 8003
 DEPS_CHECK_DELAY = 30
 
+toplevel_dir = os.path.abspath(os.path.dirname(__file__))
+src_dir = os.path.join(toplevel_dir, 'src')
+
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
   def __init__(self, *args, **kwargs):
     SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
 
-  def do_GET(self):
-    if self.path == '/deps.js':
-      current_time = time.time()
-      if self.server.next_deps_check < current_time:
-        self.log_message('Regenerating ' + self.path)
-        self.server.deps = deps_generator.generate_deps_js()
-        self.server.next_deps_check = current_time + DEPS_CHECK_DELAY
+  def do_GET_json_tests(self):
+    def is_test(x):
+      basename = os.path.basename(x)
+      if basename.startswith('.'):
+        return False
 
-      self.send_response(200)
-      self.send_header('Content-Type', 'application/javascript')
-      self.send_header('content-Length', len(self.server.deps))
-      self.end_headers()
-      self.wfile.write(self.server.deps)
+      if basename.endswith('_test.html'):
+        return True
+      return False
+
+    test_filenames = []
+    for dirpath, dirnames, filenames in os.walk(src_dir):
+      for f in filenames:
+        x = os.path.join(dirpath, f)
+        y = '/' + os.path.relpath(x, toplevel_dir)
+        if is_test(y):
+          test_filenames.append(y)
+
+    test_filenames.sort()
+
+    tests_as_json = json.dumps(test_filenames)
+
+    self.send_response(200)
+    self.send_header('Content-Type', 'application/json')
+    self.send_header('Cache-Control', 'no-cache')
+    self.send_header('Content-Length', len(tests_as_json))
+    self.end_headers()
+    self.wfile.write(tests_as_json)
+
+  def do_GET_deps(self):
+    current_time = time.time()
+    if self.server.next_deps_check < current_time:
+      self.log_message('Regenerating ' + self.path)
+      self.server.deps = deps_generator.generate_deps_js()
+      self.server.next_deps_check = current_time + DEPS_CHECK_DELAY
+
+    self.send_response(200)
+    self.send_header('Content-Type', 'application/javascript')
+    self.send_header('Cache-Control', 'no-cache')
+    self.send_header('Content-Length', len(self.server.deps))
+    self.end_headers()
+    self.wfile.write(self.server.deps)
+
+  def do_GET(self):
+    if self.path == '/json/tests':
+      self.do_GET_json_tests()
+      return
+
+    if self.path == '/deps.js':
+      self.do_GET_deps()
       return
 
     return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
@@ -64,4 +105,5 @@ def Main(args):
   server.serve_forever()
 
 if __name__ == '__main__':
+  os.chdir(toplevel_dir)
   sys.exit(Main(sys.argv[1:]))
