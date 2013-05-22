2527c7d356edfed633ca37104a7bae4840cdd1f4
johnjbarton@chromium.org <johnjbarton@chromium.org@0e6d7f2b-9903-5b78-7403-59d27f066143>
2013-05-22 03:52:26 +0000

    Convert base.Settings to a singleton
    base.Settings().get() or new base.Settings() then get() both work
    
    BUG=178
    R=nduca@chromium.org
    
    Review URL: https://codereview.appspot.com/9613046

--- src/base/settings.js
+++ src/base/settings.js
@@ -5,101 +5,95 @@
 'use strict';
 
 /**
- * @fileoverview Provides the Settings class.
+ * @fileoverview Provides the Settings object.
  */
 base.exportTo('base', function() {
-  var alternativeStorageInstance = undefined;
+  var storage_ = localStorage;
 
   /**
    * Settings is a simple wrapper around local storage, to make it easier
    * to test classes that have settings.
    *
+   * May be called as new base.Settings() or simply base.Settings()
    * @constructor
    */
   function Settings() {
-    if (alternativeStorageInstance) {
-      this.storage_ = alternativeStorageInstance;
-    } else {
-      this.storage_ = localStorage;
+    return Settings;
+  };
+
+  /**
+   * Get the setting with the given name.
+   *
+   * @param {string} key The name of the setting.
+   * @param {string=} opt_default The default value to return if not set.
+   * @param {string=} opt_namespace If set, the setting name will be prefixed
+   * with this namespace, e.g. "categories.settingName". This is useful for
+   * a set of related settings.
+   */
+  Settings.get = function(key, opt_default, opt_namespace) {
+    key = Settings.namespace_(key, opt_namespace);
+    var val = storage_.getItem(key);
+    if (val === null || val === undefined)
+      return opt_default;
+    return String(val);
+  },
+
+  /**
+   * Set the setting with the given name to the given value.
+   *
+   * @param {string} key The name of the setting.
+   * @param {string} value The value of the setting.
+   * @param {string=} opt_namespace If set, the setting name will be prefixed
+   * with this namespace, e.g. "categories.settingName". This is useful for
+   * a set of related settings.
+   */
+  Settings.set = function(key, value, opt_namespace) {
+    storage_.setItem(Settings.namespace_(key, opt_namespace), String(value));
+  },
+
+  /**
+   * Return a list of all the keys, or all the keys in the given namespace
+   * if one is provided.
+   *
+   * @param {string=} opt_namespace If set, only return settings which
+   * begin with this prefix.
+   */
+  Settings.keys = function(opt_namespace) {
+    var result = [];
+    opt_namespace = opt_namespace || '';
+    for (var i = 0; i < storage_.length; i++) {
+      var key = storage_.key(i);
+      if (Settings.isnamespaced_(key, opt_namespace))
+        result.push(Settings.unnamespace_(key, opt_namespace));
     }
+    return result;
+  },
+
+  Settings.isnamespaced_ = function(key, opt_namespace) {
+    return key.indexOf(Settings.normalize_(opt_namespace)) == 0;
+  },
+
+  Settings.namespace_ = function(key, opt_namespace) {
+    return Settings.normalize_(opt_namespace) + key;
+  },
+
+  Settings.unnamespace_ = function(key, opt_namespace) {
+    return key.replace(Settings.normalize_(opt_namespace), '');
+  },
+
+  /**
+   * All settings are prefixed with a global namespace to avoid collisions.
+   * Settings may also be namespaced with an additional prefix passed into
+   * the get, set, and keys methods in order to group related settings.
+   * This method makes sure the two namespaces are always set properly.
+   */
+  Settings.normalize_ = function(opt_namespace) {
+    return Settings.NAMESPACE + (opt_namespace ? opt_namespace + '.' : '');
   }
 
   Settings.setAlternativeStorageInstance = function(instance) {
-    alternativeStorageInstance = instance;
+    storage_ = instance;
   }
-
-  Settings.prototype = {
-
-    /**
-     * Get the setting with the given name.
-     *
-     * @param {string} key The name of the setting.
-     * @param {string=} opt_default The default value to return if not set.
-     * @param {string=} opt_namespace If set, the setting name will be prefixed
-     * with this namespace, e.g. "categories.settingName". This is useful for
-     * a set of related settings.
-     */
-    get: function(key, opt_default, opt_namespace) {
-      key = this.namespace_(key, opt_namespace);
-      var val = this.storage_.getItem(key);
-      if (val === null || val === undefined)
-        return opt_default;
-      return String(val);
-    },
-
-    /**
-     * Set the setting with the given name to the given value.
-     *
-     * @param {string} key The name of the setting.
-     * @param {string} value The value of the setting.
-     * @param {string=} opt_namespace If set, the setting name will be prefixed
-     * with this namespace, e.g. "categories.settingName". This is useful for
-     * a set of related settings.
-     */
-    set: function(key, value, opt_namespace) {
-      this.storage_.setItem(this.namespace_(key, opt_namespace), String(value));
-    },
-
-    /**
-     * Return a list of all the keys, or all the keys in the given namespace
-     * if one is provided.
-     *
-     * @param {string=} opt_namespace If set, only return settings which
-     * begin with this prefix.
-     */
-    keys: function(opt_namespace) {
-      var result = [];
-      opt_namespace = opt_namespace || '';
-      for (var i = 0; i < this.storage_.length; i++) {
-        var key = this.storage_.key(i);
-        if (this.isnamespaced_(key, opt_namespace))
-          result.push(this.unnamespace_(key, opt_namespace));
-      }
-      return result;
-    },
-
-    isnamespaced_: function(key, opt_namespace) {
-      return key.indexOf(this.normalize_(opt_namespace)) == 0;
-    },
-
-    namespace_: function(key, opt_namespace) {
-      return this.normalize_(opt_namespace) + key;
-    },
-
-    unnamespace_: function(key, opt_namespace) {
-      return key.replace(this.normalize_(opt_namespace), '');
-    },
-
-    /**
-     * All settings are prefixed with a global namespace to avoid collisions.
-     * Settings may also be namespaced with an additional prefix passed into
-     * the get, set, and keys methods in order to group related settings.
-     * This method makes sure the two namespaces are always set properly.
-     */
-    normalize_: function(opt_namespace) {
-      return Settings.NAMESPACE + (opt_namespace ? opt_namespace + '.' : '');
-    }
-  };
 
   Settings.NAMESPACE = 'trace-viewer';
 
