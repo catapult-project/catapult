e19fdfeb4fd11c167cf3586d4ec7714f33f9574c
Nat Duca <nduca@chromium.org>
2013-06-04 02:27:12 +0000

    Use clear instead of fillRect to clear

--- src/ui/quad_view.js
+++ src/ui/quad_view.js
@@ -195,8 +195,7 @@ base.exportTo('ui', function() {
       var ctx = this.canvas_.getContext('2d');
 
       var vp = this.viewport_;
-      ctx.fillStyle = 'rgba(255,255,255,0)';
-      ctx.fillRect(
+      ctx.clearRect(
           0, 0,
           this.canvas_.width, this.canvas_.height);
       ctx.save();
