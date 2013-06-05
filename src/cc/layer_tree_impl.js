62e7b0929c7100a5d972d3d9885dd5a2c1b4a584
Nat Duca <nduca@chromium.org>
2013-06-05 08:25:41 +0000

    Add LTI.iterLayers and LTI.findLayerWithId

--- src/cc/layer_tree_impl.js
+++ src/cc/layer_tree_impl.js
@@ -31,6 +31,28 @@ base.exportTo('cc', function() {
           this, ['rootLayer',
             'renderSurfaceLayerList']);
       this.rootLayer.layerTreeImpl = this;
+    },
+
+    iterLayers: function(func) {
+      function visitLayer(layer, depth, isMask, isReplica) {
+        func(layer, depth, isMask, isReplica);
+        for (var i = 0; i < layer.children.length; i++)
+          visitLayer(layer.children[i], depth + 1);
+        if (layer.maskLayer)
+          visitLayer(layer, depth + 1, true, false);
+        if (layer.replicaLayer)
+          visitLayer(layer, depth + 1, false, true);
+      }
+      visitLayer(this.rootLayer, 0, false, false);
+    },
+    findLayerWithId: function(id) {
+      var foundLayer = undefined;
+      function visitLayer(layer) {
+        if (layer.layerId == id)
+          foundLayer = layer;
+      }
+      this.iterLayers(visitLayer);
+      return foundLayer;
     }
   };
 
