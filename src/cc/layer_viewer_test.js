88f745213cd722ad042652bfb8d0dfb962664b13
johnjbarton@chromium.org <johnjbarton@chromium.org@0e6d7f2b-9903-5b78-7403-59d27f066143>
2013-06-13 22:45:43 +0000

    Two test.js files missing data. They work in the test.html because another test.js file pulls the data in. But when run individually they fail
    BUG=
    R=dsinclair@chromium.org
    
    Review URL: https://codereview.appspot.com/10255044

--- src/cc/layer_viewer_test.js
+++ src/cc/layer_viewer_test.js
@@ -1,6 +1,11 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 base.require('cc');
 base.require('tracing.importer.trace_event_importer');
 base.require('tracing.trace_model');
+base.require('cc.layer_tree_host_impl_test_data');
 
 'use strict';
 
--- src/cc/raster_task_slice_view_test.js
+++ src/cc/raster_task_slice_view_test.js
@@ -1,6 +1,11 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 base.require('cc.raster_task_slice_view');
 base.require('tracing.importer.trace_event_importer');
 base.require('tracing.trace_model');
+base.require('cc.layer_tree_host_impl_test_data');
 
 'use strict';
 
