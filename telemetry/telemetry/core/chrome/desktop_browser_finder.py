331a786f4e7d9ed4917b7e851277bf462c9b529c
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 21:48:06 +0000

    [Telemetry] Use PPAPI flash on Mac.
    This causes us to use PPAPI flash on Mac like the official builds
    do. This makes telemetry tests against chromium more realistic.
    
    PERF SHERIFFS: I expect this to cause a major performance and memory
    "regression" on the mac page cycler. This is just the test getting
    more realistic, it is not an actual regression.
    
    BUG=255056
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/18184013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209509 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/chrome/desktop_browser_finder.py
+++ telemetry/telemetry/core/chrome/desktop_browser_finder.py
@@ -109,8 +109,9 @@ def FindAllAvailableBrowsers(options):
   if sys.platform == 'darwin':
     chromium_app_name = 'Chromium.app/Contents/MacOS/Chromium'
     content_shell_app_name = 'Content Shell.app/Contents/MacOS/Content Shell'
-    # TODO(tonyg): Implement this on mac.
-    flash_path = None
+    flash_path = os.path.join(
+        chrome_root, 'third_party', 'adobe', 'flash', 'binaries', 'ppapi',
+        'mac', 'PepperFlashPlayer.plugin')
   elif sys.platform.startswith('linux'):
     chromium_app_name = 'chrome'
     content_shell_app_name = 'content_shell'
