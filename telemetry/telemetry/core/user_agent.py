fcd8f71da22209232bc6594edd4bb1bfd1fbcd4d
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 07:46:46 +0000

    Telemetry: Bump user-agent to M27.
    An upcoming site (News) checks our user agent. Usually these
    are one-way checks (eg. <=27) so I don't think we need
    to worry about this breaking any tests.
    
    BUG=223726
    
    Review URL: https://chromiumcodereview.appspot.com/18811004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210782 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/user_agent.py
+++ telemetry/telemetry/core/user_agent.py
@@ -6,14 +6,14 @@ UA_TYPE_MAPPING = {
   'desktop':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) '
       'AppleWebKit/537.22 (KHTML, like Gecko) '
-      'Chrome/25.0.1364.2 Safari/537.22',
+      'Chrome/27.0.1453.111 Safari/537.22',
   'mobile':
       'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) '
-      'AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile '
+      'AppleWebKit/535.19 (KHTML, like Gecko) Chrome/27.0.1453.111 Mobile '
       'Safari/535.19',
   'tablet':
       'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus 7 Build/IMM76B) '
-      'AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 '
+      'AppleWebKit/535.19 (KHTML, like Gecko) Chrome/27.0.1453.111 '
       'Safari/535.19',
 }
 
