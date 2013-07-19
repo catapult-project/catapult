1283f50db556d647200b2dc3c501d0ef7b3dc7ab
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 13:30:16 +0000

    Reland r212020: Move Python setup/tear down logic into Forwarder ...
    Forwarder used to be a pain to setup/tear down across all the various
    harnesses.
    This CL should hopefully solve these issues by hiding these
    implementation details. The host daemon is now killed once the first time that
    the Forwarder class is used and the daemon running on the devices is also
    killed the first time a port is forwarded for a specific device.
    
    TBR=bulach@chromium.org
    BUG=242846
    
    Review URL: https://codereview.chromium.org/19550004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212582 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/chrome/adb_commands.py
+++ telemetry/telemetry/core/chrome/adb_commands.py
@@ -20,7 +20,6 @@ try:
   from pylib import cmd_helper # pylint: disable=F0401
   from pylib import forwarder # pylint: disable=F0401
   from pylib import ports # pylint: disable=F0401
-  from pylib import valgrind_tools # pylint: disable=F0401
 except Exception:
   android_commands = None
 
@@ -160,34 +159,24 @@ def HasForwarder(buildtype=None):
 
 class Forwarder(object):
   def __init__(self, adb, *port_pairs):
-    tool = valgrind_tools.BaseTool()
+    self._adb = adb.Adb()
     self._host_port = port_pairs[0].local_port
 
     new_port_pairs = [(port_pair.local_port, port_pair.remote_port)
                       for port_pair in port_pairs]
 
-    self._forwarder = forwarder.Forwarder(adb.Adb(), Forwarder._GetBuildType())
-    self._forwarder.Run(new_port_pairs, tool)
+    self._port_pairs = new_port_pairs
+    forwarder.Forwarder.Map(new_port_pairs, self._adb)
 
   @staticmethod
   def _GetBuildType():
     assert HasForwarder()
     return 'Debug' if HasForwarder('Debug') else 'Release'
 
-  @staticmethod
-  def KillHost():
-    forwarder.Forwarder.KillHost(Forwarder._GetBuildType())
-
-  @staticmethod
-  def KillDevice(adb):
-    forwarder.Forwarder.KillDevice(adb.Adb(), valgrind_tools.BaseTool())
-
   @property
   def url(self):
-    assert self._forwarder
     return 'http://localhost:%i' % self._host_port
 
   def Close(self):
-    if self._forwarder:
-      self._forwarder.Close()
-      self._forwarder = None
+    for (device_port, _) in self._port_pairs:
+      forwarder.Forwarder.UnmapDevicePort(device_port, self._adb)
