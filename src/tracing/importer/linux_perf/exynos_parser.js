e60751d0972107b05e07c589c0a21d44d84f75fb
dan sinclair <dj2@everburning.com>
2013-09-12 14:36:14 +0000

    Implemented a new parser to expose state change of GPU's rendering.
    (Note, this patch is by kao@, I'm committing as his internship
    has ended.)
    
    This new parser (fbmsg_parser) parses events that are from linux
    system tracing. It shows the time spent on each state when
    GPU is rendering. Specifically, it exposes more detail about how
    frame buffers are flipped. This helps developers in GPU field
    to understand and optimize GPU's efficiency more easily.
    
    On the other hand, this parser serves as a fix to existing
    exynos parser's incorrect parsing of exynos_flip. fbmsg_parser
    recognizes a new system event and fixes exynos_flip's bug by
    putting different frame buffer's info in different row.
    Therefore, events from different frame buffers are seperated,
    which effectively avoids exynos parser's bug.
    
    BUG=381
    TBR=nduca
    
    Review URL: https://codereview.appspot.com/13676043

--- src/tracing/importer/linux_perf/exynos_parser.js
+++ src/tracing/importer/linux_perf/exynos_parser.js
@@ -28,6 +28,9 @@ base.exportTo('tracing.importer.linux_perf', function() {
         ExynosParser.prototype.busfreqTargetIntEvent.bind(this));
     importer.registerEventHandler('exynos_busfreq_target_mif',
         ExynosParser.prototype.busfreqTargetMifEvent.bind(this));
+
+    importer.registerEventHandler('exynos_page_flip_state',
+        ExynosParser.prototype.pageFlipStateEvent.bind(this));
   }
 
   ExynosParser.prototype = {
@@ -106,6 +109,49 @@ base.exportTo('tracing.importer.linux_perf', function() {
         return false;
 
       this.exynosBusfreqSample('MIF Frequency', ts, parseInt(event[1]));
+      return true;
+    },
+
+    exynosPageFlipStateOpenSlice: function(ts, pipe, fb, state) {
+      var kthread = this.importer.getOrCreatePseudoThread(
+          'exynos_flip_state (pipe:' + pipe + ', fb:' + fb + ')');
+      kthread.openSliceTS = ts;
+      kthread.openSlice = state;
+    },
+
+    exynosPageFlipStateCloseSlice: function(ts, pipe, fb, args) {
+      var kthread = this.importer.getOrCreatePseudoThread(
+          'exynos_flip_state (pipe:' + pipe + ', fb:' + fb + ')');
+      if (kthread.openSlice) {
+        var slice = new tracing.trace_model.Slice('', kthread.openSlice,
+            tracing.getStringColorId(kthread.openSlice),
+            kthread.openSliceTS,
+            args,
+            ts - kthread.openSliceTS);
+        kthread.thread.sliceGroup.pushSlice(slice);
+      }
+      kthread.openSlice = undefined;
+    },
+
+    /**
+     * Parses page_flip_state events and sets up state in the importer.
+     */
+    pageFlipStateEvent: function(eventName, cpuNumber, pid, ts, eventBase) {
+      var event = /pipe=(\d+), fb=(\d+), state=(.*)/.exec(eventBase.details);
+      if (!event)
+        return false;
+
+      var pipe = parseInt(event[1]);
+      var fb = parseInt(event[2]);
+      var state = event[3];
+
+      this.exynosPageFlipStateCloseSlice(ts, pipe, fb,
+          {
+            pipe: pipe,
+            fb: fb
+          });
+      if (state !== 'flipped')
+        this.exynosPageFlipStateOpenSlice(ts, pipe, fb, state);
       return true;
     }
   };
--- src/tracing/importer/linux_perf/exynos_parser_test.js
+++ src/tracing/importer/linux_perf/exynos_parser_test.js
@@ -44,4 +44,90 @@ base.unittest.testSuite('tracing.importer.linux_perf.exynos_parser',
         assertEquals(3, c0.counters['INT Frequency'].series[0].samples.length);
         assertEquals(1, c0.counters['MIF Frequency'].series[0].samples.length);
       });
+
+      test('exynosPageFlipSlowRequestImport', function() {
+        var lines = [
+          '          <idle>-0     [000] d.h. 1000.000000: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_kds',
+          ' Chrome_IOThread-21603 [000] d.h. 1000.000001: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_apply',
+          '     kworker/0:1-25931 [000] .... 1000.000002: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_flip',
+          '     kworker/0:1-25931 [000] .... 1000.000003: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=flipped',
+          '          <idle>-0     [000] d.h. 1000.000004: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_kds',
+          ' Chrome_IOThread-21603 [000] d.h. 1000.000005: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_apply',
+          '     kworker/0:1-25931 [000] .... 1000.000006: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_flip',
+          '     kworker/0:1-25931 [000] .... 1000.000007: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=flipped'
+        ];
+        var m = new tracing.TraceModel(lines.join('\n'), false);
+        assertFalse(m.hasImportWarnings);
+
+        var threads = m.getAllThreads();
+        // there are 2 threads:
+        //   (1) "exynos_flip_state (pipe:0, fb:25)"
+        //   (2) "exynos_flip_state (pipe:0, fb:26)"
+        assertEquals(2, threads.length);
+
+        // in the test data, event of fb=26 occurs first, so it's thread[0]
+        var gfxFbId26Thread = threads[0]; // thread where fb == 26
+        var gfxFbId25Thread = threads[1]; // thread where fb == 25
+        assertEquals('exynos_flip_state (pipe:0, fb:25)', gfxFbId25Thread.name);
+        assertEquals('exynos_flip_state (pipe:0, fb:26)', gfxFbId26Thread.name);
+        // Every state (except for 'flipped') will start a new slice.
+        // The last event will not be closed, so it's not a slice
+        assertEquals(3, gfxFbId25Thread.sliceGroup.length);
+        assertEquals(3, gfxFbId26Thread.sliceGroup.length);
+      });
+
+      test('exynosPageFlipFastRequestImport', function() {
+        var lines = [
+          '          <idle>-0     [000] d.h. 1000.000000: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_kds',
+          ' Chrome_IOThread-21603 [000] d.h. 1000.000001: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_kds',
+          '               X-21385 [000] .... 1000.000002: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_apply',
+          '     kworker/0:1-25931 [000] .... 1000.000003: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_flip',
+          '               X-21385 [001] .... 1000.000004: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_apply',
+          '     kworker/0:1-25931 [000] .... 1000.000005: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=flipped',
+          '          <idle>-0     [000] d.h. 1000.000006: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_kds',
+          '               X-21385 [000] .... 1000.000007: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_flip',
+          '     kworker/0:1-25931 [000] .... 1000.000008: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=flipped',
+          '     kworker/0:1-25931 [000] .... 1000.000009: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_kds',
+          ' Chrome_IOThread-21603 [000] d.h. 1000.000010: ' +
+              'exynos_page_flip_state: pipe=0, fb=25, state=wait_apply',
+          '          <idle>-0     [000] d.h. 1000.000011: ' +
+              'exynos_page_flip_state: pipe=0, fb=26, state=wait_apply'
+        ];
+        var m = new tracing.TraceModel(lines.join('\n'), false);
+        assertFalse(m.hasImportWarnings);
+
+        var threads = m.getAllThreads();
+        // there are 2 threads:
+        //   (1) "exynos_flip_state (pipe:0, fb:25)"
+        //   (2) "exynos_flip_state (pipe:0, fb:26)"
+        assertEquals(2, threads.length);
+
+        // in the test data, event of fb=26 occurs first, so it's thread[0]
+        var gfxFbId26Thread = threads[0]; // thread where fb == 26
+        var gfxFbId25Thread = threads[1]; // thread where fb == 25
+        assertEquals('exynos_flip_state (pipe:0, fb:25)', gfxFbId25Thread.name);
+        assertEquals('exynos_flip_state (pipe:0, fb:26)', gfxFbId26Thread.name);
+        // Every state (except for 'flipped') will start a new slice.
+        // The last event will not be closed, so it's not a slice
+        assertEquals(4, gfxFbId25Thread.sliceGroup.length);
+        assertEquals(4, gfxFbId26Thread.sliceGroup.length);
+      });
     });
