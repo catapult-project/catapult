5573ef2301fb7bc62c1f94b7da77805b78acce6f
a1.gomes@sisa.samsung.com <a1.gomes@sisa.samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 22:25:33 +0000

    Telemetry: document.documentElement.scrollTop/Left is zero
    In order for bug 157855 to get fixed without breaking a related telemetry unit test (page/actions/scroll_unittest.py), this patch changes the way  scrollTop property is checked: it uses an annotation similar to the how real world  WebPages do, where both documentElement and body is checked for cross browser compatibility.
    
    Once bug 157855, it should be encouraged to adopt scrollTop of documentElement only.
    
    BUG=302707
    
    Review URL: https://codereview.chromium.org/25541005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226872 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/page/actions/scroll_unittest.py
+++ telemetry/telemetry/page/actions/scroll_unittest.py
@@ -37,7 +37,8 @@ class ScrollActionTest(tab_test_case.TabTestCase):
                               (2 * window.innerHeight + 1) + 'px';""")
 
     self.assertEquals(
-        self._tab.EvaluateJavaScript('document.body.scrollTop'), 0)
+        self._tab.EvaluateJavaScript("""document.documentElement.scrollTop
+                                   || document.body.scrollTop"""), 0)
 
     i = scroll.ScrollAction()
     i.WillRunAction(page, self._tab)
@@ -56,7 +57,8 @@ class ScrollActionTest(tab_test_case.TabTestCase):
 
     # Allow for roundoff error in scaled viewport.
     scroll_position = self._tab.EvaluateJavaScript(
-        'document.body.scrollTop + window.innerHeight')
+        """(document.documentElement.scrollTop || document.body.scrollTop)
+        + window.innerHeight""")
     scroll_height = self._tab.EvaluateJavaScript('document.body.scrollHeight')
     difference = scroll_position - scroll_height
     self.assertTrue(abs(difference) <= 1)
