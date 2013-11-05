718242c3c93c7b83be541074f0f139ab86d51970
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 13:43:55 +0000

    [Telemetry] Fix image_decoding_measurement timeout.
    util.WaitFor polls which is not appropriate for PerformActionAndWaitForNavigate.
    
    Since DispatchNotifications blocks in recv until it gets some data, there is no
    need to poll at an interval. Instead, we should call recv again as soon as
    possible after dispatching a notification.
    
    In the case of the image decoding measurement, it runs timeline recording. There
    were so many timeline messages to receive that we were not always getting to the
    important Page.navigate message before timing out.
    
    Example:
    http://build.chromium.org/p/chromium.perf/builders/Linux%20Perf%20%281%29/builds/27098
    
    BUG=314375
    
    Review URL: https://codereview.chromium.org/47013004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232959 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/util.py
+++ telemetry/telemetry/core/util.py
@@ -39,7 +39,7 @@ def AddDirToPythonPath(*path_parts):
     sys.path.append(path)
 
 
-def WaitFor(condition, timeout, pass_time_left_to_func=False):
+def WaitFor(condition, timeout):
   """Waits for up to |timeout| secs for the function |condition| to return True.
 
   Polling frequency is (elapsed_time / 10), with a min of .1s and max of 5s.
@@ -50,11 +50,7 @@ def WaitFor(condition, timeout, pass_time_left_to_func=False):
   start_time = time.time()
   while True:
     elapsed_time = time.time() - start_time
-    if pass_time_left_to_func:
-      remaining_time = timeout - elapsed_time
-      res = condition(max(remaining_time, 0.0))
-    else:
-      res = condition()
+    res = condition()
     if res:
       return res
     if elapsed_time > timeout:
