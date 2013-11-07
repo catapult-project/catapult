558595dd90df8a1460c5d2797253bb2062518bb3
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-07 21:17:25 +0000

    android: Add screen recording tool
    Add a tool to record screen capture videos from Android devices. This
    patch introduces both a standalone tool as well as a telemetry profiler
    which captures a video of a telemetry run.
    
    Note that the screen recording functionality requires Android KitKat or
    later.
    
    TEST=tools/perf/run_benchmark run --profiler=android-screen-recorder --browser=android-chrome smoothness.top_25 --page-filter=news.yahoo
    
    Review URL: https://codereview.chromium.org/54123005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233686 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ telemetry/telemetry/core/platform/profiler/android_screen_recorder_profiler.py
@@ -0,0 +1,40 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+
+from telemetry.core import util
+from telemetry.core.backends.chrome import android_browser_finder
+from telemetry.core.platform import profiler
+
+class AndroidScreenRecordingProfiler(profiler.Profiler):
+  """Captures a screen recording on Android."""
+
+  def __init__(self, browser_backend, platform_backend, output_path, state):
+    super(AndroidScreenRecordingProfiler, self).__init__(
+        browser_backend, platform_backend, output_path, state)
+    self._output_path = output_path + '.mp4'
+    self._recorder = subprocess.Popen(
+        [os.path.join(util.GetChromiumSrcDir(), 'build', 'android',
+                      'screenshot.py'), '--video', '--output',
+         self._output_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+  @classmethod
+  def name(cls):
+    return 'android-screen-recorder'
+
+  @classmethod
+  def is_supported(cls, browser_type):
+    if browser_type == 'any':
+      return android_browser_finder.CanFindAvailableBrowsers()
+    return browser_type.startswith('android')
+
+  def CollectProfile(self):
+    self._recorder.communicate(input='\n')
+    self._recorder.wait()
+
+    print 'Screen recording saved as %s' % self._output_path
+    print 'To view, open in Chrome or a video player'
+    return [self._output_path]
