cba6f631d0e6d6666b60b36176e47eb19253a716
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 06:47:36 +0000

    Rename RenderViewHostManager to RenderFrameHostManager.
    BUG=314791
    TEST=No behavior change.
    TBR=nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/92153003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237714 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/page/page_runner.py
+++ telemetry/telemetry/page/page_runner.py
@@ -93,7 +93,7 @@ class _RunState(object):
           self.browser.tabs[-1].Close()
 
       # Must wait for tab to commit otherwise it can commit after the next
-      # navigation has begun and RenderViewHostManager::DidNavigateMainFrame()
+      # navigation has begun and RenderFrameHostManager::DidNavigateMainFrame()
       # will cancel the next navigation because it's pending. This manifests as
       # the first navigation in a PageSet freezing indefinitly because the
       # navigation was silently cancelled when |self.browser.tabs[0]| was
