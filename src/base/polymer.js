0e82a567d5ffc408a82e801a2737c4f6c7b4633f
Nat Duca <nduca@chromium.org>
2013-12-03 13:27:39 +0000

    Add polymer to third_party plus base.polymer importer

---
+++ src/base/polymer.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+base.requireRawScript('../third_party/polymer/platform.min.js');
+base.requireRawScript('../third_party/polymer/polymer.min.js');
+
+base.exportTo('base', function() {
+});
