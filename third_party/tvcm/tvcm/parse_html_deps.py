f903bd7ce270607210a4d50385b3a757824b85d9
dan sinclair <dj2@everburning.com>
2013-12-04 15:35:24 +0000

    Parse HTML Templates for deps.
    This CL adds a simple module to parse HTML files to pull out the link and
    script tags.
    
    BUG=
    R=nduca@chromium.org
    
    Review URL: https://codereview.appspot.com/37000043

---
+++ third_party/tvcm/tvcm/parse_html_deps.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import re
+from HTMLParser import HTMLParser
+
+class ModuleHTMLParser(HTMLParser):
+  def __init__(self, module):
+    HTMLParser.__init__(self)
+    self.module = module
+
+    self.current_script = ""
+    self.in_script = False
+    self.in_style = False
+
+  def handle_starttag(self, tag, attrs):
+    if tag == 'link':
+      is_stylesheet = False
+      is_import = False
+      href = None
+      for attr in attrs:
+        if attr[0] == 'rel' and attr[1] == 'stylesheet':
+          is_stylesheet = True
+        elif attr[0] == 'rel' and attr[1] == 'import':
+          is_import = True
+        elif attr[0] == 'href':
+          href = attr[1]
+
+      if is_stylesheet:
+        self.module.stylesheets.append(href)
+      elif is_import:
+        self.module.imports.append(href)
+
+    elif tag == 'script':
+      for attr in attrs:
+        if attr[0] == 'src':
+          self.module.scripts_external.append(attr[1])
+          return
+      self.in_script = True
+
+    elif tag == 'style':
+      self.in_style = True
+
+  def handle_endtag(self, tag):
+    if tag == 'script' and self.in_script:
+      self.module.scripts_inline.append(self.current_script)
+      self.current_script = ""
+      self.in_script = False
+
+    elif tag == 'style' and self.in_style:
+      self.in_style = False
+
+  def handle_data(self, data):
+    if self.in_script:
+      self.current_script += data
+    elif self.in_style:
+      result = re.match(r"\s*@import url\(([^\)]*)\)", data,
+                        flags=re.IGNORECASE)
+      if result:
+        self.module.stylesheets.append(result.group(1))
+
+
+class Module(object):
+  def __init__(self):
+    self.scripts_external = []
+    self.scripts_inline = []
+    self.stylesheets = []
+    self.imports = []
+
+  def parse(self, html):
+    if html is None or len(html) == 0:
+      return
+
+    parser = ModuleHTMLParser(self)
+    parser.feed(html)
---
+++ third_party/tvcm/tvcm/parse_html_deps_unittest.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import unittest
+
+import parse_html_deps
+import re
+
+class ParseTests(unittest.TestCase):
+  def test_parse_empty(self):
+    module = parse_html_deps.Module()
+    module.parse("")
+    self.assertEquals([], module.scripts_external)
+    self.assertEquals([], module.scripts_inline)
+    self.assertEquals([], module.stylesheets)
+    self.assertEquals([], module.imports)
+
+  def test_parse_none(self):
+    module = parse_html_deps.Module()
+    module.parse(None)
+    self.assertEquals([], module.scripts_external)
+    self.assertEquals([], module.scripts_inline)
+    self.assertEquals([], module.stylesheets)
+    self.assertEquals([], module.imports)
+
+  def test_parse_script_src(self):
+    html = """<!DOCTYPE html>
+              <html>
+                <head>
+                  <script src="polymer.min.js"></script>
+                  <script src="foo.js"></script>
+                </head>
+                <body>
+                </body>
+              </html>"""
+    module = parse_html_deps.Module()
+    module.parse(html)
+    self.assertEquals(['polymer.min.js', 'foo.js'], module.scripts_external);
+    self.assertEquals([], module.scripts_inline)
+    self.assertEquals([], module.stylesheets)
+    self.assertEquals([], module.imports)
+
+  def test_parse_link_rel_import(self):
+    html = """<!DOCTYPE html>
+              <html>
+                <head>
+                  <link rel="import" href="x-foo.html">
+                </head>
+                <body>
+                </body>
+              </html>"""
+    module = parse_html_deps.Module()
+    module.parse(html)
+    self.assertEquals([], module.scripts_external);
+    self.assertEquals([], module.scripts_inline)
+    self.assertEquals([], module.stylesheets)
+    self.assertEquals(['x-foo.html'], module.imports)
+
+  def test_parse_script_inline(self):
+    html = """<polymer-element name="tk-element-proto">
+                <template>
+                </template>
+                <script>
+                  base.require("foo");
+                  base.require('bar');
+                </script>
+              </polymer-element>"""
+
+    module = parse_html_deps.Module()
+    module.parse(html)
+    self.assertEquals([], module.scripts_external);
+    self.assertEquals(1, len(module.scripts_inline))
+    self.assertEquals([], module.stylesheets)
+    self.assertEquals([], module.imports)
+
+    val = module.scripts_inline[0]
+    val = re.sub(r"\s+", '', val)
+    inner_script = """base.require("foo");base.require('bar');"""
+    self.assertEquals(inner_script, val)
+
+  def test_parse_link_rel_stylesheet(self):
+    html = """<polymer-element ...>
+                <template>
+                  <link rel="stylesheet" href="frameworkstyles.css">
+                </template>
+              </polymer-element>"""
+    module = parse_html_deps.Module()
+    module.parse(html)
+    self.assertEquals([], module.scripts_external);
+    self.assertEquals([], module.scripts_inline)
+    self.assertEquals(['frameworkstyles.css'], module.stylesheets)
+    self.assertEquals([], module.imports)
+
+  def test_parse_style_import(self):
+    html = """<polymer-element name="x-blink" ...>
+                <template>
+                  <style>
+                    @import url(awesome.css);
+                  </style>
+                </template>
+              </polymer-element>"""
+    module = parse_html_deps.Module()
+    module.parse(html)
+    self.assertEquals([], module.scripts_external);
+    self.assertEquals([], module.scripts_inline)
+    self.assertEquals(['awesome.css'], module.stylesheets)
+    self.assertEquals([], module.imports)
