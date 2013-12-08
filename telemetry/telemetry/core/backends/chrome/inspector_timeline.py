dccf913cf3edeb2952a822b577cbe730309bd231
qyearsley@chromium.org <qyearsley@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-08 07:35:16 +0000

    Make chrome inspector timeline backend use the new bufferEvents option.
    The purpose if this is to reduce the overhead of timeline recording so that speed index might be run with page cyclers. 
    
    It's been established that timeline events should be inaccessible by Telemetry measurements until recording is stopped, so it seems reasonable to switch to this mode for any timeline recording.
    
    BUG=295334
    
    Review URL: https://codereview.chromium.org/46503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239393 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/backends/chrome/inspector_timeline.py
+++ telemetry/telemetry/core/backends/chrome/inspector_timeline.py
@@ -1,15 +1,28 @@
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 from telemetry.core.timeline import model
 
+
 class TabBackendException(Exception):
+  """An exception which indicates an error response from devtools inspector."""
   pass
+
 
 class InspectorTimeline(object):
   """Implementation of dev tools timeline."""
+
   class Recorder(object):
-    """Utility class to Start / Stop recording timeline."""
+    """Utility class to Start and Stop recording timeline.
+
+    Example usage:
+
+        with inspector_timeline.InspectorTimeline.Recorder(tab):
+          # Something to run while the timeline is recording.
+
+    This is an alternative to directly calling the Start and Stop methods below.
+    """
     def __init__(self, tab):
       self._tab = tab
 
@@ -23,49 +36,65 @@ class InspectorTimeline(object):
     self._inspector_backend = inspector_backend
     self._is_recording = False
     self._timeline_model = None
-    self._raw_events = None
 
   @property
   def timeline_model(self):
     return self._timeline_model
 
   def Start(self):
-    if self._is_recording:
-      return
+    """Starts recording."""
+    assert not self._is_recording, 'Start should only be called once.'
     self._is_recording = True
-    self._timeline_model = None
-    self._raw_events = []
-    self._inspector_backend.RegisterDomain('Timeline',
-       self._OnNotification, self._OnClose)
-    req = {'method': 'Timeline.start'}
-    self._SendSyncRequest(req)
+    self._inspector_backend.RegisterDomain(
+        'Timeline', self._OnNotification, self._OnClose)
+    # The 'bufferEvents' parameter below means that events should not be sent
+    # individually as messages, but instead all at once when a Timeline.stop
+    # request is sent.
+    request = {
+        'method': 'Timeline.start',
+        'params': {'bufferEvents': True},
+    }
+    self._SendSyncRequest(request)
 
   def Stop(self):
-    if not self._is_recording:
-      raise TabBackendException('Stop() called but not started')
-    self._is_recording = False
-    self._timeline_model = model.TimelineModel(event_data=self._raw_events,
-                                               shift_world_to_zero=False)
-    req = {'method': 'Timeline.stop'}
-    self._SendSyncRequest(req)
+    """Stops recording and makes a TimelineModel with the event data."""
+    assert self._is_recording, 'Stop should be called after Start.'
+    request = {'method': 'Timeline.stop'}
+    result = self._SendSyncRequest(request)
+    raw_events = result['events']
+    self._timeline_model = model.TimelineModel(
+        event_data=raw_events, shift_world_to_zero=False)
     self._inspector_backend.UnregisterDomain('Timeline')
+    self._is_recording = False
 
-  def _SendSyncRequest(self, req, timeout=60):
-    res = self._inspector_backend.SyncRequest(req, timeout)
-    if 'error' in res:
-      raise TabBackendException(res['error']['message'])
-    return res['result']
+  def _SendSyncRequest(self, request, timeout=60):
+    """Sends a devtools remote debugging protocol request.
+
+    The types of request that are valid is determined by protocol.json:
+    https://src.chromium.org/viewvc/blink/trunk/Source/devtools/protocol.json
+
+    Args:
+      request: Request dict, may contain the keys 'method' and 'params'.
+      timeout: Number of seconds to wait for a response.
+
+    Returns:
+      The result given in the response message.
+
+    Raises:
+      TabBackendException: The response indicates an error occurred.
+    """
+    response = self._inspector_backend.SyncRequest(request, timeout)
+    if 'error' in response:
+      raise TabBackendException(response['error']['message'])
+    return response['result']
 
   def _OnNotification(self, msg):
-    if not self._is_recording:
-      return
-    if 'method' in msg and msg['method'] == 'Timeline.eventRecorded':
-      self._OnEventRecorded(msg)
-
-  def _OnEventRecorded(self, msg):
-    record = msg.get('params', {}).get('record')
-    if record:
-      self._raw_events.append(record)
+    """Handler called when a message is received."""
+    # Since 'Timeline.start' was invoked with the 'bufferEvents' parameter,
+    # there will be no timeline notifications while recording.
+    pass
 
   def _OnClose(self):
+    """Handler called when a domain is unregistered."""
     pass
+
--- telemetry/telemetry/core/backends/chrome/inspector_timeline_unittest.py
+++ telemetry/telemetry/core/backends/chrome/inspector_timeline_unittest.py
@@ -9,32 +9,39 @@ from telemetry.core import util
 from telemetry.core.backends.chrome import inspector_timeline
 from telemetry.unittest import tab_test_case
 
+
 class InspectorTimelineTabTest(tab_test_case.TabTestCase):
-  def _StartServer(self):
-    self._browser.SetHTTPServerDirectories(util.GetUnittestDataDir())
+  """Test case that opens a browser and creates and then checks an event."""
 
   def _WaitForAnimationFrame(self):
+    """Wait until the variable window.done is set on the tab."""
     def _IsDone():
-      js_is_done = """done"""
-      return bool(self._tab.EvaluateJavaScript(js_is_done))
+      return bool(self._tab.EvaluateJavaScript('window.done'))
     util.WaitFor(_IsDone, 5)
 
   def testGotTimeline(self):
     if sys.platform in ('win32', 'cygwin'):
       raise unittest.SkipTest('Test flaky on windows. http://crbug.com/321529')
 
+    # While the timeline is recording, call window.webkitRequestAnimationFrame.
+    # This will create a FireAnimationEvent, which can be checked below. See:
+    # https://developer.mozilla.org/en/docs/Web/API/window.requestAnimationFrame
     with inspector_timeline.InspectorTimeline.Recorder(self._tab):
       self._tab.ExecuteJavaScript(
-"""
-var done = false;
-function sleep(ms) {
-  var endTime = (new Date().getTime()) + ms;
-  while ((new Date().getTime()) < endTime);
-}
-window.webkitRequestAnimationFrame(function() { sleep(10); done = true; });
-""")
+          """
+          var done = false;
+          function sleep(ms) {
+            var endTime = (new Date().getTime()) + ms;
+            while ((new Date().getTime()) < endTime);
+          }
+          window.webkitRequestAnimationFrame(function() {
+            sleep(10);
+            window.done = true;
+          });
+          """)
       self._WaitForAnimationFrame()
 
-    r = self._tab.timeline_model.GetAllEventsOfName('FireAnimationFrame')
-    self.assertTrue(len(r) > 0)
-    self.assertTrue(r[0].duration > 0)
+    # There should be at least a FireAnimationFrame record with some duration.
+    events = self._tab.timeline_model.GetAllEventsOfName('FireAnimationFrame')
+    self.assertTrue(len(events) > 0)
+    self.assertTrue(events[0].duration > 0)
