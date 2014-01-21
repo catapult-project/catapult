5c1f14990dae4c0e6d526e978bdfb1007f5d1f04
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-21 20:03:03 +0000

    [Telemetry] Set SO_REUSEADDR on all websockets.
    This should fix socket in use errors on Windows.
    
    BUG=273986
    
    Review URL: https://codereview.chromium.org/142633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246095 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/backends/chrome/websocket.py
+++ telemetry/telemetry/core/backends/chrome/websocket.py
@@ -3,10 +3,26 @@
 # found in the LICENSE file.
 from __future__ import absolute_import
 
+import socket
+
 from telemetry.core import util
 
 util.AddDirToPythonPath(
     util.GetTelemetryDir(), 'third_party', 'websocket-client')
-from websocket import create_connection  # pylint: disable=W0611
-from websocket import WebSocketException  # pylint: disable=W0611
-from websocket import WebSocketTimeoutException  # pylint: disable=W0611
+# pylint: disable=W0611
+from websocket import create_connection as _create_connection
+from websocket import WebSocketException
+from websocket import WebSocketTimeoutException
+
+
+def create_connection(*args, **kwargs):
+  sockopt = kwargs.get('sockopt', [])
+
+  # By default, we set SO_REUSEADDR on all websockets used by Telemetry.
+  # This prevents spurious address in use errors on Windows.
+  #
+  # TODO(tonyg): We may want to set SO_NODELAY here as well.
+  sockopt.append((socket.SOL_SOCKET, socket.SO_REUSEADDR, 1))
+
+  kwargs['sockopt'] = sockopt
+  return _create_connection(*args, **kwargs)
--- telemetry/telemetry/core/backends/chrome/websocket_unittest.py
+++ telemetry/telemetry/core/backends/chrome/websocket_unittest.py
@@ -1,6 +1,7 @@
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import socket
 import unittest
 
 from telemetry.core.backends.chrome import websocket
@@ -8,3 +9,17 @@ from telemetry.core.backends.chrome import websocket
 class TestWebSocket(unittest.TestCase):
   def testExports(self):
     self.assertNotEqual(websocket.create_connection, None)
+    self.assertNotEqual(websocket.WebSocketException, None)
+    self.assertNotEqual(websocket.WebSocketTimeoutException, None)
+
+  def testSockOpts(self):
+    ws = websocket.create_connection('ws://echo.websocket.org')
+    self.assertNotEquals(
+        ws.sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR), 0)
+    ws = websocket.create_connection(
+        'ws://echo.websocket.org',
+        sockopt=[(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)])
+    self.assertNotEquals(
+        ws.sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR), 0)
+    self.assertNotEquals(
+        ws.sock.getsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY), 0)
