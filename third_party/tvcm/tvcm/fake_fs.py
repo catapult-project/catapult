37587cf0c7dd037611ce2f4aafc0ec8b6eb71072
Nat Duca <nduca@chromium.org>
2014-02-18 05:15:32 +0000

    Add os.walk emulation to fake_fs

--- third_party/tvcm/tvcm/fake_fs.py
+++ third_party/tvcm/tvcm/fake_fs.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 import os
 import sys
+import collections
 import StringIO
 
 class WithableStringIO(StringIO.StringIO):
@@ -22,6 +23,8 @@ class FakeFS(object):
     self._bound = False
     self._real_open = sys.modules['__builtin__'].open
     self._real_exists = os.path.exists
+    self._real_walk = os.walk
+    self._real_listdir = os.listdir
 
   def __enter__(self):
     self.Bind()
@@ -34,12 +37,16 @@ class FakeFS(object):
     assert not self._bound
     sys.modules['__builtin__'].open = self._FakeOpen
     os.path.exists = self._FakeExists
+    os.walk = self._FakeWalk
+    os.listdir = self._FakeListDir
     self._bound = True
 
   def Unbind(self):
     assert self._bound
     sys.modules['__builtin__'].open = self._real_open
     os.path.exists = self._real_exists
+    os.walk = self._real_walk
+    os.walk = self._real_listdir
     self._bound = False
 
   def AddFile(self, path, contents):
@@ -60,3 +67,48 @@ class FakeFS(object):
     if path in self._file_contents:
       return True
     return self._real_exists(path)
+
+  def _FakeWalk(self, top):
+    assert os.path.isabs(top)
+    all_filenames = self._file_contents.keys()
+    pending_prefixes = collections.deque()
+    pending_prefixes.append(top)
+    visited_prefixes = set()
+    while len(pending_prefixes):
+      prefix = pending_prefixes.popleft()
+      if prefix in visited_prefixes:
+        continue
+      visited_prefixes.add(prefix)
+      if prefix.endswith('/'):
+        prefix_with_trailing_sep = prefix
+      else:
+        prefix_with_trailing_sep = prefix + '/'
+
+      dirs = set()
+      files = []
+      for filename in all_filenames:
+        if not filename.startswith(prefix_with_trailing_sep):
+          continue
+        relative_to_prefix = os.path.relpath(filename, prefix)
+
+        dirpart = os.path.dirname(relative_to_prefix)
+        if len(dirpart) == 0:
+          files.append(relative_to_prefix)
+          continue
+        parts = dirpart.split('/')
+        if len(parts) == 0:
+          dirs.add(dirpart)
+        else:
+          if prefix.endswith('/'):
+            pending = prefix + parts[0]
+          else:
+            pending = prefix + '/' + parts[0]
+          dirs.add(parts[0])
+          pending_prefixes.appendleft(pending)
+
+      dirs = list(dirs)
+      dirs.sort()
+      yield prefix, dirs, files
+
+  def _FakeListDir(self, dirname):
+    raise NotImplementedError()
--- third_party/tvcm/tvcm/fake_fs_unittest.py
+++ third_party/tvcm/tvcm/fake_fs_unittest.py
@@ -23,3 +23,29 @@ class FakeFSUnittest(unittest.TestCase):
     with fs:
       with open('/blah/x', 'r') as f:
         self.assertEquals('foobar', f.read())
+
+  def testWalk(self):
+    fs = fake_fs.FakeFS()
+    fs.AddFile('/x/w2/w3/z3.txt', '')
+    fs.AddFile('/x/w/z.txt', '')
+    fs.AddFile('/x/y.txt', '')
+    fs.AddFile('/a.txt', 'foobar')
+    with fs:
+      gen = os.walk('/')
+      r = gen.next()
+      self.assertEquals(('/', ['x'], ['a.txt']), r)
+
+      r = gen.next()
+      self.assertEquals(('/x', ['w', 'w2'], ['y.txt']), r)
+
+      r = gen.next()
+      self.assertEquals(('/x/w', [], ['z.txt']), r)
+
+      r = gen.next()
+      self.assertEquals(('/x/w2', ['w3'], []), r)
+
+      r = gen.next()
+      self.assertEquals(('/x/w2/w3', [], ['z3.txt']), r)
+
+      self.assertRaises(StopIteration,
+                        lambda: gen.next())
