e1fabd968c3689e09e75bce54598b5b776b507fb
Nat Duca <nduca@chromium.org>
2014-02-25 09:02:14 +0000

    Yet more deflake of test_case_runner loading

--- third_party/tvcm/tvcm/module_test_case.py
+++ third_party/tvcm/tvcm/module_test_case.py
@@ -18,6 +18,7 @@ _currently_active_module_test_suite = None
 
 def _NavigateToTestCaseRunner(bc):
   bc.NavigateToPath('/tvcm/unittest/module_test_case_runner.html')
+  bc.WaitForJavaScriptExpression('tvcm !== undefined')
   bc.WaitForJavaScriptExpression('tvcm.hasPanic !== undefined')
   bc.WaitForJavaScriptExpression('window.discoverTestsInModules !== undefined')
   bc.WaitForJavaScriptExpression('window.runTestNamed !== undefined')
