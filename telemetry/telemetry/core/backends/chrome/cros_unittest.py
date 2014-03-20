88563bdb829b3b9934029dcae855c1f8a4a61fd5
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 21:14:58 +0000

    Only restart ui if we're logged in.
    * Switch to using cryptohome --action=is_mounted instead of manually checking file mounts.
    * oobe_exists can throw an exception as we log in, so handle this.
    * Use the same mechanism for determining if guest is logged in.
    * Move CryptohomePath and the mount-based IsCryptohomeMounted to CrOSTest.
    * Fixup CrOSTest for guest mode.
    
    BUG=
    TEST=manual
    NOTRY=True
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=257996
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=258135
    
    Review URL: https://codereview.chromium.org/203973003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258410 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/backends/chrome/cros_unittest.py
+++ telemetry/telemetry/core/backends/chrome/cros_unittest.py
@@ -2,7 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import json
 import logging
 import os
 import unittest
@@ -21,7 +20,8 @@ class CrOSTest(unittest.TestCase):
     self._cri = cros_interface.CrOSInterface(options.cros_remote,
                                              options.cros_ssh_identity)
     self._is_guest = options.browser_type == 'cros-chrome-guest'
-    self._username = '' if self._is_guest else options.browser_options.username
+    self._username = ('$guest' if self._is_guest
+                                else options.browser_options.username)
     self._password = options.browser_options.password
     self._load_extension = None
 
@@ -51,14 +51,20 @@ class CrOSTest(unittest.TestCase):
     self.assertTrue(extension)
     return extension
 
+  def _CryptohomePath(self, user):
+    """Returns the cryptohome mount point for |user|."""
+    return self._cri.RunCmdOnDevice(
+        ['cryptohome-path', 'user', "'%s'" % user])[0].strip()
+
   def _IsCryptohomeMounted(self):
-    """Returns True if cryptohome is mounted. as determined by the cmd
-    cryptohome --action=status"""
-    cryptohomeJSON, _ = self._cri.RunCmdOnDevice(['/usr/sbin/cryptohome',
-                                                 '--action=status'])
-    cryptohomeStatus = json.loads(cryptohomeJSON)
-    return (cryptohomeStatus['mounts'] and
-            cryptohomeStatus['mounts'][0]['mounted'])
+    """Returns True iff |user|'s cryptohome is mounted."""
+    profile_path = self._CryptohomePath(self._username)
+    mount = self._cri.FilesystemMountedAt(profile_path)
+    if not mount:
+      return False
+    if self._is_guest:
+      return mount == 'guestfs'
+    return mount.startswith('/home/.shadow/')
 
   @test.Enabled('chromeos')
   def testCryptohome(self):
@@ -76,7 +82,7 @@ class CrOSTest(unittest.TestCase):
         self.assertEquals(chronos_fs, 'guestfs')
       else:
         crypto_fs = self._cri.FilesystemMountedAt(
-            self._cri.CryptohomePath(self._username))
+            self._CryptohomePath(self._username))
         self.assertEquals(crypto_fs, chronos_fs)
 
     self.assertFalse(self._IsCryptohomeMounted())
@@ -99,6 +105,8 @@ class CrOSTest(unittest.TestCase):
   @test.Enabled('chromeos')
   def testLoginStatus(self):
     """Tests autotestPrivate.loginStatus"""
+    if self._is_guest:
+      return
     with self._CreateBrowser(autotest_ext=True) as b:
       login_status = self._GetLoginStatus(b)
       self.assertEquals(type(login_status), dict)
@@ -153,6 +161,8 @@ class CrOSTest(unittest.TestCase):
   @test.Enabled('chromeos')
   def testScreenLock(self):
     """Tests autotestPrivate.screenLock"""
+    if self._is_guest:
+      return
     with self._CreateBrowser(autotest_ext=True) as browser:
       self._LockScreen(browser)
       self._AttemptUnlockBadPassword(browser)
@@ -161,6 +171,8 @@ class CrOSTest(unittest.TestCase):
   @test.Enabled('chromeos')
   def testLogout(self):
     """Tests autotestPrivate.logout"""
+    if self._is_guest:
+      return
     with self._CreateBrowser(autotest_ext=True) as b:
       extension = self._GetAutotestExtension(b)
       try:
