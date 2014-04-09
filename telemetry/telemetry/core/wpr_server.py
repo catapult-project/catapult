e269f34bb4a19a23ce7c93079d196189e081799d
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-09 02:05:28 +0000

    Revert "[Telemetry] Move webpagereplay.py into Telemetry."
    This change reverts r262578. That change deleted a directory
    but apparently failed to update some isolate files that reference
    the directory, causing build failures on the GPU bots.
    
    TBR=tonyg@chromium.org, kbr@chromium.org, esprehn@chromium.org
    BUG=271845
    
    Review URL: https://codereview.chromium.org/229913003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262592 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/wpr_server.py
+++ telemetry/telemetry/core/wpr_server.py
@@ -3,10 +3,14 @@
 # found in the LICENSE file.
 
 from telemetry.core import forwarders
+from telemetry.core import util
+
 
 # TODO(tonyg): Move webpagereplay.py's guts into this class and
 # make ReplayServer subclass LocalServer.
-from telemetry.core import webpagereplay
+util.AddDirToPythonPath(
+    util.GetChromiumSrcDir(), 'chrome', 'test', 'functional')
+import webpagereplay  # pylint: disable=F0401
 
 
 def GetChromeFlags(replay_host, port_pairs):
