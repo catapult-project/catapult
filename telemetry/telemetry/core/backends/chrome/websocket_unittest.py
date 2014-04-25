c967603cec580bc137535671a043cdc6fb367846
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-25 08:37:45 +0000

    Telemetry fixit: use a minimal ws server for testSockOpts.
    Avoid using external servers.
    
    BUG=361658
    
    Review URL: https://codereview.chromium.org/258623002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266160 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/backends/chrome/websocket_unittest.py
+++ telemetry/telemetry/core/backends/chrome/websocket_unittest.py
@@ -1,10 +1,35 @@
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+import base64
+import BaseHTTPServer
+import hashlib
 import socket
+import threading
 import unittest
 
+
 from telemetry.core.backends.chrome import websocket
+
+
+# Minimal handler for a local websocket server.
+class _FakeWebSocketHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def do_GET(self):
+    key = self.headers.getheader('Sec-WebSocket-Key')
+
+    value = key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    hashed = base64.encodestring(hashlib.sha1(value).digest()).strip().lower()
+
+    self.send_response(101)
+
+    self.send_header('Sec-Websocket-Accept', hashed)
+    self.send_header('upgrade', 'websocket')
+    self.send_header('connection', 'upgrade')
+    self.end_headers()
+
+    self.wfile.flush()
+
 
 class TestWebSocket(unittest.TestCase):
   def testExports(self):
@@ -13,11 +38,16 @@ class TestWebSocket(unittest.TestCase):
     self.assertNotEqual(websocket.WebSocketTimeoutException, None)
 
   def testSockOpts(self):
-    ws = websocket.create_connection('ws://echo.websocket.org')
+    httpd = BaseHTTPServer.HTTPServer(('', 0), _FakeWebSocketHandler)
+    threading.Thread(target=httpd.handle_request).start()
+    ws_url = 'ws://127.0.0.1:%d' % httpd.server_port
+    ws = websocket.create_connection(ws_url)
     self.assertNotEquals(
         ws.sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR), 0)
+
+    threading.Thread(target=httpd.handle_request).start()
     ws = websocket.create_connection(
-        'ws://echo.websocket.org',
+        ws_url,
         sockopt=[(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)])
     self.assertNotEquals(
         ws.sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR), 0)
