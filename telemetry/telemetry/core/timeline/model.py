c8848417d52d8d5f596f67676f7f05b3a421323c
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 10:24:13 +0000

    [Telemetry] Update copyright header style.
    I'm tired of copy/pasting the wrong style :)
    
    http://www.chromium.org/developers/coding-style#TOC-File-headers
    
    BUG=
    
    Review URL: https://codereview.chromium.org/319813004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275374 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/core/timeline/model.py
+++ telemetry/telemetry/core/timeline/model.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 '''A container for timeline-based events and traces and can handle importing
