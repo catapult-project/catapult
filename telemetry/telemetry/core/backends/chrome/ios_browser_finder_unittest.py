0cca921ff11a537449e37b81dd47467eb83a7a0c
baxley@chromium.org <baxley@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-23 18:16:45 +0000

    Add unit test for IosBrowserFinder.
    This test checks if Chrome on iOS is running. It only
    runs on iOS platforms.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/350583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279143 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ telemetry/telemetry/core/backends/chrome/ios_browser_finder_unittest.py
@@ -0,0 +1,24 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import unittest
+
+from telemetry.core import browser_options
+from telemetry.core.backends.chrome import ios_browser_finder
+from telemetry.unittest import test
+
+class IosBrowserFinderUnitTest(unittest.TestCase):
+  # TODO(baxley): Currently the tests require a device with Chrome running.
+  # This should be stubbed out so it runs on any system, with no device
+  # dependencies.
+  @test.Enabled('ios')
+  def testFindIosChrome(self):
+    finder_options = browser_options.BrowserFinderOptions()
+    browsers = ios_browser_finder.FindAllAvailableBrowsers(finder_options)
+    self.assertTrue(browsers)
+    for browser in browsers:
+      self.assertEqual('ios-chrome', browser.browser_type)
+
+if __name__ == '__main__':
+  unittest.main()
+
