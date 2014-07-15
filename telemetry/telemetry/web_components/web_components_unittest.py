a074631c36ee68558119ce6d3279cdb0d0b57ed7
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-15 08:46:50 +0000

    Bypass web component tests when NO_TVCM=1
    R=dtu
    
    Review URL: https://codereview.chromium.org/390123003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283167 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/web_components/web_components_unittest.py
+++ telemetry/telemetry/web_components/web_components_unittest.py
@@ -2,14 +2,20 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+import unittest
+
 from telemetry.web_components import web_components_project
 from tvcm import module_test_case
 
 
 def load_tests(_, _2, _3):
   project = web_components_project.WebComponentsProject()
-  suite = module_test_case.DiscoverTestsInModule(
-      project,
-      project.telemetry_path)
-  assert suite.countTestCases() > 0, 'Expected to find at least one test.'
+  if os.getenv('NO_TVCM'):
+    suite = unittest.TestSuite()
+  else:
+    suite = module_test_case.DiscoverTestsInModule(
+        project,
+        project.telemetry_path)
+    assert suite.countTestCases() > 0, 'Expected to find at least one test.'
   return suite
