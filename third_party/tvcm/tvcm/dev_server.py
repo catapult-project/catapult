5c21af451e4e3c481d7344835187e42c8051b46e
Nat Duca <nduca@chromium.org>
2014-07-18 08:34:28 +0000

    Alphabetize imports

--- third_party/tvcm/tvcm/dev_server.py
+++ third_party/tvcm/tvcm/dev_server.py
@@ -2,13 +2,13 @@
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import base64
 import json
 import optparse
 import os
 import sys
 import time
 import traceback
-import base64
 
 from tvcm import project as project_module
 from tvcm import generate
