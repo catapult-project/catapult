f9a43ed5634503bf215360a0633cadb142112663
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 15:52:26 +0000

    [telemetry] Remove imports from telemetry/__init__.py
    These are only used in doc generation. The Telemetry imports increase the startup time of memory_cache_http_server by 100-150 ms. This is a ~10s improvement for unit test cycle time.
    
    
    BUG=388256, 255326
    TEST=tools/telemetry/run_tests
    
    Review URL: https://codereview.chromium.org/475213002
    
    Cr-Commit-Position: refs/heads/master@{#290564}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290564 0039d316-1c4b-4281-b951-d872f2087c98

--- telemetry/telemetry/__init__.py
+++ telemetry/telemetry/__init__.py
@@ -4,9 +4,6 @@
 
 """A library for cross-platform browser tests."""
 
-import inspect
-import logging
-import os
 import sys
 
 # Ensure Python >= 2.7.
@@ -16,24 +13,3 @@ if sys.version_info < (2, 7):
 
 from telemetry.util import global_hooks
 global_hooks.InstallHooks()
-
-from telemetry.core.browser import Browser
-from telemetry.core.browser_options import BrowserFinderOptions
-from telemetry.core.tab import Tab
-
-from telemetry.page.page_runner import Run as RunPage
-
-
-__all__ = []
-
-# Find all local vars that are classes or functions and make sure they're in the
-# __all__ array so they're included in docs.
-for x in dir():
-  if x.startswith('_'):
-    continue
-  if x in (inspect, os, sys):
-    continue
-  m = sys.modules[__name__]
-  if (inspect.isclass(getattr(m, x)) or
-      inspect.isfunction(getattr(m, x))):
-    __all__.append(x)
