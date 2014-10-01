61b7ee073efcd698329bec69a9eb682a1bc032d3
dtu <dtu@chromium.org>
2014-10-01 16:43:01 -0700

    Add py_trace_event to DEPS.
    py_trace_event lets you instrument Python code to get Chrome traces.
    In third_party without a README.chromium, because
    it's by Chromium authors and Chromium-licensed.
    
    BUG=None.
    TEST=gclient sync
    
    Review URL: https://codereview.chromium.org/615363002
    
    Cr-Commit-Position: refs/heads/master@{#297736}

---
+++ telemetry/telemetry/util/trace.py
@@ -0,0 +1,10 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.core import util
+
+
+util.AddDirToPythonPath(util.GetChromiumSrcDir(),
+                        'third_party', 'py_trace_event', 'src')
+from trace_event import *  # pylint: disable=F0401
