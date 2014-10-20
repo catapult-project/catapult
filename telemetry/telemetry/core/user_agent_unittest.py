a799454ff54108d21617f53064f390a6417d4ab1
daniimms <daniimms@amazon.com>
2014-10-20 15:08:46 -0700

    Increase coverage of telemetry's user_agent_unittest
    Added a mobile and desktop testing in case the browser being tested has a
    user agent that defaults to the tablet user agent.
    
    BUG=421229
    
    Review URL: https://codereview.chromium.org/640453002
    
    Cr-Commit-Position: refs/heads/master@{#300349}

--- telemetry/telemetry/core/user_agent_unittest.py
+++ telemetry/telemetry/core/user_agent_unittest.py
@@ -6,7 +6,16 @@ from telemetry.core import user_agent
 from telemetry.unittest import tab_test_case
 
 
-class UserAgentTest(tab_test_case.TabTestCase):
+class MobileUserAgentTest(tab_test_case.TabTestCase):
+  @classmethod
+  def CustomizeBrowserOptions(cls, options):
+    options.browser_user_agent_type = 'mobile'
+
+  def testUserAgent(self):
+    ua = self._tab.EvaluateJavaScript('window.navigator.userAgent')
+    self.assertEquals(ua, user_agent.UA_TYPE_MAPPING['mobile'])
+
+class TabletUserAgentTest(tab_test_case.TabTestCase):
   @classmethod
   def CustomizeBrowserOptions(cls, options):
     options.browser_user_agent_type = 'tablet'
@@ -14,3 +23,12 @@ class UserAgentTest(tab_test_case.TabTestCase):
   def testUserAgent(self):
     ua = self._tab.EvaluateJavaScript('window.navigator.userAgent')
     self.assertEquals(ua, user_agent.UA_TYPE_MAPPING['tablet'])
+
+class DesktopUserAgentTest(tab_test_case.TabTestCase):
+  @classmethod
+  def CustomizeBrowserOptions(cls, options):
+    options.browser_user_agent_type = 'desktop'
+
+  def testUserAgent(self):
+    ua = self._tab.EvaluateJavaScript('window.navigator.userAgent')
+    self.assertEquals(ua, user_agent.UA_TYPE_MAPPING['desktop'])
