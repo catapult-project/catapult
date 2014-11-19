d0b2323c1a20b6044f6cd27d7514a864e964389a
miletus <miletus@chromium.org>
2014-11-19 12:35:10 -0800

    [telemetry] Suppress unused import
    ** Presubmit Warnings **
    Pylint (436 files) (39.05s) failed
    ************* Module telemetry.core.backends.chrome.websocket
    W: 14, 0: Unused import WebSocketException (unused-import)
    W: 15, 0: Unused import WebSocketTimeoutException (unused-import)
    
    BUG=
    
    Review URL: https://codereview.chromium.org/742803002
    
    Cr-Commit-Position: refs/heads/master@{#304881}

--- telemetry/telemetry/core/backends/chrome/websocket.py
+++ telemetry/telemetry/core/backends/chrome/websocket.py
@@ -9,7 +9,7 @@ from telemetry.core import util
 
 util.AddDirToPythonPath(
     util.GetTelemetryDir(), 'third_party', 'websocket-client')
-# pylint: disable=import-error
+# pylint: disable=import-error, unused-import
 from websocket import create_connection as _create_connection
 from websocket import WebSocketException
 from websocket import WebSocketTimeoutException
