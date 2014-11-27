98a196934695a6e91957e24b4476421f0b6cccbf
nednguyen <nednguyen@google.com>
2014-11-27 09:18:59 -0800

    Reenable CloudStorage.testExistsReturnsFalse test on all platforms except windows
    TBR=dtu@chromium.org, kbr@chromium.org
    
    BUG=437115
    
    Review URL: https://codereview.chromium.org/725343007
    
    Cr-Commit-Position: refs/heads/master@{#306001}

--- telemetry/telemetry/util/cloud_storage_unittest.py
+++ telemetry/telemetry/util/cloud_storage_unittest.py
@@ -4,12 +4,13 @@
 
 import unittest
 
-# TODO(kbr): put test back in once harness is fixed.  crbug.com/437115
-# from telemetry.unittest_util import system_stub
+from telemetry import decorators
+
+from telemetry.unittest_util import system_stub
 from telemetry.util import cloud_storage
 
-# def _FakeFindGsutil():
-#   return 'fake gsutil path'
+def _FakeFindGsutil():
+  return 'fake gsutil path'
 
 class CloudStorageUnitTest(unittest.TestCase):
 
@@ -26,16 +27,18 @@ class CloudStorageUnitTest(unittest.TestCase):
                      '/b/chromium-telemetry/o/test-remote-path.html',
                      cloud_url)
 
-  # TODO(kbr): put test back in once harness is fixed. crbug.com/437115
-  # def testExistsReturnsFalse(self):
-  #   stubs = system_stub.Override(cloud_storage, ['subprocess'])
-  #   try:
-  #     stubs.subprocess.Popen.communicate_result = (
-  #       '',
-  #       'CommandException: One or more URLs matched no objects.\n')
-  #     stubs.subprocess.Popen.returncode_result = 1
-  #     cloud_storage.FindGsutil = _FakeFindGsutil
-  #     self.assertFalse(cloud_storage.Exists('fake bucket',
-  #                                           'fake remote path'))
-  #   finally:
-  #     stubs.Restore()
+
+  @decorators.Disabled('win')
+  # TODO(kbr): reenable test on windows once harness is fixed. crbug.com/437115
+  def testExistsReturnsFalse(self):
+    stubs = system_stub.Override(cloud_storage, ['subprocess'])
+    try:
+      stubs.subprocess.Popen.communicate_result = (
+        '',
+        'CommandException: One or more URLs matched no objects.\n')
+      stubs.subprocess.Popen.returncode_result = 1
+      cloud_storage.FindGsutil = _FakeFindGsutil
+      self.assertFalse(cloud_storage.Exists('fake bucket',
+                                            'fake remote path'))
+    finally:
+      stubs.Restore()
