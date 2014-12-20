31521ae602d46a028458a9c42c094953d4605674
mthiesse <mthiesse@chromium.org>
2014-12-19 16:18:22 -0800

    Initial implementation of the screen finder for touch latency testing.
    BUG=437525
    
    Review URL: https://codereview.chromium.org/601313002
    
    Cr-Commit-Position: refs/heads/master@{#309305}

--- telemetry/telemetry/image_processing/cv_util.py
+++ telemetry/telemetry/image_processing/cv_util.py
@@ -13,10 +13,10 @@ np = external_modules.ImportRequiredModule('numpy')
 def AreLinesOrthogonal(line1, line2, tolerance):
   """Returns true if lines are within tolerance radians of being orthogonal."""
   # Map each line onto an angle between 0 and 180.
-  theta1 = np.arctan2(np.float(line1[0][1] - line1[1][1]),
-                      np.float(line1[0][0] - line1[1][0]))
-  theta2 = np.arctan2(np.float(line2[0][1] - line2[1][1]),
-                      np.float(line2[0][0] - line2[1][0]))
+  theta1 = np.arctan2(np.float(line1[1] - line1[3]),
+                      np.float(line1[0] - line1[2]))
+  theta2 = np.arctan2(np.float(line2[1] - line2[3]),
+                      np.float(line2[0] - line2[2]))
   angle2 = abs(theta2 - theta1)
   if angle2 >= np.pi:
     angle2 -= np.pi
@@ -32,10 +32,10 @@ def FindLineIntersection(line1, line2):
   # Compute g, and h, the factor by which each line must be extended to
   # exactly touch the other line. If both are between 0 and 1, then the lines
   # currently intersect. We use h to compute their intersection.
-  line1p1 = np.asfarray(line1[1])
-  line1p0 = np.asfarray(line1[0])
-  line2p1 = np.asfarray(line2[1])
-  line2p0 = np.asfarray(line2[0])
+  line1p1 = line1[:2]
+  line1p0 = line1[2:]
+  line2p1 = line2[:2]
+  line2p0 = line2[2:]
   E = np.subtract(line1p1, line1p0)
   F = np.subtract(line2p1, line2p0)
   Pe = np.asfarray((-E[1], E[0]))
@@ -45,44 +45,46 @@ def FindLineIntersection(line1, line2):
   g = np.dot(np.subtract(line2p0, line1p0), Pf)
   g = np.divide(g, np.dot(E, Pf))
   intersection = np.add(line2p0, np.dot(F, h))
-  if h >= -0.000001 and h <= 1.000001 and g >= -0.000001 and g <= 1.000001:
-    return True, (intersection[0], intersection[1])
-  return False, (intersection[0], intersection[1])
+  intersect = (h >= -0.000001 and h <= 1.000001 and
+               g >= -0.000001 and g <= 1.000001)
+  return intersect, intersection
 
 
 def ExtendLines(lines, length):
   """Extends lines in an array to a given length, maintaining the center
   point. Does not necessarily maintain point order."""
-  extended = []
   half_length = length / 2.0
-  for x1, y1, x2, y2 in lines:
-    angle = np.arctan2(np.float(y1 - y2), np.float(x1 - x2))
-    xoffset = half_length * np.cos(angle)
-    yoffset = half_length * np.sin(angle)
-    centerx = (x1 + x2) / 2.0
-    centery = (y1 + y2) / 2.0
-    x1 = centerx - xoffset
-    x2 = centerx + xoffset
-    y1 = centery - yoffset
-    y2 = centery + yoffset
-    extended.append(((x1, y1), (x2, y2)))
-  return extended
+  angles = np.arctan2(lines[:, 1] - lines[:, 3], lines[:, 0] - lines[:, 2])
+  xoffsets = half_length * np.cos(angles)
+  yoffsets = half_length * np.sin(angles)
+  centerx = (lines[:, 0] + lines[:, 2]) / 2.0
+  centery = (lines[:, 1] + lines[:, 3]) / 2.0
+  lines[:, 0] = centerx - xoffsets
+  lines[:, 2] = centerx + xoffsets
+  lines[:, 1] = centery - yoffsets
+  lines[:, 3] = centery + yoffsets
+  return lines
 
 
 def IsPointApproxOnLine(point, line, tolerance=1):
   """Approximates distance between point and line for small distances using
   the determinant and checks whether it's within the tolerance. Tolerance is
   an approximate distance in pixels, precision decreases with distance."""
-  xd = line[0][0] - line[1][0]
-  yd = line[0][1] - line[1][1]
-  det = (((xd) * (point[1] - line[1][1])) -
-         ((yd) * (point[0] - line[1][0])))
+  xd = line[0] - line[2]
+  yd = line[1] - line[3]
+  det = ((xd) * (point[1] - line[3])) - ((yd) * (point[0] - line[2]))
   tolerance = float(tolerance) * (abs(xd) + abs(yd))
   return abs(det) * 2.0 <= tolerance
 
 
+def SqDistances(points1, points2):
+  """Computes the square of the distance between two sets of points, or a
+  set of points and a point."""
+  d = np.square(points1 - points2)
+  return d[:, 0] + d[:, 1]
+
+
 def SqDistance(point1, point2):
   """Computes the square of the distance between two points."""
-  x = point1[0] - point2[0]
-  y = point1[1] - point2[1]
-  return (x * x) + (y * y)
+  d = np.square(point1 - point2)
+  return d[0] + d[1]
