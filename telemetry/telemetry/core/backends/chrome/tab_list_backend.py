4f49786db8e9d60b22c4e256b849095110aae85a
erikchen <erikchen@chromium.org>
2015-03-11 19:34:52 -0700

    Telemetry: Improve documentation and exception handling for TabListBackend.
    This CL adds documentation to public methods of TabListBackend to indicate
    which exceptions can be raised. This CL improves the exception handling so that
    the exception raised is always a subclass of exceptions.Error, and does not
    lose the original signature. This CL converts several asserts into exceptions.
    
    This CL removes the unused method GetTabUrl.
    
    BUG=460625
    
    Review URL: https://codereview.chromium.org/989383002
    
    Cr-Commit-Position: refs/heads/master@{#320208}

--- telemetry/telemetry/core/backends/chrome/tab_list_backend.py
+++ telemetry/telemetry/core/backends/chrome/tab_list_backend.py
@@ -2,13 +2,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import urllib2
+import sys
 
 from telemetry.core import exceptions
 from telemetry.core import tab
 from telemetry.core import util
 from telemetry.core.backends.chrome_inspector import devtools_http
 from telemetry.core.backends.chrome_inspector import inspector_backend_list
+
+
+class TabNotFoundError(exceptions.Error):
+  pass
+
+
+class TabUnexpectedResponseException(exceptions.Error):
+  pass
 
 
 class TabListBackend(inspector_backend_list.InspectorBackendList):
@@ -22,11 +30,24 @@ class TabListBackend(inspector_backend_list.InspectorBackendList):
     return self._browser_backend.devtools_client.devtools_http
 
   def New(self, timeout):
+    """Makes a new tab.
+
+    Raises:
+      devtools_http.DevToolsClientConnectionError
+    """
     assert self._browser_backend.supports_tab_control
     self._devtools_http.Request('new', timeout=timeout)
     return self[-1]
 
   def CloseTab(self, debugger_url, timeout=None):
+    """Closes the tab with the given debugger_url.
+
+    Raises:
+      devtools_http.DevToolsClientConnectionError
+      TabNotFoundError
+      TabUnexpectedResponseException
+      exceptions.TimeoutException
+    """
     assert self._browser_backend.supports_tab_control
     tab_id = inspector_backend_list.DebuggerUrlToId(debugger_url)
     # TODO(dtu): crbug.com/160946, allow closing the last tab on some platforms.
@@ -37,26 +58,38 @@ class TabListBackend(inspector_backend_list.InspectorBackendList):
       response = self._devtools_http.Request('close/%s' % tab_id,
                                              timeout=timeout)
     except devtools_http.DevToolsClientUrlError:
-      raise Exception('Unable to close tab, tab id not found: %s' % tab_id)
-    assert response == 'Target is closing'
+      error = TabNotFoundError(
+          'Unable to close tab, tab id not found: %s' % tab_id)
+      raise error, None, sys.exc_info()[2]
+
+    if response != 'Target is closing':
+      raise TabUnexpectedResponseException('Received response: %s' % response)
+
     util.WaitFor(lambda: tab_id not in self, timeout=5)
 
   def ActivateTab(self, debugger_url, timeout=None):
+    """Activates the tab with the given debugger_url.
+
+    Raises:
+      devtools_http.DevToolsClientConnectionError
+      TabNotFoundError
+      TabUnexpectedResponseException
+    """
     assert self._browser_backend.supports_tab_control
     tab_id = inspector_backend_list.DebuggerUrlToId(debugger_url)
-    assert tab_id in self
+
+    if tab_id not in self:
+      raise TabNotFoundError('Tab id not found: %s' % tab_id)
+
     try:
       response = self._devtools_http.Request('activate/%s' % tab_id,
                                              timeout=timeout)
     except devtools_http.DevToolsClientUrlError:
-      raise Exception('Unable to activate tab, tab id not found: %s' % tab_id)
-    assert response == 'Target activated'
-
-  def GetTabUrl(self, debugger_url):
-    tab_id = inspector_backend_list.DebuggerUrlToId(debugger_url)
-    tab_info = self.GetContextInfo(tab_id)
-    assert tab_info is not None
-    return tab_info['url']
+      error = TabNotFoundError(
+          'Unable to close tab, tab id not found: %s' % tab_id)
+      raise error, None, sys.exc_info()[2]
+    if response != 'Target activated':
+      raise TabUnexpectedResponseException('Received response: %s' % response)
 
   def Get(self, index, ret):
     """Returns self[index] if it exists, or ret if index is out of bounds."""
