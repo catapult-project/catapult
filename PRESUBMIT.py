7093faca6413095b4727ff8e66af31ff09e90716
Nat Duca <nduca@chromium.org>
2015-03-24 22:05:34 -0700

    Add empty presubmit hook.

---
+++ PRESUBMIT.py
@@ -0,0 +1,8 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+def CheckChangeOnUpload(input_api, output_api):
+  return []
+
+def CheckChangeOnCommit(input_api, output_api):
+  return []
