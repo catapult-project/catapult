7cb3773c8f6ac865f731ae28bc25a284d88310ed
Annie Sullivan <sullivan@chromium.org>
2015-04-02 17:42:33 -0400

    Added rjsmin to tvcm/third_party in anticipation of switching to it as our javascript minifier.

---
+++ third_party/tvcm/third_party/rjsmin/bench.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+export PYTHONPATH=$PWD
+cleanup() {
+    rm -f -- "$out"
+}
+out="$(mktemp)"
+trap cleanup EXIT
+
+for v in 3.4 3.3 3.2 3.1 3.0 2.7 2.6 2.5 2.4; do
+(
+    set -e
+
+    p=python$v
+    $p make.py makefile || continue
+    CFLAGS=-O3 make clean compile
+    $p -OO bench/main.py -p >( cat - >>"$out" ) bench/*.js
+)
+done
+python make.py makefile
+
+[ "$1" = "-w" ] && \
+    python -mbench.write \
+    -p docs/BENCHMARKS \
+    -t docs/_userdoc/benchmark.txt \
+    <"$out" \
+    || true
