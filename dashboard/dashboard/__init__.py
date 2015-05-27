10aee2bb3db0242ceb0f88f0e1016201f7413516
qyearsley <qyearsley@chromium.org>
2015-05-27 16:06:13 -0700

    Add third-party libraries required by webtest (bs4, six).
    After running the dashboard unit tests on the catapult waterfall, it became clear that webtest (which dashboard tests depend on)
    depends on bs4 and six. See: https://codereview.chromium.org/1151153006/
    
    Review URL: https://codereview.chromium.org/1152763004

--- dashboard/dashboard/__init__.py
+++ dashboard/dashboard/__init__.py
@@ -6,9 +6,10 @@ import sys
 _DASHBOARD_PARENT = os.path.join(os.path.dirname(__file__), os.path.pardir)
 _THIRD_PARTY = os.path.join(_DASHBOARD_PARENT, os.path.pardir, 'third_party')
 _THIRD_PARTY_PATHS = [
+    os.path.join(_THIRD_PARTY, 'beautifulsoup4'),
     os.path.join(_THIRD_PARTY, 'mock-1.0.1'),
+    os.path.join(_THIRD_PARTY, 'six-1.9.0'),
     os.path.join(_THIRD_PARTY, 'webtest'),
-    os.path.join(_THIRD_PARTY, 'oauth2client'),
 ]
 
 sys.path.extend(_THIRD_PARTY_PATHS)
