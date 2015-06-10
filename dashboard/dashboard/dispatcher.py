4bf5f3bed65aa5d9cd137f15feebce5ef7579724
qyearsley <qyearsley@chromium.org>
2015-06-10 10:22:43 -0700

    Import model classes in dispatcher.py to register them with NDB.
    BUG=
    
    Review URL: https://codereview.chromium.org/1171093002

--- dashboard/dashboard/dispatcher.py
+++ dashboard/dashboard/dispatcher.py
@@ -5,6 +5,9 @@ import webapp2
 from dashboard import ip_whitelist
 from dashboard import main
 
+# Importing Model classes in order to register them with NDB.
+from dashboard.models import *  # pylint: disable=wildcard-import
+
 _ROUTING_TABLE = [
      ('/ip_whitelist', ip_whitelist.IpWhitelistHandler),
      ('/', main.MainHandler),
