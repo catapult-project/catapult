d3557d1d972ac33cb8ade9645e19cc71a5712424
aiolos <aiolos@chromium.org>
2015-07-06 13:46:53 -0700

    Remove catching of import errors in discover
    BUG=506353
    
    Review URL: https://codereview.chromium.org/1215403002
    
    Cr-Commit-Position: refs/heads/master@{#337465}

--- telemetry/telemetry/core/discover.py
+++ telemetry/telemetry/core/discover.py
@@ -41,10 +41,7 @@ def DiscoverModules(start_dir, top_level_dir, pattern='*'):
       module_name = re.sub(r'[/\\]', '.', os.path.splitext(module_rel_path)[0])
 
       # Import the module.
-      try:
-        module = __import__(module_name, fromlist=[True])
-      except ImportError:
-        continue
+      module = __import__(module_name, fromlist=[True])
       modules.append(module)
   return modules
 
