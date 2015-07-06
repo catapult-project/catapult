e91f14887912a8ea7d6122b995a09dda603dfdc3
qyearsley <qyearsley@chromium.org>
2015-07-06 11:38:24 -0700

    Convert xsrf_token to string before validating token in xsrf.py.
    Apparently the fact that self.request.get('xsrf_token') was returning
    a unicode object rather than str was causing validation to fail.
    
    This affected the tests run on the bot environment, but not locally,
    possibly because a new version of App Engine SDK was in use, but I didn't
    verify this.
    
    This should fix dashboard tests (ip_whitelist_test 403 errors) on
    the catapult waterfall.
    
    BUG=505849
    
    Review URL: https://codereview.chromium.org/1218103006

--- dashboard/dashboard/xsrf.py
+++ dashboard/dashboard/xsrf.py
@@ -1,3 +1,7 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 """Decorator that validates XSRF tokens."""
 
 import os
@@ -41,7 +45,8 @@ def TokenRequired(handler_method):
 
   def CheckToken(self, *args, **kwargs):
     user = users.get_current_user()
-    if not user or not _ValidateToken(self.request.get('xsrf_token'), user):
+    token = str(self.request.get('xsrf_token'))
+    if not user or not _ValidateToken(token, user):
       self.abort(403)
     handler_method(self, *args, **kwargs)
 
---
+++ dashboard/dashboard/xsrf_test.py
@@ -0,0 +1,60 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for xsrf module."""
+
+import unittest
+
+import webapp2
+import webtest
+
+from google.appengine.api import users
+
+from dashboard import request_handler
+from dashboard import testing_common
+from dashboard import xsrf
+
+
+class ExampleHandler(request_handler.RequestHandler):
+  """Example request handler that uses a XSRF token."""
+
+  @xsrf.TokenRequired
+  def post(self):
+    pass
+
+
+class XsrfTest(testing_common.TestCase):
+
+  def setUp(self):
+    super(XsrfTest, self).setUp()
+    app = webapp2.WSGIApplication([('/example', ExampleHandler)])
+    self.testapp = webtest.TestApp(app)
+    self.SetCurrentUser('foo@bar.com')
+
+  def testGenerateToken_CanBeValidatedWithSameUser(self):
+    token = xsrf.GenerateToken(users.get_current_user())
+    self.assertTrue(xsrf._ValidateToken(token, users.get_current_user()))
+
+  def testGenerateToken_CanNotBeValidatedWithDifferentUser(self):
+    token = xsrf.GenerateToken(users.get_current_user())
+    self.SetCurrentUser('other@bar.com', user_id='y')
+    self.assertFalse(xsrf._ValidateToken(token, users.get_current_user()))
+
+  def testTokenRequired_NoToken_Returns403(self):
+    self.testapp.post('/example', {}, status=403)
+
+  def testTokenRequired_BogusToken_Returns403(self):
+    self.testapp.post(
+        '/example',
+        {'xsrf_token': 'abcdefghijklmnopqrstuvwxyz0123456789'},
+        status=403)
+
+  def testTokenRequired_CorrectToken_Success(self):
+    self.SetCurrentUser('foo@bar.com')
+    token = xsrf.GenerateToken(users.get_current_user())
+    self.testapp.post('/example', {'xsrf_token': token})
+
+
+if __name__ == '__main__':
+  unittest.main()
