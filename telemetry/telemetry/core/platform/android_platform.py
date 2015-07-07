37b4839e1928cb6b07f74911a4fb6025ed90c589
perezju <perezju@chromium.org>
2015-07-07 05:30:43 -0700

    Reland of: [Telemetry] Add benchmark for memory health plan
    Original CL: http://crrev.com/1208933003
    
    No changes were needed. Previous version got reverted due to crbug.com/507236 now fixed.
    
    Original description:
    
    New timeline based Telemetry becnhmark and user story to gather memory
    metrics for the System Health Plan. This will measure and report metrics
    gathered via memory-infra tracing events.
    
    BUG=486009,507236
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1213443008
    
    Cr-Commit-Position: refs/heads/master@{#337592}

--- telemetry/telemetry/core/platform/android_platform.py
+++ telemetry/telemetry/core/platform/android_platform.py
@@ -19,7 +19,8 @@ class AndroidPlatform(platform.Platform):
   def android_action_runner(self):
     return self._android_action_runner
 
-  def LaunchAndroidApplication(self, start_intent, is_app_ready_predicate=None):
+  def LaunchAndroidApplication(self, start_intent, is_app_ready_predicate=None,
+                               app_has_webviews=True):
     """Launches an Android application given the intent.
 
     Args:
@@ -27,9 +28,17 @@ class AndroidPlatform(platform.Platform):
       is_app_ready_predicate: A predicate function to determine
           whether the app is ready. This is a function that takes an
           AndroidApp instance and return a boolean. When it is not passed in,
-          the app is ready by default.
+          the app is ready when the intent to launch it is completed.
+      app_has_webviews: A boolean indicating whether the app is expected to
+          contain any WebViews. If True, the app will be launched with
+          appropriate webview flags, and the GetWebViews method of the returned
+          object may be used to access them.
+
+    Returns:
+      A reference to the android_app launched.
     """
     self._platform_backend.DismissCrashDialogIfNeeded()
     app_backend = android_app_backend.AndroidAppBackend(
-        self._platform_backend, start_intent, is_app_ready_predicate)
+        self._platform_backend, start_intent, is_app_ready_predicate,
+        app_has_webviews)
     return android_app.AndroidApp(app_backend, self._platform_backend)
