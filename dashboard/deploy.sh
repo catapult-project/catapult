2b7393fa41859b08124a7d7868ed44708933e21b
sullivan <sullivan@chromium.org>
2015-08-04 04:30:05 -0700

    Fix copyright headers on dashboard deploy.sh and buildbot experimental scripts.
    TBR=qyearsley@chromium.org
    
    Review URL: https://codereview.chromium.org/1263933003

--- dashboard/deploy.sh
+++ dashboard/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # This script prepares the dashboard to be deployed and then
 # invokes appcfg.py to upload it. If successful, this uploads
 # a new version but doesn't set that version as default.
