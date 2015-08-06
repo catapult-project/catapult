571667c4ed9641c079a73bb449c45612515a8f58
nednguyen <nednguyen@google.com>
2015-08-06 10:09:35 -0700

    [Telemetry] Add std to list_of_scalar_values.
    BUG=516344
    
    Review URL: https://codereview.chromium.org/1280493002
    
    Cr-Commit-Position: refs/heads/master@{#342134}

--- telemetry/telemetry/value/list_of_scalar_values.py
+++ telemetry/telemetry/value/list_of_scalar_values.py
@@ -3,9 +3,58 @@
 # found in the LICENSE file.
 
 import numbers
+import math
 
 from telemetry import value as value_module
 from telemetry.value import none_values
+
+
+def Variance(sample):
+  """ Compute the population variance.
+
+    Args:
+      sample: a list of numbers.
+  """
+  k = len(sample) - 1  # Bessel correction
+  if k <= 0:
+    return 0
+  m = _Mean(sample)
+  return sum((x - m)**2 for x in sample)/k
+
+
+def StandardDeviation(sample):
+  """ Compute standard deviation for a list of numbers.
+
+    Args:
+      sample: a list of numbers.
+  """
+  return math.sqrt(Variance(sample))
+
+
+def PooledStandardDeviation(list_of_samples, list_of_variances=None):
+  """ Compute standard deviation for a list of samples.
+
+  See: https://en.wikipedia.org/wiki/Pooled_variance for the formula.
+
+  Args:
+    list_of_samples: a list of lists, each is a list of numbers.
+    list_of_variances: a list of numbers, the i-th element is the variance of
+      the i-th sample in list_of_samples. If this is None, we use
+      Variance(sample) to get the variance of the i-th sample.
+  """
+  pooled_variance = 0.0
+  total_degrees_of_freedom = 0
+  for i in xrange(len(list_of_samples)):
+    l = list_of_samples[i]
+    k = len(l) - 1  # Bessel correction
+    if k <= 0:
+      continue
+    variance = list_of_variances[i] if list_of_variances else Variance(l)
+    pooled_variance += k * variance
+    total_degrees_of_freedom += k
+  if total_degrees_of_freedom:
+    return (pooled_variance/total_degrees_of_freedom) ** 0.5
+  return 0
 
 
 def _Mean(values):
@@ -13,20 +62,42 @@ def _Mean(values):
 
 
 class ListOfScalarValues(value_module.Value):
+  """ ListOfScalarValues represents a list of numbers.
+
+  By default, std is the standard deviation of all numbers in the list. Std can
+  also be specified in the constructor if the numbers are not from the same
+  population.
+  """
   def __init__(self, page, name, units, values,
                important=True, description=None,
                tir_label=None, none_value_reason=None,
-               same_page_merge_policy=value_module.CONCATENATE):
+               std=None, same_page_merge_policy=value_module.CONCATENATE):
     super(ListOfScalarValues, self).__init__(page, name, units, important,
                                              description, tir_label)
     if values is not None:
       assert isinstance(values, list)
       assert len(values) > 0
       assert all(isinstance(v, numbers.Number) for v in values)
+      assert std is None or isinstance(std, numbers.Number)
+    else:
+      assert std is None
     none_values.ValidateNoneValueReason(values, none_value_reason)
     self.values = values
     self.none_value_reason = none_value_reason
     self.same_page_merge_policy = same_page_merge_policy
+    if values is not None and std is None:
+      std = StandardDeviation(values)
+    assert std is None or std >= 0, (
+        'standard deviation cannot be negative: %s' % std)
+    self._std = std
+
+  @property
+  def std(self):
+    return self._std
+
+  @property
+  def variance(self):
+    return self._std ** 2
 
   def __repr__(self):
     if self.page:
@@ -74,6 +145,7 @@ class ListOfScalarValues(value_module.Value):
   def AsDict(self):
     d = super(ListOfScalarValues, self).AsDict()
     d['values'] = self.values
+    d['std'] = self.std
 
     if self.none_value_reason is not None:
       d['none_value_reason'] = self.none_value_reason
@@ -84,6 +156,7 @@ class ListOfScalarValues(value_module.Value):
   def FromDict(value_dict, page_dict):
     kwargs = value_module.Value.GetConstructorKwArgs(value_dict, page_dict)
     kwargs['values'] = value_dict['values']
+    kwargs['std'] = value_dict['std']
 
     if 'none_value_reason' in value_dict:
       kwargs['none_value_reason'] = value_dict['none_value_reason']
@@ -118,17 +191,24 @@ class ListOfScalarValues(value_module.Value):
   def _MergeLikeValues(cls, values, page, name, tir_label):
     v0 = values[0]
     merged_values = []
+    list_of_samples = []
     none_value_reason = None
+    pooled_std = None
     for v in values:
       if v.values is None:
         merged_values = None
         none_value_reason = none_values.MERGE_FAILURE_REASON
         break
       merged_values.extend(v.values)
+      list_of_samples.append(v.values)
+    if merged_values:
+      pooled_std = PooledStandardDeviation(
+          list_of_samples, list_of_variances=[v.variance for v in values])
     return ListOfScalarValues(
         page, name, v0.units,
         merged_values,
         important=v0.important,
         tir_label=tir_label,
         same_page_merge_policy=v0.same_page_merge_policy,
+        std=pooled_std,
         none_value_reason=none_value_reason)
--- telemetry/telemetry/value/list_of_scalar_values_unittest.py
+++ telemetry/telemetry/value/list_of_scalar_values_unittest.py
@@ -11,6 +11,46 @@ from telemetry.value import list_of_scalar_values
 from telemetry.value import none_values
 
 
+class StatisticComputationTest(unittest.TestCase):
+  def testVariance(self):
+    self.assertAlmostEqual(
+        list_of_scalar_values.Variance([]), 0)
+    self.assertAlmostEqual(
+        list_of_scalar_values.Variance([3]), 0)
+    self.assertAlmostEqual(
+        list_of_scalar_values.Variance([600, 470, 170, 430, 300]), 27130)
+
+  def testStandardDeviation(self):
+    self.assertAlmostEqual(
+        list_of_scalar_values.StandardDeviation([]), 0)
+    self.assertAlmostEqual(
+        list_of_scalar_values.StandardDeviation([1]), 0)
+    self.assertAlmostEqual(
+        list_of_scalar_values.StandardDeviation([600, 470, 170, 430, 300]),
+        164.71186, places=4)
+
+  def testPooledVariance(self):
+    self.assertAlmostEqual(
+        list_of_scalar_values.PooledStandardDeviation([[], [], []]), 0)
+    self.assertAlmostEqual(
+        list_of_scalar_values.PooledStandardDeviation([[1], [], [3], []]), 0)
+    self.assertAlmostEqual(
+        list_of_scalar_values.PooledStandardDeviation([[1], [2], [3], [4]]), 0)
+    self.assertAlmostEqual(list_of_scalar_values.PooledStandardDeviation(
+        [[600, 470, 170, 430, 300],           # variance = 27130, std = 164.7
+        [4000, 4020, 4230],                   # variance = 16233, std = 127.41
+        [260, 700, 800, 900, 0, 120, 150]]),  # variance = 136348, std = 369.2
+        282.7060,  # SQRT((27130 4 + 16233*2 + 136348*6)/12)
+        places=4)
+    self.assertAlmostEqual(list_of_scalar_values.PooledStandardDeviation(
+        [[600, 470, 170, 430, 300],
+         [4000, 4020, 4230],
+         [260, 700, 800, 900, 0, 120, 150]],
+        list_of_variances=[100000, 200000, 300000]),
+        465.47466,  # SQRT((100000*4 + 200000* 2 + 300000*6)/12)
+        places=4)
+
+
 class TestBase(unittest.TestCase):
   def setUp(self):
     story_set = story.StorySet(base_dir=os.path.dirname(__file__))
@@ -31,10 +71,10 @@ class ValueTest(TestBase):
     page0 = self.pages[0]
     v0 = list_of_scalar_values.ListOfScalarValues(
         page0, 'x', 'unit',
-        [1, 2], same_page_merge_policy=value.CONCATENATE)
+        [10, 9, 9, 7], same_page_merge_policy=value.CONCATENATE)
     v1 = list_of_scalar_values.ListOfScalarValues(
         page0, 'x', 'unit',
-        [3, 4], same_page_merge_policy=value.CONCATENATE)
+        [300, 302, 303, 304], same_page_merge_policy=value.CONCATENATE)
     self.assertTrue(v1.IsMergableWith(v0))
 
     vM = (list_of_scalar_values.ListOfScalarValues.
@@ -44,7 +84,9 @@ class ValueTest(TestBase):
     self.assertEquals('unit', vM.units)
     self.assertEquals(value.CONCATENATE, vM.same_page_merge_policy)
     self.assertEquals(True, vM.important)
-    self.assertEquals([1, 2, 3, 4], vM.values)
+    self.assertEquals([10, 9, 9, 7, 300, 302, 303, 304], vM.values)
+    # SQRT((19/12 * 3 + 35/12 * 3)/6) = 1.5
+    self.assertAlmostEqual(1.5, vM.std)
 
   def testListSamePageMergingWithPickFirstPolicy(self):
     page0 = self.pages[0]
@@ -120,9 +162,26 @@ class ValueTest(TestBase):
         same_page_merge_policy=value.PICK_FIRST, important=False)
     d = v.AsDictWithoutBaseClassEntries()
 
-    self.assertEquals(d, {
-          'values': [1, 2]
-        })
+    self.assertEquals(d['values'], [1, 2])
+    self.assertAlmostEqual(d['std'], 0.7071, places=4)
+
+  def testMergedValueAsDict(self):
+    page0 = self.pages[0]
+    v0 = list_of_scalar_values.ListOfScalarValues(
+        page0, 'x', 'unit',
+        [10, 9, 9, 7], same_page_merge_policy=value.CONCATENATE)
+    v1 = list_of_scalar_values.ListOfScalarValues(
+        page0, 'x', 'unit',
+        [300, 302, 303, 304], same_page_merge_policy=value.CONCATENATE)
+    self.assertTrue(v1.IsMergableWith(v0))
+
+    vM = (list_of_scalar_values.ListOfScalarValues.
+          MergeLikeValuesFromSamePage([v0, v1]))
+    d = vM.AsDict()
+    self.assertEquals(d['values'], [10, 9, 9, 7, 300, 302, 303, 304])
+    # SQRT((19/12 * 3 + 35/12 * 3)/6)
+    self.assertAlmostEqual(d['std'], 1.5)
+
 
   def testNoneValueAsDict(self):
     v = list_of_scalar_values.ListOfScalarValues(
@@ -132,7 +191,8 @@ class ValueTest(TestBase):
 
     self.assertEquals(d, {
           'values': None,
-          'none_value_reason': 'n'
+          'none_value_reason': 'n',
+          'std': None
         })
 
   def testFromDictInts(self):
@@ -140,24 +200,28 @@ class ValueTest(TestBase):
       'type': 'list_of_scalar_values',
       'name': 'x',
       'units': 'unit',
-      'values': [1, 2]
+      'values': [1, 2],
+      'std': 0.7071
     }
     v = value.Value.FromDict(d, {})
 
     self.assertTrue(isinstance(v, list_of_scalar_values.ListOfScalarValues))
     self.assertEquals(v.values, [1, 2])
+    self.assertEquals(v.std, 0.7071)
 
   def testFromDictFloats(self):
     d = {
       'type': 'list_of_scalar_values',
       'name': 'x',
       'units': 'unit',
-      'values': [1.3, 2.7]
+      'values': [1.3, 2.7, 4.5, 2.1, 3.4],
+      'std': 0.901
     }
     v = value.Value.FromDict(d, {})
 
     self.assertTrue(isinstance(v, list_of_scalar_values.ListOfScalarValues))
-    self.assertEquals(v.values, [1.3, 2.7])
+    self.assertEquals(v.values, [1.3, 2.7, 4.5, 2.1, 3.4])
+    self.assertEquals(v.std, 0.901)
 
   def testFromDictNoneValue(self):
     d = {
@@ -165,6 +229,7 @@ class ValueTest(TestBase):
       'name': 'x',
       'units': 'unit',
       'values': None,
+      'std': None,
       'none_value_reason': 'n'
     }
     v = value.Value.FromDict(d, {})
