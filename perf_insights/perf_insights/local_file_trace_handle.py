ade2e1a90cc69169b8c613606b44ac7503b64503
dstockwell <dstockwell@chromium.org>
2015-08-13 00:06:48 -0700

    Attempt to gunzip traces in python
    Fixes https://github.com/catapult-project/catapult/issues/1193
    
    Review URL: https://codereview.chromium.org/1287193002

--- perf_insights/perf_insights/local_file_trace_handle.py
+++ perf_insights/perf_insights/local_file_trace_handle.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import gzip
 import os
+import shutil
+import tempfile
 
 from perf_insights import trace_handle
 
@@ -12,5 +15,13 @@ class LocalFileTraceHandle(trace_handle.TraceHandle):
     self.filename = filename
 
   def Open(self):
-    return open(self.filename, 'r')
-
+    temp_trace_file = tempfile.NamedTemporaryFile(
+        dir=os.path.dirname(self.filename))
+    try:
+      with gzip.GzipFile(self.filename) as unzipped:
+        shutil.copyfileobj(unzipped, temp_trace_file)
+        temp_trace_file.flush()
+        return temp_trace_file
+    except IOError:
+      temp_trace_file.close()
+      return open(self.filename, 'r')
