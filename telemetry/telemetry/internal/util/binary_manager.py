deb7eed6c8f40c38214f6f2da31315340455a831
binjin <binjin@chromium.org>
2015-08-14 03:11:00 -0700

    Revert of Add dependency_manager initialization to binary_manager (patchset #10 id:230001 of https://codereview.chromium.org/1280903003/ )
    Reason for revert:
    speculative reverts: break the XP bots
    http://build.chromium.org/p/chromium.win/builders/XP%20Tests%20%281%29/builds/39447
    
    Original issue's description:
    > Add dependency_manager initialization to binary_manager
    >
    > BUG=510231
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    >
    > Committed: https://crrev.com/f592ba18312f43fe4186cc526db00960a1db0b48
    > Cr-Commit-Position: refs/heads/master@{#343320}
    
    TBR=eakuefner@chromium.org,nednguyen@google.com,sullivan@chromium.org,dtu@chromium.org,dpranke@google.com,dpranke@chromium.org,aiolos@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=510231
    
    Review URL: https://codereview.chromium.org/1296563002
    
    Cr-Commit-Position: refs/heads/master@{#343360}

--- telemetry/telemetry/internal/util/binary_manager.py
+++ telemetry/telemetry/internal/util/binary_manager.py
@@ -2,63 +2,18 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-#TODO(aiolos): Remove the debug logging once the entire Dependency Manager
-#system has landed.
-import logging
-import os
-
 from catapult_base import support_binaries
-from catapult_base import dependency_manager
-from telemetry.core import exceptions
-from telemetry.core import util
-
-
-TELEMETRY_PROJECT_CONFIG = os.path.join(
-    util.GetTelemetryDir(), 'telemetry', 'internal', 'binary_dependencies.json')
-
-
-_dependency_manager = None
-
-
-def NeedsInit():
-  return not _dependency_manager
-
-
-def InitDependencyManager(environment_config):
-  global _dependency_manager
-  if _dependency_manager:
-    raise exceptions.InitializationError(
-        'Trying to re-initialize the binary manager with config %s'
-        % environment_config)
-  config_files = [TELEMETRY_PROJECT_CONFIG]
-  if environment_config:
-    config_files.append(environment_config)
-  logging.debug('Initializing the dependency manager with config files: %s.'
-                % config_files)
-  _dependency_manager = dependency_manager.DependencyManager(config_files)
-  logging.debug('Successfully initialized the dependency manager.')
 
 
 def FetchPath(binary_name, platform, arch):
   """ Return a path to the appropriate executable for <binary_name>, downloading
       from cloud storage if needed, or None if it cannot be found.
   """
-  logging.debug('Called FetchPath for binary: %s on platform: %s and arch: %s'
-                % (binary_name, platform, arch))
-  if _dependency_manager is None:
-    raise exceptions.InitializationError(
-        'Called FetchPath with uninitialized binary manager.')
   return support_binaries.FindPath(binary_name, platform, arch)
-
 
 def LocalPath(binary_name, platform, arch):
   """ Return a local path to the given binary name, or None if an executable
       cannot be found. Will not download the executable.
       """
-  logging.debug('Called LocalPath for binary: %s on platform: %s and arch: '
-                '%s' % (binary_name, platform, arch))
-  if _dependency_manager is None:
-    raise exceptions.InitializationError(
-        'Called LocalPath with uninitialized binary manager.')
   del platform, arch
   return support_binaries.FindLocallyBuiltPath(binary_name)
