37b71a9c37e454445335eb63e519feec39644df5
nednguyen <nednguyen@google.com>
2015-08-14 12:49:35 -0700

    [Telemetry] Remove the cache decorator in support_binaries
    BUG=510231
    
    Review URL: https://codereview.chromium.org/1292403002
    
    Cr-Commit-Position: refs/heads/master@{#343461}

--- telemetry/catapult_base/support_binaries.py
+++ telemetry/catapult_base/support_binaries.py
@@ -6,7 +6,6 @@ import os
 import stat
 
 from catapult_base import cloud_storage
-from telemetry import decorators
 from telemetry.internal.util import path
 
 
@@ -20,7 +19,6 @@ def _IsInCloudStorage(binary_name, arch_name, platform_name):
       _GetBinPath(binary_name, arch_name, platform_name) + '.sha1')
 
 
-@decorators.Cache
 def FindLocallyBuiltPath(binary_name):
   """Finds the most recently built |binary_name|."""
   command = None
@@ -39,7 +37,6 @@ def FindLocallyBuiltPath(binary_name):
   return command
 
 
-@decorators.Cache
 def FindPath(binary_name, arch_name, platform_name):
   """Returns the path to the given binary name, pulling from the cloud if
   necessary."""
@@ -47,10 +44,9 @@ def FindPath(binary_name, arch_name, platform_name):
     binary_name += '.exe'
   command = FindLocallyBuiltPath(binary_name)
   if not command and _IsInCloudStorage(binary_name, arch_name, platform_name):
-    cloud_storage.GetIfChanged(
-        _GetBinPath(binary_name, arch_name, platform_name),
-                    cloud_storage.PUBLIC_BUCKET)
     command = _GetBinPath(binary_name, arch_name, platform_name)
+    cloud_storage.GetIfChanged(
+        command, cloud_storage.PUBLIC_BUCKET)
 
     # Ensure the downloaded file is actually executable.
     if command and os.path.exists(command):
