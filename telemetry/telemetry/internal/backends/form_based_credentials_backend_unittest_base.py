209536294467bf4cb1b7b25633ef6a72bb72d1b0
nednguyen <nednguyen@google.com>
2015-08-24 09:14:43 -0700

    [Telemetry] Remove reference to chromium src dir in form_based_credentials_backend_unittest_base
    These tests are stale for long long time (tools/perf/data no longer exists).
    In the future, new tests that need login should use
    tools/perf/page_sets/login_helpers/
    
    BUG=511332
    
    Review URL: https://codereview.chromium.org/1311443003
    
    Cr-Commit-Position: refs/heads/master@{#345086}

--- telemetry/telemetry/internal/backends/form_based_credentials_backend_unittest_base.py
+++ telemetry/telemetry/internal/backends/form_based_credentials_backend_unittest_base.py
@@ -2,94 +2,16 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
-import os
 import unittest
 
-from telemetry import benchmark
-from telemetry.core import util
-from telemetry.internal.browser import browser_finder
-from telemetry.testing import options_for_unittests
 from telemetry.testing import simple_mock
 
 _ = simple_mock.DONT_CARE
 
 
-def _GetCredentialsPath():
-  # TODO: This shouldn't depend on tools/perf.
-  credentials_path = os.path.join(util.GetChromiumSrcDir(),
-      'tools', 'perf', 'data', 'credentials.json')
-  if not os.path.exists(credentials_path):
-    return None
-  return credentials_path
-
-
 class FormBasedCredentialsBackendUnitTestBase(unittest.TestCase):
   def setUp(self):
     self._credentials_type = None
-
-  @benchmark.Disabled
-  def testRealLoginIfPossible(self):
-    credentials_path = _GetCredentialsPath()
-    if not credentials_path:
-      logging.warning('Credentials file not found, skipping test.')
-      return
-
-    options = options_for_unittests.GetCopy()
-    with browser_finder.FindBrowser(options).Create(options) as b:
-      b.credentials.credentials_path = credentials_path
-      if not b.credentials.CanLogin(self._credentials_type):
-        return
-      ret = b.credentials.LoginNeeded(b.tabs[0], self._credentials_type)
-      self.assertTrue(ret)
-
-  @benchmark.Disabled
-  def testRealLoginWithDontOverrideProfileIfPossible(self):
-    credentials_path = _GetCredentialsPath()
-    if not credentials_path:
-      logging.warning('Credentials file not found, skipping test.')
-      return
-
-    options = options_for_unittests.GetCopy()
-
-    # Login once to make sure our default profile is logged in.
-    with browser_finder.FindBrowser(options).Create() as b:
-      b.credentials.credentials_path = credentials_path
-
-      if not b.credentials.CanLogin(self._credentials_type):
-        return
-
-      tab = b.tabs[0]
-
-      # Should not be logged in, since this is a fresh credentials
-      # instance.
-      self.assertFalse(b.credentials.IsLoggedIn(self._credentials_type))
-
-      # Log in.
-      ret = b.credentials.LoginNeeded(tab, self._credentials_type)
-
-      # Make sure login was successful.
-      self.assertTrue(ret)
-      self.assertTrue(b.credentials.IsLoggedIn(self._credentials_type))
-
-      # Reset state. Now the backend thinks we're logged out, even
-      # though we are logged in in our current browser session. This
-      # simulates the effects of running with --dont-override-profile.
-      b.credentials._ResetLoggedInState() # pylint: disable=W0212
-
-      # Make sure the backend thinks we're logged out.
-      self.assertFalse(b.credentials.IsLoggedIn(self._credentials_type))
-      self.assertTrue(b.credentials.CanLogin(self._credentials_type))
-
-      # Attempt to login again. This should detect that we've hit
-      # the 'logged in' page instead of the login form, and succeed
-      # instead of timing out.
-      ret = b.credentials.LoginNeeded(tab, self._credentials_type)
-
-      # Make sure our login attempt did in fact succeed and set the
-      # backend's internal state to 'logged in'.
-      self.assertTrue(ret)
-      self.assertTrue(b.credentials.IsLoggedIn(self._credentials_type))
 
   def testLoginUsingMock(self):
     raise NotImplementedError()
