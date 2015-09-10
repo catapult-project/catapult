c3ea36d4c1501d3d08c0a3fde8a14cbf9339e4ae
jbudorick <jbudorick@chromium.org>
2015-09-10 08:38:21 -0700

    [Android] Don't use a device blacklist if one isn't provided. (RELAND)
    This is a reland of https://codereview.chromium.org/1314823011
    
    BUG=517709
    
    Review URL: https://codereview.chromium.org/1304303006
    
    Cr-Commit-Position: refs/heads/master@{#348158}

--- devil/devil/android/device_utils.py
+++ devil/devil/android/device_utils.py
@@ -25,7 +25,6 @@ from devil.utils import cmd_helper
 from devil.android import apk_helper
 from devil.android import device_signal
 from devil.android import decorators
-from devil.android import device_blacklist
 from devil.android import device_errors
 from devil.android import device_temp_file
 from devil.android import logcat_monitor
@@ -1932,11 +1931,7 @@ class DeviceUtils(object):
 
   @classmethod
   def HealthyDevices(cls, blacklist=None, **kwargs):
-    if not blacklist:
-      # TODO(jbudorick): Remove once clients pass in the blacklist.
-      blacklist = device_blacklist.Blacklist(device_blacklist.BLACKLIST_JSON)
-
-    blacklisted_devices = blacklist.Read()
+    blacklisted_devices = blacklist.Read() if blacklist else []
     def blacklisted(adb):
       if adb.GetDeviceSerial() in blacklisted_devices:
         logging.warning('Device %s is blacklisted.', adb.GetDeviceSerial())
