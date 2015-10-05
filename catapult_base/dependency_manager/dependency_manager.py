5858895d8458b2db7c7ed0958f87579362b7671c
aiolos <aiolos@chromium.org>
2015-10-05 16:47:57 -0700

    Add crash_service to Telemetry's isolates.
    Switch to using binary_manager for the crash_service executable,
    eliminating downloads of this binary from cloud storage. (A follow-on
    CL will likely forbid the bots from downloading this and other
    executables from cloud storage entirely, rather than continuing to use
    this as a fallback path.)
    
    Telemetry's isolates are currently specified fairly differently for
    the GYP and GN builds; other CLs underway will unify their
    specification more.
    
    authors=aiolos@,kbr@
    
    BUG=466877
    
    TBR=phajdan.jr@chromium.org
    
    Review URL: https://codereview.chromium.org/1372943003
    
    Cr-Commit-Position: refs/heads/master@{#352465}

--- catapult_base/dependency_manager/dependency_manager.py
+++ catapult_base/dependency_manager/dependency_manager.py
@@ -265,4 +265,3 @@ class DependencyManager(object):
     os.chmod(download_path,
              stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP)
     return os.path.abspath(download_path)
-
