c4a1d97969db77b508f44050afd33864af93ff75
jbudorick <jbudorick@chromium.org>
2015-10-22 17:06:04 -0700

    [Android] Add a configurable environment for devil/. (RELAND)
    This is a reland of https://codereview.chromium.org/1316413003/
    
    BUG=476719
    
    Review URL: https://codereview.chromium.org/1399273002
    
    Cr-Commit-Position: refs/heads/master@{#355683}

--- catapult_base/dependency_manager/dependency_manager.py
+++ catapult_base/dependency_manager/dependency_manager.py
@@ -89,8 +89,6 @@ class DependencyManager(object):
     """
     dependency_info = self._GetDependencyInfo(dependency, platform)
     if not dependency_info:
-      logging.error(
-          'The dependency_manager was not initialized with the dependency.')
       if not try_support_binaries:
         raise exceptions.NoPathFoundError(dependency, platform)
       # TODO(aiolos): Remove the support_binaries call and always raise
@@ -141,8 +139,6 @@ class DependencyManager(object):
     # system.
     dependency_info = self._GetDependencyInfo(dependency, platform)
     if not dependency_info:
-      logging.error(
-          'The dependency_manager was not initialized with the dependency.')
       if not try_support_binaries:
         raise exceptions.NoPathFoundError(dependency, platform)
       return support_binaries.FindLocallyBuiltPath(dependency)
