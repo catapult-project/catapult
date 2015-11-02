75ee101a137409e7e3bf088848fd12f64cb7d69d
qyearsley <qyearsley@chromium.org>
2015-11-02 13:15:53 -0800

    Refactor auto_triage_test (put sample data inside test methods).
    In this CL:
     - Main change: put sample data inside test methods that use them.
     - Change _AddTestData to not take sample revision numbers.
     - Move patch annotation that's used in all methods to class level.
     - Change assertEqual(actual, expected) -> assertEqual(expected, actual)
     - Remove a few comments which appear unnecessary.
    
    Review URL: https://codereview.chromium.org/1419213004

--- dashboard/dashboard/auto_triage_test.py
+++ dashboard/dashboard/auto_triage_test.py
@@ -19,61 +19,19 @@ from dashboard.models import bug_data
 from dashboard.models import graph_data
 from dashboard.models import sheriff
 
-# Sample series.
-_TEST_ROW_RECOVERED = [
-    [1, 990], [2, 1000], [3, 1010],
-    [4, 1010], [5, 1010], [6, 1010],
-    [7, 990], [8, 1000], [9, 1010]]
 
-_TEST_ROW_ABS_NOT_RECOVERED = [
-    [1, 990], [2, 1000], [3, 1010],
-    [4, 1010], [5, 1010], [6, 1010],
-    [7, 1000], [8, 1010], [9, 1020]]
-
-_TEST_ROW_ABS_RECOVERED = [
-    [1, 990], [2, 1000], [3, 1010],
-    [4, 1010], [5, 1010], [6, 1010],
-    [7, 995], [8, 1005], [9, 1015]]
-
-_TEST_ROW_REL_NOT_RECOVERED = [
-    [1, 49], [2, 50], [3, 51],
-    [4, 55], [5, 55], [6, 55],
-    [7, 44], [8, 55], [9, 56]]
-
-_TEST_ROW_REL_RECOVERED = [
-    [1, 40], [2, 50], [3, 60],
-    [4, 60], [5, 60], [6, 60],
-    [7, 44], [8, 54], [9, 64]]
-
-_TEST_ROW_STD_NOT_RECOVERED = [
-    [1, 990], [2, 1000], [3, 1010],
-    [4, 1010], [5, 1010], [6, 1010],
-    [7, 1010], [8, 1020], [9, 1030]]
-
-_TEST_ROW_NOT_ENOUGH_DATA = [
-    [1, 990], [2, 1000], [3, 1010],
-    [4, 1010], [5, 1010], [6, 1010],
-    [7, 995], [8, 1005]]
-
-_TEST_ROW_IMPROVEMENTS = [
-    [1, 990], [2, 1000], [3, 1010],
-    [4, 1010], [5, 1010], [6, 1010],
-    [7, 890], [8, 900], [9, 910]]
-
-
+@mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
 class AutoTriageTest(testing_common.TestCase):
 
   def setUp(self):
     super(AutoTriageTest, self).setUp()
-    app = webapp2.WSGIApplication([(
-        '/auto_triage', auto_triage.AutoTriageHandler)])
+    app = webapp2.WSGIApplication(
+        [('/auto_triage', auto_triage.AutoTriageHandler)])
     self.testapp = webtest.TestApp(app)
 
-  def _AddTestData(
-      self, test_name, rows, sheriff_key_key,
-      improvement_direction=anomaly.UNKNOWN):
+  def _AddTestData(self, test_name, rows, sheriff_key,
+                   improvement_direction=anomaly.UNKNOWN):
     """Adds a sample Test and associated data and returns the Test."""
-
     testing_common.AddTests(
         ['ChromiumGPU'],
         ['linux-release'], {
@@ -86,15 +44,15 @@ class AutoTriageTest(testing_common.TestCase):
     test.improvement_direction = improvement_direction
     test_container_key = utils.GetTestContainerKey(test.key)
 
-    sheriff_key = sheriff_key_key.get()
+    sheriff_key = sheriff_key.get()
     if sheriff_key.patterns:
       sheriff_key.patterns.append(test.test_path)
     else:
       sheriff_key.patterns = [test.test_path]
     sheriff_key.put()
 
-    for row in rows:
-      graph_data.Row(id=row[0], value=row[1], parent=test_container_key).put()
+    for i, val in enumerate(rows):
+      graph_data.Row(id=(i+1), value=val, parent=test_container_key).put()
 
     # Add test config.
     overridden_config = {
@@ -104,9 +62,7 @@ class AutoTriageTest(testing_common.TestCase):
     anomaly_config.AnomalyConfig(
         id='config_' + test_name, config=overridden_config,
         patterns=[test.test_path]).put()
-
     test.put()
-
     return test
 
   def _AddAnomalyForTest(
@@ -128,7 +84,7 @@ class AutoTriageTest(testing_common.TestCase):
       bug = ndb.Key('Bug', int(bug_id)).get()
       if not bug:
         bug_data.Bug(id=bug_id).put()
-    anomaly_key = anomaly.Anomaly(
+    return anomaly.Anomaly(
         start_revision=4,
         end_revision=4,
         test=test_key,
@@ -138,106 +94,97 @@ class AutoTriageTest(testing_common.TestCase):
         std_dev_before_anomaly=std_dev_before_anomaly,
         bug_id=bug_id,
         sheriff=sheriff_key).put()
-    return anomaly_key
 
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   def testAnomalyRecovery_AbsoluteCheck(self):
     sheriff_key = sheriff.Sheriff(email='a@google.com', id='sheriff_key').put()
-    test = self._AddTestData('t1', _TEST_ROW_ABS_NOT_RECOVERED, sheriff_key)
-    self._AddAnomalyForTest(1000, 10, sheriff_key, None, test.key)
-    test = self._AddTestData('t2', _TEST_ROW_ABS_RECOVERED, sheriff_key)
-    self._AddAnomalyForTest(1000, 10, sheriff_key, None, test.key)
+    abs_not_recovered = [990, 1000, 1010, 1010, 1010, 1010, 1000, 1010, 1020]
+    t1 = self._AddTestData('t1', abs_not_recovered, sheriff_key)
+    self._AddAnomalyForTest(1000, 10, sheriff_key, None, t1.key)
+    abs_recovered = [990, 1000, 1010, 1010, 1010, 1010, 995, 1005, 1015]
+    t2 = self._AddTestData('t2', abs_recovered, sheriff_key)
+    self._AddAnomalyForTest(1000, 10, sheriff_key, None, t2.key)
     self.testapp.post('/auto_triage')
-    # Fetched anomalies are in order that were added.
-    new_anomalies = anomaly.Anomaly.query().fetch()
-    self.assertEqual(len(new_anomalies), 2)
-    self.assertFalse(new_anomalies[0].recovered)
-    self.assertTrue(new_anomalies[1].recovered)
+    anomalies = anomaly.Anomaly.query().fetch()
+    self.assertEqual(2, len(anomalies))
+    self.assertEqual(t1.key, anomalies[0].test)
+    self.assertEqual(t2.key, anomalies[1].test)
+    self.assertFalse(anomalies[0].recovered)
+    self.assertTrue(anomalies[1].recovered)
 
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   def testAnomalyRecovery_RelativeCheck(self):
     sheriff_key = sheriff.Sheriff(email='a@google.com', id='sheriff_key').put()
-    test = self._AddTestData('t1', _TEST_ROW_REL_NOT_RECOVERED, sheriff_key)
-    self._AddAnomalyForTest(50, 10, sheriff_key, None, test.key)
-    test = self._AddTestData('t2', _TEST_ROW_REL_RECOVERED, sheriff_key)
-    self._AddAnomalyForTest(50, 10, sheriff_key, None, test.key)
+    rel_not_recovered = [49, 50, 51, 55, 55, 55, 44, 55, 56]
+    t1 = self._AddTestData('t1', rel_not_recovered, sheriff_key)
+    self._AddAnomalyForTest(50, 10, sheriff_key, None, t1.key)
+    rel_recovered = [40, 50, 60, 60, 60, 60, 44, 54, 64]
+    t2 = self._AddTestData('t2', rel_recovered, sheriff_key)
+    self._AddAnomalyForTest(50, 10, sheriff_key, None, t2.key)
     self.testapp.post('/auto_triage')
-    # Fetched anomalies are in order that were added.
-    new_anomalies = anomaly.Anomaly.query().fetch()
-    self.assertEqual(len(new_anomalies), 2)
-    self.assertFalse(new_anomalies[0].recovered)
-    self.assertTrue(new_anomalies[1].recovered)
+    anomalies = anomaly.Anomaly.query().fetch()
+    self.assertEqual(2, len(anomalies))
+    self.assertEqual(t1.key, anomalies[0].test)
+    self.assertEqual(t2.key, anomalies[1].test)
+    self.assertFalse(anomalies[0].recovered)
+    self.assertTrue(anomalies[1].recovered)
 
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   def testAnomalyRecovery_StdDevCheck(self):
     sheriff_key = sheriff.Sheriff(email='a@google.com', id='sheriff_key').put()
-    test = self._AddTestData('t1', _TEST_ROW_STD_NOT_RECOVERED, sheriff_key)
+    std_not_recovered = [990, 1000, 1010, 1010, 1010, 1010, 1010, 1020, 1030]
+    test = self._AddTestData('t1', std_not_recovered, sheriff_key)
     self._AddAnomalyForTest(1000, 10, sheriff_key, None, test.key)
     self.testapp.post('/auto_triage')
-    # Fetched anomalies are in order that were added.
-    new_anomalies = anomaly.Anomaly.query().fetch()
-    self.assertEqual(len(new_anomalies), 1)
-    self.assertFalse(new_anomalies[0].recovered)
+    anomalies = anomaly.Anomaly.query().fetch()
+    self.assertEqual(1, len(anomalies))
+    self.assertFalse(anomalies[0].recovered)
 
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   def testAnomalyRecovery_ImprovementCheck(self):
     sheriff_key = sheriff.Sheriff(email='a@google.com', id='sheriff_key').put()
-    test = self._AddTestData('t1', _TEST_ROW_IMPROVEMENTS, sheriff_key,
-                             anomaly.DOWN)
+    improvements = [990, 1000, 1010, 1010, 1010, 1010, 890, 900, 910]
+    test = self._AddTestData('t1', improvements, sheriff_key, anomaly.DOWN)
     self._AddAnomalyForTest(1000, 10, sheriff_key, None, test.key)
     self.testapp.post('/auto_triage')
-    # Fetched anomalies are in order that were added.
-    new_anomalies = anomaly.Anomaly.query().fetch()
-    self.assertEqual(len(new_anomalies), 1)
-    self.assertTrue(new_anomalies[0].recovered)
+    anomalies = anomaly.Anomaly.query().fetch()
+    self.assertEqual(1, len(anomalies))
+    self.assertTrue(anomalies[0].recovered)
 
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   def testAnomalyRecover_IgnoredCheck(self):
     sheriff_key = sheriff.Sheriff(email='a@google.com', id='sheriff_key').put()
-    test = self._AddTestData('t1', _TEST_ROW_RECOVERED, sheriff_key)
+    recovered = [990, 1000, 1010, 1010, 1010, 1010, 990, 1000, 1010]
+    test = self._AddTestData('t1', recovered, sheriff_key)
     self._AddAnomalyForTest(1000, 10, sheriff_key, -1, test.key)
     self.testapp.post('/auto_triage')
-    # Fetched anomalies are in order that were added.
-    new_anomalies = anomaly.Anomaly.query().fetch()
-    self.assertEqual(len(new_anomalies), 1)
-    self.assertFalse(new_anomalies[0].recovered)
+    anomalies = anomaly.Anomaly.query().fetch()
+    self.assertEqual(1, len(anomalies))
+    self.assertFalse(anomalies[0].recovered)
 
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   @mock.patch.object(
       auto_triage.rietveld_service, 'Credentials', mock.MagicMock())
   @mock.patch.object(
       auto_triage.issue_tracker_service.IssueTrackerService, 'AddBugComment')
   def testPost_AllAnomaliesRecovered_AddsComment(self, add_bug_comment_mock):
     sheriff_key = sheriff.Sheriff(email='a@google.com', id='sheriff_key').put()
-    test = self._AddTestData('t1', _TEST_ROW_RECOVERED, sheriff_key)
-    self._AddAnomalyForTest(1000, 10, sheriff_key, 1234, test.key)
-    test = self._AddTestData('t2', _TEST_ROW_ABS_RECOVERED, sheriff_key)
-    self._AddAnomalyForTest(1000, 10, sheriff_key, 1234, test.key)
-
+    recovered = [990, 1000, 1010, 1010, 1010, 1010, 990, 1000, 1010]
+    t1 = self._AddTestData('t1', recovered, sheriff_key)
+    self._AddAnomalyForTest(1000, 10, sheriff_key, 1234, t1.key)
+    abs_recovered = [990, 1000, 1010, 1010, 1010, 1010, 995, 1005, 1015]
+    t2 = self._AddTestData('t2', abs_recovered, sheriff_key)
+    self._AddAnomalyForTest(1000, 10, sheriff_key, 1234, t2.key)
     self.testapp.post('/auto_triage')
     self.ExecuteTaskQueueTasks('/auto_triage', auto_triage._TASK_QUEUE_NAME)
+    anomalies = anomaly.Anomaly.query().fetch()
+    self.assertEqual(2, len(anomalies))
+    self.assertTrue(anomalies[0].recovered)
+    self.assertTrue(anomalies[1].recovered)
+    add_bug_comment_mock.assert_called_once_with(mock.ANY, mock.ANY)
 
-    # Fetched anomalies are in order that were added.
-    # Both Anomaly entities should be marked as recovered.
-    new_anomalies = anomaly.Anomaly.query().fetch()
-    self.assertEqual(len(new_anomalies), 2)
-    self.assertTrue(new_anomalies[0].recovered)
-    self.assertTrue(new_anomalies[1].recovered)
-
-    # A request should be made to add a comment to the bug.
-    add_bug_comment_mock.assert_called_once_with(
-        mock.ANY, mock.ANY)
-
-  @mock.patch.object(utils, 'TickMonitoringCustomMetric', mock.MagicMock())
   @mock.patch.object(auto_triage.TriageBugs, '_CommentOnRecoveredBug')
   def testPost_BugHasNoAlerts_NotMarkRecovered(self, close_recovered_bug_mock):
     bug_id = 1234
     bug_data.Bug(id=bug_id).put()
     self.testapp.post('/auto_triage')
     self.ExecuteTaskQueueTasks('/auto_triage', auto_triage._TASK_QUEUE_NAME)
-
     bug = ndb.Key('Bug', bug_id).get()
-    self.assertEqual(bug.status, bug_data.BUG_STATUS_CLOSED)
+    self.assertEqual(bug_data.BUG_STATUS_CLOSED, bug.status)
     self.assertFalse(close_recovered_bug_mock.called)
 
 
