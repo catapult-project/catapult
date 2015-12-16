fdbe55f42bf8f0be84342ab47e71952d8380d52b
newt <newt@chromium.org>
2015-12-15 16:13:23 -0800

    Update permissions blacklist.
    android.permission.AUTHENTICATE_ACCOUNTS can't be granted on M+ devices
    since it's been removed from Android.
    
    Review URL: https://codereview.chromium.org/1518643004
    
    Cr-Commit-Position: refs/heads/master@{#365400}

--- devil/devil/android/device_utils.py
+++ devil/devil/android/device_utils.py
@@ -67,6 +67,7 @@ _RESTART_ADBD_SCRIPT = """
 _PERMISSIONS_BLACKLIST = [
     'android.permission.ACCESS_MOCK_LOCATION',
     'android.permission.ACCESS_NETWORK_STATE',
+    'android.permission.AUTHENTICATE_ACCOUNTS',
     'android.permission.BLUETOOTH',
     'android.permission.BLUETOOTH_ADMIN',
     'android.permission.DOWNLOAD_WITHOUT_NOTIFICATION',
