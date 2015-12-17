5ac66f0b46ff963c5a2066433ada7e7cbeed9e70
nednguyen <nednguyen@google.com>
2015-12-16 19:55:03 -0800

    [Telemetry] Remove catapult_base's dependencies on telemetry/
    BUG=565604, 459787
    
    To make sure that this completely removes catapult_base's dep
    on telemetry, one can try:
    Patch this patch.
    git rm -r tools/telemetry/telemetry
    ./tools/telemetry/catapult_base/bin/run_tests
    
    Review URL: https://codereview.chromium.org/1528133003
    
    Cr-Commit-Position: refs/heads/master@{#365732}

--- catapult_base/__init__.py
+++ catapult_base/__init__.py
@@ -4,3 +4,24 @@
 
 # All files in this directory should be moved to catapult/base/ after moving
 # to the new repo.
+
+import os
+import sys
+
+from catapult_base import util
+
+def _AddDirToPythonPath(*path_parts):
+  path = os.path.abspath(os.path.join(*path_parts))
+  if os.path.isdir(path) and path not in sys.path:
+    # Some callsite that use telemetry assumes that sys.path[0] is the directory
+    # containing the script, so we add these extra paths to right after it.
+    sys.path.insert(1, path)
+
+_AddDirToPythonPath(os.path.join(util.GetCatapultDir(), 'third_party', 'mock'))
+
+# TODO(nednguyen, aiolos): Copy these lib to catapult/third_party before
+# actually moving catapult_base/ to catapult/
+_AddDirToPythonPath(
+    os.path.join(os.path.dirname(__file__), '..', 'third_party', 'mox3'))
+_AddDirToPythonPath(
+    os.path.join(os.path.dirname(__file__), '..', 'third_party', 'pyfakefs'))
