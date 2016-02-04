86f875c848d19ab93a92331d4d0cc514a2fa1fbf
aiolos <aiolos@chromium.org>
2016-02-03 18:47:05 -0800

    Move reference builds to their own config in catapult_base.
    This is the first step toward a catapult-wide solution for accessing builds
    for Chrome channels.
    
    BUG=catapult:#1968
    
    Review URL: https://codereview.chromium.org/1668013003

--- telemetry/telemetry/internal/util/binary_manager_unittest.py
+++ telemetry/telemetry/internal/util/binary_manager_unittest.py
@@ -26,11 +26,17 @@ class BinaryManagerTest(unittest.TestCase):
   def testInitializationNoEnvironmentConfig(
       self, base_config_mock, dep_manager_mock):
     base_config_mock.side_effect = ['base_config_object1',
-                                    'base_config_object2']
+                                    'base_config_object2',
+                                    'base_config_object3']
     binary_manager.InitDependencyManager(None)
-    base_config_mock.assert_called_once_with(
-        binary_manager.TELEMETRY_PROJECT_CONFIG)
-    dep_manager_mock.assert_called_once_with(['base_config_object1'])
+    base_config_mock.assert_has_calls([
+        mock.call.base_config.BaseConfig(
+            binary_manager.TELEMETRY_PROJECT_CONFIG),
+        mock.call.base_config.BaseConfig(
+            binary_manager.CHROME_BINARY_CONFIG)])
+    self.assertEqual(2, base_config_mock.call_count)
+    dep_manager_mock.assert_called_once_with(['base_config_object1',
+                                              'base_config_object2'])
 
   @mock.patch(
       'telemetry.internal.util.binary_manager.dependency_manager.DependencyManager') # pylint: disable=line-too-long
@@ -38,16 +44,18 @@ class BinaryManagerTest(unittest.TestCase):
       'telemetry.internal.util.binary_manager.dependency_manager.BaseConfig')
   def testInitializationWithEnvironmentConfig(
       self, base_config_mock, dep_manager_mock):
-    base_config_mock.side_effect = ['base_config_object1',
-                                    'base_config_object2']
+    base_config_mock.side_effect = ['base_config_object1',  # TELEMETRY_PROJECT
+                                    'base_config_object2',  # CHROME_BINARY
+                                    'base_config_object3']  # ENVIRONMENT
     environment_config = os.path.join('some', 'config', 'path')
     binary_manager.InitDependencyManager(environment_config)
     expected_calls = [mock.call(binary_manager.TELEMETRY_PROJECT_CONFIG),
+                      mock.call(binary_manager.CHROME_BINARY_CONFIG),
                       mock.call(environment_config)]
     self.assertEqual(expected_calls, base_config_mock.call_args_list)
     # Make sure the environment config is passed first.
     dep_manager_mock.assert_called_once_with(
-        ['base_config_object2', 'base_config_object1'])
+        ['base_config_object3', 'base_config_object1', 'base_config_object2'])
 
   def testReinitialization(self):
     binary_manager.InitDependencyManager(None)
@@ -67,8 +75,12 @@ class BinaryManagerTest(unittest.TestCase):
     binary_manager.InitDependencyManager(None)
     binary_manager.FetchPath('dep', 'plat', 'arch')
     dep_manager_mock.assert_call_args(expected)
-    base_config_mock.assert_called_once_with(
-        binary_manager.TELEMETRY_PROJECT_CONFIG)
+    base_config_mock.assert_has_calls([
+        mock.call.base_config.BaseConfig(
+            binary_manager.TELEMETRY_PROJECT_CONFIG),
+        mock.call.base_config.BaseConfig(
+            binary_manager.CHROME_BINARY_CONFIG)])
+    self.assertEqual(2, base_config_mock.call_count)
 
   def testFetchPathUninitialized(self):
     self.assertRaises(exceptions.InitializationError,
@@ -87,8 +99,12 @@ class BinaryManagerTest(unittest.TestCase):
     binary_manager.InitDependencyManager(None)
     binary_manager.LocalPath('dep', 'plat', 'arch')
     dep_manager_mock.assert_call_args(expected)
-    base_config_mock.assert_called_once_with(
-        binary_manager.TELEMETRY_PROJECT_CONFIG)
+    base_config_mock.assert_has_calls([
+        mock.call.base_config.BaseConfig(
+            binary_manager.TELEMETRY_PROJECT_CONFIG),
+        mock.call.base_config.BaseConfig(
+            binary_manager.CHROME_BINARY_CONFIG)])
+    self.assertEqual(2, base_config_mock.call_count)
 
   def testLocalPathUninitialized(self):
     self.assertRaises(exceptions.InitializationError,
