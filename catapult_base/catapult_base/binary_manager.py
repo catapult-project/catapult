78a6f08e843d2a4752bcb7ecac4b4b45ce18ed5a
aiolos <aiolos@chromium.org>
2016-02-10 16:52:25 -0800

    Add ability to specify os_version to the catapult_base binary_manager.
    This is needed to support finding the android chrome apks.
    
    BUG=catapult:#1968
    
    Review URL: https://codereview.chromium.org/1666253002

--- catapult_base/catapult_base/binary_manager.py
+++ catapult_base/catapult_base/binary_manager.py
@@ -2,7 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import dependency_manager  # pylint: disable=import-error
+import logging
+
+import dependency_manager
 
 
 class BinaryManager(object):
@@ -13,19 +15,37 @@ class BinaryManager(object):
   def __init__(self, configs):
     self._dependency_manager = dependency_manager.DependencyManager(configs)
 
-  def FetchPath(self, binary_name, arch, platform):
+  def FetchPath(self, binary_name, arch, os_name, os_version=None):
     """ Return a path to the executable for <binary_name>, or None if not found.
 
     Will attempt to download from cloud storage if needed.
     """
-    return self._dependency_manager.FetchPath(
-        binary_name, '%s_%s' % (platform, arch))
+    platform = '%s_%s' % (os_name, arch)
+    if os_version:
+      try:
+        versioned_platform = '%s_%s_%s' % (os_name, os_version, arch)
+        return self._dependency_manager.FetchPath(
+            binary_name, versioned_platform)
+      except dependency_manager.NoPathFoundError:
+        logging.warning(
+            'Cannot find path for %s on platform %s. Falling back to %s.',
+            binary_name, versioned_platform, platform)
+    return self._dependency_manager.FetchPath(binary_name, platform)
 
 
-  def LocalPath(self, binary_name, arch, platform):
+  def LocalPath(self, binary_name, arch, os_name, os_version=None):
     """ Return a local path to the given binary name, or None if not found.
 
     Will not download from cloud_storage.
     """
-    return self._dependency_manager.LocalPath(
-        binary_name, '%s_%s' % (platform, arch))
+    platform = '%s_%s' % (os_name, arch)
+    if os_version:
+      try:
+        versioned_platform = '%s_%s_%s' % (os_name, os_version, arch)
+        return self._dependency_manager.LocalPath(
+            binary_name, versioned_platform)
+      except dependency_manager.NoPathFoundError:
+        logging.warning(
+            'Cannot find local path for %s on platform %s. Falling back to %s.',
+            binary_name, versioned_platform, platform)
+    return self._dependency_manager.LocalPath(binary_name, platform)
