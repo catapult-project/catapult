b81d883874bd78aa5e7011b64d188aaa38589c55
zhenw <zhenw@chromium.org>
2016-03-18 17:24:57 -0700

    [Telemetry] Always close browser backend in Close()
    We should always close browser backend. If exception
    happens in browser.Close(), e.g., due to device error,
    browser backend will not be closed and then devtools client
    will not be closed. Devtools client will also not be
    garbage collected because chrome_tracing_devtools_manager
    has a map holding all devtool clients. This leads to
    actually 'stale' devtools client still being active. And
    then in the next story run, Telemetry will try to start
    tracing on the actually 'stale' devtools client.
    
    BUG=chromium:595766
    
    Review URL: https://codereview.chromium.org/1818593002

--- telemetry/telemetry/internal/browser/browser.py
+++ telemetry/telemetry/internal/browser/browser.py
@@ -222,17 +222,19 @@ class Browser(app.App):
 
   def Close(self):
     """Closes this browser."""
-    if self._browser_backend.IsBrowserRunning():
-      self._platform_backend.WillCloseBrowser(self, self._browser_backend)
+    try:
+      if self._browser_backend.IsBrowserRunning():
+        self._platform_backend.WillCloseBrowser(self, self._browser_backend)
 
-    self._browser_backend.profiling_controller_backend.WillCloseBrowser()
-    if self._browser_backend.supports_uploading_logs:
-      try:
-        self._browser_backend.UploadLogsToCloudStorage()
-      except cloud_storage.CloudStorageError as e:
-        logging.error('Cannot upload browser log: %s' % str(e))
-    self._browser_backend.Close()
-    self.credentials = None
+      self._browser_backend.profiling_controller_backend.WillCloseBrowser()
+      if self._browser_backend.supports_uploading_logs:
+        try:
+          self._browser_backend.UploadLogsToCloudStorage()
+        except cloud_storage.CloudStorageError as e:
+          logging.error('Cannot upload browser log: %s' % str(e))
+    finally:
+      self._browser_backend.Close()
+      self.credentials = None
 
 
   def GetStandardOutput(self):
