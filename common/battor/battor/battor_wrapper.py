4411881631aa959b820380e88a39b3eda86bf75c
rnephew <rnephew@chromium.org>
2016-04-07 10:48:59 -0700

    [Battor] Add python wrapper for communicating with battor_agent_binary
    BUG=chromium:574888
    R=aiolos@chromium.org, charliea@chromium.org, nednguyen@google.com
    
    Review URL: https://codereview.chromium.org/1828143004 .

---
+++ common/battor/battor/battor_wrapper.py
@@ -0,0 +1,167 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import platform
+import subprocess
+import sys
+import tempfile
+import time
+
+import dependency_manager
+from devil.utils import battor_device_mapping
+from devil.utils import find_usb_devices
+
+
+class BattorError(Exception):
+  pass
+
+class BattorWrapper(object):
+  """A class for communicating with a BattOr in python."""
+  _START_TRACING_CMD = 'StartTracing'
+  _STOP_TRACING_CMD = 'StopTracing'
+  _SUPPORTS_CLOCKSYNC_CMD = 'SupportsExplicitClockSync'
+  _RECORD_CLOCKSYNC_CMD = 'RecordClockSyncMarker'
+
+  def __init__(self, target_platform, android_device=None, battor_path=None,
+               battor_map=None):
+    """Constructor.
+
+    Args:
+      target_platform: Platform BattOr is attached to.
+      android_device: Serial number of Android device.
+      battor_path: Path to BattOr device.
+      battor_map: BattOr device map mapping device serial to BattOr path.
+
+    Attributes:
+      _battor_path: Path to BattOr. Typically similar to /tty/USB0.
+      _battor_agent_binary: Path to the BattOr agent binary used to communicate
+        with the BattOr.
+      _tracing: A bool saying if tracing has been started.
+      _battor_shell: A subprocess running the bator_agent_binary
+      _trace_results_path: Path to BattOr trace results file.
+    """
+    self._battor_path = self._GetBattorPath(
+        target_platform, android_device, battor_path, battor_map)
+    config = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'battor_binary_dependencies.json')
+
+    dm = dependency_manager.DependencyManager(
+        [dependency_manager.BaseConfig(config)])
+    self._battor_agent_binary = dm.FetchPath(
+        'battor_agent_binary', '%s_%s' % (sys.platform, platform.machine()))
+
+    self._tracing = False
+    self._battor_shell = None
+    self._trace_results_path = None
+    self._start_tracing_time = None
+    self._stop_tracing_time = None
+    self._trace_results = None
+
+  def IsShellRunning(self):
+    """Determines if shell is running."""
+    return self._battor_shell.poll() is None
+
+  def StartShell(self):
+    """Start BattOr binary shell."""
+    assert not self._battor_shell, 'Attempting to start running BattOr shell.'
+    battor_cmd = [self._battor_agent_binary]
+    if self._battor_path:
+      battor_cmd.append('--battor-path=%s' % self._battor_path)
+    self._battor_shell = self._StartShellImpl(battor_cmd)
+    assert self.IsShellRunning(), 'Shell did not start properly.'
+
+  def StartTracing(self):
+    """Start tracing on the BattOr."""
+    assert self._battor_shell, 'Must start shell before tracing'
+    assert not self._tracing, 'Tracing already started.'
+    self._SendBattorCommand(self._START_TRACING_CMD)
+    self._tracing = True
+    self._start_tracing_time = int(time.time())
+
+  def StopTracing(self):
+    """Stop tracing on the BattOr."""
+    assert self._tracing, 'Must run StartTracing before StopTracing'
+    # Create temp file to reserve location for saving results.
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    self._trace_results_path = temp_file.name
+    temp_file.close()
+    self._SendBattorCommand(
+        '%s %s' % (self._STOP_TRACING_CMD, self._trace_results_path),
+        check_return=False)
+    self._tracing = False
+    self._stop_tracing_time = int(time.time())
+
+  def CollectTraceData(self, timeout=None):
+    """Collect trace data from battor.
+    Args:
+      timeout: timeout for waiting on the BattOr process to terminate in
+        seconds.
+    Returns: Trace data in form of a list.
+    """
+    # The BattOr shell terminates after returning the results.
+    if timeout is None:
+      timeout = self._stop_tracing_time - self._start_tracing_time
+    self._battor_shell.wait()
+    with open(self._trace_results_path) as results:
+      self._trace_results = results.read()
+    self._battor_shell = None
+    return self._trace_results.splitlines()
+
+  def SupportsExplicitClockSync(self):
+    """Returns if BattOr supports Clock Sync events."""
+    return bool(int(self._SendBattorCommand(self._SUPPORTS_CLOCKSYNC_CMD,
+                                            check_return=False)))
+
+  def RecordClockSyncMarker(self, sync_id):
+    """Record clock sync event on BattOr."""
+    if not isinstance(sync_id, basestring):
+      raise TypeError('sync_id must be a string.')
+    self._SendBattorCommand('%s %s' % (self._RECORD_CLOCKSYNC_CMD, sync_id))
+
+  def _GetBattorPath(self, target_platform, android_device=None,
+                     battor_path=None, battor_map_file=None, battor_map=None):
+    """Determines most likely path to the correct BattOr."""
+    device_tree = find_usb_devices.GetBusNumberToDeviceTreeMap(fast=True)
+    if battor_path:
+      if not isinstance(battor_path, basestring):
+        raise BattorError('An invalid BattOr path was specified.')
+      return battor_path
+
+    if target_platform == 'android':
+      if not android_device:
+        raise BattorError('Must specify device for Android platform.')
+      if not battor_map_file and not battor_map:
+        # No map was passed, so must create one.
+        battor_map = battor_device_mapping.GenerateSerialMap()
+
+      return battor_device_mapping.GetBattorPathFromPhoneSerial(
+          android_device, serial_map_file=battor_map_file,
+          serial_map=battor_map)
+
+    # Not Android and no explicitly passed BattOr.
+    battors = battor_device_mapping.GetBattorList(device_tree)
+    if len(battors) != 1:
+      raise BattorError('For non-Android platforms, exactly one BattOr must be '
+                        'attached unless address is explicitly given.')
+    return '/dev/%s' % battors.pop()
+
+  def _SendBattorCommandImpl(self, cmd, return_results=True):
+    """Sends command to the BattOr."""
+    self._battor_shell.stdin.write('%s\n' % cmd)
+    if return_results:
+      return self._battor_shell.stdout.readline()
+    return
+
+  def _SendBattorCommand(self, cmd, check_return=True):
+    status = self._SendBattorCommandImpl(cmd, return_results=check_return)
+    if check_return and status != 'Done.\n':
+      raise BattorError('BattOr did not complete command \'%s\' correctly.\n'
+                        'Outputted: %s' % (cmd, status))
+    return status
+
+  def _StartShellImpl(self, battor_cmd):
+    return subprocess.Popen(
+        battor_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
---
+++ common/battor/battor/battor_wrapper_unittest.py
@@ -0,0 +1,166 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import unittest
+
+import dependency_manager
+
+from battor import battor_wrapper
+from devil.utils import battor_device_mapping
+from devil.utils import find_usb_devices
+
+
+class DependencyManagerMock(object):
+  def __init__(self, _):
+    self._fetch_return = 'path'
+
+  def FetchPath(self, _, *unused):
+    return self._fetch_return
+
+
+class PopenMock(object):
+  def __init__(self, *unused):
+    pass
+
+
+class BattorWrapperTest(unittest.TestCase):
+  def setUp(self):
+    self.battor = None
+    self._MAPPING = {}
+    self._IS_BATTOR = True
+    self._battor_list = ['battor1']
+    self._should_pass = True
+    self._fake_map = {'battor1': 'device1'}
+
+    self._get_battor_path_from_phone_serial = (
+        battor_device_mapping.GetBattorPathFromPhoneSerial)
+    self._get_bus_number_to_device_tree_map = (
+        find_usb_devices.GetBusNumberToDeviceTreeMap)
+    self._dependency_manager = dependency_manager.DependencyManager
+    self._get_battor_list = battor_device_mapping.GetBattorList
+    self._is_battor = battor_device_mapping.IsBattor
+    self._generate_serial_map = battor_device_mapping.GenerateSerialMap
+
+
+    battor_device_mapping.GetBattorPathFromPhoneSerial = (
+        lambda x, serial_map_file=None, serial_map=None: x + '_battor')
+    find_usb_devices.GetBusNumberToDeviceTreeMap = lambda fast=False: True
+    dependency_manager.DependencyManager = DependencyManagerMock
+    battor_device_mapping.GetBattorList = lambda x: self._battor_list
+    battor_device_mapping.IsBattor = lambda x, y: self._IS_BATTOR
+    battor_device_mapping.GenerateSerialMap = lambda: self._fake_map
+
+  def tearDown(self):
+    battor_device_mapping.GetBattorPathFromPhoneSerial = (
+        self._get_battor_path_from_phone_serial)
+    find_usb_devices.GetBusNumberToDeviceTreeMap = (
+        self._get_bus_number_to_device_tree_map)
+    dependency_manager.DependencyManager = self._dependency_manager
+    battor_device_mapping.GetBattorList = self._get_battor_list
+    battor_device_mapping.IsBattor = self._is_battor
+    battor_device_mapping.GenerateSerialMap = self._generate_serial_map
+
+  def _DefaultBattorReplacements(self):
+    self._battor._StartShellImpl = lambda *unused: PopenMock
+    self._battor.IsShellRunning = lambda *unused: True
+    self._battor._SendBattorCommandImpl = lambda x, return_results: 'Done.\n'
+    self._battor._StopTracingImpl = lambda *unused: ('Done.\n', None)
+
+  def testInitAndroidWithBattor(self):
+    self._battor = battor_wrapper.BattorWrapper('android', android_device='abc')
+    self.assertEquals(self._battor._battor_path, 'abc_battor')
+
+  def testInitAndroidWithoutBattor(self):
+    self._battor_list = []
+    self._fake_map = {}
+    battor_device_mapping.GetBattorPathFromPhoneSerial = (
+        self._get_battor_path_from_phone_serial)
+    with self.assertRaises(KeyError):
+      self._battor = battor_wrapper.BattorWrapper('android',
+                                                  android_device='abc')
+
+  def testInitBattorPathIsBattor(self):
+    battor_path = 'battor/path/here'
+    self._battor = battor_wrapper.BattorWrapper(
+        'android', android_device='abc', battor_path=battor_path)
+    self.assertEquals(self._battor._battor_path, battor_path)
+
+  def testInitNonAndroidWithBattor(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self.assertEquals(self._battor._battor_path, '/dev/battor1')
+
+  def testInitNonAndroidWithMultipleBattor(self):
+    self._battor_list.append('battor2')
+    with self.assertRaises(battor_wrapper.BattorError):
+      self._battor = battor_wrapper.BattorWrapper('platform')
+
+  def testInitNonAndroidWithoutBattor(self):
+    self._battor_list = []
+    with self.assertRaises(battor_wrapper.BattorError):
+      self._battor = battor_wrapper.BattorWrapper('platform')
+
+  def testStartShellPass(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor.StartShell()
+    self.assertIsNotNone(self._battor._battor_shell)
+
+  def testStartShellDoubleStart(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor.StartShell()
+    with self.assertRaises(AssertionError):
+      self._battor.StartShell()
+
+  def testStartShellFail(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor.IsShellRunning = lambda *unused: False
+    with self.assertRaises(AssertionError):
+      self._battor.StartShell()
+
+  def testStartTracingPass(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor.StartShell()
+    self._battor.StartTracing()
+    self.assertTrue(self._battor._tracing)
+
+  def testStartTracingDoubleStart(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor.StartShell()
+    self._battor.StartTracing()
+    with self.assertRaises(AssertionError):
+      self._battor.StartTracing()
+
+  def testStartTracingCommandFails(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor._SendBattorCommandImpl = lambda x, return_results: 'Fail.\n'
+    self._battor.StartShell()
+    with self.assertRaises(battor_wrapper.BattorError):
+      self._battor.StartTracing()
+
+  def testStopTracingPass(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    self._battor.StartShell()
+    self._battor.StartTracing()
+    self._battor.IsShellRunning = lambda *unused: False
+    self._battor.StopTracing()
+    self.assertFalse(self._battor._tracing)
+
+  def testStopTracingNotRunning(self):
+    self._battor = battor_wrapper.BattorWrapper('platform')
+    self._DefaultBattorReplacements()
+    with self.assertRaises(AssertionError):
+      self._battor.StopTracing()
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.DEBUG)
+  unittest.main(verbosity=2)
+
