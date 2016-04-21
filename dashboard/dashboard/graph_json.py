2fa70ef71ac97e84a2b37f7ee3f429b045e152a9
sullivan <sullivan@chromium.org>
2016-04-21 07:04:34 -0700

    Add workaround for stdio prefix being incorrectly stored in expanded form as 'None' with no master.
    BUG=chromium:604990
    
    Review URL: https://codereview.chromium.org/1902983003

--- dashboard/dashboard/graph_json.py
+++ dashboard/dashboard/graph_json.py
@@ -228,7 +228,24 @@ def _PointInfoDict(row, anomaly_annotation_map):
   if anomaly_annotation_map.get(row.revision):
     anomaly_entity = anomaly_annotation_map.get(row.revision)
     point_info['g_anomaly'] = alerts.GetAnomalyDict(anomaly_entity)
-  for name, val in row.to_dict().iteritems():
+  row_dict = row.to_dict()
+  for name, val in row_dict.iteritems():
+    # TODO(sullivan): Remove this hack when data containing these broken links
+    # is sufficiently stale, after June 2016.
+    if (_IsMarkdownLink(val) and
+        val.find('(None') != -1 and
+        'a_stdio_uri_prefix' in row_dict):
+      # Many data points have been added with a stdio prefix expanded out to
+      # 'None' when 'a_stdio_uri_prefix' is set correctly. Fix them up.
+      # Add in the master name as well; if the waterfall is 'CamelCase' it
+      # should be 'camel.client.case'.
+      master_camel_case = utils.TestPath(row.parent_test).split('/')[0]
+      master_parts = re.findall('([A-Z][a-z0-9]+)', master_camel_case)
+      master_name = '%s.client.%s' % (
+          master_parts[1].lower(), master_parts[0].lower())
+      val = val.replace('(None', '(%s/%s/' % (
+          row_dict['a_stdio_uri_prefix'], master_name))
+
     if name.startswith('r_'):
       point_info[name] = val
     elif name == 'a_default_rev':
