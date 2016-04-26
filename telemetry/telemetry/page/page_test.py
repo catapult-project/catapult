7b47b5cfeac070ce31552b30bf905c04aa760132
nednguyen <nednguyen@google.com>
2016-04-26 08:26:17 -0700

    Expose all the public symbol in page_test
    BUG=chromium:606706
    BUG=catapult:#2218
    TBR=eakuefner@chromium.org
    
    Review URL: https://codereview.chromium.org/1917173002

--- telemetry/telemetry/page/page_test.py
+++ telemetry/telemetry/page/page_test.py
@@ -4,6 +4,9 @@
 
 from telemetry.page import legacy_page_test
 
-# TODO(nednguyen): Remove this shim after all the call sites are updated to
+# TODO(nednguyen): Remove these shims after all the call sites are updated to
 # reference telemetry.legacy_page_test (crbug.com/606643)
 PageTest = legacy_page_test.LegacyPageTest
+
+# pylint: disable=wildcard-import, unused-wildcard-import
+from telemetry.page.legacy_page_test import *
