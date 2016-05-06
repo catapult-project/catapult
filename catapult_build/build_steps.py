ace7d54b491694abda926d593c442ec8b7e772d0
rnephew <rnephew@chromium.org>
2016-05-06 16:37:41 -0700

    [BattOr] Disable BattOr smoke test while HW issue is ongoing.
    BUG=catapult:#2321
    
    Review-Url: https://codereview.chromium.org/1954043003

--- catapult_build/build_steps.py
+++ catapult_build/build_steps.py
@@ -26,6 +26,7 @@ _CATAPULT_TESTS = [
     {
         'name': 'BattOr Smoke Tests',
         'path': 'common/battor/battor/battor_wrapper_devicetest.py',
+        'disabled': ['win'],
     },
     {
         'name': 'BattOr Unit Tests',
