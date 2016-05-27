c42198c5801d242b85e87cd81a2bbbd6f3d7518b
nednguyen <nednguyen@google.com>
2016-05-27 14:46:12 -0700

    Disable Tracing Dev Server Tests Canary
    BUG=catapult:#2372
    TBR=sullivan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2011203003

--- catapult_build/build_steps.py
+++ catapult_build/build_steps.py
@@ -141,6 +141,8 @@ _CATAPULT_TESTS = [
             '--channel=canary'
         ],
         'outputs_presentation_json': True,
+        # https://github.com/catapult-project/catapult/issues/2372
+        'disabled': ['linux', 'mac', 'win'],
     },
     {
         'name': 'Tracing Dev Server Tests Stable',
