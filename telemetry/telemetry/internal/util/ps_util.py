6177191d52ce6b0a04ed0150b881a1a9be2829d8
nednguyen <nednguyen@google.com>
2016-05-31 16:07:13 -0700

    [Telemetry] Log info about potentially leaked processes using log warning instead of log error
    Review-Url: https://codereview.chromium.org/2024163003

--- telemetry/telemetry/internal/util/ps_util.py
+++ telemetry/telemetry/internal/util/ps_util.py
@@ -85,7 +85,7 @@ def EnableListingStrayProcessesUponExitHook():
         except Exception as e:
           logging.warning(str(e))
         leak_processes_info.append(process_info)
-      logging.error('Telemetry leaks these processes: %s',
-                    ', '.join(leak_processes_info))
+      logging.warning('Telemetry leaks these processes: %s',
+                      ', '.join(leak_processes_info))
 
   atexit_with_log.Register(_ListAllSubprocesses)
