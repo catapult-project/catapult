347d0200ca51edc1b43440a9d8dc6b05f8dfd7c4
nednguyen <nednguyen@google.com>
2016-06-02 16:18:53 -0700

    [Telemetry] Add smoke test for first_paint_metrics
    BUG=catapult:#2383
    
    Review-Url: https://codereview.chromium.org/2032243002

--- telemetry/telemetry/web_perf/timeline_based_page_test_unittest.py
+++ telemetry/telemetry/web_perf/timeline_based_page_test_unittest.py
@@ -134,3 +134,37 @@ class TimelineBasedPageTestTest(page_test_test_case.PageTestTestCase):
     self.assertEquals(len(v_baz_avg), 1)
     self.assertEquals(len(v_baz_sum), 1)
     self.assertEquals(len(v_baz_count), 1)
+
+
+  @decorators.Disabled('chromeos')
+  def testFirstPaintMetricSmoke(self):
+    ps = self.CreateEmptyPageSet()
+    ps.AddStory(TestTimelinebasedMeasurementPage(ps, ps.base_dir))
+
+    cat_filter = tracing_category_filter.TracingCategoryFilter(
+        filter_string='*,blink.console,navigation,blink.user_timing,loading')
+
+    options = tbm_module.Options(overhead_level=cat_filter)
+    options.SetTimelineBasedMetric('firstPaintMetric')
+
+    tbm = tbm_module.TimelineBasedMeasurement(options)
+    results = self.RunMeasurement(tbm, ps, self._options)
+
+    self.assertEquals(0, len(results.failures), results.failures)
+    v_fcp_max = results.FindAllPageSpecificValuesNamed(
+        'firstContentfulPaint_max')
+    self.assertEquals(len(v_fcp_max), 1)
+    self.assertIsNotNone(v_fcp_max[0].page)
+    # TODO(kouhei): enable this once the reference build of telemetry is
+    # updated.
+    #  self.assertGreater(v_fcp_max[0].value, 0)
+
+    # TODO(ksakamoto): enable this once FMP implementation is fixed.
+    # https://github.com/catapult-project/catapult/issues/2383
+
+    # v_fmp_max = results.FindAllPageSpecificValuesNamed(
+    #    'firstMeaningfulPaint_max')
+    # self.assertEquals(len(v_fmp_max), 1)
+    # TODO(ksakamoto): enable this once the reference build of telemetry is
+    # updated.
+    # self.assertIsNotNone(v_fmp_max[0].page)
