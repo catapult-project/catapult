3c24d5f5000fb038bdd6499d4f3d2a83cb47562f
aschulman <aschulman@chromium.org>
2016-06-14 14:38:16 -0700

    Temporary fix for battor_agent failing to run within telemetry.
    There is a difficult bug that needs to be fixed in the battor_agent
    where input commands can cause a segfault if they are sent within the
    first few seconds of executon of the battor_agent.
    
    This patch adds a sleep before commands are send to the battor_agent to
    avoid this bug. We need this tempporary patch until the battor_agent issue https://crbug.com/620010 is resolved.
    
    BUG=catapult:#2381
    
    Review-Url: https://codereview.chromium.org/2063913004

--- common/battor/battor/battor_wrapper.py
+++ common/battor/battor/battor_wrapper.py
@@ -226,5 +226,12 @@ class BattorWrapper(object):
     return status
 
   def _StartShellImpl(self, battor_cmd):
-    return subprocess.Popen(
+    status = subprocess.Popen(
         battor_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
+
+    # TODO(aschulman) We need to sleep to wait for battor_agent threads to
+    # start until https://bugs.chromium.org/p/chromium/issues/detail?id=620010
+    # is resolved. More details are captured in catapult:#2381.
+    time.sleep(1)
+
+    return status
