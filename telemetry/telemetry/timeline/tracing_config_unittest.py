ea4633b677af6cde215b0476fdfd5f1363cab1bf
zhenw <zhenw@chromium.org>
2016-06-22 10:11:39 -0700

    Revert of Separate trace config classes into multiple files (patchset #3 id:40001 of https://codereview.chromium.org/2085683002/ )
    Reason for revert:
    causing catapult roll failure: telemetry_perf_unittests
    
    [1/1] measurements.measurement_smoke_test.MeasurementSmokeTest.testAllMeasurementInstance failed unexpectedly 0.0945s:
      Traceback (most recent call last):
        File "/tmp/isolated_run4dWHfM/tools/perf/measurements/measurement_smoke_test.py", line 63, in testAllMeasurementInstance
          _GetAllPossiblePageTestInstances()
        File "/tmp/isolated_run4dWHfM/tools/perf/measurements/measurement_smoke_test.py", line 52, in _GetAllPossiblePageTestInstances
          pt = benchmark_class().CreatePageTest(options)
        File "/tmp/isolated_run4dWHfM/third_party/catapult/telemetry/telemetry/benchmark.py", line 229, in CreatePageTest
          opts = self.CreateTimelineBasedMeasurementOptions()
        File "/tmp/isolated_run4dWHfM/tools/perf/benchmarks/tracing.py", line 45, in CreateTimelineBasedMeasurementOptions
          options.config.chrome_trace_config.SetMemoryDumpConfig(memory_dump_config)
        File "/tmp/isolated_run4dWHfM/third_party/catapult/telemetry/telemetry/timeline/chrome_trace_config.py", line 104, in SetMemoryDumpConfig
          'Must pass SetMemoryDumpConfig a MemoryDumpConfig instance')
      TypeError: Must pass SetMemoryDumpConfig a MemoryDumpConfig instance
    
    https://build.chromium.org/p/tryserver.chromium.linux/builders/linux_chromium_rel_ng/builds/250550/steps/telemetry_perf_unittests%20%28with%20patch%29%20on%20Ubuntu-12.04/logs/stdio
    
    Original issue's description:
    > Separate trace config classes into multiple files
    >
    > BUG=catapult:#2413
    > TBR=nednguyen@google.com
    >
    > Committed: https://chromium.googlesource.com/external/github.com/catapult-project/catapult/+/b4b7367a811e5c877df6ed0ef069342b03d8666d
    
    TBR=nednguyen@google.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=catapult:#2413
    
    Review-Url: https://codereview.chromium.org/2085343004

--- telemetry/telemetry/timeline/tracing_config_unittest.py
+++ telemetry/telemetry/timeline/tracing_config_unittest.py
@@ -5,38 +5,41 @@
 import unittest
 
 from telemetry.timeline import tracing_category_filter
-from telemetry.timeline import chrome_trace_config
+from telemetry.timeline import tracing_config
 
 
-class ChromeTraceConfigTests(unittest.TestCase):
+class TracingConfigTests(unittest.TestCase):
   def testDefault(self):
-    config = chrome_trace_config.ChromeTraceConfig()
+    config = tracing_config.TracingConfig()
+    chrome_trace_config = config.chrome_trace_config
 
     # Trace config for startup tracing.
     self.assertEquals({
         'enable_systrace': False,
         'record_mode': 'record-as-much-as-possible'
-    }, config.GetChromeTraceConfigForStartupTracing())
+    }, chrome_trace_config.GetChromeTraceConfigForStartupTracing())
 
     # Trace config for DevTools (modern API).
     self.assertEquals({
         'enableSystrace': False,
         'recordMode': 'recordAsMuchAsPossible'
-    }, config.GetChromeTraceConfigForDevTools())
+    }, chrome_trace_config.GetChromeTraceConfigForDevTools())
 
     # Trace categories and options for DevTools (legacy API).
-    self.assertFalse(config.requires_modern_devtools_tracing_start_api)
+    self.assertFalse(
+        chrome_trace_config.requires_modern_devtools_tracing_start_api)
     self.assertEquals(
         ('', 'record-as-much-as-possible'),
-        config.GetChromeTraceCategoriesAndOptionsForDevTools())
+        chrome_trace_config.GetChromeTraceCategoriesAndOptionsForDevTools())
 
   def testBasic(self):
     category_filter = tracing_category_filter.TracingCategoryFilter(
         'x,-y,disabled-by-default-z,DELAY(7;foo)')
-    config = chrome_trace_config.ChromeTraceConfig()
-    config.SetTracingCategoryFilter(category_filter)
-    config.enable_systrace = True
-    config.record_mode = chrome_trace_config.RECORD_UNTIL_FULL
+    config = tracing_config.TracingConfig()
+    chrome_trace_config = config.chrome_trace_config
+    chrome_trace_config.SetTracingCategoryFilter(category_filter)
+    chrome_trace_config.enable_systrace = True
+    chrome_trace_config.record_mode = tracing_config.RECORD_UNTIL_FULL
 
     # Trace config for startup tracing.
     self.assertEquals({
@@ -45,7 +48,7 @@ class ChromeTraceConfigTests(unittest.TestCase):
         'included_categories': ['x', 'disabled-by-default-z'],
         'record_mode': 'record-until-full',
         'synthetic_delays': ['DELAY(7;foo)']
-    }, config.GetChromeTraceConfigForStartupTracing())
+    }, chrome_trace_config.GetChromeTraceConfigForStartupTracing())
 
     # Trace config for DevTools (modern API).
     self.assertEquals({
@@ -54,39 +57,41 @@ class ChromeTraceConfigTests(unittest.TestCase):
         'includedCategories': ['x', 'disabled-by-default-z'],
         'recordMode': 'recordUntilFull',
         'syntheticDelays': ['DELAY(7;foo)']
-    }, config.GetChromeTraceConfigForDevTools())
+    }, chrome_trace_config.GetChromeTraceConfigForDevTools())
 
     # Trace categories and options for DevTools (legacy API).
-    self.assertFalse(config.requires_modern_devtools_tracing_start_api)
+    self.assertFalse(
+        chrome_trace_config.requires_modern_devtools_tracing_start_api)
     self.assertEquals(
         ('x,disabled-by-default-z,-y,DELAY(7;foo)',
          'record-until-full,enable-systrace'),
-        config.GetChromeTraceCategoriesAndOptionsForDevTools())
+        chrome_trace_config.GetChromeTraceCategoriesAndOptionsForDevTools())
 
   def testMemoryDumpConfigFormat(self):
-    config = chrome_trace_config.ChromeTraceConfig()
-    config.record_mode = chrome_trace_config.ECHO_TO_CONSOLE
-    dump_config = chrome_trace_config.MemoryDumpConfig()
-    config.SetMemoryDumpConfig(dump_config)
+    config = tracing_config.TracingConfig()
+    config.chrome_trace_config.record_mode = tracing_config.ECHO_TO_CONSOLE
+    dump_config = tracing_config.MemoryDumpConfig()
+    config.chrome_trace_config.SetMemoryDumpConfig(dump_config)
 
     # Trace config for startup tracing.
     self.assertEquals({
         'enable_systrace': False,
         'memory_dump_config': {'triggers': []},
         'record_mode': 'trace-to-console'
-    }, config.GetChromeTraceConfigForStartupTracing())
+    }, config.chrome_trace_config.GetChromeTraceConfigForStartupTracing())
 
     # Trace config for DevTools (modern API).
     self.assertEquals({
         'enableSystrace': False,
         'memoryDumpConfig': {'triggers': []},
         'recordMode': 'traceToConsole'
-    }, config.GetChromeTraceConfigForDevTools())
+    }, config.chrome_trace_config.GetChromeTraceConfigForDevTools())
 
     # Trace categories and options for DevTools (legacy API).
-    self.assertTrue(config.requires_modern_devtools_tracing_start_api)
+    self.assertTrue(
+        config.chrome_trace_config.requires_modern_devtools_tracing_start_api)
     with self.assertRaises(AssertionError):
-      config.GetChromeTraceCategoriesAndOptionsForDevTools()
+      config.chrome_trace_config.GetChromeTraceCategoriesAndOptionsForDevTools()
 
     dump_config.AddTrigger('light', 250)
     dump_config.AddTrigger('detailed', 2000)
@@ -101,7 +106,7 @@ class ChromeTraceConfigTests(unittest.TestCase):
             ]
         },
         'record_mode': 'trace-to-console'
-    }, config.GetChromeTraceConfigForStartupTracing())
+    }, config.chrome_trace_config.GetChromeTraceConfigForStartupTracing())
 
     # Trace config for DevTools (modern API).
     self.assertEquals({
@@ -113,9 +118,10 @@ class ChromeTraceConfigTests(unittest.TestCase):
             ]
         },
         'recordMode': 'traceToConsole'
-    }, config.GetChromeTraceConfigForDevTools())
+    }, config.chrome_trace_config.GetChromeTraceConfigForDevTools())
 
     # Trace categories and options for DevTools (legacy API).
-    self.assertTrue(config.requires_modern_devtools_tracing_start_api)
+    self.assertTrue(
+        config.chrome_trace_config.requires_modern_devtools_tracing_start_api)
     with self.assertRaises(AssertionError):
-      config.GetChromeTraceCategoriesAndOptionsForDevTools()
+      config.chrome_trace_config.GetChromeTraceCategoriesAndOptionsForDevTools()
