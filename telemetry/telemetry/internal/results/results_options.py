bd5d7db6e8d39b18ea49ffc50b066b5b682144e6
nednguyen <nednguyen@google.com>
2016-08-03 05:52:10 -0700

    Reland of [Telemetry] Remove BuildbotOutputFormatter (patchset #1 id:1 of https://codereview.chromium.org/2064753008/ )
    Reason for revert:
    crbug.com/620464 is fixed. There should be no one who still actively uses BuildbotOutputFormatter.
    
    Removal notice: https://groups.google.com/a/chromium.org/forum/#!topic/telemetry-announce/UVrK9pvRC7I
    
    Original issue's description:
    > Revert of [Telemetry] Remove BuildbotOutputFormatter (patchset #1 id:1 of https://codereview.chromium.org/2012793002/ )
    >
    > Reason for revert:
    > Other team is still using it.
    >
    > Original issue's description:
    > > [Telemetry] Remove BuildbotOutputFormatter
    > >
    > > BUG=chromium:477056
    > >
    > > Committed: https://chromium.googlesource.com/external/github.com/catapult-project/catapult/+/0fdcac0b5843b7002bee2f87b9af2f8d96ddba06
    >
    > TBR=nednguyen@chromium.org,prasadv@chromium.org,robertocn@chromium.org,eakuefner@chromium.org
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=chromium:477056
    >
    > Committed: https://chromium.googlesource.com/external/github.com/catapult-project/catapult/+/d1858050f3e805c900f1628f34d90c13d67b4cf6
    
    TBR=nednguyen@chromium.org,prasadv@chromium.org,robertocn@chromium.org,eakuefner@chromium.org,achuith@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=chromium:477056
    
    Review-Url: https://codereview.chromium.org/2180343002

--- telemetry/telemetry/internal/results/results_options.py
+++ telemetry/telemetry/internal/results/results_options.py
@@ -11,7 +11,6 @@ import time
 from catapult_base import cloud_storage  # pylint: disable=import-error
 
 from telemetry.core import util
-from telemetry.internal.results import buildbot_output_formatter
 from telemetry.internal.results import chart_json_output_formatter
 from telemetry.internal.results import csv_pivot_table_output_formatter
 from telemetry.internal.results import gtest_progress_reporter
@@ -23,7 +22,8 @@ from telemetry.internal.results import progress_reporter
 from telemetry.internal.results import valueset_output_formatter
 
 # Allowed output formats. The default is the first item in the list.
-_OUTPUT_FORMAT_CHOICES = ('html', 'html2', 'buildbot', 'gtest', 'json',
+
+_OUTPUT_FORMAT_CHOICES = ('html', 'html2', 'gtest', 'json',
     'chartjson', 'csv-pivot-table', 'valueset', 'none')
 
 
@@ -53,7 +53,7 @@ def AddResultsOptions(parser):
   group.add_option('--output-trace-tag',
                     default='',
                     help='Append a tag to the key of each result trace. Use '
-                    'with html, buildbot, csv-pivot-table output formats.')
+                    'with html, csv-pivot-table output formats.')
   group.add_option('--reset-results', action='store_true',
                     help='Delete all stored results.')
   group.add_option('--upload-results', action='store_true',
@@ -92,9 +92,6 @@ def _GetOutputStream(output_format, output_dir):
   assert output_format in _OUTPUT_FORMAT_CHOICES, 'Must specify a valid format.'
   assert output_format not in ('gtest', 'none'), (
       'Cannot set stream for \'gtest\' or \'none\' output formats.')
-
-  if output_format == 'buildbot':
-    return sys.stdout
 
   assert output_format in _OUTPUT_FILENAME_LOOKUP, (
       'No known filename for the \'%s\' output format' % output_format)
@@ -135,19 +132,7 @@ def CreateResults(benchmark_metadata, options,
       output_formatters.append(
           csv_pivot_table_output_formatter.CsvPivotTableOutputFormatter(
               output_stream, trace_tag=options.output_trace_tag))
-    elif output_format == 'buildbot':
-      output_formatters.append(
-          buildbot_output_formatter.BuildbotOutputFormatter(
-              output_stream, trace_tag=options.output_trace_tag))
     elif output_format == 'html':
-      # TODO(chrishenry): We show buildbot output so that users can grep
-      # through the results easily without needing to open the html
-      # file.  Another option for this is to output the results directly
-      # in gtest-style results (via some sort of progress reporter),
-      # as we plan to enable gtest-style output for all output formatters.
-      output_formatters.append(
-          buildbot_output_formatter.BuildbotOutputFormatter(
-              sys.stdout, trace_tag=options.output_trace_tag))
       output_formatters.append(html_output_formatter.HtmlOutputFormatter(
           output_stream, benchmark_metadata, options.reset_results,
           options.upload_results, options.browser_type,
