ca10e840fb1eabf13a2909647258add3dc68fcb6
nednguyen <nednguyen@google.com>
2016-08-31 14:19:57 -0700

    Revert of Disable WebviewInExtensionTest.testWebviewInExtension on all platforms (patchset #2 id:20001 of https://codereview.chromium.org/2287203003/ )
    Reason for revert:
    The root cause should be fixed (see attaching bug)
    
    Original issue's description:
    > Disable WebviewInExtensionTest.testWebviewInExtension on all platforms
    >
    > It's flaky everywhere
    >
    > BUG=chromium:508325
    > TBR=shadi@chromium.org, nednguyen@google.com
    >
    > Committed: https://chromium.googlesource.com/external/github.com/catapult-project/catapult/+/9dd53f60db07a5df78cc6a8078e5bd4f56275c65
    
    TBR=shadi@chromium.org,achuith@chromium.org,afakhry@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=chromium:508325
    
    Review-Url: https://codereview.chromium.org/2302583002

--- telemetry/telemetry/internal/browser/extension_unittest.py
+++ telemetry/telemetry/internal/browser/extension_unittest.py
@@ -185,8 +185,8 @@ class ComponentExtensionTest(unittest.TestCase):
 
 class WebviewInExtensionTest(ExtensionTest):
 
-  # Flaky on all platforms, hits an exception: http://crbug.com/508325
-  @decorators.Disabled('all')
+  # Flaky on windows, hits an exception: http://crbug.com/508325
+  @decorators.Disabled('win', 'linux')
   def testWebviewInExtension(self):
     """Tests GetWebviewContext() for a web app containing <webview> element."""
     if not self.CreateBrowserWithExtension('webview_app'):
