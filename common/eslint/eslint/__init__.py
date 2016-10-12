4e95ba533eafeb8062cdabb263ded2a08d500f40
charliea <charliea@chromium.org>
2016-10-12 08:10:45 -0700

    [eslint] Add an ability to pass extra args to the eslint command
    This is useful when trying to run things like 'eslint --help', or
    'eslint --no-color', which can be useful when doing bulk fixing of
    existing errors.
    
    With this new feature, you could do:
    
    $ common/eslint/bin/run_eslint --all --extra-args='--no-color' > tmp.txt
    
    Review-Url: https://codereview.chromium.org/2410163002

--- common/eslint/eslint/__init__.py
+++ common/eslint/eslint/__init__.py
@@ -40,20 +40,24 @@ DEFAULT_ESLINT_RULES_DIR = os.path.join(
     py_utils.GetCatapultDir(), 'common', 'eslint', 'rules')
 
 
-def _CreateEslintCommand(rulesdir):
-  return BASE_ESLINT_CMD + [
+def _CreateEslintCommand(rulesdir, extra_args):
+  eslint_cmd = BASE_ESLINT_CMD + [
       '--rulesdir', rulesdir
   ]
+  if extra_args:
+    eslint_cmd += [extra_args]
+  return eslint_cmd
 
 
 def RunEslintOnDirs(dirs,
-                    rules_dir=DEFAULT_ESLINT_RULES_DIR):
+                    rules_dir=DEFAULT_ESLINT_RULES_DIR,
+                    extra_args=None):
   if type(dirs) is not list or len(dirs) == 0:
     raise ValueError('Must specify a non-empty list of directories to lint.')
 
   try:
     find_cmd = ['find'] + dirs + ['-name', '*.html']
-    eslint_cmd = _CreateEslintCommand(rules_dir)
+    eslint_cmd = _CreateEslintCommand(rules_dir, extra_args)
     p1 = subprocess.Popen(find_cmd, stdout=subprocess.PIPE)
     output = subprocess.check_output(['xargs'] + eslint_cmd, stdin=p1.stdout)
     p1.wait()
@@ -63,12 +67,13 @@ def RunEslintOnDirs(dirs,
 
 
 def RunEslintOnFiles(filenames,
-                     rules_dir=DEFAULT_ESLINT_RULES_DIR):
+                     rules_dir=DEFAULT_ESLINT_RULES_DIR,
+                     extra_args=None):
   if type(filenames) is not list or len(filenames) == 0:
     raise ValueError('Must specify a non-empty list of files to lint.')
 
   try:
-    eslint_cmd = _CreateEslintCommand(rules_dir)
+    eslint_cmd = _CreateEslintCommand(rules_dir, extra_args)
     return subprocess.check_output(eslint_cmd + filenames,
                                    stderr=subprocess.STDOUT)
   except subprocess.CalledProcessError as e:
