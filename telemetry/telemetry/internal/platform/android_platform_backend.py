0e59c53e3a07c3380e923ba82c5bfc408142b897
rnephew <rnephew@chromium.org>
2016-10-21 13:40:16 -0700

    [Android][Telemetry] Show output of adb.ForwardList when error detected.
    BUG=chromium:655653
    
    Review-Url: https://chromiumcodereview.appspot.com/2426393007

--- telemetry/telemetry/internal/platform/android_platform_backend.py
+++ telemetry/telemetry/internal/platform/android_platform_backend.py
@@ -494,7 +494,8 @@ class AndroidPlatformBackend(
     self._device.adb.Forward('tcp:%d' % host_port, device_port)
 
   def StopForwardingHost(self, host_port):
-    for line in self._device.adb.ForwardList().strip().splitlines():
+    adb_forward_list = self._device.adb.ForwardList().strip().splitlines()
+    for line in adb_forward_list:
       line = line.split(' ')
       if len(line) >= 2:
         if line[0] == self._device and line[1] == 'tcp:%s' % host_port:
@@ -504,6 +505,8 @@ class AndroidPlatformBackend(
         logging.warning('ADB forwarder list output format unexpected. Expected '
                         'format "<device id> tcp:<host port> but got:\n%s',
                         line)
+        logging.warning('Complete output from ADB forwarder list:\n%s',
+                        '\n'.join(adb_forward_list))
     else:
       logging.warning('Port %s not found in adb forward --list for device %s',
                       host_port, self._device)
