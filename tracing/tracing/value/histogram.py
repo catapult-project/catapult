e2a178a9c0cdc4e6aa745af113bc9b7ae3651aa8
benjhayden <benjhayden@chromium.org>
2017-01-18 13:12:18 -0800

    Translate RunningStatistics to python.
    JS: https://github.com/catapult-project/catapult/blob/master/tracing/tracing/base/running_statistics.html
    JS test: https://github.com/catapult-project/catapult/blob/master/tracing/tracing/base/running_statistics_test.html
    
    Full histogram.py CL: https://codereview.chromium.org/2630473002
    
    BUG=catapult:#3144
    
    Review-Url: https://codereview.chromium.org/2639093002

--- tracing/tracing/value/histogram.py
+++ tracing/tracing/value/histogram.py
@@ -2,6 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import math
+
+
+# This should be equal to sys.float_info.max, but that value might differ
+# between platforms, whereas ECMA Script specifies this value for all platforms.
+# The specific value should not matter in normal practice.
+JS_MAX_VALUE = 1.7976931348623157e+308
+
 
 class Range(object):
   def __init__(self):
@@ -42,3 +50,139 @@ class Range(object):
 
     self._max = max(x, self._max)
     self._min = min(x, self._min)
+
+
+# This class computes statistics online in O(1).
+class RunningStatistics(object):
+  def __init__(self):
+    self._mean = 0
+    self._count = 0
+    self._max = -JS_MAX_VALUE
+    self._min = JS_MAX_VALUE
+    self._sum = 0
+    self._variance = 0
+    # Mean of logarithms of samples, or undefined if any samples were <= 0.
+    self._meanlogs = 0
+
+  @property
+  def count(self):
+    return self._count
+
+  @property
+  def geometric_mean(self):
+    if self._meanlogs is None:
+      return None
+    return math.exp(self._meanlogs)
+
+  @property
+  def mean(self):
+    if self._count == 0:
+      return None
+    return self._mean
+
+  @property
+  def max(self):
+    return self._max
+
+  @property
+  def min(self):
+    return self._min
+
+  @property
+  def sum(self):
+    return self._sum
+
+  @property
+  def variance(self):
+    if self.count == 0:
+      return None
+    if self.count == 1:
+      return 0
+    return self._variance / (self.count - 1)
+
+  @property
+  def stddev(self):
+    if self.count == 0:
+      return None
+    return math.sqrt(self.variance)
+
+  def Add(self, x):
+    self._count += 1
+    self._max = max(self._max, x)
+    self._min = min(self._min, x)
+    self._sum += x
+
+    if x <= 0:
+      self._meanlogs = None
+    elif self._meanlogs is not None:
+      self._meanlogs += (math.log(abs(x)) - self._meanlogs) / self.count
+
+    # The following uses Welford's algorithm for computing running mean and
+    # variance. See http://www.johndcook.com/blog/standard_deviation.
+    if self.count == 1:
+      self._mean = x
+      self._variance = 0
+    else:
+      old_mean = self._mean
+      old_variance = self._variance
+
+      # Using the 2nd formula for updating the mean yields better precision but
+      # it doesn't work for the case oldMean is Infinity. Hence we handle that
+      # case separately.
+      if abs(old_mean) == float('inf'):
+        self._mean = self._sum / self.count
+      else:
+        self._mean = old_mean + float(x - old_mean) / self.count
+      self._variance = old_variance + (x - old_mean) * (x - self._mean)
+
+  def Merge(self, other):
+    result = RunningStatistics()
+    result._count = self._count + other._count
+    result._sum = self._sum + other._sum
+    result._min = min(self._min, other._min)
+    result._max = max(self._max, other._max)
+    if result._count == 0:
+      result._mean = 0
+      result._variance = 0
+      result._meanlogs = 0
+    else:
+      # Combine the mean and the variance using the formulas from
+      # https://goo.gl/ddcAep.
+      result._mean = float(result._sum) / result._count
+      delta_mean = (self._mean or 0) - (other._mean or 0)
+      result._variance = self._variance + other._variance + (
+          self._count * other._count * delta_mean * delta_mean / result._count)
+
+      # Merge the arithmetic means of logarithms of absolute values of samples,
+      # weighted by counts.
+      if self._meanlogs is None or other._meanlogs is None:
+        result._meanlogs = None
+      else:
+        result._meanlogs = (self._count * self._meanlogs +
+                            other._count * other._meanlogs) / result._count
+    return result
+
+  def AsDict(self):
+    if self._count == 0:
+      return []
+    # It's more efficient to serialize these fields in an array. If you add any
+    # other fields, you should re-evaluate whether it would be more efficient to
+    # serialize as a dict.
+    return [
+        self._count,
+        self._max,
+        self._meanlogs,
+        self._mean,
+        self._min,
+        self._sum,
+        self._variance,
+    ]
+
+  @staticmethod
+  def FromDict(dct):
+    result = RunningStatistics()
+    if len(dct) != 7:
+      return result
+    [result._count, result._max, result._meanlogs, result._mean, result._min,
+     result._sum, result._variance] = dct
+    return result
--- tracing/tracing/value/histogram_unittest.py
+++ tracing/tracing/value/histogram_unittest.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import math
 import unittest
 
 from tracing.value import histogram
@@ -21,3 +22,58 @@ class RangeUnittest(unittest.TestCase):
     self.assertEqual(r.min, 1)
     self.assertEqual(r.max, 2)
     self.assertEqual(r.center, 1.5)
+
+
+class RunningStatisticsUnittest(unittest.TestCase):
+  def _Run(self, data):
+    running = histogram.RunningStatistics()
+    for datum in data:
+      running.Add(datum)
+    return running
+
+  def testStatistics(self):
+    running = self._Run([1, 2, 3])
+    self.assertEqual(running.sum, 6)
+    self.assertEqual(running.mean, 2)
+    self.assertEqual(running.min, 1)
+    self.assertEqual(running.max, 3)
+    self.assertEqual(running.variance, 1)
+    self.assertEqual(running.stddev, 1)
+    self.assertEqual(running.geometric_mean, math.pow(6, 1./3))
+    self.assertEqual(running.count, 3)
+
+    running = self._Run([2, 4, 4, 2])
+    self.assertEqual(running.sum, 12)
+    self.assertEqual(running.mean, 3)
+    self.assertEqual(running.min, 2)
+    self.assertEqual(running.max, 4)
+    self.assertEqual(running.variance, 4./3)
+    self.assertEqual(running.stddev, math.sqrt(4./3))
+    self.assertAlmostEqual(running.geometric_mean, math.pow(64, 1./4))
+    self.assertEqual(running.count, 4)
+
+  def testMerge(self):
+    def Compare(data1, data2):
+      a_running = self._Run(data1 + data2)
+      b_running = self._Run(data1).Merge(self._Run(data2))
+      CompareRunningStatistics(a_running, b_running)
+      a_running = histogram.RunningStatistics.FromDict(a_running.AsDict())
+      CompareRunningStatistics(a_running, b_running)
+      b_running = histogram.RunningStatistics.FromDict(b_running.AsDict())
+      CompareRunningStatistics(a_running, b_running)
+
+    def CompareRunningStatistics(a_running, b_running):
+      self.assertEqual(a_running.sum, b_running.sum)
+      self.assertEqual(a_running.mean, b_running.mean)
+      self.assertEqual(a_running.min, b_running.min)
+      self.assertEqual(a_running.max, b_running.max)
+      self.assertAlmostEqual(a_running.variance, b_running.variance)
+      self.assertAlmostEqual(a_running.stddev, b_running.stddev)
+      self.assertAlmostEqual(a_running.geometric_mean, b_running.geometric_mean)
+      self.assertEqual(a_running.count, b_running.count)
+
+    Compare([], [])
+    Compare([], [1, 2, 3])
+    Compare([1, 2, 3], [])
+    Compare([1, 2, 3], [10, 20, 100])
+    Compare([1, 1, 1, 1, 1], [10, 20, 10, 40])
