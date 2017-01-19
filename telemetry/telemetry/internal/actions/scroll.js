1e05d2f8401ce9b112c2f0e21269f1d7493ce88b
hjd <hjd@chromium.org>
2017-01-19 04:44:20 -0800

    Revert of [telemetry] Bullet proof scroll.js (patchset #4 id:60001 of https://codereview.chromium.org/2532443002/ )
    Reason for revert:
    CL breaks catapult->chrome autoroller see: https://github.com/catapult-project/catapult/issues/3149
    
    Original issue's description:
    > [telemetry] Bullet proof scroll.js
    >
    > Make scroll.js give a nice error message if the scroll point is
    > off screen or if the element has zero area. Previously we just
    > got a stack trace from chrome.
    >
    > Also fix various lint errors since this is the first time this file
    > has been touched in a while.
    >
    > BUG=chromium:664515
    >
    > Review-Url: https://codereview.chromium.org/2532443002
    > Committed: https://chromium.googlesource.com/external/github.com/catapult-project/catapult/+/c3ec514d8726530643844182677304ce7f15a5c2
    
    TBR=skyostil@chromium.org,perezju@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=chromium:664515
    NOPRESUBMIT=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2644073003

--- telemetry/telemetry/internal/actions/scroll.js
+++ telemetry/telemetry/internal/actions/scroll.js
@@ -14,15 +14,15 @@
   function ScrollGestureOptions(opt_options) {
     if (opt_options) {
       this.element_ = opt_options.element;
-      this.leftStartRatio_ = opt_options.left_start_ratio;
-      this.topStartRatio_ = opt_options.top_start_ratio;
+      this.left_start_ratio_ = opt_options.left_start_ratio;
+      this.top_start_ratio_ = opt_options.top_start_ratio;
       this.direction_ = opt_options.direction;
       this.speed_ = opt_options.speed;
       this.gesture_source_type_ = opt_options.gesture_source_type;
     } else {
       this.element_ = document.scrollingElement || document.body;
-      this.leftStartRatio_ = 0.5;
-      this.topStartRatio_ = 0.5;
+      this.left_start_ratio_ = 0.5;
+      this.top_start_ratio_ = 0.5;
       this.direction_ = 'down';
       this.speed_ = 800;
       this.gesture_source_type_ = chrome.gpuBenchmarking.DEFAULT_INPUT;
@@ -40,20 +40,20 @@
   // This class scrolls a page from the top to the bottom once.
   //
   // The page is scrolled down by a single scroll gesture.
-  function ScrollAction(opt_callback, opt_distanceFunc) {
+  function ScrollAction(opt_callback, opt_distance_func) {
     var self = this;
 
     this.beginMeasuringHook = function() {};
     this.endMeasuringHook = function() {};
 
     this.callback_ = opt_callback;
-    this.distance_func_ = opt_distanceFunc;
+    this.distance_func_ = opt_distance_func;
   }
 
   ScrollAction.prototype.getScrollDistanceDown_ = function() {
     var clientHeight;
     // clientHeight is "special" for the body element.
-    if (this.element_ === document.body)
+    if (this.element_ == document.body)
       clientHeight = __GestureCommon_GetWindowHeight();
     else
       clientHeight = this.element_.clientHeight;
@@ -70,7 +70,7 @@
   ScrollAction.prototype.getScrollDistanceRight_ = function() {
     var clientWidth;
     // clientWidth is "special" for the body element.
-    if (this.element_ === document.body)
+    if (this.element_ == document.body)
       clientWidth = __GestureCommon_GetWindowWidth();
     else
       clientWidth = this.element_.clientWidth;
@@ -86,24 +86,24 @@
     if (this.distance_func_)
       return this.distance_func_();
 
-    if (this.options_.direction_ === 'down') {
+    if (this.options_.direction_ == 'down') {
       return this.getScrollDistanceDown_();
-    } else if (this.options_.direction_ === 'up') {
+    } else if (this.options_.direction_ == 'up') {
       return this.getScrollDistanceUp_();
-    } else if (this.options_.direction_ === 'right') {
+    } else if (this.options_.direction_ == 'right') {
       return this.getScrollDistanceRight_();
-    } else if (this.options_.direction_ === 'left') {
+    } else if (this.options_.direction_ == 'left') {
       return this.getScrollDistanceLeft_();
-    } else if (this.options_.direction_ === 'upleft') {
+    } else if (this.options_.direction_ == 'upleft') {
       return Math.min(this.getScrollDistanceUp_(),
                       this.getScrollDistanceLeft_());
-    } else if (this.options_.direction_ === 'upright') {
+    } else if (this.options_.direction_ == 'upright') {
       return Math.min(this.getScrollDistanceUp_(),
                       this.getScrollDistanceRight_());
-    } else if (this.options_.direction_ === 'downleft') {
+    } else if (this.options_.direction_ == 'downleft') {
       return Math.min(this.getScrollDistanceDown_(),
                       this.getScrollDistanceLeft_());
-    } else if (this.options_.direction_ === 'downright') {
+    } else if (this.options_.direction_ == 'downright') {
       return Math.min(this.getScrollDistanceDown_(),
                       this.getScrollDistanceRight_());
     }
@@ -114,30 +114,26 @@
     // Assign this.element_ here instead of constructor, because the constructor
     // ensures this method will be called after the document is loaded.
     this.element_ = this.options_.element_;
-
-    // Calculate scroll point.
-    var rect = __GestureCommon_GetBoundingVisibleRect(this.options_.element_);
-    this.startLeft = rect.left + rect.width * this.options_.leftStartRatio_;
-    this.startTop = rect.top + rect.height * this.options_.topStartRatio_;
-
-    if (rect.width === 0 || rect.height === 0) {
-      throw new Error('Scroll element has zero height and/or width.');
-    }
-
     requestAnimationFrame(this.startGesture_.bind(this));
   };
 
   ScrollAction.prototype.startGesture_ = function() {
     this.beginMeasuringHook();
 
-    var maxScrollLengthPixels = (MAX_SCROLL_LENGTH_TIME_MS / 1000) *
+    var max_scroll_length_pixels = (MAX_SCROLL_LENGTH_TIME_MS / 1000) *
         this.options_.speed_;
-    var distance = Math.min(maxScrollLengthPixels, this.getScrollDistance_());
+    var distance = Math.min(max_scroll_length_pixels,
+                            this.getScrollDistance_());
 
-   chrome.gpuBenchmarking.smoothScrollBy(
-        distance, this.onGestureComplete_.bind(this),
-        this.startLeft, this.startTop, this.options_.gesture_source_type_,
-        this.options_.direction_, this.options_.speed_);
+    var rect = __GestureCommon_GetBoundingVisibleRect(this.options_.element_);
+    var start_left =
+        rect.left + rect.width * this.options_.left_start_ratio_;
+    var start_top =
+        rect.top + rect.height * this.options_.top_start_ratio_;
+    chrome.gpuBenchmarking.smoothScrollBy(
+        distance, this.onGestureComplete_.bind(this), start_left, start_top,
+        this.options_.gesture_source_type_, this.options_.direction_,
+        this.options_.speed_);
   };
 
   ScrollAction.prototype.onGestureComplete_ = function() {
