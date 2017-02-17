5252580c24aa6c4bc322a19c9ad03cd2c94f1382
erikchen <erikchen@chromium.org>
2017-02-17 11:33:46 -0800

    Use absolute pcs and load locations to symbolize on macOS.
    atos gets confused if it's passed a relative address that's lower than the load
    location of the TEXT segment of a Mach-O file. This CL adds a utility to read
    the load address of a Mach-O file, and then computes an absolute PC/load
    location based on the relative PC.
    
    BUG=chromium:677302
    
    Review-Url: https://codereview.chromium.org/2698543003

---
+++ tracing/bin/symbolize_trace_macho_reader_unittest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+import unittest
+
+sys.path.insert(0, os.path.dirname(__file__))
+import symbolize_trace_macho_reader
+
+
+class AtosRegexTest(unittest.TestCase):
+  def testRegex(self):
+    if sys.platform != "darwin":
+      return
+    file_name = "/System/Library/Frameworks/IOKit.framework/Versions/A/IOKit"
+    result = symbolize_trace_macho_reader.ReadMachOTextLoadAddress(file_name)
+    self.assertNotEqual(None, result)
+
+    file_name = "/System/Library/Frameworks/Cocoa.framework/Versions/A/Cocoa"
+    result = symbolize_trace_macho_reader.ReadMachOTextLoadAddress(file_name)
+    self.assertNotEqual(None, result)
+
+
+if __name__ == '__main__':
+  unittest.main()
