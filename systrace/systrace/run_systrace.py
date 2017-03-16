faf60eb37f8b9828eddb30c8397b333eb1d89204
jreck <jreck@google.com>
2017-03-15 18:01:54 -0700

    Fix finding adb picking up non-path adb
    BUG=catapult:#3404
    
    Review-Url: https://codereview.chromium.org/2749033005

--- systrace/systrace/run_systrace.py
+++ systrace/systrace/run_systrace.py
@@ -26,7 +26,6 @@ if version != (2, 7):
 import optparse
 import os
 import time
-from distutils.spawn import find_executable
 
 _SYSTRACE_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.path.pardir))
@@ -92,10 +91,27 @@ def parse_options(argv):
 
   return (options, categories)
 
+def find_adb():
+  """Finds adb on the path.
+
+  This method is provided to avoid the issue of diskutils.spawn's
+  find_executable which first searches the current directory before
+  searching $PATH. That behavior results in issues where systrace.py
+  uses a different adb than the one in the path.
+  """
+  paths = os.environ['PATH'].split(os.pathsep)
+  executable = 'adb'
+  if sys.platform == 'win32':
+    executable = executable + '.exe'
+  for p in paths:
+    f = os.path.join(p, executable)
+    if os.path.isfile(f):
+      return f
+  return None
 
 def initialize_devil():
   """Initialize devil to use adb from $PATH"""
-  adb_path = find_executable('adb')
+  adb_path = find_adb()
   if adb_path is None:
     print >> sys.stderr, "Unable to find adb, is it in your path?"
     sys.exit(1)
