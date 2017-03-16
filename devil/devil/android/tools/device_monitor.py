7c1a5164227a17a05689aafedf94565883c80f1a
bpastene <bpastene@chromium.org>
2017-03-15 17:12:47 -0700

    devil: Change location of device monitoring status file.
    BUG=chromium:670879
    
    Review-Url: https://codereview.chromium.org/2745223007

--- devil/devil/android/tools/device_monitor.py
+++ devil/devil/android/tools/device_monitor.py
@@ -16,6 +16,7 @@ import logging
 import logging.handlers
 import os
 import re
+import socket
 import sys
 import time
 
@@ -42,8 +43,15 @@ CPU_TEMP_SENSORS = [
 ]
 
 DEVICE_FILE_VERSION = 1
-DEVICE_FILE = os.path.join(os.path.expanduser('~'),
-                           'android_device_status.json')
+# TODO(bpastene): Remove the old file once sysmon has been updated to read the
+# new status file.
+DEVICE_FILES = [
+    os.path.join(os.path.expanduser('~'), 'android_device_status.json'),
+    os.path.join(
+        os.path.expanduser('~'), '.android',
+        '%s__android_device_status.json' % socket.gethostname().split('.')[0]
+    ),
+]
 
 MEM_INFO_REGEX = re.compile(r'.*?\:\s*(\d+)\s*kB') # ex: 'MemTotal:   185735 kB'
 
@@ -211,8 +219,9 @@ def main(argv):
   while True:
     start = time.time()
     status_dict = get_all_status(blacklist)
-    with open(DEVICE_FILE, 'wb') as f:
-      json.dump(status_dict, f, indent=2, sort_keys=True)
+    for device_file in DEVICE_FILES:
+      with open(device_file, 'wb') as f:
+        json.dump(status_dict, f, indent=2, sort_keys=True)
     logging.info('Got status of all devices in %.2fs.', time.time() - start)
     time.sleep(60)
 
