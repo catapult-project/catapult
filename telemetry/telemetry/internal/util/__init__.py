e9dc4c57fb003d9b289f6bf8bac98f820e1fe8b4
eakuefner <eakuefner@chromium.org>
2017-06-16 10:07:42 -0700

    [Telemetry] Shim telemetry.internal.util.camel_case
    This module is being used in tools/perf. We'll fix the Chromium side and then
    delete this shim.
    
    BUG=catapult:#3612
    
    Review-Url: https://codereview.chromium.org/2939353002

--- telemetry/telemetry/internal/util/__init__.py
+++ telemetry/telemetry/internal/util/__init__.py
@@ -1,3 +1,7 @@
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+from py_utils import camel_case as camel_case_module
+
+camel_case = camel_case_module
