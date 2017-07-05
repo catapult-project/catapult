a5b1645083bd1cfad24a6bef274092407c196b1f
kraynov <kraynov@chromium.org>
2017-07-05 10:51:07 -0700

    Systrace: Fix update_systrace_trace_viewer.py script.
    1. If shell=True (which is needed on Windows) subprocess.check_output
    expects a string as command, not an argument list.
    2. Failure to get git revision doesn't break an update now.
    3. Added (or re-introduced) an option to force updating the file
    without prevention of further auto-updates.
    
    Review-Url: https://codereview.chromium.org/2967053002

--- systrace/systrace/update_systrace_trace_viewer.py
+++ systrace/systrace/update_systrace_trace_viewer.py
@@ -23,6 +23,7 @@ SYSTRACE_TRACE_VIEWER_HTML_FILE = os.path.join(
     'systrace_trace_viewer.html')
 CATAPULT_REV_ = 'CATAPULT_REV'
 NO_AUTO_UPDATE_ = 'NO_AUTO_UPDATE'
+UNKNOWN_REVISION_ = 'UNKNOWN'
 
 
 def create_catapult_rev_str_(revision):
@@ -45,15 +46,14 @@ def get_catapult_rev_in_file_(html_file):
 def get_catapult_rev_in_git_():
   try:
     catapult_rev = subprocess.check_output(
-        ['git', 'rev-parse', 'HEAD'],
+        'git rev-parse HEAD',
         shell=True, # Needed by Windows
         cwd=os.path.dirname(os.path.abspath(__file__))).strip()
   except (subprocess.CalledProcessError, OSError):
-    catapult_rev = ''
+    return None
   if not catapult_rev:
-    return ''
-  else:
-    return catapult_rev
+    return None
+  return catapult_rev
 
 
 def update(no_auto_update=False, no_min=False, force_update=False):
@@ -65,28 +65,39 @@ def update(no_auto_update=False, no_min=False, force_update=False):
 
   Args:
     no_auto_update: If true, force updating the file with revision
-                    NO_AUTO_UPDATE_. Future updates will be skipped unless this
-                    argument is true again.
+                    NO_AUTO_UPDATE_. Future auto-updates will be skipped.
     no_min:         If true, skip minification when updating the file.
     force_update:   If true, update the systrace trace viewer file no matter
                     what.
   """
-  new_rev = None
-  if not force_update:
-    if no_auto_update:
-      new_rev = NO_AUTO_UPDATE_
-    else:
-      new_rev = get_catapult_rev_in_git_()
-      if not new_rev:
-        return
+  if no_auto_update:
+    new_rev = NO_AUTO_UPDATE_
+  else:
+    new_rev = get_catapult_rev_in_git_()
+    if not new_rev:
+      # Source tree could be missing git metadata.
+      print >> sys.stderr, 'Warning: Couldn\'t determine current git revision.'
+      new_rev = UNKNOWN_REVISION_
 
-      if os.path.exists(SYSTRACE_TRACE_VIEWER_HTML_FILE):
-        rev_in_file = get_catapult_rev_in_file_(SYSTRACE_TRACE_VIEWER_HTML_FILE)
-        if rev_in_file == NO_AUTO_UPDATE_ or rev_in_file == new_rev:
-          return
+  need_update = False
+  if force_update:
+    need_update = True
+  elif no_auto_update:
+    need_update = True
+  elif not os.path.exists(SYSTRACE_TRACE_VIEWER_HTML_FILE):
+    need_update = True
+  else:
+    old_rev = get_catapult_rev_in_file_(SYSTRACE_TRACE_VIEWER_HTML_FILE)
+    if not old_rev or old_rev == UNKNOWN_REVISION_:
+      need_update = True
+    # If old_rev was set to NO_AUTO_UPDATE_ it should be skipped, since forced
+    # update cases have been already handled above.
+    if old_rev != new_rev and old_rev != NO_AUTO_UPDATE_:
+      need_update = True
 
-  if force_update and not new_rev:
-    new_rev = "none"
+  if not need_update:
+    print 'Update skipped.'
+    return
 
   print 'Generating viewer file %s with revision %s.' % (
             SYSTRACE_TRACE_VIEWER_HTML_FILE, new_rev)
@@ -104,16 +115,22 @@ def update(no_auto_update=False, no_min=False, force_update=False):
 
 def main():
   parser = optparse.OptionParser()
+  parser.add_option('--force-update', dest='force_update',
+                    default=False, action='store_true', help='force update the '
+                    'systrace trace viewer html file')
   parser.add_option('--no-auto-update', dest='no_auto_update',
                     default=False, action='store_true', help='force update the '
-                    'systrace trace viewer html file. Future auto updates will '
-                    'be skipped unless this flag is specified again.')
+                    'systrace trace viewer html file and disable auto-updates, '
+                    'delete \'systrace_trace_viewer.html\' to re-enable '
+                    'auto-updates')
   parser.add_option('--no-min', dest='no_min', default=False,
                     action='store_true', help='skip minification')
   # pylint: disable=unused-variable
   options, unused_args = parser.parse_args(sys.argv[1:])
 
-  update(no_auto_update=options.no_auto_update, no_min=options.no_min)
+  update(no_auto_update=options.no_auto_update,
+         no_min=options.no_min,
+         force_update=options.force_update)
 
 if __name__ == '__main__':
   main()
