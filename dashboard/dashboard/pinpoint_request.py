3ec578154f4f25deb8b42f58b27d380c3b5ee2e9
simonhatch <simonhatch@chromium.org>
2017-08-01 09:33:57 -0700

    Dashboard - Clone Pinpoint dialog into Dashboard
    This clones the Pinpoint job dialog into dashboard as a separate widget, trims down the fields to just a few (bug id, test, start/end) and has the backend do some lookups/guesswork in order to convert those to the params Pinpoint's API expects.
    
    Specifically, Pinpoint expects swarming dimensions instead of bot names, and also requires you to specify the isolate target.
    
    BUG=catapult:#3736
    
    Review-Url: https://codereview.chromium.org/2992663002

--- dashboard/dashboard/pinpoint_request.py
+++ dashboard/dashboard/pinpoint_request.py
@@ -8,23 +8,117 @@ import json
 
 from google.appengine.api import users
 
+from dashboard.common import namespaced_stored_object
 from dashboard.common import request_handler
 from dashboard.common import utils
 from dashboard.services import pinpoint_service
 
-_PINPOINT_URL = 'https://pinpoint-dot-chromeperf.appspot.com/api/new'
+_BOTS_TO_DIMENSIONS = 'bot_dimensions_map'
+_PINPOINT_REPOSITORIES = 'repositories'
+
+
+class InvalidParamsError(Exception):
+  pass
 
 
 class PinpointNewRequestHandler(request_handler.RequestHandler):
   def post(self):
-    user = users.get_current_user()
-    if not utils.IsValidSheriffUser():
-      message = 'User "%s" not authorized.' % user
-      self.response.out.write(json.dumps({'error': message}))
-      return
-
     job_params = dict(
         (a, self.request.get(a)) for a in self.request.arguments())
-    job_params['email'] = user.email()
 
-    self.response.write(json.dumps(pinpoint_service.NewJob(job_params)))
+    try:
+      pinpoint_params = PinpointParamsFromBisectParams(job_params)
+    except InvalidParamsError as e:
+      self.response.write(json.dumps({'error': e.message}))
+      return
+
+    self.response.write(json.dumps(pinpoint_service.NewJob(pinpoint_params)))
+
+
+def PinpointParamsFromBisectParams(params):
+  """Takes parameters from Dashboard's pinpoint-job-dialog and returns
+  a dict with parameters for a new Pinpoint job.
+
+  Args:
+    params: A dict in the following format:
+    {
+        'test_path': Test path for the metric being bisected.
+        'start_git_hash': Git hash of earlier revision.
+        'end_git_hash': Git hash of later revision.
+        'start_repository': Repository for earlier revision.
+        'end_repository': Repository for later revision.
+        'bug_id': Associated bug.
+    }
+
+  Returns:
+    A dict of params for passing to Pinpoint to start a job, or a dict with an
+    'error' field.
+  """
+  if not utils.IsValidSheriffUser():
+    user = users.get_current_user()
+    raise InvalidParamsError('User "%s" not authorized.' % user)
+
+  bots_to_dimensions = namespaced_stored_object.Get(_BOTS_TO_DIMENSIONS)
+
+  # Pinpoint takes swarming dimensions, so we need to map bot name to those.
+  test_path = params.get('test_path')
+  test_path_parts = test_path.split('/')
+  bot_name = test_path_parts[1]
+  suite = test_path_parts[2]
+  metric = test_path[-1]
+
+  dimensions = bots_to_dimensions.get(bot_name)
+  if not dimensions:
+    raise InvalidParamsError('No dimensions for bot %s defined.' % bot_name)
+
+  # Pinpoint also requires you specify which isolate target to run the
+  # test, so we derive that from the suite name. Eventually, this would
+  # ideally be stored in a SparesDiagnostic but for now we can guess.
+  isolate_targets = [
+      'angle_perftests', 'cc_perftests', 'gpu_perftests',
+      'load_library_perf_tests', 'media_perftests', 'net_perftests',
+      'performance_browser_tests', 'telemetry_perf_tests',
+      'telemetry_perf_webview_tests', 'tracing_perftests']
+
+  target = 'telemetry_perf_tests'
+  if suite in isolate_targets:
+    target = suite
+  elif 'webview' in bot_name:
+    target = 'telemetry_perf_webview_tests'
+
+  start_repository = params.get('start_repository')
+  end_repository = params.get('end_repository')
+  start_git_hash = params.get('start_git_hash')
+  end_git_hash = params.get('end_git_hash')
+
+  supported_repositories = namespaced_stored_object.Get(_PINPOINT_REPOSITORIES)
+
+  # Bail if it's not a supported repository to bisect on
+  if not start_repository in supported_repositories:
+    raise InvalidParamsError('Invalid repository: %s' % start_repository)
+  if not end_repository in supported_repositories:
+    raise InvalidParamsError('Invalid repository: %s' % end_repository)
+
+  # Pinpoint only supports chromium at the moment, so just throw up a
+  # different error for now.
+  if start_repository != 'chromium' or end_repository != 'chromium':
+    raise InvalidParamsError('Only chromium bisects supported currently.')
+
+  email = users.get_current_user().email()
+  job_name = 'Job on [%s/%s/%s] for [%s]' % (bot_name, suite, metric, email)
+
+  return {
+      'test_suite': suite,
+      'test': params.get('test'),
+      'metric': metric,
+      'start_repository': start_repository,
+      'end_repository': end_repository,
+      'start_git_hash': start_git_hash,
+      'end_git_hash': end_git_hash,
+      'bug_id': params.get('bug_id'),
+      'auto_explore': '1',
+      'target': target,
+      'dimensions': json.dumps(dimensions),
+      'email': email,
+      'name': job_name
+  }
--- dashboard/dashboard/pinpoint_request_test.py
+++ dashboard/dashboard/pinpoint_request_test.py
@@ -9,6 +9,7 @@ import webapp2
 import webtest
 
 from dashboard import pinpoint_request
+from dashboard.common import namespaced_stored_object
 from dashboard.common import testing_common
 from dashboard.common import utils
 from dashboard.services import pinpoint_service
@@ -23,23 +24,174 @@ class PinpointNewRequestHandlerTest(testing_common.TestCase):
         [(r'/pinpoint/new', pinpoint_request.PinpointNewRequestHandler)])
     self.testapp = webtest.TestApp(app)
 
+    self.SetCurrentUser('foo@chromium.org')
+
+    namespaced_stored_object.Set('bot_dimensions_map', {
+        'mac': [
+            {'key': 'foo', 'value': 'mac_dimensions'}
+        ],
+        'android-webview-nexus5x': [
+            {'key': 'foo', 'value': 'android_dimensions'}
+        ]
+    })
+
+    namespaced_stored_object.Set('repositories', {
+        'chromium': {'some': 'params'},
+        'v8': {'more': 'params'}
+    })
+
   @mock.patch.object(
       utils, 'IsValidSheriffUser', mock.MagicMock(return_value=False))
   def testPost_NotSheriff(self):
     response = self.testapp.post('/pinpoint/new')
     self.assertEqual(
-        {u'error': u'User "None" not authorized.'},
+        {u'error': u'User "foo@chromium.org" not authorized.'},
         json.loads(response.body))
 
   @mock.patch.object(
       utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
   @mock.patch.object(pinpoint_service, 'NewJob')
-  def testPost_AddsEmail_SendsToPinpoint(self, mock_pinpoint):
+  @mock.patch.object(
+      pinpoint_request, 'PinpointParamsFromBisectParams',
+      mock.MagicMock(return_value={'test': 'result'}))
+  def testPost_Succeeds(self, mock_pinpoint):
     mock_pinpoint.return_value = {'foo': 'bar'}
     self.SetCurrentUser('foo@chromium.org')
     params = {'a': 'b', 'c': 'd'}
     response = self.testapp.post('/pinpoint/new', params)
 
-    expected_args = mock.call({'a': 'b', 'c': 'd', 'email': 'foo@chromium.org'})
+    expected_args = mock.call({'test': 'result'})
     self.assertEqual([expected_args], mock_pinpoint.call_args_list)
     self.assertEqual({'foo': 'bar'}, json.loads(response.body))
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=False))
+  def testPinpointParams_InvalidSheriff_RaisesError(self):
+    params = {
+        'test_path': 'ChromiumPerf/foo/blah/foo'
+    }
+    with self.assertRaises(pinpoint_request.InvalidParamsError):
+      pinpoint_request.PinpointParamsFromBisectParams(params)
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_BotUndefined_ReturnsError(self):
+    params = {
+        'test_path': 'ChromiumPerf/foo/blah/foo'
+    }
+    with self.assertRaises(pinpoint_request.InvalidParamsError):
+      pinpoint_request.PinpointParamsFromBisectParams(params)
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_IsolateTarget_NonTelemetry(self):
+    params = {
+        'test_path': 'ChromiumPerf/mac/cc_perftests/foo',
+        'start_git_hash': 'abcd1234',
+        'end_git_hash': 'efgh5678',
+        'start_repository': 'chromium',
+        'end_repository': 'chromium',
+        'bug_id': 1,
+    }
+    results = pinpoint_request.PinpointParamsFromBisectParams(params)
+
+    self.assertEqual('cc_perftests', results['target'])
+    self.assertEqual('foo@chromium.org', results['email'])
+    self.assertEqual('chromium', results['start_repository'])
+    self.assertEqual('abcd1234', results['start_git_hash'])
+    self.assertEqual('chromium', results['end_repository'])
+    self.assertEqual('efgh5678', results['end_git_hash'])
+    self.assertEqual('1', results['auto_explore'])
+    self.assertEqual(1, results['bug_id'])
+    self.assertEqual(
+        [{'key': 'foo', 'value': 'mac_dimensions'}],
+        json.loads(results['dimensions']))
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_IsolateTarget_Telemetry(self):
+    params = {
+        'test_path': 'ChromiumPerf/mac/system_health/foo',
+        'start_git_hash': 'abcd1234',
+        'end_git_hash': 'efgh5678',
+        'start_repository': 'chromium',
+        'end_repository': 'chromium',
+        'bug_id': 1,
+    }
+    results = pinpoint_request.PinpointParamsFromBisectParams(params)
+
+    self.assertEqual('telemetry_perf_tests', results['target'])
+    self.assertEqual('foo@chromium.org', results['email'])
+    self.assertEqual('chromium', results['start_repository'])
+    self.assertEqual('abcd1234', results['start_git_hash'])
+    self.assertEqual('chromium', results['end_repository'])
+    self.assertEqual('efgh5678', results['end_git_hash'])
+    self.assertEqual('1', results['auto_explore'])
+    self.assertEqual(1, results['bug_id'])
+    self.assertEqual(
+        [{'key': 'foo', 'value': 'mac_dimensions'}],
+        json.loads(results['dimensions']))
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_IsolateTarget_WebviewTelemetry(self):
+    params = {
+        'test_path': 'ChromiumPerf/android-webview-nexus5x/system_health/foo',
+        'start_git_hash': 'abcd1234',
+        'end_git_hash': 'efgh5678',
+        'start_repository': 'chromium',
+        'end_repository': 'chromium',
+        'bug_id': 1
+    }
+    results = pinpoint_request.PinpointParamsFromBisectParams(params)
+
+    self.assertEqual('telemetry_perf_webview_tests', results['target'])
+    self.assertEqual('foo@chromium.org', results['email'])
+    self.assertEqual('chromium', results['start_repository'])
+    self.assertEqual('abcd1234', results['start_git_hash'])
+    self.assertEqual('chromium', results['end_repository'])
+    self.assertEqual('efgh5678', results['end_git_hash'])
+    self.assertEqual('1', results['auto_explore'])
+    self.assertEqual(1, results['bug_id'])
+    self.assertEqual(
+        [{'key': 'foo', 'value': 'android_dimensions'}],
+        json.loads(results['dimensions']))
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_StartRepositoryInvalid_RaisesError(self):
+    params = {
+        'test_path': 'ChromiumPerf/android-webview-nexus5x/system_health/foo',
+        'start_git_hash': 'abcd1234',
+        'end_git_hash': 'efgh5678',
+        'start_repository': 'foo',
+        'end_repository': 'chromium',
+    }
+    with self.assertRaises(pinpoint_request.InvalidParamsError):
+      pinpoint_request.PinpointParamsFromBisectParams(params)
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_StartRepositoryNoChromium_RaisesError(self):
+    params = {
+        'test_path': 'ChromiumPerf/android-webview-nexus5x/system_health/foo',
+        'start_git_hash': 'abcd1234',
+        'end_git_hash': 'efgh5678',
+        'start_repository': 'v8',
+        'end_repository': 'chromium',
+    }
+    with self.assertRaises(pinpoint_request.InvalidParamsError):
+      pinpoint_request.PinpointParamsFromBisectParams(params)
+
+  @mock.patch.object(
+      utils, 'IsValidSheriffUser', mock.MagicMock(return_value=True))
+  def testPinpointParams_EndRepositoryNoChromium_RaisesError(self):
+    params = {
+        'test_path': 'ChromiumPerf/android-webview-nexus5x/system_health/foo',
+        'start_git_hash': 'abcd1234',
+        'end_git_hash': 'efgh5678',
+        'start_repository': 'chromium',
+        'end_repository': 'v8',
+    }
+    with self.assertRaises(pinpoint_request.InvalidParamsError):
+      pinpoint_request.PinpointParamsFromBisectParams(params)
