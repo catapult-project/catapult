e67a5c2f26288b4d2b1790bdf3ea7353ba2a2d1a
Pavel Feldman <pfeldman@chromium.org>
2017-08-03 13:09:46 -0700

    Take non-default browser target location into account when opening remote debugging connection.

--- telemetry/telemetry/internal/backends/chrome_inspector/system_info_backend.py
+++ telemetry/telemetry/internal/backends/chrome_inspector/system_info_backend.py
@@ -8,15 +8,14 @@ from py_utils import camel_case
 
 
 class SystemInfoBackend(object):
-  def __init__(self, devtools_port):
-    self._port = devtools_port
+  def __init__(self, browser_target_ws):
+    self._browser_target_ws = browser_target_ws
 
   def GetSystemInfo(self, timeout=10):
     req = {'method': 'SystemInfo.getInfo'}
     websocket = inspector_websocket.InspectorWebsocket()
     try:
-      websocket.Connect('ws://127.0.0.1:%i/devtools/browser' % self._port,
-                        timeout)
+      websocket.Connect(self._browser_target_ws, timeout)
       res = websocket.SyncRequest(req, timeout)
     finally:
       websocket.Disconnect()
