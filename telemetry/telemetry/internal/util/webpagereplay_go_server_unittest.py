33a9271eb3cf5caf925293ec6a4b47c94f1ac968
achuith <achuith@chromium.org>
2017-08-04 18:09:20 -0700

    Disable testSmokeStartingWebPageReplayGoServer on chromeos.
    Error:
    NoPathFoundError: No file could be found locally, and no file to
    download from cloud storage for example_domain_wpr_go_archive on
    platform chromeos_x86_64
    
    BUG=chromium:750323
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2985313002

--- telemetry/telemetry/internal/util/webpagereplay_go_server_unittest.py
+++ telemetry/telemetry/internal/util/webpagereplay_go_server_unittest.py
@@ -7,6 +7,7 @@ import urllib2
 
 import py_utils
 
+from telemetry import decorators
 from telemetry.internal.util import binary_manager
 from telemetry.internal.util import webpagereplay_go_server
 
@@ -19,6 +20,7 @@ class WebPageReplayGoServerTest(unittest.TestCase):
         py_utils.GetHostArchName(),
         py_utils.GetHostOsName())
 
+  @decorators.Disabled('chromeos')  # crbug.com/750323
   def testSmokeStartingWebPageReplayGoServer(self):
     with webpagereplay_go_server.ReplayServer(
         self.archive_path, replay_host='127.0.0.1', http_port=0, https_port=0,
