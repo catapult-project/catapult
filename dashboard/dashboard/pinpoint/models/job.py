505e9ec415f84c240d197b363d0dc2244a22b912
dtu <dtu@chromium.org>
2017-09-11 14:51:48 -0700

    [pinpoint] Increase significance level.
    With a separate Swarming task for each test run, we may reduce the inter-task
    variability, but increase the intra-task variability. This reduces the
    sensitivity of the MWU test, so bump up the significance level.
    
    In the future, I think we want to hard-code the significance level at 0.001,
    but increase the number of repeats for regressions that are smaller compared
    to the noise. We can also do more repeats if some of the tests fail.
    
    Review-Url: https://chromiumcodereview.appspot.com/3007393002

--- dashboard/dashboard/pinpoint/models/job.py
+++ dashboard/dashboard/pinpoint/models/job.py
@@ -23,8 +23,8 @@ from dashboard.services import issue_tracker_service
 _TASK_INTERVAL = 10
 
 
-_DEFAULT_REPEAT_COUNT = 10
-_SIGNIFICANCE_LEVEL = 0.0005
+_DEFAULT_REPEAT_COUNT = 12
+_SIGNIFICANCE_LEVEL = 0.001
 
 
 _DIFFERENT = 'different'
