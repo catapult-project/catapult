0e1491041002bd91ef4ab1dc8da5f9d7412f754b
Walter Korman <wkorman@chromium.org>
2017-10-17 12:34:26 -0700

    Fetch external images after processing DOM.
    Bug: chromium:762705
    Change-Id: I0e6769fba105c734dea0316a1c0b60e1991d8297
    Reviewed-on: https://chromium-review.googlesource.com/721725
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Walter Korman <wkorman@chromium.org>

--- telemetry/telemetry/internal/snap_page_util_unittest.py
+++ telemetry/telemetry/internal/snap_page_util_unittest.py
@@ -2,15 +2,24 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import hashlib
+import mock
 import os
-import StringIO
+import shutil
+import tempfile
 import unittest
+import StringIO
 
 from telemetry.internal import snap_page_util
 from telemetry.testing import options_for_unittests
 from telemetry.testing import tab_test_case
 from telemetry.internal.browser import browser_finder
 from telemetry.internal.util import path
+
+
+class FakeResponse(object):
+  def __init__(self):
+    self.content = None
 
 
 class SnapPageTest(unittest.TestCase):
@@ -24,9 +33,9 @@ class SnapPageTest(unittest.TestCase):
     self.platform.network_controller.Close()
 
   def _SnapWithDummyValuesExceptPath(self, snapshot_path):
-    snap_page_util.SnapPage(
-        self.finder_options, 'url', interactive=False,
-        snapshot_path=snapshot_path, enable_browser_log=False)
+    snap_page_util.SnapPage(self.finder_options, 'url', interactive=False,
+                            snapshot_path=snapshot_path,
+                            enable_browser_log=False)
 
   def testSnappingToInvalidSnapshotPath(self):
     with self.assertRaises(ValueError):
@@ -45,14 +54,61 @@ class SnapPageTest(unittest.TestCase):
     html_file_path = os.path.join(path.GetUnittestDataDir(), 'green_rect.html')
     url = self.platform.http_server.UrlOf(html_file_path)
     outfile = StringIO.StringIO()
-    snap_page_util._SnapPageToFile(
-        self.finder_options, url, interactive=False, snapshot_file=outfile,
-        enable_browser_log=False)
-    self.assertIn('id="green"', outfile.getvalue())
+    test_dir = tempfile.mkdtemp()
+    try:
+      snap_page_util._SnapPageToFile(
+          self.finder_options, url, interactive=False,
+          snapshot_path=os.path.join(test_dir, 'page.html'),
+          snapshot_file=outfile, enable_browser_log=False)
+      self.assertIn('id="green"', outfile.getvalue())
+    finally:
+      shutil.rmtree(test_dir)
+
+  @mock.patch('requests.get')
+  def testSnappingPageWithImage(self, mockGet):
+    test_dir = tempfile.mkdtemp()
+    try:
+      src_html_filename = 'image.html'
+      dest_html_path = os.path.join(test_dir, src_html_filename)
+      shutil.copyfile(os.path.join(path.GetUnittestDataDir(),
+                                   src_html_filename),
+                      dest_html_path)
+      self.platform.SetHTTPServerDirectories(path.GetUnittestDataDir())
+      url = self.platform.http_server.UrlOf(
+          os.path.join(path.GetUnittestDataDir(), src_html_filename))
+      outfile = StringIO.StringIO()
+
+      # Load the test image file's content so that we can return it
+      # from the mocked url request as if we'd actually fetched the
+      # image from an external source.
+      request_response = FakeResponse()
+      expected_image_path = os.path.join(path.GetUnittestDataDir(), 'image.png')
+      with open(expected_image_path, 'rb') as image_file:
+        request_response.content = image_file.read()
+
+      # Mock out the external image url fetch to return the loaded
+      # test image content.
+      mockGet.return_value = request_response
+
+      snap_page_util._SnapPageToFile(
+          self.finder_options, url, interactive=False,
+          snapshot_path=os.path.join(test_dir, src_html_filename),
+          snapshot_file=outfile, enable_browser_log=False)
+
+      self.assertIn('id="target"', outfile.getvalue())
+
+      # Validate that the 'fetched' image was written to the
+      # destination local image path.
+      expected_fetched_md5 = hashlib.md5(request_response.content).hexdigest()
+      actual_fetched_md5 = None
+      with open(os.path.join(test_dir, 'image', '0-target.png'), 'rb') as f:
+        actual_fetched_md5 = hashlib.md5(f.read()).hexdigest()
+      self.assertEqual(expected_fetched_md5, actual_fetched_md5)
+    finally:
+      shutil.rmtree(test_dir)
 
 
 class JSONTransmissionTest(tab_test_case.TabTestCase):
-
   def testTransmittingLargeObject(self):
     # Create a large array of 1 million elements
     json_obj = [1] * 1000000
