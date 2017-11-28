cbba42a8e6eb4e5f35b02a1e1611e515e7f5013f
Juan Antonio Navarro Perez <perezju@google.com>
2017-11-28 12:12:07 +0000

    [py_utils] Log and skip through atexit_with_log exceptions
    Code running during atexit should be considered running on a best-effort
    basis, so it's ok to skip through any exceptions (while still logging
    them).
    
    Bug: chromium:789119
    Change-Id: I1451ef7f1e5b2d4e79bef3710499d1b6ada19f08
    Reviewed-on: https://chromium-review.googlesource.com/793049
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>

--- common/py_utils/py_utils/atexit_with_log.py
+++ common/py_utils/py_utils/atexit_with_log.py
@@ -5,12 +5,17 @@
 import atexit
 import logging
 
+
 def _WrapFunction(function):
   def _WrappedFn(*args, **kwargs):
     logging.debug('Try running %s', repr(function))
-    function(*args, **kwargs)
-    logging.debug('Did run %s', repr(function))
+    try:
+      function(*args, **kwargs)
+      logging.debug('Did run %s', repr(function))
+    except Exception:  # pylint: disable=broad-except
+      logging.exception('Exception running %s', repr(function))
   return _WrappedFn
+
 
 def Register(function, *args, **kwargs):
   atexit.register(_WrapFunction(function), *args, **kwargs)
