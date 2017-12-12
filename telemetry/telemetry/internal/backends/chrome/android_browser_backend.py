fbee1329c2dae184da1997a6998e9187a563728f
rnephew <rnephew@chromium.org>
2017-12-12 13:21:03 -0800

    [Telemetry] Fix not importing device_errors in android_browser_backend
    Causing other CLs to fail presubmit:
    https://build.chromium.org/p/tryserver.client.catapult/builders/Catapult%20Presubmit/builds/10345/steps/presubmit/logs/stdio
    Change-Id: I53fea351a9d09cb7b53b253c3a6168d8cc350e39
    Reviewed-on: https://chromium-review.googlesource.com/822772
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>

--- telemetry/telemetry/internal/backends/chrome/android_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/android_browser_backend.py
@@ -13,6 +13,7 @@ from telemetry.internal.backends.chrome import chrome_browser_backend
 from telemetry.internal.browser import user_agent
 
 from devil.android import app_ui
+from devil.android import device_errors
 from devil.android import device_signal
 from devil.android import flag_changer
 from devil.android.sdk import intent
