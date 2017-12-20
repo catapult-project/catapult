b83a27983a06bea1f8a94ad8b3b56c2ef6279d1b
David Bokan <bokan@chromium.org>
2017-12-20 16:45:45 -0500

    Make pinch actions take speed in viewport coords
    I was a little over-zealous in
    https://crrev.com/dbab215523c8d77a7b54fcf3776357b079c36899. I made the
    speed parameter in PinchAction be in CSS pixels. This makes sense for
    scrolls and other gestures but for pinch it means that the speed of the
    zoom depends on how far the page is currently zoomed in. This is never
    what you want for pinch-zoom.
    
    I've updated pinch.js to keep the speed invariant across page scale
    factors. I've also added a test for this and other pinch zoom aspects
    since we currently have poor test coverage.
    
    Bug: chromium:610021
    Change-Id: I7008cb6c90aa4b9a610abd2c322c88281aa97f87
    Reviewed-on: https://chromium-review.googlesource.com/783823
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: David Bokan <bokan@chromium.org>

--- telemetry/telemetry/internal/actions/pinch_unittest.py
+++ telemetry/telemetry/internal/actions/pinch_unittest.py
@@ -2,18 +2,23 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import time
+
 from telemetry import decorators
 from telemetry.internal.actions import page_action
+from telemetry.internal.actions import pinch
+from telemetry.internal.actions import utils
 from telemetry.testing import tab_test_case
 
 
 class PinchActionTest(tab_test_case.TabTestCase):
   def setUp(self):
     super(PinchActionTest, self).setUp()
+    self.Navigate('zoom.html')
+    utils.InjectJavaScript(self._tab, 'gesture_common.js')
 
   @decorators.Disabled('android')  # crbug.com/796615
   def testPinchByApiCalledWithCorrectArguments(self):
-    self.Navigate('blank.html')
     if not page_action.IsGestureSourceTypeSupported(self._tab, 'touch'):
       return
 
@@ -39,3 +44,126 @@ class PinchActionTest(tab_test_case.TabTestCase):
         action_runner.EvaluateJavaScript('!!window.__test_callback'))
     self.assertEqual(
         800, action_runner.EvaluateJavaScript('window.__test_speed'))
+
+  def testPinchSpeed(self):
+    # Testing actual speed is tricky since we need details about the device's
+    # gesture recognizer. Instead, we'll just make sure that the speed is
+    # invariant under zoom.
+
+    startTime = time.time()
+
+    i = pinch.PinchAction(
+        scale_factor=4,
+        speed_in_pixels_per_second=500)
+    i.WillRunAction(self._tab)
+    i.RunAction(self._tab)
+
+    deltaSeconds = time.time() - startTime
+
+    startTime = time.time()
+
+    i = pinch.PinchAction(
+        scale_factor=0.25,
+        speed_in_pixels_per_second=500)
+    i.WillRunAction(self._tab)
+    i.RunAction(self._tab)
+
+    newDeltaSeconds = time.time() - startTime
+
+    # Timing will always be a bit fuzzy. Additionally, slop affects the
+    # distance differently when zooming in and out (the gesture scales faster
+    # per pixel when close to the anchor) so we'll get some difference from
+    # that too. So just make sure we're in the same ballpark.
+    # TODO(bokan): I landed changes in M65 (https://crrev.com/c/784213) that
+    # should make this significantly better. We can tighten up the bounds once
+    # that's in ref builds.
+    self.assertAlmostEqual(
+        newDeltaSeconds, deltaSeconds, delta=deltaSeconds / 2)
+
+  # TODO(bokan): It looks like pinch gestures don't quite work correctly on
+  # desktop. Disable for now and investigate later. https://crbug.com/787615
+  @decorators.Disabled('linux', 'mac', 'win')
+  def testPinchScale(self):
+    starting_scale = self._tab.EvaluateJavaScript('window.visualViewport.scale')
+
+    i = pinch.PinchAction(
+        scale_factor=4,
+        speed_in_pixels_per_second=500)
+    i.WillRunAction(self._tab)
+    i.RunAction(self._tab)
+
+    scale = self._tab.EvaluateJavaScript('window.visualViewport.scale')
+
+    self.assertAlmostEqual(scale, starting_scale * 4, delta=0.2 * scale)
+
+    starting_scale = scale
+
+    i = pinch.PinchAction(
+        scale_factor=0.5,
+        speed_in_pixels_per_second=500)
+    i.WillRunAction(self._tab)
+    i.RunAction(self._tab)
+
+    scale = self._tab.EvaluateJavaScript('window.visualViewport.scale')
+
+    # Note: we have to use an approximate equality here. The pinch gestures
+    # aren't exact. Some investigation long ago showed that the touch slop
+    # distance in Android's gesture recognizer violated some assumptions made
+    # by the synthetic gestures. There's (a bit of) context in
+    # https://crbug.com/686390.
+    # TODO(bokan): I landed changes in M65 (https://crrev.com/c/784213) that
+    # should make this significantly better. We can tighten up the bounds once
+    # that's in ref builds.
+    self.assertAlmostEqual(scale, starting_scale / 2, delta=0.2 * scale)
+
+  # Test that the anchor ratio correctly centers the pinch gesture at the
+  # requested part of the viewport.
+  # TODO(bokan): It looks like pinch gestures don't quite work correctly on
+  # desktop. Disable for now and investigate later. https://crbug.com/787615
+  @decorators.Disabled('linux', 'mac', 'win')
+  def testPinchAnchor(self):
+    starting_scale = self._tab.EvaluateJavaScript('window.visualViewport.scale')
+    self.assertEquals(1, starting_scale)
+
+    width = self._tab.EvaluateJavaScript('window.innerWidth')
+    height = self._tab.EvaluateJavaScript('window.innerHeight')
+
+    # Pinch zoom into the bottom right corner. Note, we can't go exactly to the
+    # corner since the starting touch point would be outside the screen.
+    i = pinch.PinchAction(
+        left_anchor_ratio=0.9,
+        top_anchor_ratio=0.9,
+        scale_factor=2.5,
+        speed_in_pixels_per_second=500)
+    i.WillRunAction(self._tab)
+    i.RunAction(self._tab)
+
+    offset_x = self._tab.EvaluateJavaScript('window.visualViewport.offsetLeft')
+    offset_y = self._tab.EvaluateJavaScript('window.visualViewport.offsetTop')
+
+    self.assertGreater(offset_x, width / 2)
+    self.assertGreater(offset_y, height / 2)
+
+    # TODO(bokan): This early-out can be removed once setPageScaleFactor (M64)
+    # rolls into the ref builds.
+    if not self._tab.EvaluateJavaScript(
+        "'setPageScaleFactor' in chrome.gpuBenchmarking"):
+      return
+
+    self._tab.EvaluateJavaScript(
+        'chrome.gpuBenchmarking.setPageScaleFactor(1)')
+
+    # Try again in the top left.
+    i = pinch.PinchAction(
+        left_anchor_ratio=0.1,
+        top_anchor_ratio=0.1,
+        scale_factor=2.5,
+        speed_in_pixels_per_second=500)
+    i.WillRunAction(self._tab)
+    i.RunAction(self._tab)
+
+    offset_x = self._tab.EvaluateJavaScript('window.visualViewport.offsetLeft')
+    offset_y = self._tab.EvaluateJavaScript('window.visualViewport.offsetTop')
+
+    self.assertLess(offset_x + width / 2.5, width / 2)
+    self.assertLess(offset_y + height / 2.5, height / 2)
