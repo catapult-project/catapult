f1d0d2bec8962982de4dd66c6a1391ce529a3449
Juan Antonio Navarro Perez <perezju@google.com>
2018-01-17 11:30:30 +0000

    [Telemetry] Migrate extension_unittest to new browser API
    For simplicity, use a context manager to manage the lifetime of the
    browser.
    
    Bug: chromium:801578
    Change-Id: I579d81139be2c376f311824b10d26b4d55534851
    Reviewed-on: https://chromium-review.googlesource.com/867914
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>

--- telemetry/telemetry/internal/browser/extension_unittest.py
+++ telemetry/telemetry/internal/browser/extension_unittest.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
+import contextlib
 import os
 import shutil
 import tempfile
@@ -17,151 +17,122 @@ from telemetry.testing import options_for_unittests
 
 class ExtensionTest(unittest.TestCase):
   def setUp(self):
-    self._browser = None
-    self._platform = None
-    self._extension = None
-    self._extension_id = None
+    self._options = options_for_unittests.GetCopy()
 
-  def CreateBrowserWithExtension(self, ext_path):
+  def GetExtensionToLoad(self, ext_path):
     extension_path = os.path.join(util.GetUnittestDataDir(), ext_path)
-    options = options_for_unittests.GetCopy()
-    load_extension = extension_to_load.ExtensionToLoad(
-        extension_path, options.browser_type)
-    options.browser_options.extensions_to_load = [load_extension]
-    browser_to_create = browser_finder.FindBrowser(options)
+    return extension_to_load.ExtensionToLoad(
+        extension_path, self._options.browser_type)
 
+  @contextlib.contextmanager
+  def CreateBrowser(self, extensions_to_load):
+    self._options.browser_options.extensions_to_load = extensions_to_load
+    browser_to_create = browser_finder.FindBrowser(self._options)
     if not browser_to_create:
-      # May not find a browser that supports extensions.
-      return False
-    self._platform = browser_to_create.platform
-    self._platform.network_controller.Open()
-    self._browser = browser_to_create.Create(options)
-    self._extension = self._browser.extensions[load_extension]
-    self._extension_id = load_extension.extension_id
-    self.assertTrue(self._extension)
-    return True
+      self.skipTest("Did not find a browser that supports extensions")
 
-  def tearDown(self):
-    if self._browser:
-      self._browser.Close()
-      self._platform.network_controller.Close()
+    platform = browser_to_create.platform
+    try:
+      platform.network_controller.Open()
+      with browser_to_create.BrowserSession(
+          self._options.browser_options) as browser:
+        yield browser
+    finally:
+      platform.network_controller.Close()
+
+  @contextlib.contextmanager
+  def CreateBrowserWithExtension(self, ext_path):
+    load_extension = self.GetExtensionToLoad(ext_path)
+    with self.CreateBrowser([load_extension]) as browser:
+      extension = browser.extensions[load_extension]
+      self.assertTrue(extension)
+      self.assertEqual(extension.extension_id, load_extension.extension_id)
+      yield browser, extension
 
   def testExtensionBasic(self):
     """Test ExtensionPage's ExecuteJavaScript and EvaluateJavaScript."""
-    if not self.CreateBrowserWithExtension('simple_extension'):
-      logging.warning('Did not find a browser that supports extensions, '
-                      'skipping test.')
-      return
-    self.assertTrue(
-        self._extension.EvaluateJavaScript('chrome.runtime != null'))
-    self._extension.ExecuteJavaScript('setTestVar("abcdef")')
-    self.assertEquals('abcdef',
-                      self._extension.EvaluateJavaScript('_testVar'))
+    with self.CreateBrowserWithExtension('simple_extension') as (_, extension):
+      self.assertTrue(
+          extension.EvaluateJavaScript('chrome.runtime != null'))
+      extension.ExecuteJavaScript('setTestVar("abcdef")')
+      self.assertEquals('abcdef',
+                        extension.EvaluateJavaScript('_testVar'))
 
   def testExtensionGetByExtensionId(self):
     """Test GetByExtensionId for a simple extension with a background page."""
-    if not self.CreateBrowserWithExtension('simple_extension'):
-      logging.warning('Did not find a browser that supports extensions, '
-                      'skipping test.')
-      return
-    ext = self._browser.extensions.GetByExtensionId(self._extension_id)
-    self.assertEqual(1, len(ext))
-    self.assertEqual(ext[0], self._extension)
-    self.assertTrue(
-        ext[0].EvaluateJavaScript('chrome.runtime != null'))
+    with self.CreateBrowserWithExtension('simple_extension') as (
+        browser, extension):
+      extensions = browser.extensions.GetByExtensionId(extension.extension_id)
+      self.assertEqual(1, len(extensions))
+      self.assertEqual(extensions[0], extension)
+      self.assertTrue(
+          extensions[0].EvaluateJavaScript('chrome.runtime != null'))
 
   @decorators.Disabled('mac')
   def testWebApp(self):
     """Tests GetByExtensionId for a web app with multiple pages."""
-    if not self.CreateBrowserWithExtension('simple_app'):
-      logging.warning('Did not find a browser that supports extensions, '
-                      'skipping test.')
-      return
-    extensions = self._browser.extensions.GetByExtensionId(self._extension_id)
-    extension_urls = set([ext.EvaluateJavaScript('location.href;')
-                          for ext in extensions])
-    expected_urls = set(['chrome-extension://' + self._extension_id + '/' + path
-                         for path in ['main.html', 'second.html',
-                                      '_generated_background_page.html']])
+    with self.CreateBrowserWithExtension('simple_app') as (browser, extension):
+      extensions = browser.extensions.GetByExtensionId(extension.extension_id)
+      extension_urls = set([ext.EvaluateJavaScript('location.href;')
+                            for ext in extensions])
+      expected_urls = set([
+          'chrome-extension://' + extension.extension_id + '/' + path
+          for path in ['main.html', 'second.html',
+                       '_generated_background_page.html']])
+      self.assertEqual(expected_urls, extension_urls)
 
-    self.assertEqual(expected_urls, extension_urls)
 
-class NonExistentExtensionTest(unittest.TestCase):
+class NonExistentExtensionTest(ExtensionTest):
   def testNonExistentExtensionPath(self):
     """Test that a non-existent extension path will raise an exception."""
-    extension_path = os.path.join(util.GetUnittestDataDir(), 'foo')
-    options = options_for_unittests.GetCopy()
-    self.assertRaises(extension_to_load.ExtensionPathNonExistentException,
-                      lambda: extension_to_load.ExtensionToLoad(
-                          extension_path, options.browser_type))
+    with self.assertRaises(extension_to_load.ExtensionPathNonExistentException):
+      self.GetExtensionToLoad('foo')
 
   def testExtensionNotLoaded(self):
-    """Querying an extension that was not loaded will return None"""
-    extension_path = os.path.join(util.GetUnittestDataDir(), 'simple_extension')
-    options = options_for_unittests.GetCopy()
-    load_extension = extension_to_load.ExtensionToLoad(
-        extension_path, options.browser_type)
-    browser_to_create = browser_finder.FindBrowser(options)
-    try:
-      browser_to_create.platform.network_controller.Open()
-      with browser_to_create.Create(options) as b:
-        if b.supports_extensions:
-          self.assertRaises(KeyError, lambda: b.extensions[load_extension])
-    finally:
-      browser_to_create.platform.network_controller.Close()
+    """Querying an extension that was not loaded will raise an exception"""
+    load_extension = self.GetExtensionToLoad('simple_extension')
+    with self.CreateBrowser([]) as browser:
+      if browser.supports_extensions:
+        with self.assertRaises(KeyError):
+          # pylint: disable=pointless-statement
+          browser.extensions[load_extension]
 
-class MultipleExtensionTest(unittest.TestCase):
-  def setUp(self):
+
+class MultipleExtensionTest(ExtensionTest):
+  @contextlib.contextmanager
+  def ClonedExtensions(self):
     """ Copy the manifest and background.js files of simple_extension to a
     number of temporary directories to load as extensions"""
-    self._extension_dirs = [tempfile.mkdtemp() for _ in range(3)]
-    src_extension_dir = os.path.join(
-        util.GetUnittestDataDir(), 'simple_extension')
-    manifest_path = os.path.join(src_extension_dir, 'manifest.json')
-    script_path = os.path.join(src_extension_dir, 'background.js')
-    for d in self._extension_dirs:
-      shutil.copy(manifest_path, d)
-      shutil.copy(script_path, d)
-    options = options_for_unittests.GetCopy()
-    self._extensions_to_load = [
-        extension_to_load.ExtensionToLoad(d, options.browser_type)
-        for d in self._extension_dirs
-    ]
-    options.browser_options.extensions_to_load = self._extensions_to_load
-    browser_to_create = browser_finder.FindBrowser(options)
-    self._platform = None
-    self._browser = None
-    # May not find a browser that supports extensions.
-    if browser_to_create:
-      self._platform = browser_to_create.platform
-      self._platform.network_controller.Open()
-      self._browser = browser_to_create.Create(options)
+    extension_dirs = [tempfile.mkdtemp() for _ in range(3)]
+    try:
+      src_dir = os.path.join(util.GetUnittestDataDir(), 'simple_extension')
+      manifest_path = os.path.join(src_dir, 'manifest.json')
+      script_path = os.path.join(src_dir, 'background.js')
+      for tmp_dir in extension_dirs:
+        shutil.copy(manifest_path, tmp_dir)
+        shutil.copy(script_path, tmp_dir)
 
-  def tearDown(self):
-    if self._platform:
-      self._platform.network_controller.Close()
-    if self._browser:
-      self._browser.Close()
-    for d in self._extension_dirs:
-      shutil.rmtree(d)
+      yield [
+          extension_to_load.ExtensionToLoad(d, self._options.browser_type)
+          for d in extension_dirs]
+    finally:
+      for tmp_dir in extension_dirs:
+        shutil.rmtree(tmp_dir)
 
   def testMultipleExtensions(self):
-    if not self._browser:
-      logging.warning('Did not find a browser that supports extensions, '
-                      'skipping test.')
-      return
-
-    # Test contains.
-    loaded_extensions = [e for e in self._extensions_to_load
-                         if e in self._browser.extensions]
-    self.assertEqual(len(loaded_extensions), len(self._extensions_to_load))
-    for load_extension in self._extensions_to_load:
-      extension = self._browser.extensions[load_extension]
-      assert extension
-      self.assertTrue(
-          extension.EvaluateJavaScript('chrome.runtime != null'))
-      extension.ExecuteJavaScript('setTestVar("abcdef")')
-      self.assertEquals('abcdef', extension.EvaluateJavaScript('_testVar'))
+    with self.ClonedExtensions() as extensions_to_load:
+      with self.CreateBrowser(extensions_to_load) as browser:
+        # Test contains.
+        loaded_extensions = [e for e in extensions_to_load
+                             if e in browser.extensions]
+        self.assertEqual(len(loaded_extensions), len(extensions_to_load))
+        for load_extension in extensions_to_load:
+          extension = browser.extensions[load_extension]
+          self.assertTrue(extension)
+          self.assertTrue(
+              extension.EvaluateJavaScript('chrome.runtime != null'))
+          extension.ExecuteJavaScript('setTestVar("abcdef")')
+          self.assertEquals('abcdef', extension.EvaluateJavaScript('_testVar'))
 
 
 class WebviewInExtensionTest(ExtensionTest):
@@ -172,23 +143,19 @@ class WebviewInExtensionTest(ExtensionTest):
   @decorators.Disabled('win', 'linux', 'mac', 'chromeos')
   def testWebviewInExtension(self):
     """Tests GetWebviewContext() for a web app containing <webview> element."""
-    if not self.CreateBrowserWithExtension('webview_app'):
-      logging.warning('Did not find a browser that supports extensions, '
-                      'skipping test.')
-      return
-
-    self._browser.extensions.GetByExtensionId(self._extension_id)
-    webview_contexts = self._extension.GetWebviewContexts()
-    self.assertEquals(1, len(webview_contexts))
-    webview_context = webview_contexts[0]
-    webview_context.WaitForDocumentReadyStateToBeComplete()
-    # Check that the context has the right url from the <webview> element.
-    self.assertTrue(webview_context.GetUrl().startswith('data:'))
-    # Check |test_input_id| element is accessible from the webview context.
-    self.assertTrue(
-        webview_context.EvaluateJavaScript(
-            'document.getElementById("test_input_id") != null'))
-    # Check that |test_input_id| is not accessible from outside webview context
-    self.assertFalse(
-        self._extension.EvaluateJavaScript(
-            'document.getElementById("test_input_id") != null'))
+    with self.CreateBrowserWithExtension('webview_app') as (_, extension):
+      webview_contexts = extension.GetWebviewContexts()
+      self.assertEquals(1, len(webview_contexts))
+      webview_context = webview_contexts[0]
+      webview_context.WaitForDocumentReadyStateToBeComplete()
+      # Check that the context has the right url from the <webview> element.
+      self.assertTrue(webview_context.GetUrl().startswith('data:'))
+      # Check |test_input_id| element is accessible from the webview context.
+      self.assertTrue(
+          webview_context.EvaluateJavaScript(
+              'document.getElementById("test_input_id") != null'))
+      # Check that |test_input_id| is not accessible from outside of the
+      # webview context.
+      self.assertFalse(
+          extension.EvaluateJavaScript(
+              'document.getElementById("test_input_id") != null'))
