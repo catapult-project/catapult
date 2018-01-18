1e70b8208a77f9b1c918590b74462b8da9e7dbd0
Ned Nguyen <nednguyen@google.com>
2018-01-18 01:48:14 +0000

    Revert "[Telemetry] Migrate SeriallyExecutedBrowserTestCase to new API"
    This reverts commit 739be847e9987e0d069a60dd114abcdcd2ec5df1.
    
    Reason for revert: break catapult roll https://chromium-review.googlesource.com/c/chromium/src/+/871696
    
    Original change's description:
    > [Telemetry] Migrate SeriallyExecutedBrowserTestCase to new API
    > 
    > Within Start-/StopBrowser we add, as appropriate, calls to
    > SetUp-/CleanUpEnvironment.
    > 
    > Bug: chromium:801578
    > Change-Id: Ie332e95c042d3d46c1ff9aa6909040d827b8ae22
    > Reviewed-on: https://chromium-review.googlesource.com/870873
    > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    
    TBR=perezju@chromium.org,nednguyen@google.com
    
    Change-Id: Ia84f83a103c4f26d744ed48fda94d3b9d7c61091
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:801578
    Reviewed-on: https://chromium-review.googlesource.com/872291
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>

--- telemetry/telemetry/testing/serially_executed_browser_test_case.py
+++ telemetry/telemetry/testing/serially_executed_browser_test_case.py
@@ -105,25 +105,13 @@ class SeriallyExecutedBrowserTestCase(unittest.TestCase):
         'starting WPR')
     assert not cls.browser, 'Browser is started. Must close it first'
 
-    try:
-      # TODO(crbug.com/803104): Note cls._browser_options actually is a
-      # FinderOptions object, and we need to access the real browser_option's
-      # contained inside.
-      cls._browser_to_create.SetUpEnvironment(
-          cls._browser_options.browser_options)
-      cls.browser = cls._browser_to_create.Create()
-    except Exception:
-      cls._browser_options.CleanUpEnvironment()
-      raise
+    cls.browser = cls._browser_to_create.Create(cls._browser_options)
 
   @classmethod
   def StopBrowser(cls):
     assert cls.browser, 'Browser is not started'
-    try:
-      cls.browser.Close()
-      cls.browser = None
-    finally:
-      cls._browser_options.CleanUpEnvironment()
+    cls.browser.Close()
+    cls.browser = None
 
   @classmethod
   def TearDownProcess(cls):
