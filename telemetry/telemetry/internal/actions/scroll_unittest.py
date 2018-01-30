52dc3feb012cce1d690b12d0b137c6c9bd5a4ff1
Bret Sepulveda <bsep@chromium.org>
2018-01-29 17:06:16 -0800

    Disable ScrollActionTest.testWheelScrollDistanceWhileZoomed (flaky).
    Bug: chromium:805523
    Change-Id: If4a20b9e1b68e09eb840f3ce6e152a9776eb98ca
    Reviewed-on: https://chromium-review.googlesource.com/892322
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>

--- telemetry/telemetry/internal/actions/scroll_unittest.py
+++ telemetry/telemetry/internal/actions/scroll_unittest.py
@@ -114,6 +114,7 @@ class ScrollActionTest(tab_test_case.TabTestCase):
     self._RunScrollDistanceTest(
         1000, 300, page_action.GESTURE_SOURCE_MOUSE, 100)
 
+  @decorators.Disabled('chromeos', 'linux')  # crbug.com/805523
   def testWheelScrollDistanceWhileZoomed(self):
     # TODO(bokan): This API was added recently so only run the test once it's
     # available. Remove this check once it rolls into stable builds.
