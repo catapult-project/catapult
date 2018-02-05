b04cc95039057d394434debec0fc9c902863338b
Ethan Kuefner <eakuefner@chromium.org>
2018-02-05 10:01:53 -0800

    [Telemetry] Simplify and test HtmlOutputFormatter
    HtmlOutputFormatter duplicates a ton of code that is present in PageTestResults
    and run by StoryRunner nowadays. As well, it accesses PageTestResults.histograms
    directly, which is not necessary. This CL cleans up the duplicated code and
    switches HtmlOutputFormatter to call PageTestResults.AsHistogramDicts() instead.
    
    Bug: catapult:#4228
    Change-Id: I05599ce7c1ad3dfb7c63f5f935f62ec24884009e
    Reviewed-on: https://chromium-review.googlesource.com/902163
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ethan Kuefner <eakuefner@chromium.org>

--- telemetry/telemetry/internal/results/html_output_formatter.py
+++ telemetry/telemetry/internal/results/html_output_formatter.py
@@ -3,18 +3,13 @@
 # found in the LICENSE file.
 
 import datetime
-import json
 import logging
 import os
-import tempfile
 
 from py_utils import cloud_storage
 
-from telemetry.internal.results import chart_json_output_formatter
 from telemetry.internal.results import output_formatter
 
-from tracing.value import convert_chart_json
-from tracing.value import histogram_set
 from tracing_build import vulcanize_histograms_viewer
 
 
@@ -26,39 +21,13 @@ class HtmlOutputFormatter(output_formatter.OutputFormatter):
     self._upload_bucket = upload_bucket
     self._reset_results = reset_results
 
-  def _ConvertChartJson(self, page_test_results):
-    chart_json = chart_json_output_formatter.ResultsAsChartDict(
-        self._metadata, page_test_results.all_page_specific_values,
-        page_test_results.all_summary_values)
-    info = page_test_results.telemetry_info
-    chart_json['label'] = info.label
-    chart_json['benchmarkStartMs'] = info.benchmark_start_epoch * 1000.0
-
-    file_descriptor, chart_json_path = tempfile.mkstemp()
-    os.close(file_descriptor)
-    json.dump(chart_json, file(chart_json_path, 'w'))
-
-    vinn_result = convert_chart_json.ConvertChartJson(chart_json_path)
-
-    os.remove(chart_json_path)
-
-    if vinn_result.returncode != 0:
-      logging.error('Error converting chart json to Histograms:\n' +
-                    vinn_result.stdout)
-      return []
-    return json.loads(vinn_result.stdout)
-
   def Format(self, page_test_results):
-    histograms = page_test_results.histograms
-    if not histograms:
-      histograms = self._ConvertChartJson(page_test_results)
-    if isinstance(histograms, histogram_set.HistogramSet):
-      histograms = histograms.AsDicts()
+    histograms = page_test_results.AsHistogramDicts()
 
     vulcanize_histograms_viewer.VulcanizeAndRenderHistogramsViewer(
         histograms, self._output_stream, self._reset_results)
-    file_path = os.path.abspath(self._output_stream.name)
     if self._upload_bucket:
+      file_path = os.path.abspath(self._output_stream.name)
       remote_path = ('html-results/results-%s' %
                      datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
       try:
