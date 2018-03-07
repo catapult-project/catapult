6fbfa7cb20f44fdf0a0336137a33febc44f6fd84
Juan Antonio Navarro Pérez <perezju@chromium.org>
2018-03-07 10:01:24 +0000

    Revert "[Telemetry] Move apk finding code to Android backend settings"
    This reverts commit 5e6b5c9c68522f902b12289f20037d561764b5cb.
    
    Reason for revert: Blocking catapult roll.
    
    Original change's description:
    > [Telemetry] Move apk finding code to Android backend settings
    >
    > A first step towards encapsulating the logic to find and install apks
    > for individual Android browser variants, this CL introduces a
    > backend_settings.FindLocalApk() method.
    >
    > As a side effect of this change the logic of the corresponding browser
    > finder becomes cleaner and now, even if --chrome-root is not given,
    > allows to select any known browser which is already installed on the
    > device.
    >
    > We also take the chance to make _HaveLocalAPK and
    > _HaveWebViewEmbedderAPK private, as the logic to handle those will
    > be eventually moved to the banckend_settings as well.
    >
    > Bug: chromium:815133
    > Change-Id: I92315d5e169ed308ea685213203244ba4812c186
    > Reviewed-on: https://chromium-review.googlesource.com/934322
    > Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    
    TBR=perezju@chromium.org,nednguyen@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    NOTRY=True
    Bug: chromium:815133
    Change-Id: Iee8372c13a8f27ee83701c07cd0cdeec1c04aa75
    Reviewed-on: https://chromium-review.googlesource.com/951606
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>

--- telemetry/telemetry/internal/backends/android_browser_backend_settings.py
+++ telemetry/telemetry/internal/backends/android_browser_backend_settings.py
@@ -2,23 +2,18 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-
-from telemetry.core import util
-
 import py_utils
 
 
 class AndroidBrowserBackendSettings(object):
 
-  def __init__(self, package, activity, command_line_name, apk_name,
+  def __init__(self, package, activity, command_line_name,
                devtools_port=NotImplemented, supports_tab_control=True):
     self._package = package
     self._activity = activity
     self._command_line_name = command_line_name
     self._devtools_port = devtools_port
     self._supports_tab_control = supports_tab_control
-    self._apk_name = apk_name
 
   @property
   def package(self):
@@ -46,39 +41,14 @@ class AndroidBrowserBackendSettings(object):
     # By default return the devtools_port defined in the constructor.
     return self._devtools_port
 
-  def FindLocalApk(self, device, chrome_root):
-    # Subclasses may override this method to pick the correct apk based on
-    # specific device features.
-    del device  # Unused.
-    if self._apk_name is None:
-      return None
-    else:
-      return _FindLocalApk(chrome_root, self._apk_name)
 
-
-class ChromeBaseBackendSettings(AndroidBrowserBackendSettings):
-  def __init__(self, package, apk_name=None):
-    super(ChromeBaseBackendSettings, self).__init__(
-        package=package,
-        activity='com.google.android.apps.chrome.Main',
-        command_line_name='chrome-command-line',
-        devtools_port='localabstract:chrome_devtools_remote',
-        apk_name=apk_name)
-
-
-class ChromePublicBackendSettings(ChromeBaseBackendSettings):
-  def __init__(self, package):
-    super(ChromePublicBackendSettings, self).__init__(
-        package=package,
-        apk_name='ChromePublic.apk')
-
-
-class ChromeBackendSettings(ChromeBaseBackendSettings):
+class ChromeBackendSettings(AndroidBrowserBackendSettings):
   def __init__(self, package):
     super(ChromeBackendSettings, self).__init__(
         package=package,
-        # TODO(crbug.com/813047): Should override FindLocalApk instead.
-        apk_name='Chrome.apk')
+        activity='com.google.android.apps.chrome.Main',
+        command_line_name='chrome-command-line',
+        devtools_port='localabstract:chrome_devtools_remote')
 
 
 class ContentShellBackendSettings(AndroidBrowserBackendSettings):
@@ -88,24 +58,20 @@ class ContentShellBackendSettings(AndroidBrowserBackendSettings):
         activity='org.chromium.content_shell_apk.ContentShellActivity',
         command_line_name='content-shell-command-line',
         devtools_port='localabstract:content_shell_devtools_remote',
-        supports_tab_control=False,
-        apk_name='ContentShell.apk')
+        supports_tab_control=False)
 
 
 class WebviewBackendSettings(AndroidBrowserBackendSettings):
   def __init__(self,
                package,
                activity='org.chromium.webview_shell.TelemetryActivity',
-               command_line_name='webview-command-line',
-               apk_name='SystemWebView.apk'):
+               command_line_name='webview-command-line'):
     super(WebviewBackendSettings, self).__init__(
         package=package,
         activity=activity,
         command_line_name=command_line_name,
         devtools_port='localabstract:webview_devtools_remote_{pid}',
-        supports_tab_control=False,
-        # TODO(crbug.com/813047): Should override FindLocalApk instead.
-        apk_name=apk_name)
+        supports_tab_control=False)
 
   def GetDevtoolsRemotePort(self, device):
     # The DevTools socket name for WebView depends on the activity PID's.
@@ -121,20 +87,4 @@ class WebviewShellBackendSettings(WebviewBackendSettings):
     super(WebviewShellBackendSettings, self).__init__(
         package=package,
         activity='org.chromium.android_webview.shell.AwShellActivity',
-        command_line_name='android-webview-command-line',
-        apk_name='WebViewInstrumentation.apk')
-
-
-def _FindLocalApk(chrome_root, apk_name):
-  found_apk_path = None
-  found_last_changed = None
-  for build_path in util.GetBuildDirectories(chrome_root):
-    apk_path = os.path.join(build_path, 'apks', apk_name)
-    if os.path.exists(apk_path):
-      last_changed = os.path.getmtime(apk_path)
-      # Keep the most recently updated apk only.
-      if last_changed is None or last_changed > found_last_changed:
-        found_apk_path = apk_path
-        found_last_changed = last_changed
-
-  return found_apk_path
+        command_line_name='android-webview-command-line')
