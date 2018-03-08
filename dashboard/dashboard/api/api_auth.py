78d89b42c293bff9ae1fec4b849a8853e25f419b
Kendal Harland <kjharland@google.com>
2018-03-08 14:10:53 -0800

    [Dashboard] Whitelist Fuchsia garnet-ci dev acct
    Change-Id: I1d8cc61adebf1813627318828472d7b475b2b7f2
    Reviewed-on: https://chromium-review.googlesource.com/955926
    Commit-Queue: Kendal Harland <kjharland@google.com>
    Reviewed-by: Ethan Kuefner <eakuefner@chromium.org>

--- dashboard/dashboard/api/api_auth.py
+++ dashboard/dashboard/api/api_auth.py
@@ -22,7 +22,12 @@ OAUTH_CLIENT_ID_WHITELIST = [
     # This oauth client id is used to upload histograms from the perf waterfall.
     '113172445342431053212',
     'chromeperf@webrtc-perf-test.google.com.iam.gserviceaccount.com',
+    # This oauth client id is used to upload histograms when debugging Fuchsia
+    # locally (e.g. in a cron-job).
     'catapult-uploader@fuchsia-infra.iam.gserviceaccount.com',
+    # This oauth client id is used to upload histograms from Fuchsia dev
+    # builders.
+    'garnet-ci-builder-dev@fuchsia-infra.iam.gserviceaccount.com'
     # This oauth client id used to upload histograms from cronet bots.
     '113172445342431053212'
 ]
