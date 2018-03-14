6c4a8ca2e9bc7d1d8fc526efe3c2e8ce087b730c
Dave Tu <dtu@chromium.org>
2018-03-14 09:23:05 -0700

    [telemetry] Add results label to benchmark_total_duration histogram.
    Bug: catapult:#4271
    Change-Id: I0856a362968b0b199ece4eed03d321e15d57de0b
    Reviewed-on: https://chromium-review.googlesource.com/961594
    Commit-Queue: David Tu <dtu@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ethan Kuefner <eakuefner@chromium.org>

--- telemetry/telemetry/internal/results/page_test_results.py
+++ telemetry/telemetry/internal/results/page_test_results.py
@@ -28,6 +28,7 @@ from telemetry.value import trace
 from tracing.value import convert_chart_json
 from tracing.value import histogram
 from tracing.value import histogram_set
+from tracing.value.diagnostics import generic_set
 from tracing.value.diagnostics import reserved_infos
 
 class TelemetryInfo(object):
@@ -415,6 +416,10 @@ class PageTestResults(object):
     hist = histogram.Histogram(
         'benchmark_total_duration', 'ms_smallerIsBetter')
     hist.AddSample(duration_in_milliseconds)
+    # TODO(eakuefner): Add his diagnostic in AddHistogram().
+    # https://github.com/catapult-project/catapult/issues/4244
+    hist.diagnostics[reserved_infos.LABELS.name] = generic_set.GenericSet(
+        [self.telemetry_info.label])
     self._histograms.AddHistogram(hist)
 
   def AddHistogram(self, hist):
