265b640382d848b339831252aac1bceff2ca3e58
Simon <simonhatch@chromium.org>
2018-05-17 10:42:02 -0400

    Dashboard - Move some code to histogram_helpers
    Just trying to make /add_histograms_queue a bit clearer to read.
    
    TBR=eakuefner@chromium.org
    
    Bug: catapult:#4457
    Change-Id: I70284cc9ed9452f588a5d24433c3e1d4ad53b729
    Reviewed-on: https://chromium-review.googlesource.com/1064002
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Simon Hatch <simonhatch@chromium.org>

--- dashboard/dashboard/add_histograms_queue.py
+++ dashboard/dashboard/add_histograms_queue.py
@@ -6,7 +6,6 @@
 
 import json
 import logging
-import re
 import sys
 
 from google.appengine.ext import ndb
@@ -19,6 +18,7 @@ from dashboard import add_point_queue
 from dashboard import find_anomalies
 from dashboard import graph_revisions
 from dashboard.common import datastore_hooks
+from dashboard.common import histogram_helpers
 from dashboard.common import request_handler
 from dashboard.common import stored_object
 from dashboard.common import utils
@@ -46,79 +46,6 @@ DIAGNOSTIC_NAMES_TO_ANNOTATION_NAMES = {
     reserved_infos.FUCHSIA_TOPAZ_REVISIONS.name: 'r_fuchsia_topaz_git',
     reserved_infos.FUCHSIA_ZIRCON_REVISIONS.name: 'r_fuchsia_zircon_git'
 }
-
-
-# List of non-TBMv2 chromium.perf Telemetry benchmarks
-LEGACY_BENCHMARKS = [
-    'blink_perf.bindings',
-    'blink_perf.canvas',
-    'blink_perf.css',
-    'blink_perf.dom',
-    'blink_perf.events',
-    'blink_perf.image_decoder',
-    'blink_perf.layout',
-    'blink_perf.owp_storage',
-    'blink_perf.paint',
-    'blink_perf.parser',
-    'blink_perf.shadow_dom',
-    'blink_perf.svg',
-    'cronet_perf_tests',
-    'dromaeo',
-    'dummy_benchmark.noisy_benchmark_1',
-    'dummy_benchmark.stable_benchmark_1',
-    'jetstream',
-    'kraken',
-    'octane',
-    'rasterize_and_record_micro.partial_invalidation',
-    'rasterize_and_record_micro.top_25',
-    'scheduler.tough_scheduling_cases',
-    'smoothness.desktop_tough_pinch_zoom_cases',
-    'smoothness.gpu_rasterization.polymer',
-    'smoothness.gpu_rasterization.top_25_smooth',
-    'smoothness.gpu_rasterization.tough_filters_cases',
-    'smoothness.gpu_rasterization.tough_path_rendering_cases',
-    'smoothness.gpu_rasterization.tough_pinch_zoom_cases',
-    'smoothness.gpu_rasterization.tough_scrolling_cases',
-    'smoothness.gpu_rasterization_and_decoding.image_decoding_cases',
-    'smoothness.image_decoding_cases',
-    'smoothness.key_desktop_move_cases',
-    'smoothness.key_mobile_sites_smooth',
-    'smoothness.key_silk_cases',
-    'smoothness.maps',
-    'smoothness.pathological_mobile_sites',
-    'smoothness.simple_mobile_sites',
-    'smoothness.sync_scroll.key_mobile_sites_smooth',
-    'smoothness.top_25_smooth',
-    'smoothness.tough_ad_cases',
-    'smoothness.tough_animation_cases',
-    'smoothness.tough_canvas_cases',
-    'smoothness.tough_filters_cases',
-    'smoothness.tough_image_decode_cases',
-    'smoothness.tough_path_rendering_cases',
-    'smoothness.tough_pinch_zoom_cases',
-    'smoothness.tough_scrolling_cases',
-    'smoothness.tough_texture_upload_cases',
-    'smoothness.tough_webgl_ad_cases',
-    'smoothness.tough_webgl_cases',
-    'speedometer',
-    'speedometer-future',
-    'speedometer2',
-    'speedometer2-future',
-    'start_with_url.cold.startup_pages',
-    'start_with_url.warm.startup_pages',
-    'thread_times.key_hit_test_cases',
-    'thread_times.key_idle_power_cases',
-    'thread_times.key_mobile_sites_smooth',
-    'thread_times.key_noop_cases',
-    'thread_times.key_silk_cases',
-    'thread_times.simple_mobile_sites',
-    'thread_times.tough_compositor_cases',
-    'thread_times.tough_scrolling_cases',
-    'v8.detached_context_age_in_gc'
-]
-
-
-STATS_BLACKLIST = ['std', 'count', 'max', 'min', 'sum']
 
 
 class BadRequestError(Exception):
@@ -253,11 +180,12 @@ def _ProcessRowAndHistogram(params, bot_whitelist):
   legacy_parent_tests = {}
 
   # TODO(#4213): Stop doing this.
-  if benchmark_name in LEGACY_BENCHMARKS:
+  if histogram_helpers.IsLegacyBenchmark(benchmark_name):
     statistics_scalars = {}
 
   for stat_name, scalar in statistics_scalars.iteritems():
-    if _ShouldFilter(histogram_name, benchmark_name, stat_name):
+    if histogram_helpers.ShouldFilterStatistic(
+        histogram_name, benchmark_name, stat_name):
       continue
     extra_args = GetUnitArgs(scalar.unit)
     suffixed_name = '%s/%s_%s' % (
@@ -271,69 +199,6 @@ def _ProcessRowAndHistogram(params, bot_whitelist):
   return [
       _AddRowsFromData(params, revision, parent_test, legacy_parent_tests),
       _AddHistogramFromData(params, revision, test_key, internal_only)]
-
-
-def _ShouldFilter(test_name, benchmark_name, stat_name):
-  if test_name == 'benchmark_total_duration':
-    return True
-  if benchmark_name.startswith('memory') and not benchmark_name.startswith(
-      'memory.long_running'):
-    if 'memory:' in test_name and stat_name in STATS_BLACKLIST:
-      return True
-  if benchmark_name.startswith('memory.long_running'):
-    value_name = '%s_%s' % (test_name, stat_name)
-    return not _ShouldAddMemoryLongRunningValue(value_name)
-  if benchmark_name == 'media.desktop' or benchmark_name == 'media.mobile':
-    value_name = '%s_%s' % (test_name, stat_name)
-    return not _ShouldAddMediaValue(value_name)
-  if benchmark_name.startswith('system_health'):
-    if stat_name in STATS_BLACKLIST:
-      return True
-  if benchmark_name.startswith('v8.browsing'):
-    value_name = '%s_%s' % (test_name, stat_name)
-    return not _ShouldAddV8BrowsingValue(value_name)
-  return False
-
-
-def _ShouldAddMediaValue(value_name):
-  media_re = re.compile(
-      r'(?<!dump)(?<!process)_(std|count|max|min|sum|pct_\d{4}(_\d+)?)$')
-  return not media_re.search(value_name)
-
-
-def _ShouldAddMemoryLongRunningValue(value_name):
-  v8_re = re.compile(
-      r'renderer_processes:'
-      r'(reported_by_chrome:v8|reported_by_os:system_memory:[^:]+$)')
-  if 'memory:chrome' in value_name:
-    return ('renderer:subsystem:v8' in value_name or
-            'renderer:vmstats:overall' in value_name or
-            bool(v8_re.search(value_name)))
-  return 'v8' in value_name
-
-
-def _ShouldAddV8BrowsingValue(value_name):
-  v8_gc_re = re.compile(
-      r'^v8-gc-('
-      r'full-mark-compactor_|'
-      r'incremental-finalize_|'
-      r'incremental-step_|'
-      r'latency-mark-compactor_|'
-      r'memory-mark-compactor_|'
-      r'scavenger_|'
-      r'total_)')
-  stats_re = re.compile(r'_(std|count|min|sum|pct_\d{4}(_\d+)?)$')
-  v8_stats_re = re.compile(
-      r'_(idle_deadline_overrun|percentage_idle|outside_idle)')
-  if 'memory:unknown_browser' in value_name:
-    return ('renderer_processes' in value_name and
-            not stats_re.search(value_name))
-  if 'memory:chrome' in value_name:
-    return ('renderer_processes' in value_name and
-            not stats_re.search(value_name))
-  if 'v8-gc' in value_name:
-    return v8_gc_re.search(value_name) and not v8_stats_re.search(value_name)
-  return True
 
 
 @ndb.tasklet
