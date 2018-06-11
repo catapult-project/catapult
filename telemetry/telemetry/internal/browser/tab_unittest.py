9f7abcb46e2fcb69143b8581f8c09ebe5ad414bd
nednguyen <nednguyen@google.com>
2018-06-11 14:28:48 -0400

    Disable BrowserTest.testActivateTab on Mac
    Bug: chromium:851463
    Change-Id: Id573a87b09f076f326f8698951bb2210135cd4ba
    TBR=kbr@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1095856
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>

--- telemetry/telemetry/internal/browser/tab_unittest.py
+++ telemetry/telemetry/internal/browser/tab_unittest.py
@@ -88,6 +88,7 @@ class TabTest(tab_test_case.TabTestCase):
           context.exception.message)
 
   @decorators.Enabled('has tabs')
+  @decorators.Disabled('mac')  # crbug.com/851463
   def testActivateTab(self):
     py_utils.WaitFor(lambda: _IsDocumentVisible(self._tab), timeout=5)
     new_tab = self._browser.tabs.New()
