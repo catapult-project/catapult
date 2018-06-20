44fc6f687b352f954863d75823765b501823b175
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-06-20 12:59:19 +0100

    [soundwave] Fix GetAlertData API call
    This CL fixes:
    - renaming 'benchmark' arg to 'test_case', otherwise the option is
      ignored.
    - allows using 'all' to disable filtering by sheriff.
    
    Bug: catapult:#4479
    Change-Id: Ia7b411a1d38b9054ea3c1ab43f4ca4e139c26fd9
    Reviewed-on: https://chromium-review.googlesource.com/1107801
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>

--- experimental/soundwave/soundwave/dashboard_api.py
+++ experimental/soundwave/soundwave/dashboard_api.py
@@ -200,12 +200,14 @@ class PerfDashboardCommunicator(object):
     for bug_id in bug_ids:
       yield self._MakeApiRequest('bugs/%d' % bug_id)
 
-  def GetAlertData(self, benchmark, sheriff, days=30):
-    """Returns alerts for the given benchmark."""
+  def GetAlertData(self, test_suite, sheriff, days=30):
+    """Returns alerts for the given test_suite."""
+    # TODO(#4479): Also needs to follow possible next_cursor on response.
     min_timestamp = datetime.datetime.now() - datetime.timedelta(days=days)
     options = {
-        'benchmark': benchmark,
-        'sheriff': sheriff,
+        'test_suite': test_suite,
         'min_timestamp': min_timestamp.isoformat(),
     }
+    if sheriff != 'all':
+      options['sheriff'] = sheriff
     return self._MakeApiRequest('alerts?' + urllib.urlencode(options))
