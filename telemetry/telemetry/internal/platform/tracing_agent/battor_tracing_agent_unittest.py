d96c633790000d036c5bbcf943d2c4f1f06d9820
Ben Pastene <bpastene@chromium.org>
2018-06-25 14:35:46 -0700

    Restore battor_wrapper.IsBattOrConnected in battor_tracing_agent_unittest.
    After the unittest ran, the mocked function would remain for the
    remainder of the test. This restores it to the true implementation in
    tear-down.
    
    Bug: chromium:836447
    Change-Id: Iea62bb69236a5ae75668f1f6ace1934c98bf95c6
    Reviewed-on: https://chromium-review.googlesource.com/1114189
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>

--- telemetry/telemetry/internal/platform/tracing_agent/battor_tracing_agent_unittest.py
+++ telemetry/telemetry/internal/platform/tracing_agent/battor_tracing_agent_unittest.py
@@ -101,6 +101,8 @@ class BattOrTracingAgentTest(unittest.TestCase):
     self._battery_utils = battery_utils.BatteryUtils
     battery_utils.BatteryUtils = FakeBatteryUtils
 
+    self._is_battor_connected = battor_wrapper.IsBattOrConnected
+
     # Agents and backends.
     self.android_backend = FakeAndroidPlatformBackend()
     self.desktop_backend = FakeDesktopPlatformBackend()
@@ -112,6 +114,7 @@ class BattOrTracingAgentTest(unittest.TestCase):
   def tearDown(self):
     battor_wrapper.BattOrWrapper = self._battor_wrapper
     battery_utils.BatteryUtils = self._battery_utils
+    battor_wrapper.IsBattOrConnected = self._is_battor_connected
 
   def testInit(self):
     self.assertTrue(isinstance(self.android_agent._platform_backend,
