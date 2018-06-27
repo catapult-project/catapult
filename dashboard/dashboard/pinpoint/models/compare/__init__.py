89b78185add12c0865c4743545a6d52e017a2530
Dave Tu <dtu@chromium.org>
2018-06-26 19:49:13 -0700

    [pinpoint] Fix and test JobState.Explore and Compare.
    Bug: catapult:#4459
    Change-Id: I6ace923e82a8da04e9d85f64e32adfdda231e014
    Reviewed-on: https://chromium-review.googlesource.com/1111100
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: David Tu <dtu@chromium.org>

--- dashboard/dashboard/pinpoint/models/compare/__init__.py
+++ dashboard/dashboard/pinpoint/models/compare/__init__.py
@@ -2,4 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from dashboard.pinpoint.models.compare.compare import DIFFERENT
+from dashboard.pinpoint.models.compare.compare import PENDING
+from dashboard.pinpoint.models.compare.compare import SAME
+from dashboard.pinpoint.models.compare.compare import UNKNOWN
+
 from dashboard.pinpoint.models.compare.compare import Compare
--- dashboard/dashboard/pinpoint/models/job_state_test.py
+++ dashboard/dashboard/pinpoint/models/job_state_test.py
@@ -9,6 +9,70 @@ from dashboard.pinpoint.models import job_state
 from dashboard.pinpoint.models import quest
 
 
+class ExploreTest(unittest.TestCase):
+
+  def testDifferent(self):
+    # TODO(dtu): Implement when we have more powerful mocking available.
+    pass
+
+  def testPending(self):
+    state = job_state.JobState([_QuestStub(_ExecutionSpin)],
+                               comparison_mode=job_state.PERFORMANCE)
+    state.AddChange('change 1')
+    state.AddChange('change 2')
+
+    state.Explore()
+
+    # The results are pending. Do not add any Attempts or Changes.
+    self.assertEqual(len(state._changes), 2)
+    attempt_count_1 = len(state._attempts['change 1'])
+    attempt_count_2 = len(state._attempts['change 2'])
+    self.assertEqual(attempt_count_1, attempt_count_2)
+
+  def testSame(self):
+    state = job_state.JobState([_QuestStub(_ExecutionPass)],
+                               comparison_mode=job_state.FUNCTIONAL)
+    state.AddChange('change 1')
+    state.AddChange('change 2')
+    for _ in xrange(5):
+      # More Attempts give more confidence that they are, indeed, the same.
+      state.AddAttempts('change 1')
+      state.AddAttempts('change 2')
+
+    state.ScheduleWork()
+    state.Explore()
+
+    # The Changes are the same. Do not add any Attempts or Changes.
+    self.assertEqual(len(state._changes), 2)
+    attempt_count_1 = len(state._attempts['change 1'])
+    attempt_count_2 = len(state._attempts['change 2'])
+    self.assertEqual(attempt_count_1, attempt_count_2)
+
+  def testUnknown(self):
+    state = job_state.JobState([_QuestStub(_ExecutionPass)],
+                               comparison_mode=job_state.FUNCTIONAL)
+    state.AddChange('change 1')
+    state.AddChange('change 2')
+
+    state.ScheduleWork()
+    state.Explore()
+
+    # Need more information. Add more attempts to one Change.
+    self.assertEqual(len(state._changes), 2)
+    attempt_count_1 = len(state._attempts['change 1'])
+    attempt_count_2 = len(state._attempts['change 2'])
+    self.assertGreater(attempt_count_1, attempt_count_2)
+
+    state.ScheduleWork()
+    state.Explore()
+
+    # We still need more information. Add more attempts to the other Change.
+    self.assertEqual(len(state._changes), 2)
+    attempt_count_1 = len(state._attempts['change 1'])
+    attempt_count_2 = len(state._attempts['change 2'])
+    self.assertEqual(attempt_count_1, attempt_count_2)
+
+
 class ScheduleWorkTest(unittest.TestCase):
 
   def testNoAttempts(self):
