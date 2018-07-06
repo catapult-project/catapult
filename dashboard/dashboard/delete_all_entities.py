aa04e549d7fc391cb5f53b3a3b60f03c5d670471
benshayden <benjhayden@chromium.org>
2018-07-06 13:00:00 -0700

    dashboard.DeleteAllEntities(kind)
    There are currently many entities in the datastore whose models have been or can
    be removed from the source.
    Attempts to access these entities raises KindError, which are occasionally seen
    in the logs.
    This CL adds a function that deletes all entities of a given kind.
    It can be called from dev console:
    from dashboard import delete_all_entities
    delete_all_entities.DeleteAllEntities('StoppageAlert')
    
    Bug: chromium:851127
    Change-Id: I01c467cb0264ff0eadf36218c960ddf4ebaf1807
    Reviewed-on: https://chromium-review.googlesource.com/1127470
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

---
+++ dashboard/dashboard/delete_all_entities.py
@@ -0,0 +1,29 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+
+from google.appengine.ext import deferred
+from google.appengine.ext import ndb
+
+
+QUERY_PAGE_LIMIT = 1000
+
+
+def DeleteAllEntities(kind):
+  """DELETES ALL ENTITIES OF KIND |kind|.
+
+  Args:
+    kind: Required string name of model.
+  """
+  if not kind:
+    # Query(kind='') would delete the entire datastore.
+    raise ValueError('"kind" cannot be empty')
+
+  keys, _, more = ndb.Query(kind=kind).fetch_page(
+      QUERY_PAGE_LIMIT, keys_only=True)
+  logging.info('Fetched %d keys; more=%r', len(keys), more)
+  ndb.delete_multi(keys)
+  if more:
+    deferred.defer(DeleteAllEntities, kind)
---
+++ dashboard/dashboard/delete_all_entities_test.py
@@ -0,0 +1,48 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import mock
+
+from google.appengine.ext import ndb
+
+from dashboard import delete_all_entities
+from dashboard.common import testing_common
+from dashboard.common import utils
+from dashboard.models import anomaly
+from dashboard.models import page_state
+
+
+class DeleteAllEntitiesTest(testing_common.TestCase):
+
+  def testOnlyDeleteKind(self):
+    anomaly.Anomaly(id='anomaly', test=utils.TestMetadataKey(
+        'master/bot/suite/measurement')).put()
+    page_state.PageState(id='page_state').put()
+    self.assertEqual(1, len(ndb.Query(kind='PageState').fetch(keys_only=True)))
+    self.assertEqual(1, len(ndb.Query(kind='Anomaly').fetch(keys_only=True)))
+
+    delete_all_entities.DeleteAllEntities('PageState')
+    self.assertEqual(0, len(ndb.Query(kind='PageState').fetch(keys_only=True)))
+    self.assertEqual(1, len(ndb.Query(kind='Anomaly').fetch(keys_only=True)))
+
+  def testDeleteAllViaTaskqueue(self):
+    page_state.PageState(id=1).put()
+    page_state.PageState(id=2).put()
+
+    limit_patch = mock.patch.object(delete_all_entities, 'QUERY_PAGE_LIMIT', 1)
+    limit_patch.start()
+    self.addCleanup(limit_patch.stop)
+
+    deferred_patch = mock.patch.object(delete_all_entities, 'deferred')
+    deferred_patch.start()
+    self.addCleanup(deferred_patch.stop)
+
+    delete_all_entities.DeleteAllEntities('PageState')
+    self.assertEqual(1, len(ndb.Query(kind='PageState').fetch(keys_only=True)))
+    delete_all_entities.deferred.defer.assert_called_once_with(
+        delete_all_entities.DeleteAllEntities, 'PageState')
+
+  def testRequireKind(self):
+    with self.assertRaises(ValueError):
+      delete_all_entities.DeleteAllEntities('')
