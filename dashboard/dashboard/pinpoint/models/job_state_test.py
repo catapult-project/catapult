985295aafdbfaecd1f27cb6357871f616ca92c99
Dave Tu <dtu@chromium.org>
2018-07-09 22:29:34 -0700

    [pinpoint] Add unit test and stub for quest.py
    These stubs can be used by the unit tests for JobState and Job, and
    future integration tests, to cover various scenarios.
    
    Unit tests for quest.py are also included, mostly for coverage.
    
    Bug: catapult:#4495
    Change-Id: I09eaad9f06760e5aadca963321b763e71eb49a4c
    Reviewed-on: https://chromium-review.googlesource.com/1128454
    Commit-Queue: David Tu <dtu@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

--- dashboard/dashboard/pinpoint/models/job_state_test.py
+++ dashboard/dashboard/pinpoint/models/job_state_test.py
@@ -2,12 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import itertools
 import unittest
 
 from dashboard.pinpoint.models import job_state
-from dashboard.pinpoint.models import quest
-from dashboard.pinpoint.models.quest import execution_test
+from dashboard.pinpoint.models.quest import quest_test
 
 
 class ExploreTest(unittest.TestCase):
@@ -17,8 +15,8 @@ class ExploreTest(unittest.TestCase):
     pass
 
   def testPending(self):
-    state = job_state.JobState([_QuestStub(execution_test.ExecutionSpin)],
-                               comparison_mode=job_state.PERFORMANCE)
+    quests = [quest_test.QuestSpin()]
+    state = job_state.JobState(quests, comparison_mode=job_state.PERFORMANCE)
     state.AddChange('change 1')
     state.AddChange('change 2')
 
@@ -31,8 +29,8 @@ class ExploreTest(unittest.TestCase):
     self.assertEqual(attempt_count_1, attempt_count_2)
 
   def testSame(self):
-    state = job_state.JobState([_QuestStub(execution_test.ExecutionPass)],
-                               comparison_mode=job_state.FUNCTIONAL)
+    quests = [quest_test.QuestPass()]
+    state = job_state.JobState(quests, comparison_mode=job_state.FUNCTIONAL)
     state.AddChange('change 1')
     state.AddChange('change 2')
     for _ in xrange(5):
@@ -50,8 +48,8 @@ class ExploreTest(unittest.TestCase):
     self.assertEqual(attempt_count_1, attempt_count_2)
 
   def testUnknown(self):
-    state = job_state.JobState([_QuestStub(execution_test.ExecutionPass)],
-                               comparison_mode=job_state.FUNCTIONAL)
+    quests = [quest_test.QuestPass()]
+    state = job_state.JobState(quests, comparison_mode=job_state.FUNCTIONAL)
     state.AddChange('change 1')
     state.AddChange('change 2')
 
@@ -81,40 +79,24 @@ class ScheduleWorkTest(unittest.TestCase):
     self.assertFalse(state.ScheduleWork())
 
   def testWorkLeft(self):
-    q = _QuestStub(execution_test.ExecutionPass, execution_test.ExecutionSpin)
-    state = job_state.JobState([q])
+    quests = [quest_test.QuestCycle(quest_test.QuestPass, quest_test.QuestSpin)]
+    state = job_state.JobState(quests)
     state.AddChange('change')
     self.assertTrue(state.ScheduleWork())
 
   def testNoWorkLeft(self):
-    state = job_state.JobState([_QuestStub(execution_test.ExecutionPass)])
+    quests = [quest_test.QuestPass()]
+    state = job_state.JobState(quests)
     state.AddChange('change')
     self.assertTrue(state.ScheduleWork())
     self.assertFalse(state.ScheduleWork())
 
   def testAllAttemptsFail(self):
-    q = _QuestStub(execution_test.ExecutionFail, execution_test.ExecutionFail,
-                   execution_test.ExecutionFail2)
-    state = job_state.JobState([q])
+    quests = [quest_test.QuestCycle(
+        quest_test.QuestFail, quest_test.QuestFail, quest_test.QuestFail2)]
+    state = job_state.JobState(quests)
     state.AddChange('change')
     expected_regexp = '7/10.*\nException: Expected error for testing.$'
     self.assertTrue(state.ScheduleWork())
     with self.assertRaisesRegexp(Exception, expected_regexp):
       self.assertFalse(state.ScheduleWork())
-
-
-class _QuestStub(quest.Quest):
-
-  def __init__(self, *execution_classes):
-    self._execution_classes = itertools.cycle(execution_classes)
-
-  def __str__(self):
-    return 'Quest'
-
-  def Start(self, change):
-    del change
-    return self._execution_classes.next()()
-
-  @classmethod
-  def FromDict(cls, arguments):
-    return cls
