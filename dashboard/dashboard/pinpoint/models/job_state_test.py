f2b2ba032556c67722c029e7dc7a24bfec18dcd7
Dave Tu <dtu@chromium.org>
2018-07-10 18:35:02 -0700

    [pinpoint] Add unit test for JobState when adding a midpoint.
    Bug: catapult:#4250
    Change-Id: I8721a68355e0840de822baf9d80ac6ec2a9fb852
    Reviewed-on: https://chromium-review.googlesource.com/1128456
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: David Tu <dtu@chromium.org>

--- dashboard/dashboard/pinpoint/models/job_state_test.py
+++ dashboard/dashboard/pinpoint/models/job_state_test.py
@@ -4,62 +4,100 @@
 
 import unittest
 
+from dashboard.pinpoint import test
 from dashboard.pinpoint.models import job_state
+from dashboard.pinpoint.models.change import change_test
 from dashboard.pinpoint.models.quest import quest_test
 
 
-class ExploreTest(unittest.TestCase):
+class ExploreTest(test.TestCase):
 
-  def testDifferent(self):
-    # TODO(dtu): Implement when we have more powerful mocking available.
-    pass
+  def testDifferentWithMidpoint(self):
+    quests = [quest_test.QuestByChange({
+        change_test.Change(1): quest_test.QuestPass,
+        change_test.Change(9): quest_test.QuestFail,
+    })]
+    state = job_state.JobState(quests, comparison_mode=job_state.PERFORMANCE)
+    state.AddChange(change_test.Change(1))
+    state.AddChange(change_test.Change(9))
+
+    state.ScheduleWork()
+    state.Explore()
+
+    # The Changes are different. Add the midpoint.
+    expected = [change_test.Change(1), change_test.Change(5),
+                change_test.Change(9)]
+    self.assertEqual(state._changes, expected)
+    attempt_count_1 = len(state._attempts[change_test.Change(1)])
+    attempt_count_2 = len(state._attempts[change_test.Change(5)])
+    attempt_count_3 = len(state._attempts[change_test.Change(9)])
+    self.assertEqual(attempt_count_1, attempt_count_2)
+    self.assertEqual(attempt_count_2, attempt_count_3)
+
+  def testDifferentNoMidpoint(self):
+    quests = [quest_test.QuestByChange({
+        change_test.Change(1): quest_test.QuestPass,
+        change_test.Change(2): quest_test.QuestFail,
+    })]
+    state = job_state.JobState(quests, comparison_mode=job_state.PERFORMANCE)
+    state.AddChange(change_test.Change(1))
+    state.AddChange(change_test.Change(2))
+
+    state.ScheduleWork()
+    state.Explore()
+
+    # The Changes are different, but there's no midpoint. We're done.
+    self.assertEqual(len(state._changes), 2)
+    attempt_count_1 = len(state._attempts[change_test.Change(1)])
+    attempt_count_2 = len(state._attempts[change_test.Change(2)])
+    self.assertEqual(attempt_count_1, attempt_count_2)
 
   def testPending(self):
     quests = [quest_test.QuestSpin()]
     state = job_state.JobState(quests, comparison_mode=job_state.PERFORMANCE)
-    state.AddChange('change 1')
-    state.AddChange('change 2')
+    state.AddChange(change_test.Change(1))
+    state.AddChange(change_test.Change(9))
 
     state.Explore()
 
     # The results are pending. Do not add any Attempts or Changes.
     self.assertEqual(len(state._changes), 2)
-    attempt_count_1 = len(state._attempts['change 1'])
-    attempt_count_2 = len(state._attempts['change 2'])
+    attempt_count_1 = len(state._attempts[change_test.Change(1)])
+    attempt_count_2 = len(state._attempts[change_test.Change(9)])
     self.assertEqual(attempt_count_1, attempt_count_2)
 
   def testSame(self):
     quests = [quest_test.QuestPass()]
     state = job_state.JobState(quests, comparison_mode=job_state.FUNCTIONAL)
-    state.AddChange('change 1')
-    state.AddChange('change 2')
+    state.AddChange(change_test.Change(1))
+    state.AddChange(change_test.Change(9))
     for _ in xrange(5):
       # More Attempts give more confidence that they are, indeed, the same.
-      state.AddAttempts('change 1')
-      state.AddAttempts('change 2')
+      state.AddAttempts(change_test.Change(1))
+      state.AddAttempts(change_test.Change(9))
 
     state.ScheduleWork()
     state.Explore()
 
     # The Changes are the same. Do not add any Attempts or Changes.
     self.assertEqual(len(state._changes), 2)
-    attempt_count_1 = len(state._attempts['change 1'])
-    attempt_count_2 = len(state._attempts['change 2'])
+    attempt_count_1 = len(state._attempts[change_test.Change(1)])
+    attempt_count_2 = len(state._attempts[change_test.Change(9)])
     self.assertEqual(attempt_count_1, attempt_count_2)
 
   def testUnknown(self):
     quests = [quest_test.QuestPass()]
     state = job_state.JobState(quests, comparison_mode=job_state.FUNCTIONAL)
-    state.AddChange('change 1')
-    state.AddChange('change 2')
+    state.AddChange(change_test.Change(1))
+    state.AddChange(change_test.Change(9))
 
     state.ScheduleWork()
     state.Explore()
 
     # Need more information. Add more attempts to one Change.
     self.assertEqual(len(state._changes), 2)
-    attempt_count_1 = len(state._attempts['change 1'])
-    attempt_count_2 = len(state._attempts['change 2'])
+    attempt_count_1 = len(state._attempts[change_test.Change(1)])
+    attempt_count_2 = len(state._attempts[change_test.Change(9)])
     self.assertGreater(attempt_count_1, attempt_count_2)
 
     state.ScheduleWork()
@@ -67,8 +105,8 @@ class ExploreTest(unittest.TestCase):
 
     # We still need more information. Add more attempts to the other Change.
     self.assertEqual(len(state._changes), 2)
-    attempt_count_1 = len(state._attempts['change 1'])
-    attempt_count_2 = len(state._attempts['change 2'])
+    attempt_count_1 = len(state._attempts[change_test.Change(1)])
+    attempt_count_2 = len(state._attempts[change_test.Change(9)])
     self.assertEqual(attempt_count_1, attempt_count_2)
 
 
@@ -81,13 +119,13 @@ class ScheduleWorkTest(unittest.TestCase):
   def testWorkLeft(self):
     quests = [quest_test.QuestCycle(quest_test.QuestPass, quest_test.QuestSpin)]
     state = job_state.JobState(quests)
-    state.AddChange('change')
+    state.AddChange(change_test.Change(123))
     self.assertTrue(state.ScheduleWork())
 
   def testNoWorkLeft(self):
     quests = [quest_test.QuestPass()]
     state = job_state.JobState(quests)
-    state.AddChange('change')
+    state.AddChange(change_test.Change(123))
     self.assertTrue(state.ScheduleWork())
     self.assertFalse(state.ScheduleWork())
 
@@ -95,7 +133,7 @@ class ScheduleWorkTest(unittest.TestCase):
     quests = [quest_test.QuestCycle(
         quest_test.QuestFail, quest_test.QuestFail, quest_test.QuestFail2)]
     state = job_state.JobState(quests)
-    state.AddChange('change')
+    state.AddChange(change_test.Change(123))
     expected_regexp = '7/10.*\nException: Expected error for testing.$'
     self.assertTrue(state.ScheduleWork())
     with self.assertRaisesRegexp(Exception, expected_regexp):
