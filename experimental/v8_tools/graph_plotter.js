296c9eb4922f341d8547472f74e17cbe821dfb97
Anthony Alridge <anthonyalridge@google.com>
2018-07-25 12:47:35 +0100

    Create ability to zoom on line charts.
    Allows the user to zoom in on areas of the chart. This commit also
    adds a reset button which resets the chart to it's initial state
    before zooming.
    
    Bug: chromium:866423
    Change-Id: Iebb32e73d8a83b674730dac68f1f83b0038dbd27
    Reviewed-on: https://chromium-review.googlesource.com/1150035
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Anthony Alridge <anthonyalridge@google.com>

--- experimental/v8_tools/graph_plotter.js
+++ experimental/v8_tools/graph_plotter.js
@@ -17,12 +17,12 @@ class GraphPlotter {
       top: 15,
       right: 200,
       left: 80,
-      bottom: 50
+      bottom: 50,
     };
     /** @private {number} */
-    this.canvasHeight_ = 400;
+    this.canvasHeight_ = 720;
     /** @private {number} */
-    this.canvasWidth_ = 750;
+    this.canvasWidth_ = 1280;
     /** @private {number} */
     this.chartWidth_ =
       this.canvasWidth_ - this.margins_.left - this.margins_.right;
@@ -37,25 +37,42 @@ class GraphPlotter {
     this.chart_ = this.background_.append('g')
         .attr('transform', `translate(${this.margins_.left}, 
                   ${this.margins_.top})`);
+    // Appending a clip path rectangle prevents any drawings with the
+    // clip-path: url(#plot-clip) attribute from being displayed outside
+    // of the chart area.
+    this.chart_.append('defs')
+        .append('clipPath')
+        .attr('id', 'plot-clip')
+        .append('rect')
+        .attr('width', this.chartWidth_)
+        .attr('height', this.chartHeight_);
     /** @private {Object} */
     this.legend_ = this.createLegend_();
     /** @private {Object} */
-    this.scaleForXAxis_ = this.setXAxisScale_();
+    this.scaleForXAxis_ = this.createXAxisScale_();
     /** @private {Object} */
-    this.scaleForYAxis_ = this.setYAxisScale_();
+    this.scaleForYAxis_ = this.createYAxisScale_();
     /** @private {Object} */
-    this.createAxis_();
+    this.xAxisGenerator_ = d3.axisBottom(this.scaleForXAxis_);
     /** @private {Object} */
+    this.yAxisGenerator_ = d3.axisLeft(this.scaleForYAxis_);
+    // Draw the x-axis.
+    this.chart_.append('g')
+        .call(this.xAxisGenerator_)
+        .attr('transform', `translate(0, ${this.chartHeight_})`);
+    /** @private {Object} Keep a reference to y-axis for updating on zoom */
+    this.yAxisDrawing_ = this.chart_.append('g')
+        .call(this.yAxisGenerator_);
     this.labelAxis_();
   }
 
-  setXAxisScale_() {
+  createXAxisScale_() {
     return d3.scaleLinear()
         .domain([0, this.graph_.xAxisMax()])
         .range([0, this.chartWidth_]);
   }
 
-  setYAxisScale_() {
+  createYAxisScale_() {
     return d3.scaleLinear()
         .domain([this.graph_.yAxisMax(), 0])
         .range([0, this.chartHeight_]);
@@ -66,15 +83,6 @@ class GraphPlotter {
         .attr('class', 'legend')
         .attr('transform',
             `translate(${this.chartWidth_}, ${this.margins_.top})`);
-  }
-
-  createAxis_() {
-    this.chart_.append('g')
-        .call(d3.axisBottom(this.scaleForXAxis_))
-        .attr('transform', `translate(0, ${this.chartHeight_})`);
-
-    this.chart_.append('g')
-        .call(d3.axisLeft(this.scaleForYAxis_));
   }
 
   labelAxis_() {
@@ -97,7 +105,7 @@ class GraphPlotter {
    * plot them both and label their colors in the legend.
    */
   linePlot() {
-    const lineF = d3.line()
+    const pathGenerator = d3.line()
         .x(datum => this.scaleForXAxis_(datum.x))
         .y(datum => this.scaleForYAxis_(datum.y))
         .curve(d3.curveMonotoneX);
@@ -110,18 +118,96 @@ class GraphPlotter {
           .attr('cx', datum => this.scaleForXAxis_(datum.x))
           .attr('cy', datum => this.scaleForYAxis_(datum.y))
           .attr('r', 3)
-          .attr('fill', color);
+          .attr('fill', color)
+          .attr('class', 'line-dot')
+          .attr('clip-path', 'url(#plot-clip)');
       this.chart_.append('path')
           .datum(data)
-          .attr('d', lineF)
+          .attr('d', pathGenerator)
           .attr('stroke', color)
           .attr('fill', 'none')
           .attr('stroke-width', 2)
-          .attr('data-legend', key);
+          .attr('data-legend', key)
+          .attr('class', 'line-plot')
+          .attr('clip-path', 'url(#plot-clip)');
       this.legend_.append('text')
           .text(key)
           .attr('y', index + 'em')
           .attr('fill', color);
     });
+    const zoom = d3.zoom();
+    this.setUpZoom_(zoom);
+    this.setUpZoomReset_(zoom);
+  }
+
+  transformLinePlot_(yAxisScale) {
+    const pathGenerator = d3.line()
+        .x(d => this.scaleForXAxis_(d.x))
+        .y(d => yAxisScale(d.y))
+        .curve(d3.curveMonotoneX);
+    this.yAxisGenerator_.scale(yAxisScale);
+    this.yAxisDrawing_.call(this.yAxisGenerator_);
+    this.chart_.selectAll('.line-dot')
+        .attr('cx', datum => this.scaleForXAxis_(datum.x))
+        .attr('cy', datum => yAxisScale(datum.y));
+    this.chart_.selectAll('.line-plot')
+        .attr('d', pathGenerator);
+  }
+
+  setUpZoom_(zoom) {
+    const onZoom = () => {
+      const transform = d3.event.transform;
+      const transformedScaleForYAxis = transform.rescaleY(this.scaleForYAxis_);
+      this.transformLinePlot_(transformedScaleForYAxis);
+    };
+    zoom.on('zoom', onZoom).scaleExtent([1, Infinity]);
+    // The following invisible rectangle is there just to catch
+    // mouse events for zooming. It's not possible to listen on
+    // the chart itself because it is a g element (which does not
+    // capture mouse events).
+    this.chart_.append('rect')
+        .attr('width', this.chartWidth_)
+        .attr('height', this.chartHeight_)
+        .attr('class', 'zoom-listener')
+        .style('opacity', 0)
+        .call(zoom);
+  }
+
+  setUpZoomReset_(zoom) {
+    // Gives some padding between the x-axis and the reset button.
+    const padding = 10;
+    const resetButton = this.chart_.append('svg')
+        .attr('width', '10em')
+        .attr('height', '2em')
+        .attr('x', `${this.chartWidth_ + padding}px`)
+        .attr('y', `${this.chartHeight_}px`)
+        .attr('cursor', 'pointer');
+    // Styling for the button.
+    resetButton.append('rect')
+        .attr('rx', '5px')
+        .attr('ry', '5px')
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .attr('fill', '#1b39a8');
+    resetButton.append('text')
+        .text('RESET CHART')
+        .attr('x', '50%')
+        .attr('y', '50%')
+        .attr('fill', 'white')
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle');
+    resetButton
+        .on('mouseover', () => {
+          resetButton.attr('opacity', '0.5');
+        })
+        .on('mouseout', () => {
+          resetButton.attr('opacity', '1');
+        })
+        .on('click', () => {
+          resetButton.attr('opacity', '1');
+          this.chart_.select('.zoom-listener')
+              .call(zoom.transform, d3.zoomIdentity);
+          this.transformLinePlot_(this.scaleForYAxis_);
+        });
   }
 }
