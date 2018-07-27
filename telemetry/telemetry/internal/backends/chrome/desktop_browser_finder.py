a1e31db3c6b70369152765f14268257cf5775c49
Nghia Nguyen <nednguyen@google.com>
2018-07-26 21:16:42 -0400

    Regenerate startup args for each time trying to start the desktop browser
    Bug: chromium:865895
    Change-Id: I06121d018219adad5d5146145a28b7842a6844ab
    
    TBR=perezju@chromium.org
    
    Change-Id: I06121d018219adad5d5146145a28b7842a6844ab
    Reviewed-on: https://chromium-review.googlesource.com/1152474
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>

--- telemetry/telemetry/internal/backends/chrome/desktop_browser_finder.py
+++ telemetry/telemetry/internal/backends/chrome/desktop_browser_finder.py
@@ -130,12 +130,16 @@ class PossibleDesktopBrowser(possible_browser.PossibleBrowser):
 
     self._InitPlatformIfNeeded()
 
-    startup_args = self.GetBrowserStartupArgs(self._browser_options)
 
     num_retries = 3
     for x in range(0, num_retries):
       returned_browser = None
       try:
+        # Note: we need to regenerate the browser startup arguments for each
+        # browser startup attempt since the state of the startup arguments
+        # may not be guaranteed the same each time
+        # For example, see: crbug.com/865895#c17
+        startup_args = self.GetBrowserStartupArgs(self._browser_options)
         returned_browser = None
 
         browser_backend = desktop_browser_backend.DesktopBrowserBackend(
