717723b904a53e4d0b672772cb64291b50baad6b
Leonard Ge <wangge@google.com>
2018-08-15 10:28:36 +0100

    Redo Telemetry Compatibility Mode.
    In this CL, we change the `--compatibility-mode` flag from a binary switch to
    store a list of compatibility change that we want to enforce. We might add some
    new chocies in later stage.
    
    Minor style change has also been made in this CL.
    
    Bug: chromium:866881
    Change-Id: I96e749c201398bf17924b4407d325a2094a327c4
    Reviewed-on: https://chromium-review.googlesource.com/1174721
    Commit-Queue: Leonard Ge <wangge@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>

---
+++ telemetry/telemetry/compact_mode_options.py
@@ -0,0 +1,10 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Acceptable values for the Telemetry compatibility_mode flag.
+"""
+
+NO_FIELD_TRIALS = 'no-field-trials'
+IGNORE_CERTIFICATE_ERROR = 'ignore-certificate-errors'
+LEGACY_COMMAND_LINE_PATH = 'legacy-command-line-path'
+GPU_BENCHMARKING_FALLBACKS = 'gpu-benchmarking-fallbacks'
