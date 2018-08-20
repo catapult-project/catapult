ef706bbba53aaa1cc199bc5914f1a319894089d7
Ben Hayden <benjhayden@chromium.org>
2018-08-20 15:40:29 -0700

    Add checkbox to V2SPA.
    This is a simple drop-in replacement for paper-checkbox.
    
    Demo: https://v2spa-dot-chromeperf.appspot.com/#chart
    
    See also the CSS vars CL:
    https://chromium-review.googlesource.com/c/catapult/+/1180346
    
    As a followup, this could be moved to chops-ui.
    
    Bug: catapult:#4461
    Change-Id: I7b9a15f48cf11f32fb80bb1e2a5987d12c25c26d
    Reviewed-on: https://chromium-review.googlesource.com/1180607
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

---
+++ dashboard/dashboard/spa/cp-checkbox.js
@@ -0,0 +1,24 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+(() => {
+  class CpCheckbox extends Polymer.Element {
+    static get is() { return 'cp-checkbox'; }
+
+    onChange_(event) {
+      this.dispatchEvent(new CustomEvent('change', {
+        bubbles: true,
+        composed: true,
+      }));
+    }
+  }
+
+  CpCheckbox.properties = {
+    checked: {type: Boolean},
+    disabled: {type: Boolean},
+  };
+
+  customElements.define(CpCheckbox.is, CpCheckbox);
+})();
