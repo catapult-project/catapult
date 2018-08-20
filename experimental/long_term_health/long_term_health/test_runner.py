3858e90e319fc0f14d1349737edb05e72e24c57c
Leonard Ge <wangge@google.com>
2018-08-20 09:16:14 +0100

    Restructure Long Term Health Tool Output File Structure.
    In this CL, we've changed the output file structure of the long term health tool
    so there will be a separate directory for each individual run of the long term
    health tool, and inside which contains directories for each milestones. This
    change will make our data analysis process easier. More details can be found in
    https://docs.google.com/document/d/1lNoZhHdZ-Q-PcUzXSaP38yWM7ZimGiq3582rVQTSlNI/edit?usp=sharing
    
    Some flag change is also made in this CL due to the change of `compatibility_mode` flag,
    as in CL:1174721.
    
    Bug: chromium:863390
    Change-Id: I112122ee37c974b95b717dabb753ca4a399c735b
    Reviewed-on: https://chromium-review.googlesource.com/1174381
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>

--- experimental/long_term_health/long_term_health/test_runner.py
+++ experimental/long_term_health/long_term_health/test_runner.py
@@ -3,36 +3,42 @@
 # found in the LICENSE file.
 """Helper function to run the benchmark.
 """
-import datetime
 import os
 import subprocess
 
 from long_term_health import utils
+from long_term_health.apk_finder import ChromeVersion
 
 
-def RunBenchmark(path_to_apk):
+def RunBenchmark(path_to_apk, run_label):
   """Install the APK and run the benchmark on it.
 
   Args:
     path_to_apk(string): the *relative* path to the APK
+    run_label(string): the name of the directory to contains all the output
+    from this run
   """
-  apk_name = path_to_apk.split('/')[-1]
+  # `path_to_apk` is similar to `./out/59.0.3071.132_arm_MonochromeStable.apk`
+  chrome_version = ChromeVersion(path_to_apk.split('/')[-1].split('_')[0])
   subprocess.call(['adb', 'install', '-r', '-d', path_to_apk])
   subprocess.call([os.path.join(utils.CHROMIUM_SRC, 'tools',
                                 'perf', 'run_benchmark'),
                    '--browser=android-system-chrome',
                    '--pageset-repeat=1',  # could remove this later
-                   '--results-label=%s' % apk_name,  # could remove this as well
+                   '--results-label=%s' % str(chrome_version),
                    # TODO(wangge):not sure if we should run in compatibility
                    # mode even for the later version, probably add a check in
                    # caller to determine if we should run it in compatibility
                    # mode and add an argument `run_in_compatibility_mode` to
                    # the `RunBenchmark` function
-                   '--compatibility-mode',
+                   '--compatibility-mode=no-field-trials',
+                   '--compatibility-mode=ignore-certificate-errors',
+                   '--compatibility-mode=legacy-command-line-path',
+                   '--compatibility-mode=gpu-benchmarking-fallbacks',
                    '--story-filter=wikipedia',  # could remove this
                    # thinking of adding an argument to the tool to set this
                    '--output-dir=%s' % os.path.join(
-                       utils.APP_ROOT, 'results', apk_name,
-                       datetime.datetime.now().isoformat()),
+                       utils.APP_ROOT, 'results', run_label,
+                       str(chrome_version.milestone)),
                    # thinking of adding an argument to the tool to set this too
                    'system_health.memory_mobile'])
