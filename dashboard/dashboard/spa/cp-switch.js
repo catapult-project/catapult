2b1ba7916d1f1ec7dbd6a7d9bc2fc4ce85b05ad4
Ben Hayden <benjhayden@chromium.org>
2018-08-23 15:20:59 -0700

    Add a Material switch for V2SPA.
    Demo (Improvements and Triaged):
    https://v2spa-dot-chromeperf.appspot.com/#sheriff=Chromium_Perf_Sheriff
    
    Bug: catapult:#4461
    Change-Id: Id0cd33c5cf1fbde90761cfaa7a58bf12b3843595
    Reviewed-on: https://chromium-review.googlesource.com/1181534
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

---
+++ dashboard/dashboard/spa/cp-switch.js
@@ -0,0 +1,24 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+(() => {
+  class CpSwitch extends Polymer.Element {
+    static get is() { return 'cp-switch'; }
+
+    onChange_(event) {
+      this.dispatchEvent(new CustomEvent('change', {
+        bubbles: true,
+        composed: true,
+      }));
+    }
+  }
+
+  CpSwitch.properties = {
+    checked: {type: Boolean},
+    disabled: {type: Boolean},
+  };
+
+  customElements.define(CpSwitch.is, CpSwitch);
+})();
