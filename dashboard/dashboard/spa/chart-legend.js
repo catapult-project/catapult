72d86a11948add1e4c9a2ff7fa9db2a4cd4a1561
benshayden <benjhayden@chromium.org>
2018-09-12 14:00:41 -0700

    Add chart-legend for v2spa
    This simple component takes a tree of items {label, color, children} and
    displays them recursively. It dispatches events when users mouse over, mouse
    out, or click on leaves.
    
    Bug: catapult:#4461
    Change-Id: Ibbd770e3ec448be19077ab6379189936f1687776
    Reviewed-on: https://chromium-review.googlesource.com/1214845
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

---
+++ dashboard/dashboard/spa/chart-legend.js
@@ -0,0 +1,44 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+tr.exportTo('cp', () => {
+  class ChartLegend extends Polymer.Element {
+    static get is() { return 'chart-legend'; }
+
+    async onLeafMouseOver_(event) {
+      this.dispatchEvent(new CustomEvent('leaf-mouseover', {
+        bubbles: true,
+        composed: true,
+        detail: event.model.item,
+      }));
+    }
+
+    async onLeafMouseOut_(event) {
+      this.dispatchEvent(new CustomEvent('leaf-mouseout', {
+        bubbles: true,
+        composed: true,
+        detail: event.model.item,
+      }));
+    }
+
+    async onLeafClick_(event) {
+      this.dispatchEvent(new CustomEvent('leaf-click', {
+        bubbles: true,
+        composed: true,
+        detail: event.model.item,
+      }));
+    }
+  }
+
+  ChartLegend.properties = {
+    items: {type: Array},
+  };
+
+  customElements.define(ChartLegend.is, ChartLegend);
+
+  return {
+    ChartLegend,
+  };
+});
