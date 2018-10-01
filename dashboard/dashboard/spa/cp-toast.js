a0582c6a3ed2a16092a551a0cffc0e893b9faf81
benshayden <benjhayden@chromium.org>
2018-10-01 12:06:54 -0700

    Add cp-toast for v2spa.
    This is a simple version of paper-toast that animates using iron-collapse.
    
    Demo: close this chart and look in the bottom left corner:
    https://v2spa-dot-chromeperf.appspot.com/#testSuite=system_health.memory_desktop&measurement=memory%253Achrome%253Aall_processes%253Areported_by_chrome%253Aeffective_size&bot=ChromiumPerf%253Alinux-perf&minRev=575244&maxRev=588597
    
    Bug: catapult:#4461
    Change-Id: Ibc8573343a58ffe0e3083ec2a1a7a70a630bb0cf
    Reviewed-on: https://chromium-review.googlesource.com/1238618
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

---
+++ dashboard/dashboard/spa/cp-toast.js
@@ -0,0 +1,35 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+(() => {
+  class CpToast extends Polymer.Element {
+    static get is() { return 'cp-toast'; }
+
+    /*
+     * Autocloses after `wait` ms if open() is not called again in the interim.
+     * Does not autoclose if `wait` is false.
+     * `wait` can also be a Promise.
+     */
+    async open(wait = 10000) {
+      this.opened = true;
+      if (!wait) return;
+      const start = this.openedTimestamp_ = performance.now();
+      if (typeof wait === 'number') wait = cp.timeout(wait);
+      await wait;
+      if (this.openedTimestamp_ !== start) return;
+      this.opened = false;
+    }
+  }
+
+  CpToast.properties = {
+    opened: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
+    },
+  };
+
+  customElements.define(CpToast.is, CpToast);
+})();
