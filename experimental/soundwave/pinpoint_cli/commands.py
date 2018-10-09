30b1adb1ae227411598233a194e4788637e72330
Juan Antonio Navarro Perez <perezju@chromium.org>
2018-10-09 14:09:37 +0100

    [pinpoint_cli] Allow to get-csv from multiple jobs at a time
    This is handy to quickly get data from multiple related jobs into a
    single csv for further exploration/data analysis.
    
    Bug: chromium:879526
    Change-Id: Id0306ece7c48c7d925a1a97d291f2a49965e350b
    Reviewed-on: https://chromium-review.googlesource.com/c/1264516
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>

--- experimental/soundwave/pinpoint_cli/commands.py
+++ experimental/soundwave/pinpoint_cli/commands.py
@@ -10,21 +10,22 @@ from pinpoint_cli import histograms_df
 from pinpoint_cli import job_results
 
 
-def DownloadJobResultsAsCsv(api, job_id, output_file):
+def DownloadJobResultsAsCsv(api, job_ids, output_file):
   """Download the perf results of a job as a csv file."""
-  job = api.pinpoint.Job(job_id, with_state=True)
-  # TODO: Make this also work for jobs that ran on windows platform.
-  results_file = posixpath.join(
-      job['arguments']['benchmark'], 'perf_results.json')
-  print 'Fetching results for %s job %s:' % (job['status'].lower(), job_id)
   with open(output_file, 'wb') as f:
     writer = csv.writer(f)
-    writer.writerow(('change', 'isolate') + histograms_df.COLUMNS)
+    writer.writerow(('job_id', 'change', 'isolate') + histograms_df.COLUMNS)
     num_rows = 0
-    for change_id, isolate_hash in job_results.IterTestOutputIsolates(job):
-      print '- isolate: %s ...' % isolate_hash
-      histograms = api.isolate.RetrieveFile(isolate_hash, results_file)
-      for row in histograms_df.IterRows(json.loads(histograms)):
-        writer.writerow((change_id, isolate_hash) + row)
-        num_rows += 1
+    for job_id in job_ids:
+      job = api.pinpoint.Job(job_id, with_state=True)
+      # TODO: Make this also work for jobs that ran on windows platform.
+      results_file = posixpath.join(
+          job['arguments']['benchmark'], 'perf_results.json')
+      print 'Fetching results for %s job %s:' % (job['status'].lower(), job_id)
+      for change_id, isolate_hash in job_results.IterTestOutputIsolates(job):
+        print '- isolate: %s ...' % isolate_hash
+        histograms = api.isolate.RetrieveFile(isolate_hash, results_file)
+        for row in histograms_df.IterRows(json.loads(histograms)):
+          writer.writerow((job_id, change_id, isolate_hash) + row)
+          num_rows += 1
   print 'Wrote data from %d histograms in %s.' % (num_rows, output_file)
