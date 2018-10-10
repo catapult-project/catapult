16794eb24d425c5b4f2584157f2704b675ee927a
benshayden <benjhayden@chromium.org>
2018-10-10 10:13:28 -0700

    Add service worker for v2spa.
    The service worker is compiled using webpack.
    
    Parts of this are from sbalana:
    https://chromium-review.googlesource.com/c/catapult/+/1130105
    
    Bug: catapult:#4461
    Change-Id: Ie1e6444d9967f919c73d36450c74118fa1ea52c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1257826
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

---
+++ dashboard/dashboard/spa/index.js
@@ -0,0 +1,28 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+tr.exportTo('window', () => {
+  const DEBUG = location.hostname === 'localhost';
+
+  // Register the Service Worker when in production. Service Workers are not
+  // helpful in development mode because all backend responses are being mocked.
+  if ('serviceWorker' in navigator && !DEBUG) {
+    document.addEventListener('DOMContentLoaded', async() => {
+      await navigator.serviceWorker.register(
+          'service-worker.js?' + VULCANIZED_TIMESTAMP.getTime());
+
+      if (navigator.serviceWorker.controller === null) {
+        // Technically, everything would work without the service worker, but it
+        // would be unbearably slow. Reload so that the service worker can
+        // finish installing.
+        location.reload();
+      }
+    });
+  }
+
+  return {
+  };
+});
---
+++ dashboard/dashboard/spa/webpack.config.js
@@ -0,0 +1,33 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+const path = require('path');
+
+const {
+  WEBPACK_OUTPUT_PATH: outputPath,
+  WEBPACK_NODE_MODULES: nodeModules,
+  WEBPACK_THIRD_PARTY: thirdParty,
+} = process.env;
+
+module.exports = {
+  entry: {
+    'service-worker': path.resolve(__dirname, 'service-worker.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: outputPath,
+  },
+  resolve: {
+    modules: [thirdParty],
+    alias: {
+      '/idb/idb.js': path.resolve(thirdParty, 'idb', 'idb.js'),
+    },
+  },
+  resolveLoader: {
+    modules: [nodeModules],
+  },
+  mode: 'production',
+};
