05a906f487cf9935570edf2b0c92deb2a9d7b6d7
benshayden <benjhayden@chromium.org>
2018-10-11 13:12:17 -0700

    Add task queue for v2spa service worker.
    This task queue will be used by CacheRequestBase to delay writing to the
    database until after all requests are complete in order to keep database reads
    fast.
    
    Bug: catapult:#4461
    Change-Id: I46b9b39971de7ce6f8962ed0e531b600b23e95e2
    Reviewed-on: https://chromium-review.googlesource.com/c/1277768
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

---
+++ dashboard/dashboard/spa/task-queue.js
@@ -0,0 +1,54 @@
+/* Copyright 2018 The Chromium Authors. All rights reserved.
+   Use of this source code is governed by a BSD-style license that can be
+   found in the LICENSE file.
+*/
+'use strict';
+
+const PAUSED = 'paused';
+const RUNNING = 'running';
+const PAUSING = 'pausing';
+
+let STATE = PAUSED;
+let TIMEOUT_ID;
+const QUEUE = [];
+
+function schedule(task) {
+  QUEUE.push(task);
+}
+
+function scheduleFlush(delayMs = 1000) {
+  TIMEOUT_ID = setTimeout(flush, delayMs);
+}
+
+function cancelFlush() {
+  if (TIMEOUT_ID) clearTimeout(TIMEOUT_ID);
+  if (STATE === RUNNING) STATE = PAUSING;
+}
+
+async function flush() {
+  // This async method could potentially take several seconds, during which
+  // time the caller could schedule another task. In order to prevent
+  // deadlocks, there should only be a single flush() running at once.
+  if (STATE !== PAUSED) return;
+  STATE = RUNNING;
+
+  // eslint-disable-next-line no-unmodified-loop-condition
+  while (QUEUE.length && (STATE === RUNNING)) {
+    const task = QUEUE.shift();
+    try {
+      await task();
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.warn(err);
+    }
+  }
+
+  STATE = PAUSED;
+}
+
+export default {
+  schedule,
+  scheduleFlush,
+  cancelFlush,
+  flush,
+};
