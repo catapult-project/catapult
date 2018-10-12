81a94591fc88add34d4c74ea6de78d6f3a8535e2
benshayden <benjhayden@chromium.org>
2018-10-12 08:22:04 -0700

    Add ts_mon metrics to common/timing.py
    The chromeperf dashboard backend uses this utility to record CPU usage
    and wall time durations of various operations.
    
    Demo:
    https://dev-benjhayden-4f5d40d1-dot-chromeperf.appspot.com/_ah/dev_console/interactive
    from dashboard.common.timing import *
    with WallTimeLogger('1217565','test'):time.sleep(3)
    
    Bug: chromium:867060
    Change-Id: I3ebbf85a6e7f0b4d9d76df753e3ced9b064c2bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/1217565
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

--- dashboard/dashboard/common/timing.py
+++ dashboard/dashboard/common/timing.py
@@ -2,15 +2,24 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import gae_ts_mon
 import logging
 import time
 
 
 class WallTimeLogger(object):
-  def __init__(self, label):
+  def __init__(self, label, description=''):
+    """Initialize a context manager labeled `label` that measures the wall time
+    between enter and exit.
+    If description is truthy, the measurement is reported to ts mon.
+    """
     self._label = label
     self._start = None
     self.seconds = 0
+    self._metric = None
+    if description:
+      self._metric = gae_ts_mon.FloatMetric(
+          self._Suffix(), description, [gae_ts_mon.StringField('label')], 's')
 
   def _Now(self):
     return time.time()
@@ -24,6 +33,8 @@ class WallTimeLogger(object):
   def __exit__(self, *unused_args):
     self.seconds = self._Now() - self._start
     logging.info('%s:%s=%f', self._label, self._Suffix(), self.seconds)
+    if self._metric:
+      self._metric.set(self.seconds, dict(label=self._label))
 
 
 class CpuTimeLogger(WallTimeLogger):
