6ab9bc6fcb3db488b84b57e14ac740783e86eeeb
Ethan Kuefner <eakuefner@chromium.org>
2018-10-18 13:55:17 -0700

    [Dashboard] Rewrite TODOs in add_histograms to mention bugs instead of me
    Change-Id: I1d53a0c21009e0c37f2d37e0959070720e10a574
    Reviewed-on: https://chromium-review.googlesource.com/c/1289709
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Ben Hayden <benjhayden@chromium.org>
    Auto-Submit: Ethan Kuefner <eakuefner@chromium.org>

--- dashboard/dashboard/add_histograms.py
+++ dashboard/dashboard/add_histograms.py
@@ -179,7 +179,7 @@ def ProcessHistogramSet(histogram_dicts):
   with timing.WallTimeLogger('InlineDenseSharedDiagnostics'):
     InlineDenseSharedDiagnostics(histograms)
 
-  # TODO(eakuefner): Get rid of this.
+  # TODO(#4242): Get rid of this.
   # https://github.com/catapult-project/catapult/issues/4242
   with timing.WallTimeLogger('_PurgeHistogramBinData'):
     _PurgeHistogramBinData(histograms)
@@ -227,7 +227,7 @@ def ProcessHistogramSet(histogram_dicts):
     suite_level_sparse_diagnostic_entities = FindSuiteLevelSparseDiagnostics(
         histograms, suite_key, revision, internal_only)
 
-  # TODO(eakuefner): Refactor master/bot computation to happen above this line
+  # TODO(896856): Refactor master/bot computation to happen above this line
   # so that we can replace with a DiagnosticRef rather than a full diagnostic.
   with timing.WallTimeLogger('DeduplicateAndPut'):
     new_guids_to_old_diagnostics = (
@@ -283,7 +283,7 @@ def _BatchHistogramsIntoTasks(
   for hist in histograms:
     diagnostics = FindHistogramLevelSparseDiagnostics(hist)
 
-    # TODO(eakuefner): Don't compute full diagnostics, because we need anyway to
+    # TODO(896856): Don't compute full diagnostics, because we need anyway to
     # call GetOrCreate here and in the queue.
     test_path = '%s/%s' % (suite_path, histogram_helpers.ComputeTestPath(hist))
 
@@ -292,7 +292,7 @@ def _BatchHistogramsIntoTasks(
           'Duplicate histogram detected: %s' % test_path)
     duplicate_check.add(test_path)
 
-    # TODO(eakuefner): Batch these better than one per task.
+    # TODO(#4135): Batch these better than one per task.
     task_dict = _MakeTaskDict(
         hist, test_path, revision, benchmark_description, diagnostics)
 
@@ -413,7 +413,7 @@ def ComputeRevision(histograms):
 
 
 def InlineDenseSharedDiagnostics(histograms):
-  # TODO(eakuefner): Delete inlined diagnostics from the set
+  # TODO(896856): Delete inlined diagnostics from the set
   for hist in histograms:
     diagnostics = hist.diagnostics
     for name, diag in diagnostics.iteritems():
