ebf0d23ee62ec1b476cc8a0c8616b6d295271680
bsheedy <bsheedy@chromium.org>
2019-01-03 11:53:17 -0800

    Add gtest perf test conversion script
    Adds a conversion script to go from the data format output by gtest
    perf tests to histograms.
    
    Bug: chromium:843643
    Change-Id: I5cded981f9e7bf88abdef418dd3e82cf77637324
    Reviewed-on: https://chromium-review.googlesource.com/c/1377773
    Reviewed-by: Ben Hayden <benjhayden@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>

---
+++ tracing/tracing/value/gtest_json_converter.py
@@ -0,0 +1,110 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+
+from tracing.value import histogram
+from tracing.value import histogram_set
+from tracing.value.diagnostics import generic_set
+from tracing.value.diagnostics import reserved_infos
+
+
+def ConvertGtestJson(gtest_json):
+  """Convert JSON from a gtest perf test to Histograms.
+
+  Incoming data is in the following format:
+  {
+    'metric1': {
+      'units': 'unit1',
+      'traces': {
+        'story1': ['mean', 'std_dev'],
+        'story2': ['mean', 'std_dev'],
+      },
+      'important': ['testcase1', 'testcase2'],
+    },
+    'metric2': {
+      'units': 'unit2',
+      'traces': {
+        'story1': ['mean', 'std_dev'],
+        'story2': ['mean', 'std_dev'],
+      },
+      'important': ['testcase1', 'testcase2'],
+    },
+    ...
+  }
+  We ignore the 'important' fields and just assume everything should be
+  considered important.
+
+  We also don't bother adding any reserved diagnostics like mastername in this
+  script since that should be handled by the upload script.
+
+  Args:
+    gtest_json: A JSON dict containing perf output from a gtest
+
+  Returns:
+    A HistogramSet containing equivalent histograms and diagnostics
+  """
+
+  hs = histogram_set.HistogramSet()
+
+  for metric, metric_data in gtest_json.iteritems():
+    # Maintain the same unit if we're able to find an exact match, converting
+    # time units if possible. Otherwise use 'unitless'.
+    unit, multiplier = _ConvertUnit(metric_data.get('units'))
+
+    for story, story_data in metric_data['traces'].iteritems():
+      # We should only ever have the mean and standard deviation here.
+      assert len(story_data) == 2
+      h = histogram.Histogram(metric, unit)
+      h.diagnostics[reserved_infos.STORIES.name] = generic_set.GenericSet(
+          [story])
+      mean = float(story_data[0]) * multiplier
+      std_dev = float(story_data[1]) * multiplier
+      h.AddSample(mean)
+
+      # Synthesize the running statistics since we only have the mean + standard
+      # deviation instead of the actual data points.
+      h._running = histogram.RunningStatistics.FromDict([
+          2, # count, we need this to be >1 in order for variance to work
+          mean, # max
+          0, # meanlogs
+          mean, # mean
+          mean, # min
+          mean, # sum
+          std_dev * std_dev, # variance
+      ])
+
+      hs.AddHistogram(h)
+
+  return hs
+
+def ConvertGtestJsonFile(filepath):
+  """Convert JSON in a file from a gtest perf test to Histograms.
+
+  Contents of the given file will be overwritten with the new Histograms data.
+
+  Args:
+    filepath: The filepath to the JSON file to read/write from/to.
+  """
+  with open(filepath, 'r') as f:
+    data = json.load(f)
+  histograms = ConvertGtestJson(data)
+  with open(filepath, 'w') as f:
+    json.dump(histograms.AsDicts(), f)
+
+
+def _ConvertUnit(unit):
+  # We assume that smaller is better since we don't have an actual way to
+  # determine what the improvement direction is and most or all metrics from
+  # gtest perf tests have a downward improvement direction.
+  if unit in histogram.UNIT_NAMES:
+    return unit + '_smallerIsBetter', 1
+  # A number of existing gtest perf tests report time in units like
+  # microseconds, but histograms only support milliseconds. So, convert here if
+  # we can.
+  if unit == 'us':
+    return 'msBestFitFormat_smallerIsBetter', 1000
+  if unit == 'ns':
+    return 'msBestFitFormat_smallerIsBetter', 1000000
+  return 'unitless_smallerIsBetter', 1
---
+++ tracing/tracing/value/gtest_json_converter_unittest.py
@@ -0,0 +1,108 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from tracing.value import gtest_json_converter
+from tracing.value.diagnostics import reserved_infos
+
+
+NANO_TO_MILLISECONDS = 1000000
+
+
+class GtestJsonConverterUnittest(unittest.TestCase):
+
+  def testConvertBasic(self):
+    data = {
+        'metric1': {
+            'units': 'ms',
+            'traces': {
+                'story1': ['10.12345', '0.54321'],
+                'story2': ['30', '0'],
+            }
+        },
+        'metric2': {
+            'units': 'ns',
+            'traces': {
+                'story1': ['100000.0', '2543.543'],
+                'story2': ['12345.6789', '301.2'],
+            },
+        }
+    }
+    histograms = gtest_json_converter.ConvertGtestJson(data)
+    self.assertEqual(len(histograms), 4)
+
+    metric_histograms = histograms.GetHistogramsNamed('metric1')
+    self.assertEqual(len(metric_histograms), 2)
+    story1 = None
+    story2 = None
+    if metric_histograms[0].diagnostics[
+        reserved_infos.STORIES.name].GetOnlyElement() == 'story1':
+      story1 = metric_histograms[0]
+      story2 = metric_histograms[1]
+    else:
+      story2 = metric_histograms[0]
+      story1 = metric_histograms[1]
+
+    # assertAlmostEqual necessary to avoid floating point precision issues.
+    self.assertAlmostEqual(story1.average, 10.12345)
+    self.assertAlmostEqual(story1.standard_deviation, 0.54321)
+    self.assertEqual(story2.average, 30)
+    self.assertEqual(story2.standard_deviation, 0)
+    self.assertEqual(story1.unit, story2.unit)
+    self.assertEqual(story1.unit, 'ms_smallerIsBetter')
+
+    metric_histograms = histograms.GetHistogramsNamed('metric2')
+    self.assertEqual(len(metric_histograms), 2)
+    if metric_histograms[0].diagnostics[
+        reserved_infos.STORIES.name].GetOnlyElement() == 'story1':
+      story1 = metric_histograms[0]
+      story2 = metric_histograms[1]
+    else:
+      story2 = metric_histograms[0]
+      story1 = metric_histograms[1]
+
+    # assertAlmostEqual necessary to avoid floating point precision issues.
+    # We expect the numbers to be different than what was initially provided
+    # since this should be converted to milliseconds.
+    self.assertAlmostEqual(story1.average, 100000 * NANO_TO_MILLISECONDS)
+    self.assertAlmostEqual(story1.standard_deviation,
+                           2543.543 * NANO_TO_MILLISECONDS)
+    self.assertAlmostEqual(story2.average, 12345.6789 * NANO_TO_MILLISECONDS)
+    self.assertAlmostEqual(story2.standard_deviation,
+                           301.2 * NANO_TO_MILLISECONDS)
+    self.assertEqual(story1.unit, story2.unit)
+    self.assertEqual(story1.unit, 'msBestFitFormat_smallerIsBetter')
+
+  def testConvertUnknownUnit(self):
+    data = {
+        'metric1': {
+            'units': 'SomeUnknownUnit',
+            'traces': {
+                'story1': ['10', '1'],
+                'story2': ['123.4', '7.89'],
+            }
+        }
+    }
+    histograms = gtest_json_converter.ConvertGtestJson(data)
+    self.assertEqual(len(histograms), 2)
+
+    metric_histograms = histograms.GetHistogramsNamed('metric1')
+    self.assertEqual(len(metric_histograms), 2)
+    story1 = None
+    story2 = None
+    if metric_histograms[0].diagnostics[
+        reserved_infos.STORIES.name].GetOnlyElement() == 'story1':
+      story1 = metric_histograms[0]
+      story2 = metric_histograms[1]
+    else:
+      story2 = metric_histograms[0]
+      story1 = metric_histograms[1]
+
+    self.assertEqual(story1.average, 10)
+    self.assertEqual(story1.standard_deviation, 1)
+    self.assertAlmostEqual(story2.average, 123.4)
+    self.assertAlmostEqual(story2.standard_deviation, 7.89)
+    self.assertEqual(story1.unit, story2.unit)
+    self.assertEqual(story1.unit, 'unitless_smallerIsBetter')
