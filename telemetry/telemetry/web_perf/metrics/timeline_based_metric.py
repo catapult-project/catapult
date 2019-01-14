030e4644c80d04cb3f75f6e1cec22583a1592f00
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-01-14 09:42:24 +0000

    [Telemetry] Remove TBMv1 metrics.SmoothnessMetric
    This is now dead code, no longer used by any benchmark.
    
    Bug: chromium:691581
    Bug: chromium:921000
    Change-Id: Ia537614d4fe0a30d163d14bdd91c7d285c218762
    Reviewed-on: https://chromium-review.googlesource.com/c/1406993
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>

--- telemetry/telemetry/web_perf/metrics/timeline_based_metric.py
+++ telemetry/telemetry/web_perf/metrics/timeline_based_metric.py
@@ -46,8 +46,9 @@ def IsEventInInteractions(event, interaction_records):
 class TimelineBasedMetric(object):
 
   def __init__(self):
-    """Computes metrics from a telemetry.timeline Model and a range
+    """Computes TBMv1 metrics from a telemetry.timeline Model and a range.
 
+    This class is deprecated.
     """
     super(TimelineBasedMetric, self).__init__()
 
