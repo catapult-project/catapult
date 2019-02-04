c276258fdacf5bd626d43c1deb584653a1095d01
benshayden <benjhayden@chromium.org>
2019-02-04 12:29:02 -0800

    Reland add_reserved_diagnostics --max_size
    Some benchmarks are producing too much data for the chromeperf dashboard
    to handle in a single POST. This CL adds an option to
    add_reserved_diagnostics to split the results into multiple files.
    This option is currently unused, but will be used in
    https://cs.chromium.org/chromium/src/tools/perf/core/upload_results_to_perf_dashboard.py
    
    Bug: chromium:909961
    
    Revert: https://chromium-review.googlesource.com/c/catapult/+/1451976
    Initial: https://chromium-review.googlesource.com/c/catapult/+/1446275
    Change-Id: Ie1f5b0f7e5ac496b29792567cc7dce5a405f7c03
    Reviewed-on: https://chromium-review.googlesource.com/c/1452140
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

--- tracing/tracing/value/diagnostics/add_reserved_diagnostics_unittest.py
+++ tracing/tracing/value/diagnostics/add_reserved_diagnostics_unittest.py
@@ -26,6 +26,31 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
           [True])
     return h
 
+  def testStrict(self):
+    hs = histogram_set.HistogramSet()
+    hs.CreateHistogram('name', 'count', [])
+    self.assertEqual(1, len(add_reserved_diagnostics.Batch(
+        hs, 1000, strict=True)))
+    self.assertEqual(1, len(add_reserved_diagnostics.Batch(hs, 1)))
+    with self.assertRaises(ValueError):
+      add_reserved_diagnostics.Batch(hs, 1, strict=True)
+
+  def testBatch(self):
+    hs = histogram_set.HistogramSet([
+        self._CreateHistogram(
+            'measurement%d' % (i / 10), ['story%d' % (i % 10)])
+        for i in range(100)])
+    monolith = add_reserved_diagnostics.AddReservedDiagnostics(
+        hs.AsDicts(), {})
+    self.assertEqual(1, len(monolith))
+    self.assertGreater(len(monolith[0]), 100)
+    max_size = len(monolith[0]) / 10
+    results = add_reserved_diagnostics.AddReservedDiagnostics(
+        hs.AsDicts(), {}, max_size)
+    self.assertGreater(len(results), 10)
+    for part in results:
+      self.assertGreater(max_size, len(part))
+
   def testAddReservedDiagnostics_InvalidDiagnostic_Raises(self):
     hs = histogram_set.HistogramSet([
         self._CreateHistogram('foo')])
@@ -43,7 +68,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
         self._CreateHistogram('blah')])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -62,7 +87,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
         self._CreateHistogram('blah')])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -92,7 +117,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
         self._CreateHistogram('foo2', stories=['foo2'])])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -119,7 +144,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
         self._CreateHistogram('bar', stories=['bar'])])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -148,7 +173,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
         self._CreateHistogram('bar')])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -166,7 +191,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
     ])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -198,7 +223,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
     ])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
@@ -224,7 +249,7 @@ class AddReservedDiagnosticsUnittest(unittest.TestCase):
         self._CreateHistogram('foo', ['bar'], had_failures=True)])
 
     new_hs_json = add_reserved_diagnostics.AddReservedDiagnostics(
-        hs.AsDicts(), {'benchmarks': 'bar'})
+        hs.AsDicts(), {'benchmarks': 'bar'})[0]
 
     new_hs = histogram_set.HistogramSet()
     new_hs.ImportDicts(json.loads(new_hs_json))
