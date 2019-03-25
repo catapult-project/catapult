76f631ec041f528ffda590d7aedb39db675516b1
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-03-25 10:22:46 +0000

    [dashboard] Switch system health report to chrome.perf bots
    No longer use the internal bots which are due to be deprecated.
    
    Bug: chromium:907040
    Change-Id: I6d61baf8031f3d65f99a67bf8de0fb4e7bbb063d
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1535857
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Ben Hayden <benjhayden@chromium.org>
    Auto-Submit: Juan Antonio Navarro Pérez <perezju@chromium.org>

--- dashboard/dashboard/common/system_health_report.py
+++ dashboard/dashboard/common/system_health_report.py
@@ -91,17 +91,17 @@ def IterTemplateRows(browser, bot):
   yield dict(STARTUP_BY_BROWSER[browser], label='Startup:Time', bots=[bot])
 
 
-def CreateSystemHealthReport(template_id, name, builder, is_internal, modified):
+def CreateSystemHealthReport(template_id, name, builder, is_fyi, modified):
   # The browser (Chrome/WebView) is always the second part of the report name,
   # and is used to build the right template.
   browser = name.split(':')[1].lower()
-  master = 'ClankInternal' if is_internal else 'ChromiumPerf'
+  master = 'ChromiumPerfFyi' if is_fyi else 'ChromiumPerf'
   bot = ':'.join([master, builder])
 
   @report_template.Static(
       template_id=template_id,
       name=name,
-      internal_only=is_internal,
+      internal_only=False,
       modified=modified)
   def Report(revisions):
     # Template is updated on each call to the handler to make sure that we use
@@ -118,28 +118,28 @@ def CreateSystemHealthReport(template_id, name, builder, is_internal, modified):
 
 CreateSystemHealthReport(
     template_id=2013652838,
-    name='Health:Chrome:Android Go (Internal)',
-    builder='perf-go-phone-1024',
-    is_internal=True,
-    modified=datetime.datetime(2018, 8, 2))
+    name='Health:Chrome:Android Go',
+    builder='android-go-perf',
+    is_fyi=False,
+    modified=datetime.datetime(2019, 3, 22))
 
 CreateSystemHealthReport(
     template_id=434658613,
-    name='Health:Chrome:Nexus 5 (Internal)',
-    builder='health-plan-clankium-phone',
-    is_internal=True,
-    modified=datetime.datetime(2018, 8, 2))
+    name='Health:Chrome:Pixel 2',
+    builder='android-pixel2-perf',
+    is_fyi=True,
+    modified=datetime.datetime(2019, 3, 22))
 
 CreateSystemHealthReport(
     template_id=1371943537,
-    name='Health:WebView:Android Go (Internal)',
-    builder='perf-go-webview-phone',
-    is_internal=True,
-    modified=datetime.datetime(2018, 8, 2))
+    name='Health:WebView:Android Go',
+    builder='android-go_webview-perf',
+    is_fyi=False,
+    modified=datetime.datetime(2019, 3, 22))
 
 CreateSystemHealthReport(
     template_id=191176182,
-    name='Health:WebView:Nexus 5 (Internal)',
-    builder='health-plan-webview-phone',
-    is_internal=True,
-    modified=datetime.datetime(2018, 8, 2))
+    name='Health:WebView:Pixel 2',
+    builder='android-pixel2_webview-perf',
+    is_fyi=True,
+    modified=datetime.datetime(2019, 3, 22))
