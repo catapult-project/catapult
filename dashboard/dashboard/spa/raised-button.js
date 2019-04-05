437b0ff23cd59bc691273a00e4aa172fdd7cb1d8
Ben Hayden <benjhayden@chromium.org>
2019-04-05 14:47:35 -0700

    Migrate raised-button from dom-module to Polymer.html
    No functional changes.
    
    Bug: chromium:950011
    Change-Id: I3488ae6b1876dc370df445c2cc0cc088a0feae49
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1553937
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

--- dashboard/dashboard/spa/raised-button.js
+++ dashboard/dashboard/spa/raised-button.js
@@ -6,6 +6,53 @@
 (() => {
   class RaisedButton extends Polymer.Element {
     static get is() { return 'raised-button'; }
+
+    static get template() {
+      return Polymer.html`
+        <style>
+          :host {
+            align-items: center;
+            background-color: var(--primary-color-light, lightblue);
+            background-position: center;
+            border-radius: 24px;
+            border: 0px none hsl(231, 50%, 50%);
+            box-shadow: var(--elevation-1);
+            color: var(--primary-color-dark, blue);
+            cursor: pointer;
+            display: flex;
+            height: var(--icon-size, 24px);
+            justify-content: center;
+            margin: 4px 8px;
+            padding: 4px 8px;
+            text-transform: uppercase;
+            transition: background var(--transition-long, 1s);
+            user-select: none;
+          }
+          * {
+            flex-grow: 1
+          }
+          :host([disabled]) {
+            background-color: var(--neutral-color-light, lightgrey);
+            box-shadow: none;
+            color: var(--neutral-color-dark, grey);
+            cursor: auto;
+            pointer-events: none;
+          }
+          :host(:hover) {
+            background: var(--primary-color-light, lightgrey) radial-gradient(
+              circle, transparent 1%, var(--primary-color-light, lightgrey) 1%)
+              center/15000%;
+          }
+          :host(:active) {
+            background-color: var(--primary-color-medium, dodgerblue);
+            background-size: 100%;
+            transition: background 0s;
+          }
+        </style>
+
+        <slot></slot>
+      `;
+    }
   }
   customElements.define(RaisedButton.is, RaisedButton);
 })();
