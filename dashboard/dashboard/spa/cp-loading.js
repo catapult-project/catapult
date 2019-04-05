027339630567d8d16c8af8673325dfdaec0b08a1
Ben Hayden <benjhayden@chromium.org>
2019-04-05 10:45:37 -0700

    Migrate cp-loading from dom-module to Polymer.html
    No functional changes.
    
    Bug: chromium:950011
    Change-Id: I3eabcb5233c7b4c82393d2dbbf510ad981f8d389
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1553730
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

--- dashboard/dashboard/spa/cp-loading.js
+++ dashboard/dashboard/spa/cp-loading.js
@@ -6,6 +6,76 @@
 (() => {
   class CpLoading extends Polymer.Element {
     static get is() { return 'cp-loading'; }
+
+    static get template() {
+      return Polymer.html`
+        <style>
+          :host {
+            display: block;
+            height: 4px;
+            overflow: hidden;
+            position: relative;
+            width: 100%;
+          }
+          div {
+            border-radius: 50%;
+            height: 4px;
+            overflow-x: hidden;
+            position: absolute;
+            transform-origin: left center;
+            transform: scaleX(0);
+            width: 100%;
+            will-change: transform;
+          }
+          div:after {
+            content: "";
+            position: absolute;
+            height: 5px;
+          }
+          :host([loading]) div {
+            background: var(--primary-color-dark, blue);
+            transform-origin: right center;
+            animation: indeterminate-bar 2s linear infinite;
+          }
+          :host([loading]) div:after {
+            content: "";
+            transform-origin: center center;
+            animation: indeterminate-splitter 2s linear infinite;
+          }
+          @keyframes indeterminate-bar {
+            0% {
+              transform: scaleX(1) translateX(-100%);
+            }
+            50% {
+              transform: scaleX(1) translateX(0%);
+            }
+            75% {
+              transform: scaleX(1) translateX(0%);
+              animation-timing-function: cubic-bezier(.28,.62,.37,.91);
+            }
+            100% {
+              transform: scaleX(0) translateX(0%);
+            }
+          }
+          @keyframes indeterminate-splitter {
+            0% {
+              transform: scaleX(.75) translateX(-125%);
+            }
+            30% {
+              transform: scaleX(.75) translateX(-125%);
+              animation-timing-function: cubic-bezier(.42,0,.6,.8);
+            }
+            90% {
+              transform: scaleX(.75) translateX(125%);
+            }
+            100% {
+              transform: scaleX(.75) translateX(125%);
+            }
+          }
+        </style>
+        <div></div>
+      `;
+    }
   }
   customElements.define(CpLoading.is, CpLoading);
 })();
