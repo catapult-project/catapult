27980609ac44bfbce0fd4d752f7c1efd5f32d58a
Ben Hayden <benjhayden@chromium.org>
2019-04-05 10:35:06 -0700

    Migrate place-holder from dom-module to Polymer.html
    No functional changes.
    
    Bug: chromium:950011
    Change-Id: I578df26a8ccc26aa861d330e0066f6bcbcb9a49f
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1553929
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>

--- dashboard/dashboard/spa/place-holder.js
+++ dashboard/dashboard/spa/place-holder.js
@@ -6,6 +6,55 @@
 tr.exportTo('cp', () => {
   class PlaceHolder extends Polymer.Element {
     static get is() { return 'place-holder'; }
+
+    static get template() {
+      return Polymer.html`
+        <style>
+          :host {
+            position: relative;
+            display: flex;
+          }
+
+          #container {
+            position: absolute;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          #content {
+            background: var(--background-color, white);
+            padding: 8px;
+          }
+
+          :host, #container, svg {
+            width: 100%;
+            height: 100%;
+          }
+
+          pattern rect {
+            fill: var(--neutral-color-light, lightgrey);
+          }
+        </style>
+
+        <div id="container">
+          <div id="content">
+            <slot></slot>
+          </div>
+        </div>
+
+        <svg>
+          <pattern id="pattern"
+                  x="0" y="0" width="20" height="20"
+                  patternUnits="userSpaceOnUse">
+            <rect x="0" width="10" height="10" y="0" />
+            <rect x="10" width="10" height="10" y="10" />
+          </pattern>
+
+          <rect fill="url(#pattern)" x="0" y="0" width="100%" height="100%" />
+        </svg>
+      `;
+    }
   }
 
   customElements.define(PlaceHolder.is, PlaceHolder);
