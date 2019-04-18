9f474a8e0e8141698551a4a0aebb6e591cb74b4d
Ben Hayden <benjhayden@chromium.org>
2019-04-18 15:37:25 -0700

    Migrate cp-switch to es6 module
    No functional changes.
    
    Bug: chromium:950011
    Change-Id: Idef8116114099a608525047a17b4fd654823782c
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1574106
    Commit-Queue: Ben Hayden <benjhayden@chromium.org>
    Reviewed-by: Tiffany Zhang <zhangtiff@chromium.org>

--- dashboard/dashboard/spa/cp-switch.js
+++ dashboard/dashboard/spa/cp-switch.js
@@ -3,83 +3,82 @@
    found in the LICENSE file.
 */
 'use strict';
-(() => {
-  class CpSwitch extends Polymer.Element {
-    static get is() { return 'cp-switch'; }
 
-    static get template() {
-      return Polymer.html`
-        <style>
-          :host {
-            padding: 8px;
-          }
-          input {
-            display: none;
-          }
-          label {
-            position: relative;
-            cursor: pointer;
-            padding: 8px 0 8px 44px;
-          }
-          label:before, label:after {
-            content: "";
-            position: absolute;
-            margin: 0;
-            outline: 0;
-            top: 50%;
-            transform: translate(0, -50%);
-            transition: all var(--transition-short, 0.2s) ease;
-          }
-          label:before {
-            left: 1px;
-            width: 34px;
-            height: 14px;
-            background-color: var(--neutral-color-dark, grey);
-            border-radius: 8px;
-          }
-          label:after {
-            background-color: var(--neutral-color-light, lightgrey);
-            border-radius: 50%;
-            box-shadow: var(--elevation-2);
-            height: 20px;
-            left: 0;
-            width: 20px;
-          }
-          input:checked + label:before {
-            background-color: var(--primary-color-medium, lightblue);
-          }
-          input:checked + label:after {
-            background-color: var(--primary-color-dark, blue);
-            transform: translate(80%, -50%);
-          }
-        </style>
+export default class CpSwitch extends Polymer.Element {
+  static get is() { return 'cp-switch'; }
 
-        <input
-            type="checkbox"
-            id="native"
-            checked="{{checked}}"
-            disabled$="[[disabled]]"
-            on-change="onChange_">
-        <label for="native"><slot></slot></label>
-      `;
-    }
+  static get template() {
+    return Polymer.html`
+      <style>
+        :host {
+          padding: 8px;
+        }
+        input {
+          display: none;
+        }
+        label {
+          position: relative;
+          cursor: pointer;
+          padding: 8px 0 8px 44px;
+        }
+        label:before, label:after {
+          content: "";
+          position: absolute;
+          margin: 0;
+          outline: 0;
+          top: 50%;
+          transform: translate(0, -50%);
+          transition: all var(--transition-short, 0.2s) ease;
+        }
+        label:before {
+          left: 1px;
+          width: 34px;
+          height: 14px;
+          background-color: var(--neutral-color-dark, grey);
+          border-radius: 8px;
+        }
+        label:after {
+          background-color: var(--neutral-color-light, lightgrey);
+          border-radius: 50%;
+          box-shadow: var(--elevation-2);
+          height: 20px;
+          left: 0;
+          width: 20px;
+        }
+        input:checked + label:before {
+          background-color: var(--primary-color-medium, lightblue);
+        }
+        input:checked + label:after {
+          background-color: var(--primary-color-dark, blue);
+          transform: translate(80%, -50%);
+        }
+      </style>
 
-    click() {
-      this.$.native.click();
-    }
-
-    onChange_(event) {
-      this.dispatchEvent(new CustomEvent('change', {
-        bubbles: true,
-        composed: true,
-      }));
-    }
+      <input
+          type="checkbox"
+          id="native"
+          checked="{{checked}}"
+          disabled$="[[disabled]]"
+          on-change="onChange_">
+      <label for="native"><slot></slot></label>
+    `;
   }
 
-  CpSwitch.properties = {
-    checked: {type: Boolean},
-    disabled: {type: Boolean},
-  };
+  click() {
+    this.$.native.click();
+  }
 
-  customElements.define(CpSwitch.is, CpSwitch);
-})();
+  onChange_(event) {
+    this.dispatchEvent(new CustomEvent('change', {
+      bubbles: true,
+      composed: true,
+    }));
+  }
+}
+
+CpSwitch.properties = {
+  checked: {type: Boolean},
+  disabled: {type: Boolean},
+};
+
+customElements.define(CpSwitch.is, CpSwitch);
--- dashboard/dashboard/spa/index.js
+++ dashboard/dashboard/spa/index.js
@@ -37,10 +37,11 @@ tr.exportTo('window', () => {
 });
 
 import './chromeperf-app.js';
+import './cp-checkbox.js';
 import './cp-dialog.js';
 import './cp-input.js';
+import './cp-loading.js';
+import './cp-switch.js';
 import './cp-tab-bar.js';
 import './cp-tab.js';
 import './cp-toast.js';
-import './cp-checkbox.js';
-import './cp-loading.js';
