30511b0a6c71505679c9d82514febe971d10d990
Mikhail Khokhlov <khokhlov@google.com>
2019-04-18 08:51:02 +0000

    Revert "Skip non-module dirs when discovering classes"
    This reverts commit d631df5aa657a54cd7cd89d57d3d7a68bee0fe5d.
    
    Reason for revert: CronetPerfTestBenchmark fails, crbug.com/953776
    
    Original change's description:
    > Skip non-module dirs when discovering classes
    > 
    > Some directories may contain .py files (e.g. some executable scripts)
    > but not __init__.py file. So they are technically not python modules
    > and trying to import from them will fail.
    > 
    > The proposed fix is to skip directories without __init__.py file
    > when discovering classes.
    > 
    > Bug: chromium:952789
    > Change-Id: If7327419a06d82cdf4463e02a0f7b47b1bdc42d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1567486
    > Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    
    TBR=crouleau@chromium.org,khokhlov@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:952789, chromium:953776
    Change-Id: I07f5a2c11537c8bb38e8b6dc81d2154d705b819c
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1571656
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>

--- common/py_utils/py_utils/test_data/discoverable_classes/non_module_dir/not_a_module.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2019 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-""" This file will fail to import because there's no __init__.py in its folder.
-This is a test case for the py_utils/discover.py.
-See crbug.com/952789 for details.
-"""
-
