a41edbef7e5cf0f76a94330586af1b97d91cea38
Nate Fischer <ntfschr@chromium.org>
2019-07-17 14:35:21 -0700

    Devil: document script_common methods
    No change to logic, only docs.
    
    This improves documentation for a few methods in script_common, since it
    was previously unclear how to use the methods. This also mentions
    script_common.InitializeEnvironment() should not be called in other
    projects (ex. chromium prefers devil_chromium.Initialize()).
    
    Bug: chromium:978697
    Test: N/A (docs-only change)
    Change-Id: Ib351faa3c39cd91ad2350004fb3d6469c41738ed
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1706901
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>

--- devil/devil/android/tools/script_common.py
+++ devil/devil/android/tools/script_common.py
@@ -11,13 +11,38 @@ from devil.android import device_utils
 
 
 def AddEnvironmentArguments(parser):
-  """Adds environment-specific arguments to the provided parser."""
+  """Adds environment-specific arguments to the provided parser.
+
+  After adding these arguments, you must pass the user-specified values when
+  initializing devil. See the InitializeEnvironment() to determine how to do so.
+
+  Args:
+    parser: an instance of argparse.ArgumentParser
+  """
   parser.add_argument(
       '--adb-path', type=os.path.realpath,
       help='Path to the adb binary')
 
 
 def InitializeEnvironment(args):
+  """Initializes devil based on the args added by AddEnvironmentArguments().
+
+  This initializes devil, and configures it to use the adb binary specified by
+  the '--adb-path' flag (if provided by the user, otherwise this defaults to
+  devil's copy of adb). Although this is one possible way to initialize devil,
+  you should check if your project has prefered ways to initialize devil (ex.
+  the chromium project uses devil_chromium.Initialize() to have different
+  defaults for dependencies).
+
+  This method requires having previously called AddEnvironmentArguments() on the
+  relevant argparse.ArgumentParser.
+
+  Note: you should only initialize devil once, and subsequent calls to any
+  method wrapping devil_env.config.Initialize() will have no effect.
+
+  Args:
+    args: the parsed args returned by an argparse.ArgumentParser
+  """
   devil_dynamic_config = devil_env.EmptyConfig()
   if args.adb_path:
     devil_dynamic_config['dependencies'].update(
@@ -28,7 +53,11 @@ def InitializeEnvironment(args):
 
 
 def AddDeviceArguments(parser):
-  """Adds device and blacklist arguments to the provided parser."""
+  """Adds device and blacklist arguments to the provided parser.
+
+  Args:
+    parser: an instance of argparse.ArgumentParser
+  """
   parser.add_argument(
       '-d', '--device', dest='devices', action='append',
       help='Serial number of the Android device to use. (default: use all)')
