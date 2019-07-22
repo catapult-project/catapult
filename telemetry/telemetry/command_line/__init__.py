5edc9691c639cdad3146b82d5eafa35c07b51e7a
John Chen <johnchen@chromium.org>
2019-07-22 15:51:40 +0000

    Revert "[Telemetry] Implement new top level parser"
    This reverts commit 7cc5fbbb8580f2bf1c113d3ee1859cc584e85ea4.
    
    Reason for revert: Failures in telemetry_perf_unittests in chromium autoroll (https://crrev.com/c/1712017 and https://crrev.com/c/1710136)
    
    Original change's description:
    > [Telemetry] Implement new top level parser
    > 
    > This new top level parser, implemented using argparse, for the moment
    > just handles choosing a command (either list or run), and hands of
    > parsing of the rest of the args to the specific parser of the chosen
    > command.
    > 
    > The old "Help" command is also removed, relying instead on the default
    > behaviour of the --help flag.
    > 
    > Tests are added to make sure that various common command line patterns
    > are still parsed and interpreted correctly.
    > 
    > Bug: chromium:928275
    > Change-Id: I328892f7a4e29e057d7712bf7bd7d86a11ba5067
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1704197
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    
    TBR=perezju@chromium.org,crouleau@chromium.org,khokhlov@google.com
    
    Change-Id: I8964e734e7ba3f7dd1457597e5a15b510cb8cc2c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:928275
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1712448
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/command_line/__init__.py
+++ telemetry/telemetry/command_line/__init__.py
@@ -2,6 +2,4 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from telemetry.command_line.parser import ParseArgs
-from telemetry.command_line.parser import RunCommand
 from telemetry.command_line.parser import main
