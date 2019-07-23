249e608964291a3db12869d6158b6df5679c7a22
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-07-23 09:54:31 +0100

    Reland "[Telemetry] Implement new top level parser"
    This is a reland of 7cc5fbbb8580f2bf1c113d3ee1859cc584e85ea4
    
    Original change's description:
    > [Telemetry] Implement new top level parser
    >
    > This new top level parser, implemented using argparse, for the moment
    > just handles choosing a command (either list or run), and hands of
    > parsing of the rest of the args to the specific parser of the chosen
    > command.
    >
    > The old "Help" command is also removed, relying instead on the default
    > behaviour of the --help flag.
    >
    > Tests are added to make sure that various common command line patterns
    > are still parsed and interpreted correctly.
    >
    > Bug: chromium:928275
    > Change-Id: I328892f7a4e29e057d7712bf7bd7d86a11ba5067
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1704197
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    
    TBR=crouleau@chromium.org
    Bug: chromium:928275
    Change-Id: I774ca72d216f63d9d6634c5a44dc2c38e07479b8
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1713628
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>

--- telemetry/telemetry/command_line/__init__.py
+++ telemetry/telemetry/command_line/__init__.py
@@ -2,4 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from telemetry.command_line.parser import ParseArgs
+from telemetry.command_line.parser import RunCommand
 from telemetry.command_line.parser import main
--- telemetry/telemetry/command_line/parser.py
+++ telemetry/telemetry/command_line/parser.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 """Builds the command line parser, processes args, and runs commands."""
 
+import argparse
 import logging
 import sys
 
@@ -16,61 +17,129 @@ DEFAULT_LOG_FORMAT = (
     '%(message)s')
 
 
-def main(environment):
+_COMMANDS = {
+    'run': commands.Run,
+    'list': commands.List,
+}
+
+
+def ArgumentParser(results_arg_parser=None):
+  """Build the top level argument parser.
+
+  Currently this only defines two (mostly) empty parsers for 'run' and 'list'
+  commands. After the selected command is parsed from the command line,
+  remaining unknown args may be passed to the respective legacy opt_parser of
+  the chosen command.
+
+  TODO(crbug.com/981349): Other options should be migrated away from optparse
+  and registered here instead using the corresponding argparse methods.
+
+  Args:
+    results_arg_parser: An optional parser defining extra command line options
+      for an external results_processor. These are appended to the options of
+      the 'run' command.
+  Returns:
+    An argparse.ArgumentParser object.
+  """
+  parser = argparse.ArgumentParser(
+      description='Command line tool to run performance benchmarks.',
+      epilog='To get help about a command use e.g.: %(prog)s run --help')
+  subparsers = parser.add_subparsers(dest='command', title='commands')
+  subparsers.required = True
+
+  subparsers.add_parser(
+      'run',
+      help='run a benchmark (default)',
+      description='Run a benchmark.',
+      parents=[results_arg_parser] if results_arg_parser else [],
+      add_help=False)
+
+  subparsers.add_parser(
+      'list',
+      help='list benchmarks or stories',
+      description='List available benchmarks or stories.',
+      add_help=False)
+
+  return parser
+
+
+def ParseArgs(environment, args=None, results_arg_parser=None):
+  """Parse command line arguments.
+
+  Args:
+    environment: A ProjectConfig object with information about the benchmark
+      runtime environment.
+    args: An optional list of arguments to parse. Defaults to obtain the
+      arguments from sys.argv.
+    results_arg_parser: An optional parser defining extra command line options
+      for an external results_processor.
+
+  Returns:
+    An options object with the values parsed from the command line.
+  """
+  if args is None:
+    args = sys.argv[1:]
+  if len(args) > 0:
+    if args[0] == 'help':
+      # The old command line allowed "help" as a command. Now just translate
+      # to "--help" to teach users about the new interface.
+      args[0] = '--help'
+    elif args[0] not in ['list', 'run', '-h', '--help']:
+      args.insert(0, 'run')  # Default command.
+
+  # TODO(crbug.com/981349): When optparse is gone, this should just call
+  # parse_args on the fully formed parser as returned by.ArgumentParser.
+  # For now we still need allow unknown args, which are then passed below to
+  # the legacy parsers.
+  parser = ArgumentParser(results_arg_parser)
+  parsed_args, unknown = parser.parse_known_args(args)
+
+  # TODO(crbug.com/981349): Ideally, most of the following should be moved
+  # to after argument parsing is completed and before (or at the time) when
+  # arguments are processed.
+
   # The log level is set in browser_options.
   # Clear the log handlers to ensure we can set up logging properly here.
   logging.getLogger().handlers = []
   logging.basicConfig(format=DEFAULT_LOG_FORMAT)
 
-  ps_util.EnableListingStrayProcessesUponExitHook()
-
-  # Get the command name from the command line.
-  if len(sys.argv) > 1 and sys.argv[1] == '--help':
-    sys.argv[1] = 'help'
-
-  command_name = 'run'
-  for arg in sys.argv[1:]:
-    if not arg.startswith('-'):
-      command_name = arg
-      break
-
-  # TODO(eakuefner): Remove this hack after we port to argparse.
-  if command_name == 'help' and len(sys.argv) > 2 and sys.argv[2] == 'run':
-    command_name = 'run'
-    sys.argv[2] = '--help'
-
-  # Validate and interpret the command name.
-  matching_commands = commands.MatchingCommands(command_name)
-  if len(matching_commands) > 1:
-    print >> sys.stderr, (
-        '"%s" is not a %s command. Did you mean one of these?' %
-        (command_name, commands.ScriptName()))
-    for command in matching_commands:
-      print >> sys.stderr, '  %-10s %s' % (command.Name(),
-                                           command.Description())
-    return 1
-  if matching_commands:
-    command = matching_commands[0]
-  else:
-    command = commands.Run
-
   binary_manager.InitDependencyManager(environment.client_configs)
 
-  # Parse and run the command.
-  parser = command.CreateParser()
-  command.AddCommandLineArgs(parser, environment)
+  command = _COMMANDS[parsed_args.command]
+  opt_parser = command.CreateParser()
+  command.AddCommandLineArgs(opt_parser, environment)
 
   # Set the default chrome root variable.
-  parser.set_defaults(chrome_root=environment.default_chrome_root)
+  opt_parser.set_defaults(chrome_root=environment.default_chrome_root)
 
-  options, args = parser.parse_args()
-  if matching_commands:
-    args = args[1:]
-  options.positional_args = args
-  command.ProcessCommandLineArgs(parser, options, environment)
+  options, positional_args = opt_parser.parse_args(unknown)
+  options.positional_args = positional_args
+  command.ProcessCommandLineArgs(opt_parser, options, environment)
 
-  return_code = command().Run(options)
+  # Merge back our argparse args with the optparse options.
+  for arg in vars(parsed_args):
+    setattr(options, arg, getattr(parsed_args, arg))
+  return options
+
+
+def RunCommand(options):
+  """Run a selected command from parsed command line args.
+
+  Args:
+    options: The return value from ParseArgs.
+
+  Returns:
+    The exit_code from the command execution.
+  """
+  ps_util.EnableListingStrayProcessesUponExitHook()
+  return_code = _COMMANDS[options.command]().Run(options)
   if return_code == -1:
-    logging.warn('No stories were run.')
+    logging.warning('No stories were run.')
     return 0
   return return_code
+
+
+def main(environment):
+  """Parse command line arguments and immediately run the selected command."""
+  options = ParseArgs(environment)
+  return RunCommand(options)
