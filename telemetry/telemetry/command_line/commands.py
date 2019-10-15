955f75d036630b0cb9da26b3733942643ad2a695
Caleb Rouleau <crouleau@chromium.org>
2019-10-15 11:54:58 -0700

    [Telemetry] Refactor expectations code into story_filter.py.
    Before this change, expectations code was littered around in several
    places including benchmark.py, story_runner.py, commands.py, etc.
    Each of those modules needed their own unit tests for
    expectations-related behavior as well. This change factors out all the
    expectations-related code into a single ShouldSkip function in
    story_filter.py. story_filter.py comes to have two related
    functionalities:
    1. Filtering stories that should not be run or marked skipped.
    2. Skipping stories that should be skipped.
    
    In the future, we want to support a flag such as
    --story=<exact story name> (or maybe --stories=<list of
    exact story names>). For that feature, we will need to
    augment both 1 and 2 because:
    1. We should filter out any story that does not match
    that flag.
    2. We should probably ignore expectations if someone passes
    an exact story name. (Or maybe make this configurable?)
    
    The point of the above is that this story filtering and story skipping
    code is related and could benefit from being part of a single module.
    
    Note that this also allows improvements to the unit test
    code. In a few places, we can now mock out story_filter instead of
    having to build fake expectations. Making changes to story filtering
    or to expectations will not require editing any story_runner_unittests.
    
    Also, turn typ_expectations_unittest.py into a real unit test
    that only tests its own functionality.
    
    Note that the options_for_unittests.py change is required to
    enable system_health_smoke_tests.py and benchmark_smoke_tests.py
    to pass expectations to story_filter (via story_runner). The
    change to those chromium-side smoke tests is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1854794
    
    Bug: chromium:985103
    Change-Id: I966e9541b2df1e17f561077d119eea1c179e35d2
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1839176
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>

--- telemetry/telemetry/command_line/commands.py
+++ telemetry/telemetry/command_line/commands.py
@@ -6,7 +6,6 @@
 import json
 import logging
 import optparse
-import os
 import sys
 
 from telemetry import benchmark
@@ -14,13 +13,6 @@ from telemetry.internal.browser import browser_finder
 from telemetry.internal.browser import browser_options
 from telemetry.internal import story_runner
 from telemetry.util import matching
-
-
-def _SetExpectations(bench, path):
-  if path and os.path.exists(path):
-    with open(path) as fp:
-      bench.AugmentExpectationsWithFile(fp.read())
-  return bench.expectations
 
 
 def _IsBenchmarkSupported(benchmark_, possible_browser):
@@ -226,11 +218,6 @@ class Run(object):
   @classmethod
   def ProcessCommandLineArgs(cls, parser, options, environment):
     all_benchmarks = environment.GetBenchmarks()
-    if environment.expectations_files:
-      assert len(environment.expectations_files) == 1
-      cls._expectations_path = environment.expectations_files[0]
-    else:
-      cls._expectations_path = None
     if not options.positional_args:
       possible_browser = (browser_finder.FindBrowser(options)
                           if options.browser_type else None)
@@ -254,14 +241,13 @@ class Run(object):
     assert issubclass(benchmark_class,
                       benchmark.Benchmark), ('Trying to run a non-Benchmark?!')
 
-    story_runner.ProcessCommandLineArgs(parser, options)
+    story_runner.ProcessCommandLineArgs(parser, options, environment)
     benchmark_class.ProcessCommandLineArgs(parser, options)
 
     cls._benchmark = benchmark_class
 
   def Run(self, options):
     b = self._benchmark()
-    _SetExpectations(b, self._expectations_path)
     return min(255, b.Run(options))
 
 
