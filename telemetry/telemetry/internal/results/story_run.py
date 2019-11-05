bce667b745aac67b5786eaf352f2c140564c91a7
Juan Antonio Navarro Perez <perezju@chromium.org>
2019-11-05 17:23:42 +0000

    [Telemetry] Do not percent encode story names
    This makes it harder to properly build URLs containing this testPaths.
    To get the correct path it forces one to double escape % sings.
    
    Bug: chromium:981349
    Change-Id: I0fd1b83c88604c569ebb3676432b47e74e17bcb3
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1899432
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Auto-Submit: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>

--- telemetry/telemetry/internal/results/story_run.py
+++ telemetry/telemetry/internal/results/story_run.py
@@ -10,7 +10,6 @@ import numbers
 import os
 import posixpath
 import time
-import urllib
 
 
 PASS = 'PASS'
@@ -210,15 +209,10 @@ class StoryRun(object):
 
   @property
   def test_path(self):
-    # Some stories use URLs as names, often containing special characters.
-    # To avoid potential issues, and to make it easy to identify the components
-    # on a test_path, we percent encode those special chars.
-    # TODO(crbug.com/983993): Remove this when all stories have good names.
-    story_name = urllib.quote(self.story.name, safe='')
     if self._test_prefix is not None:
-      return '/'.join([self._test_prefix, story_name])
+      return '/'.join([self._test_prefix, self.story.name])
     else:
-      return story_name
+      return self.story.name
 
   @property
   def tbm_metrics(self):
