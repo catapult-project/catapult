0201b8a09b8803dca187630899946d1dc7cfecca
Brian Sheedy <bsheedy@chromium.org>
2019-11-21 21:48:34 +0000

    Revert "Implement standard minidump methods for Android"
    This reverts commit 6da205e7f538b67867df8f0a5b9380fa61779f59.
    
    Reason for revert: Blocking Catapult roll.
    
    Original change's description:
    > Implement standard minidump methods for Android
    > 
    > Implements the standard set of minidump-related methods such as
    > SymbolizeMinidump on Android. These are implemented on all other
    > platforms, so this brings Android in line to have consistent behavior.
    > 
    > Bug: chromium:1008075
    > Change-Id: I30bdb8e31d9c658ecbbe9752120a635d420b03df
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1913187
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    
    TBR=perezju@chromium.org,crouleau@chromium.org,bsheedy@chromium.org
    
    Change-Id: I96a8f7f50eef7b791df69ac024726d3ac274d01f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1008075
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1929852
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>

--- telemetry/telemetry/internal/backends/chrome/android_browser_backend_unittest.py
+++ telemetry/telemetry/internal/backends/chrome/android_browser_backend_unittest.py
@@ -2,9 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-import posixpath
-
 from telemetry.testing import browser_backend_test_case
 from telemetry import decorators
 
@@ -15,71 +12,3 @@ class AndroidBrowserBackendTest(
   @decorators.Enabled('android')
   def testProfileDir(self):
     self.assertIsNotNone(self._browser_backend.profile_directory)
-
-  @decorators.Enabled('android')
-  def testPullMinidumps(self):
-    """Test that minidumps can be pulled off the device and their mtimes set."""
-    def GetDumpLocation():
-      return '/sdcard/dumps/'
-
-    platform_backend = self._browser_backend.platform_backend
-    time_offset = platform_backend.GetDeviceHostClockOffset()
-    platform_backend.GetDumpLocation = GetDumpLocation
-    remote_path = posixpath.join(GetDumpLocation(), 'Crashpad', 'pending')
-    self._browser_backend.device.RunShellCommand(['mkdir', '-p', remote_path])
-    # Android's implementation of "touch" doesn't support setting time via
-    # Unix timestamps, only via dates, which are affected by timezones. So,
-    # figure out what the device's timestamp for January 2nd, 1970 is and use
-    # that to calculate the expected local timestamp. January 2nd is used
-    # instead of January 1st so that we can't get accidentally get a negative
-    # timestamp if the host-device clock offset is negative.
-    remote_dump_file = posixpath.join(remote_path, 'test_dump')
-    self._browser_backend.device.RunShellCommand(
-        ['touch', '-d', '1970-01-0200:00:00', remote_dump_file])
-    device_mtime = self._browser_backend.device.RunShellCommand(
-        ['stat', '-c', '%Y', remote_dump_file], single_line=True)
-    device_mtime = int(device_mtime.strip())
-    try:
-      self._browser_backend.PullMinidumps()
-    finally:
-      self._browser_backend.device.RemovePath(GetDumpLocation(), recursive=True)
-
-    local_path = os.path.join(
-        self._browser_backend._tmp_minidump_dir, 'test_dump')
-    self.assertTrue(os.path.exists(local_path))
-    self.assertEqual(os.path.getmtime(local_path), device_mtime - time_offset)
-
-  @decorators.Enabled('android')
-  def testPullMinidumpsOnlyNew(self):
-    """Tests that a minidump is not pulled to the host if it already exists."""
-    def GetDumpLocation():
-      return '/sdcard/dumps/'
-
-    local_old_dump_path = os.path.join(
-        self._browser_backend._tmp_minidump_dir, 'old_dump')
-    with open(local_old_dump_path, 'w'):
-      pass
-    old_dump_time = os.stat(local_old_dump_path).st_mtime
-
-    platform_backend = self._browser_backend.platform_backend
-    platform_backend.GetDumpLocation = GetDumpLocation
-    remote_path = posixpath.join(GetDumpLocation(), 'Crashpad', 'pending')
-    self._browser_backend.device.RunShellCommand(['mkdir', '-p', remote_path])
-    remote_dump_file = posixpath.join(remote_path, 'new_dump')
-    self._browser_backend.device.RunShellCommand(
-        ['touch', '-d', '1970-01-0200:00:00', remote_dump_file])
-    remote_dump_file = posixpath.join(remote_path, 'old_dump')
-    self._browser_backend.device.RunShellCommand(
-        ['touch', '-d', '1970-01-0200:00:00', remote_dump_file])
-
-    try:
-      self._browser_backend.PullMinidumps()
-    finally:
-      self._browser_backend.device.RemovePath(GetDumpLocation(), recursive=True)
-
-    local_new_dump_path = os.path.join(
-        self._browser_backend._tmp_minidump_dir, 'new_dump')
-    self.assertTrue(os.path.exists(local_new_dump_path))
-    self.assertTrue(os.path.exists(local_old_dump_path))
-    # A changed mtime would mean that the dump was re-pulled
-    self.assertEqual(os.stat(local_old_dump_path).st_mtime, old_dump_time)
