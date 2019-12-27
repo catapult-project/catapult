726986fab08b9f0e6b5acf14f239ab025a73a27c
Mikhail Khokhlov <khokhlov@google.com>
2019-12-27 22:29:57 +0000

    Revert "Reland "[Telemetry] Make proto trace format the default""
    This reverts commit 383a5c9c96f1f4cbfcf9471dcc74bcaf64f5a404.
    
    Reason for revert: The roll still fails :-(
    https://chromium-review.googlesource.com/c/chromium/src/+/1982924
    
    Original change's description:
    > Reland "[Telemetry] Make proto trace format the default"
    > 
    > This is a reland of 599a74c479ba3a74756d636e178cc1b60371c425
    > 
    > Original change's description:
    > > [Telemetry] Make proto trace format the default
    > > 
    > > This will move the proto-to-json conversion off the device and decrease
    > > the time necessary to obtain the trace.
    > > 
    > > Bug: chromium:1028612
    > > Change-Id: Ic5bb5ec29439c3a3ed3067a32d6d23c546e204d5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1980690
    > > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > > Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    > 
    > Bug: chromium:1028612
    > Change-Id: I13c14053226c2e71f7d19ab973a717e839743e20
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1983160
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    
    TBR=crouleau@chromium.org,khokhlov@google.com
    
    Change-Id: Ia0f40fcac6bc2d63a9a49467c68266eb1acafbb2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1028612
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1983161
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>

--- telemetry/telemetry/benchmark.py
+++ telemetry/telemetry/benchmark.py
@@ -200,21 +200,15 @@ class Benchmark(command_line.Command):
       tbm_options.config.atrace_config.categories = categories
     if options and options.enable_systrace:
       tbm_options.config.chrome_trace_config.SetEnableSystrace()
-    legacy_json_format = options and options.legacy_json_trace_format
-    if legacy_json_format:
-      tbm_options.config.chrome_trace_config.SetJsonTraceFormat()
-    else:
+    enable_proto_format = options and options.experimental_proto_trace_format
+    if enable_proto_format:
       tbm_options.config.chrome_trace_config.SetProtoTraceFormat()
     # TODO(crbug.com/1012687): Remove or adjust the following warnings as the
     # development of TBMv3 progresses.
     tbmv3_metrics = [m[6:] for m in tbm_options.GetTimelineBasedMetrics()
                      if m.startswith('tbmv3:')]
     if tbmv3_metrics:
-      if legacy_json_format:
-        logging.warning(
-            'Selected TBMv3 metrics will not be computed because they are not '
-            "supported in Chrome's JSON trace format.")
-      else:
+      if enable_proto_format:
         logging.warning(
             'The following TBMv3 metrics have been selected to run: %s. '
             'Please note that TBMv3 is an experimental feature in active '
@@ -222,6 +216,12 @@ class Benchmark(command_line.Command):
             'current form. Follow crbug.com/1012687 for updates and to '
             'discuss your use case before deciding to rely on this feature.',
             ', '.join(tbmv3_metrics))
+      else:
+        logging.warning(
+            'Selected TBMv3 metrics will not be computed because they are not '
+            "supported in Chrome's default trace format. "
+            'Use --experimental-proto-trace-format if you want these metrics '
+            'to be computed.')
     return tbm_options
 
   def CreatePageTest(self, options):  # pylint: disable=unused-argument
