07e30470fa669dc45cd9b8cd52776bd65dd879d9
Brian Sheedy <bsheedy@chromium.org>
2020-01-09 14:23:25 -0800

    [Telemetry] Expose ChromeOS board tags
    Exposes the board of ChromeOS devices as a typ expectation tag, prefixed
    with 'chromeos-board-'. This allows tests to be disabled on specific
    boards if an issue is not ChromeOS-wide.
    
    Bug: chromium:1038726
    Change-Id: I5947ff53a6749b66bb26877fa36212b789132537
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1986876
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>

--- telemetry/telemetry/internal/platform/cros_platform_backend.py
+++ telemetry/telemetry/internal/platform/cros_platform_backend.py
@@ -136,4 +136,6 @@ class CrosPlatformBackend(
       tags.append('chromeos-local')
     else:
       tags.append('chromeos-remote')
+    if self.cri.GetBoard():
+      tags.append('chromeos-board-%s' % self.cri.GetBoard())
     return tags
