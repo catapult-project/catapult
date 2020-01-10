1f6ff4c2bb48ae54259ed498e453235542066531
Rakib Hasan <rmhasan@google.com>
2020-01-10 01:11:02 +0000

    Revert "[Telemetry] Expose ChromeOS board tags"
    This reverts commit 07e30470fa669dc45cd9b8cd52776bd65dd879d9.
    
    Reason for revert: The change is blocking the catapult autroll.
    
    The builder,https://ci.chromium.org/p/chromium/builders/try/chromeos-amd64-generic-rel/444410 shows that the telemetry.core.cros_interface_unittest.CrOSInterfaceTest.testGetBoard
    unit test is failing.
    
    Original change's description:
    > [Telemetry] Expose ChromeOS board tags
    > 
    > Exposes the board of ChromeOS devices as a typ expectation tag, prefixed
    > with 'chromeos-board-'. This allows tests to be disabled on specific
    > boards if an issue is not ChromeOS-wide.
    > 
    > Bug: chromium:1038726
    > Change-Id: I5947ff53a6749b66bb26877fa36212b789132537
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1986876
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > Reviewed-by: Rakib Hasan <rmhasan@google.com>
    
    TBR=achuith@chromium.org,bsheedy@chromium.org,rmhasan@google.com
    
    Change-Id: I50fac6e29b81eeb51f54878abbc0a87962df0c5c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1038726
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/1994387
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>

--- telemetry/telemetry/internal/platform/cros_platform_backend.py
+++ telemetry/telemetry/internal/platform/cros_platform_backend.py
@@ -136,6 +136,4 @@ class CrosPlatformBackend(
       tags.append('chromeos-local')
     else:
       tags.append('chromeos-remote')
-    if self.cri.GetBoard():
-      tags.append('chromeos-board-%s' % self.cri.GetBoard())
     return tags
