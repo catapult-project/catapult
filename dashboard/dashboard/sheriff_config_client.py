6ebcde89ed381dbfada3040c33236eaa3ecd652c
Chenlin Fan <fancl@chromium.org>
2020-01-30 12:12:42 +1100

    Fix import path error
    Fix using relative path in import. This will cause ImportError after
    deployed but can't be detected in unit test.
    
    Change-Id: I7590b8c031773370f33031a6f64a00800630a7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2029548
    Auto-Submit: Chenlin Fan <fancl@chromium.org>
    Reviewed-by: Andrew Bennetts <abennetts@google.com>
    Commit-Queue: Andrew Bennetts <abennetts@google.com>

--- dashboard/dashboard/sheriff_config_client.py
+++ dashboard/dashboard/sheriff_config_client.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from common.utils import GetEmail
+from dashboard.common.utils import GetEmail
 from dashboard import sheriff_config_pb2
 from dashboard.models.subscription import Subscription
 import google.auth
