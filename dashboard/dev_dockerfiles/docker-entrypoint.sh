b88007fbbbc3346438703d560bd89fee624e08fe
Andrew Bennetts <abennetts@google.com>
2020-02-21 05:40:56 +0000

    Revert "Fixing dashboard & sheriff-config push-on-green"
    This reverts commit c5d36f2533740cdb7e146be938b65131435e72f5.
    
    Reason for revert: Causing ImportError
    
    Original change's description:
    > Fixing dashboard & sheriff-config push-on-green
    > 
    > Merging Dockerfiles and scripts of dashboard push-on-green for
    > simplifying. Add script to wait port open for sheriff-config testing to
    > avoid random failure.
    > 
    > Change-Id: I92d5c75de60eafb7e3eefbde22606d6181806576
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2066999
    > Auto-Submit: Chenlin Fan <fancl@chromium.org>
    > Reviewed-by: Dean Berris <dberris@chromium.org>
    > Commit-Queue: Dean Berris <dberris@chromium.org>
    
    TBR=abennetts@google.com,dberris@chromium.org,fancl@chromium.org
    
    Change-Id: I3c67db76554bc0532a566a12fde4c885ca04ebbc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2067000
    Reviewed-by: Andrew Bennetts <abennetts@google.com>
    Commit-Queue: Andrew Bennetts <abennetts@google.com>

--- dashboard/dev_dockerfiles/docker-entrypoint.sh
+++ dashboard/dev_dockerfiles/docker-entrypoint.sh
@@ -9,24 +9,20 @@ init_catapult() {
   # The current catapult deploy process & unit testing will generate
   # files in the directory. To prevent making any changes to the mounted
   # directory, we can just make a copy instead.
-  if [[ -e /image/workspace ]]; then
-    cp -r /image/workspace /workspace
-    # TODO(dberris): This is a hack, which really shouldn't be required if we
-    # remove the requirement that the deployment script be running in a git
-    # repository.
-    mkdir -p /workspace/.git/hooks
-    cd /workspace/dashboard/dashboard
+  if [[ -e /image/catapult ]]; then
+    cp -r /image/catapult /
+    cd /catapult/dashboard/dashboard
     make clean && make
-    cd /workspace # All scripts in catapult assuming you in the repository
+    cd /catapult # All scripts in catapult assuming you in the repository
   fi
 }
 
 set_user_email() {
-  # We only need to set the user email when both /image/workspace exist and
+  # We only need to set the user email when both /image/catapult exist and
   # gcloud account configured. Because in other cases we don't neet to deploy
   # the service (gcloud not authed or without code)
   email=$(gcloud config get-value account 2>/dev/null)
-  if [[ -e /image/workspace ]] && ! [[ -z "${email}" ]]; then
+  if [[ -e /image/catapult ]] && ! [[ -z "${email}" ]]; then
     git config --add user.email "${email}"
   fi
 }
