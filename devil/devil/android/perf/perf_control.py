f9b607efef1b111fe56c6d99533dbe82966bfd1c
Caleb Rouleau <crouleau@chromium.org>
2020-03-20 08:51:06 -0700

    [Benchmarking] Limit Pixel2 cpu/gpu frequencies to reduce thermal throttling noise.
    If this is identified as a culprit for a performance regression,
    please duplicate the all regression bugs to
    https://bugs.chromium.org/p/chromium/issues/detail?id=1063432
    so that we can track all performance changes caused by this throttling.
    
    This change is expected to reduce noisiness of performance
    measurements for these devices.
    
    This may also improve the longevity of these devices by preventing
    them from getting too hot.
    
    Bug: chromium:383566
    Change-Id: Id09e5bf07a1210a31bf5fc8f895108defb56dbd2
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2110690
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>

--- devil/devil/android/perf/perf_control.py
+++ devil/devil/android/perf/perf_control.py
@@ -48,6 +48,30 @@ _PERFORMANCE_MODE_DEFINITIONS = {
         },
         'default_mode_governor': 'schedutil',
     },
+    'Pixel 2': {
+        'high_perf_mode': {
+            'bring_cpu_cores_online': True,
+            # These are set to roughly 7/8 of the max frequency. The purpose of
+            # this is to ensure that thermal throttling doesn't kick in midway
+            # through a test and cause flaky results. It should also improve the
+            # longevity of the devices by keeping them cooler.
+            'cpu_max_freq': {
+                '0..3': 1670400,
+                '4..7': 2208000,
+            },
+            'gpu_max_freq': 670000000,
+        },
+        'default_mode': {
+            # These are the maximum frequencies available for these CPUs and
+            # GPUs.
+            'cpu_max_freq': {
+                '0..3': 1900800,
+                '4..7': 2457600,
+            },
+            'gpu_max_freq': 710000000,
+        },
+        'default_mode_governor': 'schedutil',
+    },
     'GT-I9300': {
         'default_mode_governor': 'pegasusq',
     },
