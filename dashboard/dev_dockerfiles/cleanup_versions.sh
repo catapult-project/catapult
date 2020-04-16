5491b2a8263c0531e052376382de4f9525f1de74
Chenlin Fan <fancl@chromium.org>
2020-04-16 17:35:45 +1000

    Cleanup old versions when push-on-green
    Remove old versions (> 3 days and without traffic) when deploying new
    version. Old appengine flex version sometimes doesn't release the
    resources which causes we run out of quota. There is also a limit on
    the total number of versions.
    
    Change-Id: I160c1b821eae73854c236ebe2ed1bb2655b765db
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2152066
    Reviewed-by: Dean Berris <dberris@chromium.org>
    Commit-Queue: Dean Berris <dberris@chromium.org>
    Auto-Submit: Chenlin Fan <fancl@chromium.org>

---
+++ dashboard/dev_dockerfiles/cleanup_versions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x -e
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+for SERVICE in "$*"; do
+  gcloud app versions list \
+    --format="table[no-heading](VERSION.ID)" \
+    --filter="SERVICE=${SERVICE} AND
+              TRAFFIC_SPLIT=0 AND
+              LAST_DEPLOYED.date()<$(date -I --date='-3 day')" \
+  | xargs --no-run-if-empty gcloud app versions delete -s ${SERVICE}
+done
