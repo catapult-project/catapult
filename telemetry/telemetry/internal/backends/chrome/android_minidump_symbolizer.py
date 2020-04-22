94f3f64f183d8c3e280a91c7c169cf67006dcf89
Brian Sheedy <bsheedy@chromium.org>
2020-04-22 14:53:07 -0700

    Fix Android minidump symbolization w/ mixed bitness
    Fixes Android minidump symbolization in cases where there's the
    potential for mixing 32 and 64 bit, e.g. in 64 bit Monochrome. Instead
    of only returning <outdir>/lib.unstripped as a potential symbol binary
    location, AndroidMinidumpSymbolizer now also returns
    <outdir>/android_clang_arm/lib.unstripped and
    <outdir>/android_clang_arm64/lib.unstripped as potential locations.
    
    Also splits the minidump_dump logic into its own method and caches the
    result. This was necessary for an earlier attempt that tried to specify
    only a single possible symbol binary directory, but was kept around
    since it is an improvement.
    
    Bug: chromium:1068487
    Change-Id: I4a326123aa503398723137dfc6edc4391a609423
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2161866
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>

--- telemetry/telemetry/internal/backends/chrome/android_minidump_symbolizer.py
+++ telemetry/telemetry/internal/backends/chrome/android_minidump_symbolizer.py
@@ -27,6 +27,8 @@ class AndroidMinidumpSymbolizer(minidump_symbolizer.MinidumpSymbolizer):
           provided directory *must* be unique per browser binary, e.g. by
           including the hash of the binary in the directory name.
     """
+    # Map from minidump path (string) to minidump_dump output (string).
+    self._minidump_dump_output = {}
     # We use the OS/arch of the host, not the device.
     super(AndroidMinidumpSymbolizer, self).__init__(
         platform.system().lower(), platform.machine(), dump_finder, build_dir,
@@ -50,8 +52,25 @@ class AndroidMinidumpSymbolizer(minidump_symbolizer.MinidumpSymbolizer):
       minidump: The path to the minidump being symbolized.
     """
     libraries = self._ExtractLibraryNamesFromDump(minidump)
-    return [os.path.join(
-        self._build_dir, 'lib.unstripped', lib) for lib in libraries]
+    # The main lib.unstripped directory is usually the correct one, but in the
+    # case of mixed bitness (e.g. with Monochrome), a different directory might
+    # be necessary.
+    # We can pretty easily check the architecture the minidump came from.
+    # However, trying to use that to only use a single directory is prone to
+    # causing symbolization to fail due to using stale symbols, e.g. if the
+    # bitness for a build directory changes without `gn clean` being run in
+    # between. So, return all valid symbol locations. This could result in a
+    # bit of unnecessary symbol dumping, but guaranteeing a symbolized minidump
+    # is worth the few extra seconds.
+    default_path = os.path.join(self._build_dir, 'lib.unstripped')
+    arm_path = os.path.join(
+        self._build_dir, 'android_clang_arm', 'lib.unstripped')
+    arm64_path = os.path.join(
+        self._build_dir, 'android_clang_arm64', 'lib.unstripped')
+    binary_paths = [os.path.join(default_path, lib) for lib in libraries]
+    binary_paths.extend([os.path.join(arm_path, lib) for lib in libraries])
+    binary_paths.extend([os.path.join(arm64_path, lib) for lib in libraries])
+    return binary_paths
 
   def GetBreakpadPlatformOverride(self):
     return 'android'
@@ -66,12 +85,58 @@ class AndroidMinidumpSymbolizer(minidump_symbolizer.MinidumpSymbolizer):
       A list of strings containing library names of interest for symbols.
     """
     default_library_name = 'libmonochrome.so'
+
+    minidump_dump_output = self._GetMinidumpDumpOutput(minidump)
+    if not minidump_dump_output:
+      logging.warning(
+          'Could not get minidump_dump output, defaulting to library %s',
+          default_library_name)
+      return [default_library_name]
+
+    library_names = []
+    module_library_line_re = re.compile(r'[(]code_file[)]\s+= '
+                                        r'"(?P<library_name>lib[^. ]+.so)"')
+    in_module = False
+    for line in minidump_dump_output.splitlines():
+      line = line.lstrip().rstrip('\n')
+      if line == 'MDRawModule':
+        in_module = True
+        continue
+      if line == '':
+        in_module = False
+        continue
+      if in_module:
+        m = module_library_line_re.match(line)
+        if m:
+          library_names.append(m.group('library_name'))
+    if not library_names:
+      logging.warning(
+          'Could not find any library name in the dump, '
+          'default to: %s', default_library_name)
+      return [default_library_name]
+    return library_names
+
+  def _GetMinidumpDumpOutput(self, minidump):
+    """Runs minidump_dump on the given minidump.
+
+    Caches the result for re-use.
+
+    Args:
+      minidump: The path to the minidump being analyzed.
+
+    Returns:
+      A string containing the output of minidump_dump, or None if it could not
+      be retrieved for some reason.
+    """
+    if minidump in self._minidump_dump_output:
+      logging.debug('Returning cached minidump_dump output for %s', minidump)
+      return self._minidump_dump_output[minidump]
+
     dumper_path = os.path.join(self._build_dir, 'minidump_dump')
     if not os.access(dumper_path, os.X_OK):
-      logging.warning(
-          'Cannot extract library name from dump because %s is not found, '
-          'default to: %s', dumper_path, default_library_name)
-      return [default_library_name]
+      logging.warning('Cannot run minidump_dump because %s is not found.',
+                      dumper_path)
+      return None
 
     stdout = None
     try:
@@ -90,26 +155,6 @@ class AndroidMinidumpSymbolizer(minidump_symbolizer.MinidumpSymbolizer):
           'Reading minidump failed, but likely not actually an issue. Saving '
           'output to artifact %s', artifact_name)
       artifact_logger.CreateArtifact(artifact_name, stdout)
-
-    library_names = []
-    module_library_line_re = re.compile(r'[(]code_file[)]\s+= '
-                                        r'"(?P<library_name>lib[^. ]+.so)"')
-    in_module = False
-    for line in stdout.splitlines():
-      line = line.lstrip().rstrip('\n')
-      if line == 'MDRawModule':
-        in_module = True
-        continue
-      if line == '':
-        in_module = False
-        continue
-      if in_module:
-        m = module_library_line_re.match(line)
-        if m:
-          library_names.append(m.group('library_name'))
-    if not library_names:
-      logging.warning(
-          'Could not find any library name in the dump, '
-          'default to: %s', default_library_name)
-      return [default_library_name]
-    return library_names
+    if stdout:
+      self._minidump_dump_output[minidump] = stdout
+    return stdout
