5cf00e2674386de93da508c80e51dc3b5191f815
Brian Sheedy <bsheedy@chromium.org>
2020-08-06 14:42:24 -0700

    [Telemetry] Disable testGetMinidumpPathOnCrash
    Test is still flaky.
    
    Bug: chromium:1108432
    Change-Id: I06fccc96321e2590655e2120fe43132100bc0b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2341760
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/internal/browser/browser_unittest.py
+++ telemetry/telemetry/internal/browser/browser_unittest.py
@@ -90,7 +90,10 @@ class BrowserTest(browser_test_case.BrowserTestCase):
 
   # This test uses the reference browser and doesn't have access to
   # helper binaries like crashpad_database_util.
-  @decorators.Enabled('linux')  # https://crbug.com/1113416
+  # TODO(https://crbug.com/1108432: Re-enable on Linux once flakiness is fixed.
+  # TODO(https://crbug.com/1113416: Re-enable everywhere once non-Linux failures
+  # are fixed.
+  @decorators.Disabled('all')
   def testGetMinidumpPathOnCrash(self):
     tab = self._browser.tabs[0]
     with self.assertRaises(exceptions.AppCrashException):
