03f545114e8edb67fb725a03818f7b74f1c3de75
Wenbin Zhang <wenbinzhang@google.com>
2020-08-17 11:15:53 -0700

    [benchmarking] Fit GetPcSystemType for win platform backend
    This CL fixed the check on PcSystemType on windowns platform. The original usage of splitlines() will introduce empty elements. Using split() to avoid them.
    
    Bug: chromium:1114681
    Bug: chromium:1115156
    Change-Id: Ia603d0148562816ed85a29b6fe5ae46ade17c049
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2360310
    Auto-Submit: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/internal/platform/win_platform_backend.py
+++ telemetry/telemetry/internal/platform/win_platform_backend.py
@@ -105,9 +105,16 @@ class WinPlatformBackend(desktop_platform_backend.DesktopPlatformBackend):
 
   def GetPcSystemType(self):
     # use: wmic computersystem get pcsystemtype
-    return subprocess.Popen(
+    # the return value of the communicate() looks like:
+    #  ('PCSystemType  \r\r\nX             \r\r\n\r\r\n', None)
+    # where X represents the system type.
+    # More on: https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-computersystem
+    lines = subprocess.Popen(
         ['wmic', 'computersystem', 'get', 'pcsystemtype'],
-        stdout=subprocess.PIPE).communicate()[0].splitlines()[1]
+        stdout=subprocess.PIPE).communicate()[0].split()
+    if len(lines) > 1 and lines[0] == 'PCSystemType':
+      return lines[1]
+    return '0'
 
   def IsLaptop(self):
     # if the pcsystemtype value is 2, then it is mobile/laptop.
