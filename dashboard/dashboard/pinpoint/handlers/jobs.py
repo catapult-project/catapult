9105610c5f6217f1440065a204c7fd8387a8a6f8
Dean Michael Berris <dberris@chromium.org>
2021-02-25 14:38:49 +1100

    pinpoint: Enhance job listing API
    This includes two enhancements:
    
    * Fixup of the email address to hide the service account email for
      Chromeperf automation.
    
    * Introduction of a filtering mechanism to the /jobs API.
    
    This makes the Pinpoint UI's jobs page more deterministic and not
    dependent on whether a user is signed in to see a list of jobs.
    
    Future changes will allow us to change the UI and other consumers of the
    API to filter jobs at a more granular level.
    
    R=abennetts@google.com, fancl@google.com
    
    Bug: chromium:1059667, chromium:1148633
    Change-Id: I7a2568e4a5a7e48e6d10f27edf3f9fdebda158bb
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2717790
    Auto-Submit: Dean Berris <dberris@chromium.org>
    Reviewed-by: Chenlin Fan <fancl@chromium.org>
    Commit-Queue: Dean Berris <dberris@chromium.org>

--- dashboard/dashboard/pinpoint/handlers/jobs.py
+++ dashboard/dashboard/pinpoint/handlers/jobs.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import logging
 import json
 import webapp2
 
@@ -20,15 +21,21 @@ class Jobs(webapp2.RequestHandler):
   """Shows an overview of recent anomalies for perf sheriffing."""
 
   def get(self):
-    self.response.out.write(json.dumps(_GetJobs(self.request.get_all('o'))))
+    self.response.out.write(
+        json.dumps(
+            _GetJobs(self.request.get_all('o'),
+                     self.request.get_all('filter'))))
 
 
-def _GetJobs(options):
-  user = utils.GetEmail()
-  if user:
-    query = job_module.Job.query(job_module.Job.user == user)
-  else:
-    query = job_module.Job.query()
+def _GetJobs(options, query_filter):
+  query = job_module.Job.query()
+
+  # Query filters should a string as described in https://google.aip.dev/160
+  # For now we only support filtering on user, so we'll look for the 'user='
+  # field in the string.
+  for f in query_filter:
+    if f.startswith('user='):
+      query = query.filter(job_module.Job.user == f[len('user='):])
 
   query = query.order(-job_module.Job.created)
   job_future = query.fetch_async(limit=_MAX_JOBS_TO_FETCH)
@@ -41,7 +48,18 @@ def _GetJobs(options):
   }
 
   jobs = job_future.get_result()
+
+  def _FixupEmails(j):
+    if 'user' not in j:
+      return j
+    email = j['user']
+    if email == utils.ServiceAccountEmail():
+      email = 'chromeperf (automation)'
+    logging.debug('email = %s', email)
+    j['user'] = email
+    return j
+
   for job in jobs:
-    result['jobs'].append(job.AsDict(options))
+    result['jobs'].append(_FixupEmails(job.AsDict(options)))
 
   return result
--- dashboard/dashboard/pinpoint/handlers/jobs_test.py
+++ dashboard/dashboard/pinpoint/handlers/jobs_test.py
@@ -13,11 +13,15 @@ from dashboard.pinpoint.handlers import jobs
 from dashboard.pinpoint.models import job as job_module
 from dashboard.pinpoint.models import results2 as results2_module
 from dashboard.pinpoint import test
+from dashboard.common import utils
+
+_SERVICE_ACCOUNT_EMAIL = 'some-service-account@example.com'
 
 
 class JobsTest(test.TestCase):
 
-  @mock.patch.object(jobs.utils, 'GetEmail', mock.MagicMock(return_value=None))
+  @mock.patch.object(utils,
+                     'ServiceAccountEmail', lambda: _SERVICE_ACCOUNT_EMAIL)
   @mock.patch.object(results2_module, 'GetCachedResults2', return_value="")
   def testGet_NoUser(self, _):
     job = job_module.Job.New((), ())
@@ -28,18 +32,46 @@ class JobsTest(test.TestCase):
     self.assertEqual(1, len(data['jobs']))
     self.assertEqual(job.AsDict([job_module.OPTION_STATE]), data['jobs'][0])
 
-  @mock.patch.object(jobs.utils, 'GetEmail',
-                     mock.MagicMock(return_value='lovely.user@example.com'))
+  @mock.patch.object(utils,
+                     'ServiceAccountEmail', lambda: _SERVICE_ACCOUNT_EMAIL)
   @mock.patch.object(results2_module, 'GetCachedResults2', return_value="")
-  def testGet_WithUser(self, _):
+  def testGet_WithUserFilter(self, _):
     job_module.Job.New((), ())
     job_module.Job.New((), (), user='lovely.user@example.com')
     job = job_module.Job.New((), (), user='lovely.user@example.com')
 
-    data = json.loads(self.testapp.get('/api/jobs?o=STATE').body)
+    data = json.loads(
+        self.testapp.get(
+            '/api/jobs?o=STATE&filter=user=lovely.user@example.com').body)
 
+    # We now always explicitly get all the jobs, unless we explicitly filter
+    # for a user's own jobs.
     self.assertEqual(2, data['count'])
     self.assertEqual(2, len(data['jobs']))
 
     sorted_data = sorted(data['jobs'], key=lambda d: d['job_id'])
     self.assertEqual(job.AsDict([job_module.OPTION_STATE]), sorted_data[-1])
+
+  @mock.patch.object(utils,
+                     'ServiceAccountEmail', lambda: _SERVICE_ACCOUNT_EMAIL)
+  @mock.patch.object(jobs.utils, 'GetEmail',
+                     mock.MagicMock(return_value=_SERVICE_ACCOUNT_EMAIL))
+  @mock.patch.object(results2_module, 'GetCachedResults2', return_value="")
+  def testGet_WithServiceAccountUser(self, _):
+    job_module.Job.New((), ())
+    job_module.Job.New((), (), user=_SERVICE_ACCOUNT_EMAIL)
+    job = job_module.Job.New((), (), user=_SERVICE_ACCOUNT_EMAIL)
+
+    data = json.loads(
+        self.testapp.get('/api/jobs?o=STATE&filter=user=%s' %
+                         (_SERVICE_ACCOUNT_EMAIL,)).body)
+
+    self.assertEqual(2, data['count'])
+    self.assertEqual(2, len(data['jobs']))
+    self.assertEqual(data['jobs'][0]['user'], 'chromeperf (automation)')
+    self.assertEqual(data['jobs'][1]['user'], 'chromeperf (automation)')
+
+    sorted_data = sorted(data['jobs'], key=lambda d: d['job_id'])
+    expected_job_dict = job.AsDict([job_module.OPTION_STATE])
+    expected_job_dict['user'] = 'chromeperf (automation)'
+    self.assertEqual(expected_job_dict, sorted_data[-1])
