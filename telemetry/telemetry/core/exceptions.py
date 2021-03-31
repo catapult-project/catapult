da171bf95e7c56975258f1914f661d5b1519e0c1
Brian Sheedy <bsheedy@chromium.org>
2021-03-31 11:35:02 -0700

    [Telemetry] Add __repr__ to Error
    There is at least one case in Telemetry where repr() is used on an Error
    instead of just printing it out as a string. This results in all the
    useful debug information included in the Error being discarded, which
    defeats the entire purpose of the custom exception type. This adds a
    __repr__ implementation to the class which is identical to the __str__
    implementation.
    
    Bug: chromium:1166310
    Change-Id: I6457ffbf456dc019b6a406d91e0a0f52627ba1db
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2797547
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/core/exceptions.py
+++ telemetry/telemetry/core/exceptions.py
@@ -29,13 +29,19 @@ class Error(Exception):
 
     self._debugging_messages.append(annotated_message)
 
-  def __str__(self):
+  def _get_debugging_messages(self, base_output):
     divider = '\n' + '*' * 80 + '\n'
-    output = super(Error, self).__str__()
+    output = base_output
     for message in self._debugging_messages:
       output += divider
       output += message
     return output
+
+  def __str__(self):
+    return self._get_debugging_messages(super(Error, self).__str__())
+
+  def __repr__(self):
+    return self._get_debugging_messages(super(Error, self).__repr__())
 
 
 class PlatformError(Error):
