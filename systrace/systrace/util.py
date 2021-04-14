361ae742a917c88673697b749d3d5aa6a6e9d51d
Wei Wang <wvw@google.com>
2021-04-14 15:29:23 -0700

    systrace: fix tracefs device path detection
    There are devices which has debugfs mounted but trace path is moved.
    Fix this by selecting trace path based on tracefs path existence.
    
    Test: systrace
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I720c35a04b950b4926e6fcd4dbd3ab2db368dd93
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2826726
    Commit-Queue: Hridya Valsaraju <hridya@google.com>
    Reviewed-by: Chris Craik <ccraik@google.com>

--- systrace/systrace/util.py
+++ systrace/systrace/util.py
@@ -61,7 +61,7 @@ def get_tracing_path(device_serial=None):
     /sys/kernel/tracing for device with tracefs support;
     /sys/kernel/debug/tracing if support can't be determined.
   """
-  mount_info_args = ['mount']
+  tracefs_path_cmd = ['ls', '-d', '/sys/kernel/tracing']
 
   if device_serial is None:
     parser = OptionParserIgnoreErrors()
@@ -69,8 +69,8 @@ def get_tracing_path(device_serial=None):
     options, _ = parser.parse_args()
     device_serial = options.device_serial
 
-  adb_output, adb_return_code = run_adb_shell(mount_info_args, device_serial)
-  if adb_return_code == 0 and 'debugfs' not in adb_output:
+  _, adb_return_code = run_adb_shell(tracefs_path_cmd, device_serial, )
+  if adb_return_code == 0:
     return '/sys/kernel/tracing'
   return '/sys/kernel/debug/tracing'
 
