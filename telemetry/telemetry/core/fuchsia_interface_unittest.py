1fbada9f29bc96a2c09248c01b91e6fe44279fcc
Stephen Roe <steveroe@google.com>
2021-05-07 15:03:38 -0700

    Reland "[fuchsia] Switch to new Fuchsia SDK symbolizer."
    This is a reland of 4dd0225362a8a3cdf520470e448df4ef2894b0ed
    
    Original change's description:
    > [fuchsia] Switch to new Fuchsia SDK symbolizer.
    >
    > The new symbolizer does not use the llvm-symbolizer executable.
    >
    > Bug: chromium:1162314
    > Change-Id: Ibb05b96037d8746de55198673474df43c3cc2339
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2851397
    > Commit-Queue: Stephen Roe <steveroe@google.com>
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    
    Bug: chromium:1162314
    Change-Id: If381f5255643fe812fa18d9a32a8c13e4d57057c
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2878572
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Stephen Roe <steveroe@google.com>

--- telemetry/telemetry/core/fuchsia_interface_unittest.py
+++ telemetry/telemetry/core/fuchsia_interface_unittest.py
@@ -9,21 +9,6 @@ import mock
 
 class FuchsiaInterfaceTests(unittest.TestCase):
 
-  def testStartSymbolizerFailsWithoutLlvmSymbolizerFile(self):
-    def side_effect(path_to_file):
-      if path_to_file == fuchsia_interface._LLVM_SYMBOLIZER_PATH:
-        return False
-      else:
-        return True
-    with mock.patch('os.path.isfile') as isfile_mock:
-      with mock.patch('subprocess.Popen',
-                      return_value='Not None') as popen_mock:
-        isfile_mock.side_effect = side_effect
-        self.assertEquals(
-            fuchsia_interface.StartSymbolizerForProcessIfPossible(
-                None, None, 'build-id-file'), None)
-        self.assertEquals(popen_mock.call_count, 0)
-
   def testStartSymbolizerFailsWithoutBuildIdFile(self):
     test_build_id_file = 'build-id-file'
     def side_effect(path_to_file):
@@ -43,8 +28,7 @@ class FuchsiaInterfaceTests(unittest.TestCase):
   def testStartSymbolizerSucceedsIfFilesFound(self):
     test_build_id_file = 'build-id-file'
     def side_effect(path_to_file):
-      return (path_to_file == test_build_id_file or
-              path_to_file == fuchsia_interface._LLVM_SYMBOLIZER_PATH)
+      return path_to_file == test_build_id_file
     with mock.patch('os.path.isfile') as isfile_mock:
       with mock.patch('subprocess.Popen',
                       return_value='Not None') as popen_mock:
