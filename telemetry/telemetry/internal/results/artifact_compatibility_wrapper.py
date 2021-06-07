a8bae77c4a096f54414062b7c5a4d9afc75682ec
John Chen <johnchen@chromium.org>
2021-06-07 08:15:51 -0700

    [Telemetry] Handle binary artifacts in Python 3
    The CreateArtifact method needs to set file mode appropriately for both
    binary artifacts and text artifacts.
    
    Bug: chromium:1198408
    Change-Id: I289d26ad0741466fa3f0641fe961e970e7c281d6
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/2944212
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/internal/results/artifact_compatibility_wrapper.py
+++ telemetry/telemetry/internal/results/artifact_compatibility_wrapper.py
@@ -10,6 +10,7 @@
 from __future__ import absolute_import
 import logging
 import os
+import six
 
 from telemetry.internal.results import story_run
 
@@ -45,7 +46,11 @@ class ArtifactCompatibilityWrapper(object):
 class TelemetryArtifactCompatibilityWrapper(ArtifactCompatibilityWrapper):
   """Wrapper around Telemetry's story_run.StoryRun class."""
   def CreateArtifact(self, name, data):
-    with self._artifact_impl.CreateArtifact(name) as f:
+    if six.PY2 or isinstance(data, bytes):
+      mode = 'w+b'
+    else:
+      mode = 'w+'
+    with self._artifact_impl.CreateArtifact(name, mode=mode) as f:
       f.write(data)
 
 
