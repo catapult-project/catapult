61f0e50fb92a3aea539bf5c22f3708e12e90dd17
Wenbin Zhang <wenbinzhang@google.com>
2021-08-02 11:25:02 -0700

    [catapult] Handle the absolute import in histogram_proto to allow python 3 tests.
    broken tests: https://ci.chromium.org/p/webrtc/g/perf/console
    Example error log: https://logs.chromium.org/logs/webrtc/buildbucket/cr-buildbucket.appspot.com/8840263745083023424/+/steps/webrtc_perf_tests_Dashboard_Upload/0/stdout
    
    Bug: chromium:1234919
    Change-Id: I8d595bbfe02a347076abed788af52a9b74f6331a
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3064844
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- tracing/tracing/proto/histogram_proto.py
+++ tracing/tracing/proto/histogram_proto.py
@@ -2,14 +2,29 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from __future__ import absolute_import
+import logging
 try:
   # Note: from tracing.proto import histogram_pb2 would make more sense here,
   # but unfortunately protoc does not generate __init__.py files if you specify
   # an out package (at least for the gn proto_library rule).
   import histogram_pb2  # pylint:disable=relative-import
   HAS_PROTO = True
-except ImportError:
-  HAS_PROTO = False
+except ImportError as e:
+  try:
+    # crbug/1234919
+    # Catapult put the generated histogram_pb2.py in the same source folder,
+    # while the others (e.g., webrtc) put it in output path. By default we
+    # try to import from the sys.path. Here allows to try import from the
+    # source folder as well.
+    logging.warning(
+        'Failed to import histogram_pb2: %s', repr(e))
+    from . import histogram_pb2 # pylint:disable=relative-import
+    logging.warning(
+        'Retried and successfully imported histogram_pb2: %s', histogram_pb2)
+    HAS_PROTO = True
+  except ImportError:
+    HAS_PROTO = False
 
 
 def _EnsureProto():
