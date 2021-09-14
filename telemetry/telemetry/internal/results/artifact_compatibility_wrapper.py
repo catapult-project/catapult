59437ef3bc708a8b8b1cbceb26e55c8f959a5eb8
Brian Sheedy <bsheedy@chromium.org>
2021-09-13 17:23:25 -0700

    [Telemetry] Add full logging artifact wrapper
    Adds the FullLoggingArtifactCompatibilityWrapper, which is the same as
    LoggingArtifactCompatibilityWrapper but logs the entire artifact instead
    of only the first 100 characters. This is so that tests can opt in to
    the extra information at the cost of increased log spam if the trade-off
    is worth it.
    
    Bug: chromium:1249302
    Change-Id: I3ce401dbc295ab631544103db7bc6149abeed3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3159362
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/internal/results/artifact_compatibility_wrapper.py
+++ telemetry/telemetry/internal/results/artifact_compatibility_wrapper.py
@@ -88,3 +88,21 @@ class LoggingArtifactCompatibilityWrapper(ArtifactCompatibilityWrapper):
         'with name %s. To store the full artifact, run the test in either a '
         'Telemetry or typ context.' % (len(data), name))
     logging.info('Artifact with name %s: %s', name, data[:min(100, len(data))])
+
+
+class FullLoggingArtifactCompatibilityWrapper(ArtifactCompatibilityWrapper):
+  """Wrapper that logs instead of actually creating artifacts.
+
+  Unlike LoggingArtifactCompatibilityWrapper, logs the entire artifact. This
+  results in more log spam, hence why it is not the default.
+  """
+  def __init__(self):
+    super(FullLoggingArtifactCompatibilityWrapper, self).__init__(None)
+
+  def CreateArtifact(self, name, data):
+    # Don't log binary files as the utf-8 encoding will cause errors.
+    # Note that some tests use the .dmp extension for both crash-dump files and
+    # text files.
+    if os.path.splitext(name)[1].lower() in ['.png', '.dmp']:
+      return
+    logging.artifact('Artifact with name %s: %s', name, data)
