c62bb0f15931ce0e7d7cb0633bd32f10a76184a2
Peter Wen <wnwen@chromium.org>
2021-09-22 11:01:15 -0400

    Remove support for old play core versions (reland)
    This is a follow-up to: https://crrev.com/c/3039018
    Also: https://crrev.com/c/3077353
    
    Although the old play core has been removed, the local_testing apks must
    still be copied rather than moved from the temporary directory, since
    the local_testing directory is on /sdcard which may be a different file
    system from /data on phones with an actual sdcard.
    
    Original CL: https://crrev.com/c/3140451
    
    Bug: chromium:1220662, chromium:1250406
    Change-Id: I3f928da81f67554bb8f5cdd3e46cc5da4c2a594a
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3169133
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>

--- devil/devil/android/device_utils.py
+++ devil/devil/android/device_utils.py
@@ -1157,7 +1157,7 @@ class DeviceUtils(object):
         self.EnableRoot()
 
   INSTALL_DEFAULT_TIMEOUT = _FILE_TRANSFER_TIMEOUT
-  MODULES_SRC_DIRECTORY_PATH = '/data/local/tmp/modules'
+  MODULES_TMP_DIRECTORY_PATH = '/data/local/tmp/modules'
   MODULES_LOCAL_TESTING_PATH_TEMPLATE = (
       '/sdcard/Android/data/{}/files/local_testing')
 
@@ -1192,7 +1192,7 @@ class DeviceUtils(object):
       modules: An iterable containing specific bundle modules to install.
           Error if set and |apk| points to an APK instead of a bundle.
       fake_modules: An iterable containing specific bundle modules that should
-          have their apks copied to |MODULES_SRC_DIRECTORY_PATH| subdirectory
+          have their apks copied to |MODULES_LOCAL_TESTING_PATH_TEMPLATE|
           rather than installed. Thus the app can emulate SplitCompat while
           running. This should not have any overlap with |modules|.
       additional_locales: An iterable with additional locales to install for a
@@ -1239,15 +1239,11 @@ class DeviceUtils(object):
 
   def _FakeInstall(self, fake_apk_paths, fake_modules, package_name):
     with tempfile_ext.NamedTemporaryDirectory() as modules_dir:
-      device_dir = posixpath.join(self.MODULES_SRC_DIRECTORY_PATH, package_name)
+      tmp_dir = posixpath.join(self.MODULES_TMP_DIRECTORY_PATH, package_name)
       dest_dir = self.MODULES_LOCAL_TESTING_PATH_TEMPLATE.format(package_name)
       if not fake_modules:
-        # Temporarily support both options until upstream switches to using
-        # local testing path only. Then support for src directory path can be
-        # removed.
-        # TODO(crbug.com/1220662): Remove push empty dir and just use rm -rf.
-        # Push empty module dir to clear device dir and update the cache.
-        self.PushChangedFiles([(modules_dir, device_dir)],
+        # Push empty module dir to clear tmp dir. Remove any previous apks.
+        self.PushChangedFiles([(modules_dir, tmp_dir)],
                               delete_device_stale=True)
         self.RunShellCommand(['rm', '-rf', dest_dir], as_root=True)
         return
@@ -1270,16 +1266,15 @@ class DeviceUtils(object):
 
       assert not still_need_master, (
           'Missing master apk file for %s' % still_need_master)
-      self.PushChangedFiles([(modules_dir, device_dir)],
-                            delete_device_stale=True)
+      self.PushChangedFiles([(modules_dir, tmp_dir)], delete_device_stale=True)
       # Create new directories until the parent of our destination since we
       # want to copy that directory over from the temporary location. This
       # indirection is necessary on Android 11 emulator as there is a permission
       # issue for the files under /sdcard/Android/data.
       self.RunShellCommand(
           ['mkdir', '-p', posixpath.dirname(dest_dir)], as_root=True)
-      # TODO(crbug.com/1220662): Use mv when compatibility is no longer needed.
-      self.RunShellCommand(['cp', '-a', device_dir, dest_dir], as_root=True)
+      # Use cp instead of mv in case destinations are on different disks.
+      self.RunShellCommand(['cp', '-a', tmp_dir, dest_dir], as_root=True)
 
   @decorators.WithTimeoutAndRetriesFromInstance(
       min_default_timeout=INSTALL_DEFAULT_TIMEOUT)
