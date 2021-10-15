93015143f8e3946e45b691f9f2672b333e04169f
Brian Sheedy <bsheedy@chromium.org>
2021-10-15 10:59:09 -0700

    [Telemetry] Fix cros_interface file mode
    Fixes an instance in cros_interface.py where files were opened in
    binary mode but used to store text, which breaks under Python 3.
    
    Bug: chromium:1198237
    Change-Id: I982fa3f1a12645a248e197bfd85ee75f0b63fd29
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3209841
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/core/cros_interface.py
+++ telemetry/telemetry/core/cros_interface.py
@@ -389,7 +389,7 @@ class CrOSInterface(object):
     Returns:
       A string containing the contents of the file.
     """
-    with tempfile.NamedTemporaryFile() as t:
+    with tempfile.NamedTemporaryFile(mode='w') as t:
       self.GetFile(filename, t.name)
       with open(t.name, 'r') as f2:
         res = f2.read()
