e5067b0582e909612b1329a0d6c62210339a964d
Chong Gu <chonggu@google.com>
2021-11-04 17:42:41 -0700

    [Fuchsia] Finish python3 migration.
    Allow subprocesses in fuchsia_interface to always return str type for
    stdout and stderr.
    
    Bug: chromium:1264931
    Change-Id: I95af50be89acf0a5aaf0197f8f74644e69a8c925
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3261636
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Chong Gu <chonggu@google.com>

--- telemetry/telemetry/core/fuchsia_interface.py
+++ telemetry/telemetry/core/fuchsia_interface.py
@@ -9,6 +9,9 @@ import os
 import platform
 import subprocess
 
+# TODO(crbug.com/1267066): Remove when python2 is deprecated.
+import six
+
 from telemetry.core import util
 
 FUCHSIA_BROWSERS = ['web-engine-shell']
@@ -57,6 +60,8 @@ class CommandRunner(object):
     ssh_args.append('-oControlMaster=no')
     ssh_command = self._GetSshCommandLinePrefix() + ssh_args + ['--'] + command
     logging.debug(' '.join(ssh_command))
+    if six.PY3:
+      kwargs['text'] = True
     return subprocess.Popen(ssh_command, **kwargs)
 
   def RunCommand(self, command=None, ssh_args=None, **kwargs):
@@ -98,11 +103,15 @@ def StartSymbolizerForProcessIfPossible(input_file, output_file, build_id_file):
     ]
 
     logging.debug('Running "%s".' % ' '.join(symbolizer_cmd))
-    return subprocess.Popen(symbolizer_cmd,
-                            stdin=input_file,
-                            stdout=output_file,
-                            stderr=subprocess.STDOUT,
-                            close_fds=True)
+    kwargs = {
+        'stdin':input_file,
+        'stdout':output_file,
+        'stderr':subprocess.STDOUT,
+        'close_fds':True
+    }
+    if six.PY3:
+      kwargs['text'] = True
+    return subprocess.Popen(symbolizer_cmd, **kwargs)
   else:
     logging.info('Symbolizer cannot be started.')
     return None
