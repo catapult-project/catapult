362d4fa5810daf7c98b44c6fc07a7639b5fc0e2d
Leina Sun <sunxiaodi@google.com>
2022-01-13 21:34:11 +0000

    adding code coverage reporting to catapult repo
    Bug: chromium:1266068
    Change-Id: I6fca317ecfbf712db2c281035300c613e299eb94
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3385244
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Leina Sun <sunxiaodi@google.com>
    Reviewed-by: Sean McCullough <seanmccullough@google.com>
    Commit-Queue: Sean McCullough <seanmccullough@google.com>

--- catapult_build/run_with_typ.py
+++ catapult_build/run_with_typ.py
@@ -30,6 +30,7 @@ def Run(top_level_dir, path=None, suffixes=None, **kwargs):
       path=(path or []),
       coverage_source=[top_level_dir],
       suffixes=suffixes,
+      coverage=True,
       **kwargs)
 
 
