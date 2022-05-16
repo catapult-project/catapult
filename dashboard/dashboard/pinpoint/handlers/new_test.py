ecd2da37113f570444d9d16af0e605f4fd994bde
John Chen <johnchen@chromium.org>
2022-05-16 17:19:55 +0000

    Revert "Pinpoint Paired Testing: Make number of cases where A goes first equal to the number of cases where B goes first"
    This reverts commit 925e601986fb75043a6351e9da4d9aecfa77be42.
    
    Reason for revert: Causing Pinpoint bisect failures https://crbug.com/1325350
    
    Original change's description:
    > Pinpoint Paired Testing: Make number of cases where A goes first equal to the number of cases where B goes first
    >
    > Bug: chromium:1320387
    > Change-Id: If95c8e450624b0edf9efa532d9fba104d998cb73
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3645988
    > Commit-Queue: Ryan Heise <heiserya@google.com>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    
    Bug: chromium:1320387, chromium:1325350
    Change-Id: I9905f5169333eff13cfea303d7d797f1a7e57cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3650960
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: John Chen <johnchen@chromium.org>

--- dashboard/dashboard/pinpoint/handlers/new_test.py
+++ dashboard/dashboard/pinpoint/handlers/new_test.py
@@ -31,8 +31,7 @@ _BASE_REQUEST = {
     'start_git_hash': '1',
     'end_git_hash': '3',
     'story': 'speedometer',
-    'comparison_mode': 'performance',
-    'initial_attempt_count': '10'
+    'comparison_mode': 'performance'
 }
 
 # TODO: Make this agnostic to the parameters the Quests take.
