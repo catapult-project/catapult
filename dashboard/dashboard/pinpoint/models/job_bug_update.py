40d8e948cfee0fb999bba616336a8bff419111ff
Leina Sun <sunxiaodi@google.com>
2022-06-02 19:35:48 +0000

    [Pinpoint] Logging to diagnose crbug/1215127 progression as regression
    Bug: chromium:1215127
    Change-Id: I2f630fe5008370a2d994ebfa3a32af2e62820bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3684363
    Auto-Submit: Leina Sun <sunxiaodi@google.com>
    Commit-Queue: Leina Sun <sunxiaodi@google.com>
    Reviewed-by: Eduardo Yap <eduardoyap@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- dashboard/dashboard/pinpoint/models/job_bug_update.py
+++ dashboard/dashboard/pinpoint/models/job_bug_update.py
@@ -163,6 +163,7 @@ class DifferencesFoundBugUpdateBuilder(object):
     """Return _BugUpdateInfo for the differences."""
     if len(self._differences) == 0:
       raise ValueError("BuildUpdate called with 0 differences")
+    logging.debug('crbug/1215127 build update + order differences called')
     differences = self._OrderedDifferencesByDelta()
     missing_values = self._DifferencesWithNoValues()
     owner, cc_list, notify_why_text = self._PeopleToNotify()
@@ -217,6 +218,8 @@ class DifferencesFoundBugUpdateBuilder(object):
     diffs_with_deltas = [(diff.MeanDelta(), diff)
                          for diff in self._differences
                          if diff.values_a and diff.values_b]
+    logging.debug('crbug/1215127 - improve dir during sorting = %s',
+                  self._improvement_direction)
     if self._improvement_direction == anomaly.UP:
       # improvement is positive, regression is negative
       ordered_diffs = [
