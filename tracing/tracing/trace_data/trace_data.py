53f907ce448203ca61aed956d096fd73982d5189
Deepanjan Roy <dproy@chromium.org>
2022-06-28 17:50:50 -0400

    Remove unnecessary python3 guard
    trace2html works fine under python3 so this guard is unnecessary.
    
    Bug: chromium:1331285
    Change-Id: I97c92c628c0023ae0f00ff44cc773a88262239a3
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3733787
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Deep Roy <dproy@chromium.org>

--- tracing/tracing/trace_data/trace_data.py
+++ tracing/tracing/trace_data/trace_data.py
@@ -11,7 +11,6 @@ import gzip
 import json
 import logging
 import os
-import platform
 import shutil
 import subprocess
 import tempfile
@@ -335,14 +334,7 @@ def SerializeAsHtml(trace_files, html_file, trace_title=None):
 
   input_size = sum(os.path.getsize(trace_file) for trace_file in trace_files)
 
-  cmd = []
-  if platform.system() == 'Windows':
-    version_cmd = ['python', '-c',
-                   'import sys\nprint(sys.version_info.major)']
-    version = subprocess.check_output(version_cmd)
-    if version.strip() == '3':
-      raise RuntimeError('trace2html cannot run with python 3.')
-    cmd.append('python')
+  cmd = [sys.executable]
   cmd.append(_TRACE2HTML_PATH)
   cmd.extend(trace_files)
   cmd.extend(['--output', html_file])
