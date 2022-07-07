a6353c856fe28808c9c7f006b91e08e48924ff16
Brian Sheedy <bsheedy@chromium.org>
2022-07-07 14:29:43 -0700

    Skip Python 2 Telemetry tests
    Adds the ability for Catapult CQ tests to specify which versions of
    Python they should run under and uses this to skip running the
    telemetry/ tests under Python 2. This directory is being switched to
    Python 3-only in the near future.
    
    Bug: chromium:1342341
    Change-Id: I2aab5983d5f8b0b2acdbddc38d9ba625efa28d99
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3751888
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- catapult_build/build_steps.py
+++ catapult_build/build_steps.py
@@ -17,6 +17,8 @@ import sys
 #   environment.
 # disabled (optional): List of platforms the test is disabled on. May contain
 #   'win', 'mac', 'linux', or 'android'.
+# python_versions (optional): A list of ints specifying the Python versions to
+#   run on. May contain "2" or "3". Defaults to running on both.
 # outputs_presentation_json (optional): If True, pass in --presentation-json
 #   argument to the test executable to allow it to update the buildbot status
 #   page. More details here:
@@ -95,6 +97,7 @@ _CATAPULT_TESTS = [
         'additional_args': ['--browser=reference',],
         'uses_sandbox_env': True,
         'disabled': ['android'],
+        'python_versions': [3],
     },
     {
         'name': 'Telemetry Tests with Stable Browser (Desktop)',
@@ -106,6 +109,7 @@ _CATAPULT_TESTS = [
         ],
         'uses_sandbox_env': True,
         'disabled': ['android'],
+        'python_versions': [3],
     },
     {
         'name': 'Telemetry Tests with Stable Browser (Android)',
@@ -117,7 +121,8 @@ _CATAPULT_TESTS = [
             '-v',
         ],
         'uses_sandbox_env': True,
-        'disabled': ['win', 'mac', 'linux']
+        'disabled': ['win', 'mac', 'linux'],
+        'python_versions': [3],
     },
     {
         'name': 'Telemetry Integration Tests with Stable Browser',
@@ -129,6 +134,7 @@ _CATAPULT_TESTS = [
         ],
         'uses_sandbox_env': True,
         'disabled': ['android', 'linux'],  # TODO(nedn): enable this on linux
+        'python_versions': [3],
     },
     {
         'name': 'Tracing Dev Server Tests',
@@ -336,6 +342,10 @@ def main(args=None):
       break
 
     if args.platform in test.get('disabled', []):
+      continue
+
+    python_version = 3 if args.use_python3 else 2
+    if python_version not in test.get('python_versions', [2, 3]):
       continue
 
     # The test "Devil Python Tests" has two executables, run_py_tests and
