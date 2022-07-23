1157c80fa75f5b847d51b20e4c48fd4faadd30fb
Wenbin Zhang <wenbinzhang@google.com>
2022-07-22 17:35:13 -0700

    [pinpoint] enable py3 tests on handlers/commit
    Also updated the existing tests:
     - add checks on how args are used
     - throw on the correct call
     - work for both webapp2 and flask.
    
    Bug: chromium:1120195
    Change-Id: I5a252c4f3ad580f4e2d71b0581b973394930281f
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3782576
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- dashboard/dashboard/pinpoint/handlers/commit_test.py
+++ dashboard/dashboard/pinpoint/handlers/commit_test.py
@@ -8,15 +8,11 @@ from __future__ import absolute_import
 
 import json
 import mock
-import sys
-import unittest
 
 from dashboard.pinpoint.models.change import commit
 from dashboard.pinpoint import test
 
 
-@unittest.skipIf(sys.version_info.major == 3,
-                   'Skipping old handler tests for python 3.')
 class CommitTest(test.TestCase):
 
   @mock.patch.object(commit.Commit, 'FromDict')
@@ -24,13 +20,17 @@ class CommitTest(test.TestCase):
     mock_dict.return_value = mock.MagicMock()
     mock_dict.return_value.AsDict = mock.MagicMock(return_value=['abc'])
 
-    data = json.loads(self.testapp.post('/api/commit').body)
+    args = '?git_hash=abc'
+    data = json.loads(self.testapp.post('/api/commit' + args).body)
 
+    self.assertEqual(mock_dict.call_args,
+                     mock.call({
+                         'repository': 'chromium',
+                         'git_hash': 'abc'
+                     }))
     self.assertEqual(['abc'], data)
 
-  @mock.patch.object(commit.Commit, 'FromDict')
-  def testPost_Fail(self, mock_dict):
-    mock_dict.return_value = mock.MagicMock()
-    mock_dict.return_value.AsDict = mock.MagicMock(side_effect=KeyError('foo'))
-
+  @mock.patch('dashboard.pinpoint.models.change.commit.Commit.FromDict',
+              mock.MagicMock(side_effect=KeyError('foo')))
+  def testPost_Fail(self):
     self.testapp.post('/api/commit', status=400)
