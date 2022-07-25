03071f17662458dfab63504e0b43184aa9e7eaa3
Wenbin Zhang <wenbinzhang@google.com>
2022-07-25 15:43:24 -0700

    [pinpoint] enable py3 tests on handlers/commits
    Bug: chromium:1120195
    Change-Id: I2b76606f7cbd14942a1e13c756b1ac330a2e3167
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3787094
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- dashboard/dashboard/pinpoint/handlers/commits_test.py
+++ dashboard/dashboard/pinpoint/handlers/commits_test.py
@@ -8,50 +8,48 @@ from __future__ import absolute_import
 
 import json
 import mock
-import sys
-import unittest
 
 from dashboard.pinpoint.models.change import commit
 from dashboard.pinpoint import test
 from dashboard.services import request
 
 
-# TODO(https://crbug.com/1262292): Update after Python2 trybots retire.
-# pylint: disable=useless-object-inheritance
-class MockCommit(object):
-
-  def __init__(self, url, msg):
-    self.url = url
-    self.msg = msg
-
-  def AsDict(self):
-    return {self.url: self.msg}
-
-
-@unittest.skipIf(sys.version_info.major == 3,
-                   'Skipping old handler tests for python 3.')
 class CommitsHandlerTest(test.TestCase):
 
+  @mock.patch('dashboard.pinpoint.models.change.commit.Commit.AsDict',
+              mock.MagicMock(side_effect=[{
+                  'git_hash': 'bar'
+              }, {
+                  'git_hash': 'foo'
+              }]))
+  @mock.patch(
+      'dashboard.pinpoint.models.change.commit.Commit.FromDict',
+      return_value=commit.Commit('chromium', 'xxx'))
   @mock.patch('dashboard.pinpoint.models.change.commit.Commit.CommitRange',
               mock.MagicMock(return_value=[{
-                  'commit': 'abc'
+                  'commit': 'bar'
               }]))
-  def testPost(self):
-    params = {'start_git_hash': 'foo', 'end_git_hash': 'bar'}
-    data = json.loads(self.testapp.post('/api/commits', params).body)
+  def testPost(self, mock_from_dict):
 
-    c1 = commit.Commit('chromium', 'foo')
-    c2 = commit.Commit('chromium', 'abc')
+    args = '?start_git_hash=foo&end_git_hash=bar'
+    data = json.loads(self.testapp.post('/api/commits' + args).body)
 
+    self.assertEqual(mock_from_dict.call_args_list, [
+        mock.call({
+            'repository': 'chromium',
+            'git_hash': 'foo'
+        }),
+        mock.call({
+            'repository': 'chromium',
+            'git_hash': 'bar'
+        })
+    ])
     self.assertEqual(2, len(data))
-    self.assertEqual(c1.AsDict(), data[0])
-    self.assertEqual(c2.AsDict(), data[1])
-    self.assertEqual('abc', data[1]['git_hash'])
+    self.assertEqual({'git_hash': 'foo'}, data[0])
+    self.assertEqual({'git_hash': 'bar'}, data[1])
+    self.assertEqual('bar', data[1]['git_hash'])
 
-  @mock.patch.object(
-      commit.Commit,
-      'CommitRange',
-      side_effect=request.RequestError('abc', '', ''))
-  @mock.patch.object(commit.Commit, 'FromDict', mock.MagicMock())
-  def testPost_Fail(self, _):
+  @mock.patch('dashboard.pinpoint.models.change.commit.Commit.FromDict',
+              mock.MagicMock(side_effect=request.RequestError('foo', '', '')))
+  def testPost_Fail(self):
     self.testapp.post('/api/commits', status=400)
