67ccd32ad3b98075fd737a0f6ab1ea30e092cf91
John Chen <johnchen@chromium.org>
2022-11-03 05:33:01 +0000

    Revert "Create linux-based device and linux_device"
    This reverts commit adcf3b492daa48da10b3c8f4cae060bca05179fa.
    
    Reason for revert: Causing test failures in Chromium repo and blocking Catapult to Chromium roller. Example: https://crrev.com/c/3990379
    
    Original change's description:
    > Create linux-based device and linux_device
    >
    > Bug: chromium:1341096
    > Change-Id: I0409f59bef0c74843c886e50279d9db0d4b87ba1
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/3984486
    > Commit-Queue: Rohan Pavone <rohpavone@chromium.org>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    
    Bug: chromium:1341096
    Change-Id: Ibeeb3b3b399a76ad959c2c895f6422d4c2834ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4001085
    Auto-Submit: John Chen <johnchen@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- telemetry/telemetry/internal/platform/cros_device.py
+++ telemetry/telemetry/internal/platform/cros_device.py
@@ -2,19 +2,54 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 from __future__ import absolute_import
+import logging
 
-from telemetry.internal.platform import linux_based_device
+from telemetry.core import cros_interface
+from telemetry.core import platform
+from telemetry.internal.platform import device
 
 
-class CrOSDevice(linux_based_device.LinuxBasedDevice):
-  OS_NAME = 'chromeos'
-  OS_PROPER_NAME = 'ChromeOs'
-  GUID_NAME = 'cros'
+class CrOSDevice(device.Device):
+  def __init__(self, host_name, ssh_port, ssh_identity, is_local):
+    super().__init__(
+        name='ChromeOs with host %s' % host_name or 'localhost',
+        guid='cros:%s' % host_name or 'localhost')
+    self._host_name = host_name
+    self._ssh_port = ssh_port
+    self._ssh_identity = ssh_identity
+    self._is_local = is_local
+
+  @classmethod
+  def GetAllConnectedDevices(cls, denylist):
+    return []
+
+  @property
+  def host_name(self):
+    return self._host_name
+
+  @property
+  def ssh_port(self):
+    return self._ssh_port
+
+  @property
+  def ssh_identity(self):
+    return self._ssh_identity
+
+  @property
+  def is_local(self):
+    return self._is_local
 
 
 def IsRunningOnCrOS():
-  return CrOSDevice.PlatformIsRunningOS()
+  return platform.GetHostPlatform().GetOSName() == 'chromeos'
 
 
 def FindAllAvailableDevices(options):
-  return CrOSDevice.FindAllAvailableDevices(options)
+  """Returns a list of available device types."""
+  use_ssh = options.remote and cros_interface.HasSSH()
+  if not use_ssh and not IsRunningOnCrOS():
+    logging.debug('No --remote specified, and not running on ChromeOs.')
+    return []
+
+  return [CrOSDevice(options.remote, options.remote_ssh_port,
+                     options.ssh_identity, not use_ssh)]
