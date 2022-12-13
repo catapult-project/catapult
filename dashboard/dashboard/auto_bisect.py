e28e9f182ed6c8f468cd44c2a908af230e4cdf16
Wenbin Zhang <wenbinzhang@google.com>
2022-12-13 14:56:59 -0800

    [chromeperf] fix for auto-bisect in python3
    Fix the issue that auto-bisect failed in python3.
    
    Bug: chromium:1298177
    Change-Id: Ia23e55f460de211b07520c4c9d0c57c68c29f88f
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4104266
    Reviewed-by: Leina Sun <sunxiaodi@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>

--- dashboard/dashboard/auto_bisect.py
+++ dashboard/dashboard/auto_bisect.py
@@ -66,6 +66,7 @@ def _StartBisectForBug(bug_id, project_id):
 
 def _StartPinpointBisect(bug_id, project_id, test_anomaly, test):
   # Convert params to Pinpoint compatible
+  anomaly_keys = utils.ConvertBytesBeforeJsonDumps([test_anomaly.key.urlsafe()])
   params = {
       'test_path': test.test_path,
       'start_commit': test_anomaly.start_revision - 1,
@@ -74,7 +75,7 @@ def _StartPinpointBisect(bug_id, project_id, test_anomaly, test):
       'project_id': project_id,
       'bisect_mode': 'performance',
       'story_filter': test.unescaped_story_name,
-      'alerts': json.dumps([test_anomaly.key.urlsafe()])
+      'alerts': json.dumps(anomaly_keys)
   }
 
   try:
