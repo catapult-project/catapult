3af33a5307cfa3d617ffda9bd4b5bd2635e3f7df
Wenbin Zhang <wenbinzhang@google.com>
2022-12-19 19:19:03 -0800

    [chromeperf] add pickle version limit for chromeperf
    Bug: chromium:1298177
    Change-Id: Ib1dc1d629bf3a47ed13d95033e7c25011d241bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4116075
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>

--- dashboard/dashboard/dispatcher.py
+++ dashboard/dashboard/dispatcher.py
@@ -6,6 +6,9 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+import pickle
+pickle.HIGHEST_PROTOCOL = 2
+
 import six
 import logging
 
