de2b84ef2a3dd00d78c4de8e57847d3b6af1b967
Brian Sheedy <bsheedy@chromium.org>
2023-02-03 23:27:23 +0000

    Revert "Change non-root profile dir"
    This reverts commit 7ebfbee57aff94ceaef7f7ddd2f2f7ff4d408873.
    
    Reason for revert: Breaks on A13 despite working on A23
    
    Original change's description:
    > Change non-root profile dir
    >
    > Changes the profile directory used on non-root devices from
    > /sdcard/Download/<package> to /sdcard/Android/data/<package> and removes
    > the "pm clear"-based workaround that the old directory required in order
    > to clear browser data between restarts.
    >
    > In practice, this should fix the flaky behavior we were seeing on some
    > devices with "pm clear" while also reducing the differences between
    > running on rooted and non-rooted devices.
    >
    > Bug: chromium:1383609
    > Change-Id: I618569f62eb47df25c186823cd4de665bff7e27c
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4216193
    > Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    > Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    
    Bug: chromium:1383609
    Change-Id: I2fc71282ab58e70c4f4faa52c0b1077fbf68da1c
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4221266
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- telemetry/telemetry/internal/platform/android_platform_backend.py
+++ telemetry/telemetry/internal/platform/android_platform_backend.py
@@ -573,6 +573,20 @@ class AndroidPlatformBackend(
         profile is to be deleted.
       ignore_list: List of files to keep.
     """
+    # If we don't have root, then we are almost certainly actually using the
+    # default profile directory instead of the one we return from GetProfileDir.
+    # This current best guess for this behavior is that it is due to SELinux,
+    # as a non-root-readable directory will not be usable by the browser due to
+    # SELinux security contexts/permissions. So, clear the default directory by
+    # wiping the application state. We still go through the regular profile
+    # directory deletion afterwards on the off chance that we are somehow using
+    # the non-default directory.
+    if not self._require_root:
+      # We specify to wait for the asynchronous intent since there have been
+      # known problems with it deleting data out from under a test. See
+      # crbug.com/1383609 for an example.
+      self._device.ClearApplicationState(
+          package, wait_for_asynchronous_intent=True)
     profile_dir = self.GetProfileDir(package)
     if not self._device.PathExists(profile_dir):
       return
@@ -594,7 +608,12 @@ class AndroidPlatformBackend(
     if self._require_root:
       return '/data/data/%s/' % package
     # We use a public location to ensure minidumps can be pulled without root.
-    return '/sdcard/Android/data/%s/' % package
+    # /data/local/tmp/package/ seems like it would be more fitting, but for
+    # some reason (maybe related to SELinux), using that directory does not
+    # work. If the package directory doesn't exist, then Chromium ends up
+    # defaulting to /data/data/package/ instead. If the directory does exist,
+    # Chromium ends up segfaulting somewhere.
+    return '/sdcard/Download/%s/' % package
 
   def GetDumpLocation(self, package):
     """Returns the location where crash dumps should be written to.
