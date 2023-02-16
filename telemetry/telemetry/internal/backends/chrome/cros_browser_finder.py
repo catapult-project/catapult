45d86ff47317e082f4d473d38741314bdada7c5b
Eduardo Yap <eduardoyap@google.com>
2023-02-16 21:52:25 +0000

    Change LaCros browser to use random dir for each story run.
    Previously, it would always use the same directory, which led stories to use the same user profile. This should be reset every run to ensure consistency.
    
    Bug: chromium:1415573
    
    Change-Id: I6f3558199bc812589c25601cc802efee9d0ab92c
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4250277
    Commit-Queue: Eduardo Yap <eduardoyap@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/internal/backends/chrome/cros_browser_finder.py
+++ telemetry/telemetry/internal/backends/chrome/cros_browser_finder.py
@@ -9,7 +9,10 @@ import os
 import platform
 import posixpath
 import random
+import shutil
+import tempfile
 
+from py_utils import file_util
 from telemetry.core import cros_interface
 from telemetry.core import platform as platform_module
 from telemetry.internal.backends.chrome import chrome_startup_args
@@ -53,6 +56,7 @@ class PossibleCrOSBrowser(possible_browser.PossibleBrowser):
     self._platform_backend = (
         cros_platform._platform_backend)  # pylint: disable=protected-access
     self._is_guest = is_guest
+    self._profile_directory = None
 
     self._existing_minidump_dir = None
     # There's no way to automatically determine the build directory, as
@@ -74,7 +78,7 @@ class PossibleCrOSBrowser(possible_browser.PossibleBrowser):
 
   @property
   def profile_directory(self):
-    return '/home/chronos/Default'
+    return self._profile_directory
 
   def _InitPlatformIfNeeded(self):
     pass
@@ -84,6 +88,15 @@ class PossibleCrOSBrowser(possible_browser.PossibleBrowser):
 
   def SetUpEnvironment(self, browser_options):
     super().SetUpEnvironment(browser_options)
+
+    self._profile_directory = tempfile.mkdtemp()
+
+    # Copy data into the profile if it hasn't already been added via
+    # |source_profile|.
+    for source, dest in self._browser_options.profile_files_to_copy:
+      full_dest_path = os.path.join(self._profile_directory, dest)
+      if not os.path.exists(full_dest_path):
+        file_util.CopyFileWithIntermediateDirectories(source, full_dest_path)
 
     # Copy extensions to temp directories on the device.
     # Note that we also perform this copy locally to ensure that
@@ -117,6 +130,10 @@ class PossibleCrOSBrowser(possible_browser.PossibleBrowser):
                           '--user=%s' % self._browser_options.username])
 
   def _TearDownEnvironment(self):
+    if self._profile_directory and os.path.exists(self._profile_directory):
+      # Remove the profile directory, which was hosted on a temp dir.
+      shutil.rmtree(self._profile_directory, ignore_errors=True)
+      self._profile_directory = None
     cri = self._platform_backend.cri
     for extension in self._browser_options.extensions_to_load:
       cri.RmRF(posixpath.dirname(extension.local_path))
@@ -160,6 +177,8 @@ class PossibleCrOSBrowser(possible_browser.PossibleBrowser):
               self._DEFAULT_CHROME_ENV,
               os_browser_backend,
               build_dir=self._build_dir))
+      if self.profile_directory is not None:
+        startup_args.append('--user-data-dir=%s' % self.profile_directory)
       return browser.Browser(
           lacros_chrome_browser_backend, self._platform_backend, startup_args)
     return browser.Browser(os_browser_backend, self._platform_backend,
--- telemetry/telemetry/internal/backends/chrome/lacros_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/lacros_browser_backend.py
@@ -5,7 +5,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 import logging
-import posixpath
+import os
 import shutil
 import time
 import six.moves._thread  # pylint: disable=import-error
@@ -17,6 +17,9 @@ from telemetry import decorators
 from telemetry.core import debug_data
 from telemetry.internal.backends.chrome import chrome_browser_backend
 from telemetry.internal.backends.chrome import minidump_finder
+
+DEVTOOLS_ACTIVE_PORT_FILE = 'DevToolsActivePort'
+UI_DEVTOOLS_ACTIVE_PORT_FILE = 'UIDevToolsActivePort'
 
 class LacrosBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
   def __init__(self, cros_platform_backend, browser_options,
@@ -61,7 +64,7 @@ class LacrosBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     return None
 
   def _GetDevToolsActivePortPath(self):
-    return '/usr/local/lacros-chrome/user_data/DevToolsActivePort'
+    return os.path.join(self.profile_directory, DEVTOOLS_ACTIVE_PORT_FILE)
 
   def _RunCommandAndLog(self, cmd):
     results = self._cri.RunCmdOnDevice(cmd)
@@ -84,9 +87,9 @@ class LacrosBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
     return devtools_port, browser_target
 
   def _FindUIDevtoolsPort(self):
-    devtools_file_path = posixpath.join(
-        '/', 'usr', 'local', 'lacros-chrome', 'user_data',
-        'UIDevToolsActivePort')
+    devtools_file_path = os.path.join(self.profile_directory,
+                                      UI_DEVTOOLS_ACTIVE_PORT_FILE)
+
     # GetFileContents may raise IOError or OSError, the caller will retry.
     lines = self._cri.GetFileContents(devtools_file_path).splitlines()
     if not lines:
@@ -131,7 +134,6 @@ class LacrosBrowserBackend(chrome_browser_backend.ChromeBrowserBackend):
            '-s', '/tmp/lacros.sock',
            '/usr/local/lacros-chrome/lacros-chrome',
            '--ozone-platform=wayland',
-           '--user-data-dir=/usr/local/lacros-chrome/user_data',
            '--enable-gpu-rasterization',
            '--enable-oop-rasterization',
            '--lang=en-US',
