53cc27b20f1bd1d5118ad1e066af46565e943013
Ian Struiksma <ianstruiksma@google.com>
2023-04-13 20:08:56 +0000

    Create a fetch for only wpr binary dependencies
    For some test frameworks like the Captured Sites, only the wpr binaries are necessary to run tests. Allow a fetch version that uses the same BaseConfig but only fetches the wpr binaries (wpr_go & httparchive_go).
    
    Bug: chromium:1433061
    Change-Id: I7b00ad1bcc248821b5bf73804472d01d3b12d15d
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4423861
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Ian Struiksma <ianstruiksma@google.com>

--- telemetry/telemetry/internal/util/binary_manager.py
+++ telemetry/telemetry/internal/util/binary_manager.py
@@ -134,9 +134,9 @@ def LocalPath(binary_name, os_name, arch, os_version=None):
   return GetBinaryManager().LocalPath(binary_name, os_name, arch, os_version)
 
 
-def FetchBinaryDependencies(
-    platform, client_configs, fetch_reference_chrome_binary):
-  """ Fetch all binary dependenencies for the given |platform|.
+def FetchBinaryDependencies(platform, client_configs,
+    fetch_reference_chrome_binary, dependency_filter=None):
+  """ Fetch all binary dependencies for the given |platform|.
 
   Note: we don't fetch browser binaries by default because the size of the
   binary is about 2Gb, and it requires cloud storage permission to
@@ -147,6 +147,8 @@ def FetchBinaryDependencies(
     client_configs: A list of paths (string) to dependencies json files.
     fetch_reference_chrome_binary: whether to fetch reference chrome binary for
       the given platform.
+    dependency_filter: A list of dependency names to limit fetch to - if not
+      provided will fetch all dependencies for the given platform.
   """
   configs = [
       dependency_manager.BaseConfig(TELEMETRY_PROJECT_CONFIG),
@@ -160,14 +162,14 @@ def FetchBinaryDependencies(
   if _IsChromeOSLocalMode(os_name):
     os_name = 'linux'
   target_platform = '%s_%s' % (os_name, platform.GetArchName())
-  dep_manager.PrefetchPaths(target_platform)
+  dep_manager.PrefetchPaths(target_platform, dependency_filter)
 
   host_platform = None
   fetch_devil_deps = False
   if os_name in ('android', 'chromeos'):
     host_platform = '%s_%s' % (
         py_utils.GetHostOsName(), py_utils.GetHostArchName())
-    dep_manager.PrefetchPaths(host_platform)
+    dep_manager.PrefetchPaths(host_platform, dependency_filter)
     if os_name == 'android':
       if host_platform == 'linux_x86_64':
         fetch_devil_deps = True
