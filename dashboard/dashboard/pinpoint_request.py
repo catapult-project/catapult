86fe6c71d698699d600b945e4fd00533fc557474
Wenbin Zhang <wenbinzhang@google.com>
2023-05-07 23:21:50 -0700

    [chromeperf] fix on apk names
     - Fix the target used by pixel4a power builders. (use trichrome as hypan@ suggested)
     - Fix the targets when the job is created by auto-triage via pinpoint_request.py
    
    Bug: chromium:1439658
    Change-Id: Id0b0ec8689b599160d461486164525f06d429e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4511111
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>

--- dashboard/dashboard/pinpoint_request.py
+++ dashboard/dashboard/pinpoint_request.py
@@ -168,19 +168,17 @@ def GetIsolateTarget(bot_name, suite):
   # binaries. Mapping based off of Chromium's
   # //tools/perf/core/perf_data_generator.py
   if bot_name == 'android-go-perf':
-    return 'performance_test_suite_android_clank_chrome'
+    return 'performance_test_suite_android_clank_monochrome'
   if bot_name == 'android-go-wembley-perf':
     return 'performance_test_suite_android_clank_trichrome_bundle'
   if bot_name == 'android-new-pixel-perf':
     return 'performance_test_suite_android_clank_trichrome_chrome_google_64_32_bundle'
   if bot_name == 'android-new-pixel-pro-perf':
     return 'performance_test_suite_android_clank_trichrome_chrome_google_64_32_bundle'
-  if bot_name == 'android-nexus5x-perf-fyi':
-    return 'performance_test_suite_android_clank_chrome'
   if bot_name == 'android-pixel2-perf-calibration':
     return 'performance_test_suite_android_clank_monochrome_64_32_bundle'
   if bot_name == 'android-pixel2-perf-fyi':
-    return 'performance_test_suite_android_clank_chrome'
+    return 'performance_test_suite_android_clank_monochrome'
   if bot_name == 'android-pixel2-perf-aab-fyi':
     return 'performance_test_suite_android_clank_monochrome_bundle'
   if bot_name == 'Android Nexus5 Perf':
@@ -190,7 +188,7 @@ def GetIsolateTarget(bot_name, suite):
   if bot_name == 'android-pixel4-perf':
     return 'performance_test_suite_android_clank_trichrome_bundle'
   if bot_name == 'android-pixel4a_power-perf':
-    return 'performance_test_suite_android_clank_chrome'
+    return 'performance_test_suite_android_clank_trichrome_bundle'
   if bot_name == 'android-pixel6-perf':
     return 'performance_test_suite_android_clank_trichrome_bundle'
   if bot_name == 'android-pixel6-pro-perf':
