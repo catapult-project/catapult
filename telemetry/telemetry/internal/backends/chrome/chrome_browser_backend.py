d28ef26c7ec6e12c7b1f38399e462282222c669f
John Chen <johnchen@chromium.org>
2023-07-11 17:06:40 +0000

    Revert "Start chrome normally on Mac."
    This reverts commit 6be04a7adfdbb198cca314e8995a3932cfc375f9.
    
    Reason for revert: Causing failures while rolling into Chromium: https://chromium-review.googlesource.com/c/chromium/src/+/4676185
    
    Original change's description:
    > Start chrome normally on Mac.
    >
    > Bug: chromium:1454294
    > Change-Id: I44e6a179cc15e8b7b3a7aacd46212d15222a29d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4647152
    > Commit-Queue: Prakhar Asthana <pasthana@google.com>
    > Reviewed-by: Leina Sun <sunxiaodi@google.com>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    
    Bug: chromium:1454294
    Change-Id: I41f6a04452b2545f509fcb9bd6819e8778b62aa2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4678860
    Auto-Submit: John Chen <johnchen@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- telemetry/telemetry/internal/backends/chrome/chrome_browser_backend.py
+++ telemetry/telemetry/internal/backends/chrome/chrome_browser_backend.py
@@ -251,12 +251,7 @@ class ChromeBrowserBackend(browser_backend.BrowserBackend):
       tracing_backend.FlushTracing()
 
     if self._devtools_client:
-      # Telemetry closes Chrome by killing the Chrome process. This doesn't
-      # happen on mac because on mac, chrome is started using `open` command
-      # and telemetry only kills the 'open' command, leaving Chrome still
-      # running. Therefore force a clean shutdown on mac.
-      if ("ENSURE_CLEAN_CHROME_SHUTDOWN" in os.environ or \
-          self.browser.platform.GetOSName() == 'mac'):
+      if "ENSURE_CLEAN_CHROME_SHUTDOWN" in os.environ:
         # Forces a clean shutdown by sending a command to close the browser via
         # the devtools client. Uses a long timeout as a clean shutdown can
         # sometime take a long time to complete.
