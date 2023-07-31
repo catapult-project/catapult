1fa5aeada2a9da7a17ff2c60d07a262fd9eeb9bb
Wenbin Zhang <wenbinzhang@google.com>
2023-07-31 11:48:33 -0700

    [chromeperf] Use google.cloud.datastore to replace the google.cloud.ndb to access datastore data
    The ndb libray requires a ndb.Model class to host the loaded data. If any property is missing in the class, the write operations will put null values on those missing fields. This creates dependencies between different service. E.g., the Anomaly kind is created on upload-processing during anomaly detection. When perf-issue-service tries to update the alert groups of an anomaly, it shouldn't have to know all the other properties of the anomaly, such as median_before_anomaly.
    
    Now I change to use the google.cloud.datastore model which represents the entities in json wich does not require knowledges of the existing fields.
    
    Bug: chromium:1468326
    Change-Id: I8962c19d05af168ca64c48fa03b642f07530aefe
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4724123
    Reviewed-by: Ashwin Verleker <ashwinpv@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- perf_issue_service/models/alert_group.py
+++ perf_issue_service/models/alert_group.py
@@ -3,11 +3,8 @@
 # found in the LICENSE file.
 
 import logging
-import uuid
 
-from google.cloud import ndb
-
-from models import anomaly
+from application.clients import datastore_client
 from application.clients import sheriff_config_client
 
 
@@ -18,112 +15,101 @@ class NoEntityFoundException(Exception):
 class SheriffConfigRequestException(Exception):
   pass
 
-
-class RevisionRange(ndb.Model):
-  repository = ndb.StringProperty()
-  start = ndb.IntegerProperty()
-  end = ndb.IntegerProperty()
-
-class BugInfo(ndb.Model):
-  project = ndb.StringProperty(indexed=True)
-  bug_id = ndb.IntegerProperty(indexed=True)
-
-class AlertGroup(ndb.Model):
-  name = ndb.StringProperty(indexed=True)
-  domain = ndb.StringProperty(indexed=True)
-  subscription_name = ndb.StringProperty(indexed=True)
-  created = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
-  updated = ndb.DateTimeProperty(indexed=False, auto_now_add=True)
-
-  class Status:
-    unknown = 0
-    untriaged = 1
-    triaged = 2
-    bisected = 3
-    closed = 4
-
-  status = ndb.IntegerProperty(indexed=False)
-
-  class Type:
-    test_suite = 0
-    logical = 1
-    reserved = 2
-
-  group_type = ndb.IntegerProperty(
-      indexed=False,
-      choices=[Type.test_suite, Type.logical, Type.reserved],
-      default=Type.test_suite,
-  )
-  active = ndb.BooleanProperty(indexed=True)
-  revision = ndb.LocalStructuredProperty(RevisionRange)
-  bug = ndb.StructuredProperty(BugInfo, indexed=True)
-  project_id = ndb.StringProperty(indexed=True, default='chromium')
-  bisection_ids = ndb.StringProperty(repeated=True)
-  anomalies = ndb.KeyProperty(repeated=True)
-  # Key of canonical AlertGroup. If not None the group is considered to be
-  # duplicate.
-  canonical_group = ndb.KeyProperty(indexed=True)
+class AlertGroup:
+  ds_client = datastore_client.DataStoreClient()
 
   @classmethod
   def FindDuplicates(cls, group_id):
-    client = ndb.Client()
-    with client.context():
-      query = cls.query(
-          cls.active == True,
-          cls.canonical_group == ndb.Key('AlertGroup', group_id))
-      duplicates = query.fetch()
-      return [g.key.string_id() for g in duplicates]
+    '''Find the alert groups which are using the current group as canonical.
+
+    Args:
+      group_id: the id of the current group.
+
+    Return:
+      a list of groud ids.
+    '''
+    filters = [('canonical_group', '=', cls.ds_client.AlertGroupKey(group_id))]
+    duplicates = list(cls.ds_client.QueryAlertGroup(extra_filters=filters))
+
+    return [cls.ds_client.GetEntityId(g, key_type='name') for g in duplicates]
 
 
   @classmethod
-  def FindCanonicalGroupByIssue(cls, current_group_key, issue_id, project_name):
-    client = ndb.Client()
-    with client.context():
-      query = cls.query(
-          cls.active == True,
-          cls.bug.project == project_name,
-          cls.bug.bug_id == issue_id)
-      query_result = query.fetch(limit=1)
-      if not query_result:
+  def FindCanonicalGroupByIssue(cls, current_group_id, issue_id, project_name):
+    '''Find the canonical group which the current group's issue is merged to.
+
+    Consider an alert group GA has a filed issue A, another group GB has filed
+    issue B. If A is merged into B, then GB is consider the canonical group of
+    GA.
+
+    Args:
+      current_group_id: the id of the current group
+      issue_id: the id of the issue which is merged to.
+      project_name: the monorail project name of the issue.
+
+    Returns:
+      The id of the canonical group.
+    '''
+    filters = [
+      ('bug.bug_id', '=', issue_id),
+      ('bug.project', '=', project_name)
+    ]
+
+    query_result = list(cls.ds_client.QueryAlertGroup(extra_filters=filters, limit=1))
+
+    if not query_result:
+      return None
+
+    canonical_group = query_result[0]
+    visited = {current_group_id}
+    while dict(canonical_group).get('canonical_group'):
+      visited.add(canonical_group.key.name)
+      next_group_key = dict(canonical_group).get('canonical_group')
+      # Visited check is just precaution.
+      # If it is true - the system previously failed to prevent loop creation.
+      if next_group_key.name in visited:
+        logging.warning(
+            'Alert group auto merge failed. Found a loop while '
+            'searching for a canonical group for %r', current_group_id)
         return None
+      canonical_group = cls.ds_client.GetEntityByKey(next_group_key)
 
-      canonical_group = query_result[0]
-      visited = set()
-      while canonical_group.canonical_group:
-        visited.add(canonical_group.key)
-        next_group_key = canonical_group.canonical_group
-        # Visited check is just precaution.
-        # If it is true - the system previously failed to prevent loop creation.
-        if next_group_key == current_group_key or next_group_key in visited:
-          logging.warning(
-              'Alert group auto merge failed. Found a loop while '
-              'searching for a canonical group for %r', current_group_key)
-          return None
-        canonical_group = next_group_key.get()
-
-      return canonical_group.key.string_id()
+    return canonical_group.key.name
 
 
   @classmethod
   def GetAnomaliesByID(cls, group_id):
-    """ Given a group id, return a list of anomaly id.
-    """
-    client = ndb.Client()
-    with client.context():
-      group = ndb.Key('AlertGroup', group_id).get()
-      if group:
-        return [a.integer_id() for a in group.anomalies]
-      raise NoEntityFoundException('No Alert Group Found with id: %s', group_id)
+    ''' Given a group id, return a list of anomaly id.
+
+    Args:
+      group_id: the id of the alert group
+
+    Returns:
+      A list of anomaly IDs.
+    '''
+    group_key = cls.ds_client.AlertGroupKey(group_id)
+    group = cls.ds_client.GetEntityByKey(group_key)
+    if group:
+      return [a.id for a in group.get('anomalies')]
+    raise NoEntityFoundException('No Alert Group Found with id: %s', group_id)
 
   @classmethod
   def Get(cls, group_name, group_type, active=True):
-    client = ndb.Client()
-    with client.context():
-      query = cls.query(
-          cls.active == active,
-          cls.name == group_name,
-      )
-      return [g for g in query.fetch() if g.group_type == group_type]
+    ''' Get a list of alert groups by the group name and group type
+
+    Args:
+      group_name: the name value of the alert group
+      group_type: the group type, which should be either 0 (test_suite)
+                  or 2 (reserved)
+    Returns:
+      A list of ids of the matched alert groups
+    '''
+    filters = [
+      ('name', '=', group_name)
+    ]
+    groups = list(cls.ds_client.QueryAlertGroup(active=active, extra_filters=filters))
+
+    return [g for g in groups if g.get('group_type') == group_type]
 
 
   @classmethod
@@ -137,9 +123,21 @@ class AlertGroup(ndb.Model):
         if no existing group is found:
          - if create_on_ungrouped is True, create a new group.
          - otherwise, use the 'ungrouped'.
+
+    Args:
+      test_key: the key of a test metadata
+      start_rev: the start revision of the anomaly
+      end_rev: the end revision of the anomaly
+      create_on_ungrouped: the located subscription will have a new alert
+          group created if this value is true; otherwise, the existing
+          'upgrouped' group will be used.
+      parity: testing flag for result parity
+
+    Returns:
+      a list of group ids.
     '''
-    client = sheriff_config_client.GetSheriffConfigClient()
-    matched_configs, err_msg = client.Match(test_key)
+    sc_client = sheriff_config_client.GetSheriffConfigClient()
+    matched_configs, err_msg = sc_client.Match(test_key)
 
     if err_msg is not None:
       raise SheriffConfigRequestException(err_msg)
@@ -155,47 +153,40 @@ class AlertGroup(ndb.Model):
     existing_groups = cls.Get(benchmark_name, 0)
     result_groups = set()
     new_groups = set()
+
     for config in matched_configs:
       s = config['subscription']
       has_overlapped = False
       for g in existing_groups:
-        if (g.domain == master_name and
-            g.subscription_name == s.get('name') and
-            g.project_id == s.get('monorail_project_id', '') and
-            max(g.revision.start, start_rev) <= min(g.revision.end, end_rev) and
-            (abs(g.revision.start - start_rev) + abs(g.revision.end - end_rev) <= 100 or g.domain != 'ChromiumPerf')):
+        if (g['domain'] == master_name and
+            g['subscription_name'] == s.get('name') and
+            g['project_id'] == s.get('monorail_project_id', '') and
+            max(g['revision']['start'], start_rev) <= min(g['revision']['end'], end_rev) and
+            (abs(g['revision']['start'] - start_rev) + abs(g['revision']['end'] - end_rev) <= 100 or g['domain'] != 'ChromiumPerf')):
           has_overlapped = True
-          result_groups.add(g.key.string_id())
+          result_groups.add(g.key.name)
       if not has_overlapped:
         if create_on_ungrouped:
-          client = ndb.Client()
-          with client.context():
-            new_id = str(uuid.uuid4())
-            new_group = cls(
-              id=new_id,
-              name=benchmark_name,
-              domain=master_name,
-              subscription_name=s.get('name'),
-              project_id=s.get('monorail_project_id', ''),
-              status=cls.Status.untriaged,
-              group_type=cls.Type.test_suite,
-              active=True,
-              revision=RevisionRange(
-                repository='chromium',
-                start=start_rev,
-                end=end_rev
-              )
-            )
-            logging.info('Saving new group %s', new_id)
-            if parity:
-              new_groups.add(new_id)
-            else:
-              new_group.put()
-          result_groups.add(new_id)
+          new_group = cls.ds_client.NewAlertGroup(
+            benchmark_name=benchmark_name,
+            master_name=master_name,
+            subscription_name=s.get('name'),
+            group_type=datastore_client.AlertGroupType.test_suite,
+            project_id=s.get('monorail_project_id', ''),
+            start_rev=start_rev,
+            end_rev=end_rev
+          )
+          logging.info('Saving new group %s', new_group.key.name)
+          if parity:
+            new_groups.add(new_group.key.name)
+          else:
+            cls.ds_client.SaveAlertGroup(new_group)
+          result_groups.add(new_group.key.name)
         else:
           # return the id of the 'ungrouped'
           ungrouped = cls._GetUngroupedGroup()
-          result_groups.add(ungrouped.key.integer_id())
+          if ungrouped:
+            result_groups.add(ungrouped.key.id)
 
     logging.debug('GetGroupsForAnomaly returning %s', result_groups)
     return list(result_groups), list(new_groups)
@@ -203,10 +194,14 @@ class AlertGroup(ndb.Model):
 
   @classmethod
   def GetAll(cls):
-    client = ndb.Client()
-    with client.context():
-      groups = cls.query(cls.active == True).fetch()
-      return [g.key.id() for g in groups]
+    """Fetch all active alert groups
+
+    Returns:
+      A list of active alert groups
+    """
+    groups = list(cls.ds_client.QueryAlertGroup())
+
+    return [cls.ds_client.GetEntityId(g) for g in groups]
 
 
   @classmethod
@@ -216,18 +211,24 @@ class AlertGroup(ndb.Model):
     The alert_group named "ungrouped" contains the alerts for further
     processing in the next iteration of of dashboard-alert-groups-update
     cron job.
+
+    Returns:
+      The 'ungrouped' entity if exists, otherwise create a new entity and
+      return None.
     '''
     ungrouped_groups = cls.Get('Ungrouped', 2)
-    client = ndb.Client()
-    with client.context():
-      if not ungrouped_groups:
-        # initiate when there is no active group called 'Ungrouped'.
-        cls(name='Ungrouped', group_type=cls.Type.reserved, active=True).put()
-        return None
-      if len(ungrouped_groups) != 1:
-        logging.warning('More than one active groups are named "Ungrouped".')
-      ungrouped = ungrouped_groups[0]
-      return ungrouped
+    if not ungrouped_groups:
+      # initiate when there is no active group called 'Ungrouped'.
+      new_group = cls.ds_client.NewAlertGroup(
+        benchmark_name='Ungrouped',
+        group_type=0
+      )
+      cls.ds_client.SaveAlertGroup(new_group)
+      return None
+    if len(ungrouped_groups) != 1:
+      logging.warning('More than one active groups are named "Ungrouped".')
+    ungrouped = ungrouped_groups[0]
+    return ungrouped
 
 
   @classmethod
@@ -241,26 +242,25 @@ class AlertGroup(ndb.Model):
     ungrouped = cls._GetUngroupedGroup()
     if not ungrouped:
       return
-    client = ndb.Client()
-    with client.context():
-      ungrouped_anomalies = ndb.get_multi(ungrouped.anomalies)
-      logging.info('Loaded %s ungrouped alerts from "ungrouped". ID(%s)',
-                  len(ungrouped_anomalies), ungrouped.key.integer_id())
 
-    parity_results = {}
-    for anomaly in ungrouped_anomalies:
-      group_ids, new_ids = cls.GetGroupsForAnomaly(
-        anomaly.test.id(), anomaly.start_revision, anomaly.end_revision,
-        create_on_ungrouped=True, parity=IS_PARITY)
-      with client.context():
-        anomaly.groups = [ndb.Key('AlertGroup', group_id) for group_id in group_ids]
+    with cls.ds_client.transaction():
+      ungrouped_anomalies = cls.ds_client.get_multi(ungrouped.anomalies)
+      logging.info('Loaded %s ungrouped alerts from "ungrouped". ID(%s)',
+                    len(ungrouped_anomalies), ungrouped.key.id)
+
+      parity_results = {}
+      for anomaly in ungrouped_anomalies:
+        group_ids, new_ids = cls.GetGroupsForAnomaly(
+          anomaly['test'].name, anomaly['start_revision'], anomaly['end_revision'],
+          create_on_ungrouped=True, parity=IS_PARITY)
+        anomaly['groups'] = [cls.ds_client.AlertGroupKey(group_id) for group_id in group_ids]
         if IS_PARITY:
-          anomaly_id = anomaly.key.integer_id()
+          anomaly_id = anomaly.key.id
           parity_results[anomaly_id] = {
             "existing_groups": list(set(group_ids) - set(new_ids)),
             "new_groups": new_ids
           }
         else:
-          anomaly.put()
+          cls.ds_client.SaveAnomaly(anomaly)
 
     return parity_results
