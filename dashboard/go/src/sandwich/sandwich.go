307ceba51d6184461eea0160448b570f1c117be4
Sean McCullough <seanmccullough@google.com>
2023-08-02 17:51:37 +0000

    [sandwich] cli tool to run sandwich workflow for a pinpoint bisect job
    Obligatory XKCD: https://xkcd.com/149/ :)
    
    This automates the manual steps outlined in https://docs.google.com/document/d/1ZzjDkOcYtZtwe4Pwy5MsLVFsPWsdaMmVs3eUxEeGs88/edit?resourcekey=0-y3p_8KyKcsg8fp7Xwvd3cQ#heading=h.m21moiozahpe
    and forces a sandwich verification workflow to execute on the same parameters
    (git revision range, configuration, benchmark, story, chart, etc)
    used by a given pinpoint autobisect job.
    
    `go run sandwich.go -pinpoint-job-id <an exixting pinpoint autobisect job id>`
    
    example from my bash history:
    ```
    > go run sandwich.go  -pinpoint-job-id 13aa46a7160000
    CreateExeuctionRequest:
    parent:"projects/chromeperf/locations/us-central1/workflows/sandwich-verification-workflow-prod" execution:{argument:"{\"anomaly\":{\"benchmark\":\"blink_perf.css\",\"bot_name\":\"android-pixel4-perf\",\"end_git_hash\":\"33414478d965f438d8348375e524ea61b8f85476\",\"measurement\":\"CustomPropertiesDependency\",\"start_git_hash\":\"2d29987d567f23860f984e4055d9436bf4d12dfb\",\"story\":\"CustomPropertiesDependency.html\",\"target\":\"performance_test_suite_android_clank_trichrome_bundle\"}}"}
    ```
    
    Bug: chromium:1419903
    Change-Id: I1638984f7afb8f7b4f8ece9344058b9cfb5a955a
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4740300
    Commit-Queue: Sean McCullough <seanmccullough@google.com>
    Reviewed-by: Leina Sun <sunxiaodi@google.com>

---
+++ dashboard/go/src/sandwich/sandwich.go
@@ -0,0 +1,126 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Program sandwich re-runs a pinpoint autobisect job as a sandwich verification workflow.
+// Runs in dry-run mode by default, so you need to specify -dry-run=false to get it to
+// actually start the workflow.
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"cloud.google.com/go/datastore"
+	executions "cloud.google.com/go/workflows/executions/apiv1"
+	executionspb "cloud.google.com/go/workflows/executions/apiv1/executionspb"
+)
+
+var (
+	app              = flag.String("app", "chromeperf", "GAE project app name")
+	location         = flag.String("location", "us-central1", "location for workflow execution")
+	workflowName     = flag.String("workflow", "sandwich-verification-workflow-prod", "name of workflow to execute")
+	pinpointJobIDStr = flag.String("pinpoint-job-id", "", "id of the pinpoint job")
+	dryRun           = flag.Bool("dry-run", true, "dry run; just print CreateExecutionRequest to stdout")
+)
+
+type BenchmarkArguments struct {
+	Benchmark string `datastore:"benchmark"`
+	Chart     string `datastore:"chart"`
+	Statistic string `datastore:"statistic"`
+	Story     string `datastore:"story"`
+}
+
+type PinpointJob struct {
+	Arguments          []byte             `datastore:"arguments"` // base64-encoded JSON
+	BenchmarkArguments BenchmarkArguments `datastore:"benchmark_arguments"`
+	ComparisonMode     string             `datastore:"comparison_mode"`
+	Configuration      string             `datastore:"configuration"`
+}
+
+func exitWithError(err error) {
+	fmt.Printf("error: %v\n", err)
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+	dsClient, err := datastore.NewClient(ctx, *app)
+	if err != nil {
+		exitWithError(err)
+	}
+	defer dsClient.Close()
+	pinpointJobID, err := strconv.ParseInt(*pinpointJobIDStr, 16, 64)
+	pinpointJobKey := datastore.IDKey("Job", pinpointJobID, nil)
+	job := &PinpointJob{}
+	err = dsClient.Get(ctx, pinpointJobKey, job)
+	if err != nil &&
+		// struct PinpointJob is a subset of fields that are in datastore, so ignore errors about missing fields on it.
+		!strings.Contains(err.Error(), "no such struct field") {
+		exitWithError(err)
+	}
+	if job.ComparisonMode != "performance" {
+		fmt.Printf("%s is not an autobisect job (its comparison_mode is '%s')\n",
+			*pinpointJobIDStr, job.ComparisonMode)
+		return
+	}
+	arguments := map[string]interface{}{}
+	err = json.Unmarshal(job.Arguments, &arguments)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	benchmark := arguments["benchmark"]
+	botName := arguments["configuration"]
+	story := arguments["story"]
+	target := arguments["target"]
+	measurement := arguments["chart"]
+	startGitHash := arguments["start_git_hash"]
+	endGitHash := arguments["end_git_hash"]
+
+	executionsClient, err := executions.NewClient(ctx)
+	if err != nil {
+		exitWithError(err)
+	}
+	defer executionsClient.Close()
+
+	workflowArgs := map[string]interface{}{
+		"anomaly": map[string]interface{}{
+			"benchmark":      benchmark,
+			"bot_name":       botName,
+			"story":          story,
+			"measurement":    measurement,
+			"target":         target,
+			"start_git_hash": startGitHash,
+			"end_git_hash":   endGitHash,
+		},
+	}
+	encodedArgs, err := json.Marshal(workflowArgs)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	createReq := &executionspb.CreateExecutionRequest{
+		Parent: fmt.Sprintf("projects/%s/locations/%s/workflows/%s", *app, *location, *workflowName),
+		Execution: &executionspb.Execution{
+			Argument: string(encodedArgs),
+		},
+	}
+	if *dryRun {
+		fmt.Printf("CreateExecutionRequest:\n%+v\n", createReq)
+		return
+	}
+	execution, err := executionsClient.CreateExecution(ctx, createReq)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	fmt.Printf("Workflow execution created successfully. Run the following command to check on its progress:\n\ngcloud workflows executions describe %s\n\n", execution.Name)
+}
