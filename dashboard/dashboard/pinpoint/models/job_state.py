a946f28d4b1da3b84489544171a36a2a8baa2210
Leina Sun <sunxiaodi@google.com>
2023-08-23 16:48:07 +0000

    Revert "[Pinpoint] prevent auto bisection from bisecting improvements"
    This reverts commit 338b5a870f426b3a5b0b431ba6f1061c7ff562f8.
    
    Reason for revert: This is causing AttributeError: 'JobState' object has no attribute '_improvement_direction' in:
    
    File "/workspace/dashboard/pinpoint/models/job_state.py", line 394, in _Compare: 
      or mean_diff < 0 and self._improvement_direction == anomaly.DOWN):
    
    Original change's description:
    > [Pinpoint] prevent auto bisection from bisecting improvements
    >
    > This CL only works on auto-bisection because manual bisections do not populate the "tags" input parameter, which is used to fetch the improvement direction from TestMetadata in DataStore.
    >
    > Bug: chromium:1351167
    > Change-Id: Id5546c31ac81b9a043396db817f6218607c5e16f
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4795192
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Leina Sun <sunxiaodi@google.com>
    > Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    
    Bug: chromium:1351167
    Change-Id: Ide6e74a716bbaa91126d57ffec6c6cce9c33e862
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/4807208
    Auto-Submit: Leina Sun <sunxiaodi@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Leina Sun <sunxiaodi@google.com>
    Commit-Queue: Leina Sun <sunxiaodi@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>

--- dashboard/dashboard/pinpoint/models/job_state.py
+++ dashboard/dashboard/pinpoint/models/job_state.py
@@ -16,7 +16,6 @@ from six.moves import http_client
 from six.moves import map  # pylint: disable=redefined-builtin
 
 from dashboard.common import math_utils
-from dashboard.models import anomaly
 from dashboard.pinpoint.models import attempt as attempt_module
 from dashboard.pinpoint.models import change as change_module
 from dashboard.pinpoint.models import compare
@@ -70,9 +69,6 @@ class JobState:
 
     self._comparison_mode = comparison_mode
     self._comparison_magnitude = comparison_magnitude
-    # only bisections and verification jobs care about the improvement direction
-    # assume unknown until specified
-    self._improvement_direction = anomaly.UNKNOWN
 
     self._pin = pin
 
@@ -137,9 +133,6 @@ class JobState:
 
     if len(self._changes) > MAX_BUILDS:
       raise errors.BuildNumberExceeded(MAX_BUILDS)
-
-  def SetImprovementDirection(self, improvement_direction):
-    self._improvement_direction = improvement_direction
 
   def Explore(self):
     """Compare Changes and bisect by adding additional Changes as needed.
@@ -388,14 +381,6 @@ class JobState:
         sample_count = (len(all_a_values) + len(all_b_values)) // 2
         logging.debug('BisectDebug: Comparing values: %s, %s',
             all_a_values, all_b_values)
-        mean_diff = Mean(all_b_values) - Mean(all_a_values)
-        # if improvement, return same
-        if ((mean_diff > 0 and self._improvement_direction == anomaly.UP)
-            or mean_diff < 0 and self._improvement_direction == anomaly.DOWN):
-          logging.debug(
-              'BisectDebug: Improvement found. Improvement direction: %s, mean diff: %s',
-              self._improvement_direction, mean_diff)
-          return compare.SAME
         comparison, _, _, _ = compare.Compare(
             all_a_values,
             all_b_values,
