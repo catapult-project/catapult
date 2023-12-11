38c800656ce8eea67d38f1ff5922f870fd128ad7
Wenbin Zhang <wenbinzhang@google.com>
2023-12-11 17:59:30 +0000

    Revert "[Chromeperf] Find anomaly workflow finds groups using anomaly id instead of the key/revisions"
    This reverts commit 1ed720248420d71ebe309ba774f5405021acfd49.
    
    Reason for revert:
    Changing the implementation.
    
    Original change's description:
    > [Chromeperf] Find anomaly workflow finds groups using anomaly id instead of the key/revisions
    >
    > This CL updated the find-anomaly workflow to use the new API created in:
    > https://chromium-review.googlesource.com/c/catapult/+/5096993?usp=dashboard
    >
    > Bug: chromium:1508132
    > Change-Id: If9ddbbd662b0b74cd8b0f3efe798d0a9076e032b
    > Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/5097607
    > Reviewed-by: Leina Sun <sunxiaodi@google.com>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    
    Bug: chromium:1508132
    Change-Id: I020c9d9f0e9bfb9278f86bc733a1614c142c3d12
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/5109551
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Leina Sun <sunxiaodi@google.com>

--- dashboard/dashboard/find_anomalies.py
+++ dashboard/dashboard/find_anomalies.py
@@ -162,20 +162,18 @@ def _ProcessTestStat(test, stat, rows, ref_rows):
     alert_groups = alert_group.AlertGroup.GetGroupsForAnomaly(a, subscriptions)
     try:
       # parity results from perf_issue_service
-      groups_by_request = perf_issue_service_client.GetAlertGroupsForAnomalyById(
-          a)
+      groups_by_request = perf_issue_service_client.GetAlertGroupsForAnomaly(a)
       group_keys = [ndb.Key('AlertGroup', g) for g in groups_by_request]
       if sorted(group_keys) != sorted(alert_groups):
-        logging.warning(
-            'Imparity found for GetAlertGroupsForAnomalyById. %s, %s',
-            group_keys, alert_groups)
+        logging.warning('Imparity found for GetAlertGroupsForAnomaly. %s, %s',
+                        group_keys, alert_groups)
         cloud_metric.PublishPerfIssueServiceGroupingImpariry(
-            'GetAlertGroupsForAnomalyById')
+            'GetAlertGroupsForAnomaly')
       a.groups = group_keys
       logging.debug('[GroupingDebug] Anomaly %s is associated with groups %s.',
                     a.key, a.groups)
     except Exception as e:  # pylint: disable=broad-except
-      logging.warning('Parity logic failed in GetAlertGroupsForAnomalyById. %s',
+      logging.warning('Parity logic failed in GetAlertGroupsForAnomaly. %s',
                       str(e))
 
   yield ndb.put_multi_async(anomalies)
