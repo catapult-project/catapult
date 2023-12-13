2b5ed3cca6c9a158bb696c0bd933545f0ff61b81
Ian Struiksma <ianstruiksma@google.com>
2023-12-13 08:56:58 +0000

    [WPR] Add support for brotli compression
    WPR cannot transform sites with Content-Encoding:br for brotli compression
    and thus cannot inject scripts when needed on these sites.
    
    This Cl adds support using the cbrotli package.
    
    Change-Id: Iabdfa3fa314fca7039ab1bfb43f7c38dd11bb4c4
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/5118529
    Commit-Queue: Ian Struiksma <ianstruiksma@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>

--- web_page_replay_go/src/webpagereplay/transformers.go
+++ web_page_replay_go/src/webpagereplay/transformers.go
@@ -21,6 +21,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/google/brotli/go/cbrotli"
 )
 
 type readerWithError struct {
@@ -133,7 +135,8 @@ func decompressBody(ce string, compressed []byte) ([]byte, error) {
 		}
 	case "deflate":
 		r = flate.NewReader(bytes.NewReader(compressed))
-	// TODO(catapult:3742): Implement Brotli support.
+	case "br":
+		r = cbrotli.NewReader(bytes.NewReader(compressed))
 	default:
 		// Unknown compression type or uncompressed.
 		return compressed, errors.New("unknown compression: " + ce)
@@ -157,6 +160,9 @@ func CompressBody(ae string, uncompressed []byte) ([]byte, string, error) {
 	case strings.Contains(ae, "deflate"):
 		w, _ = flate.NewWriter(&buf, flate.DefaultCompression) // never fails
 		outCE = "deflate"
+	case strings.Contains(ae, "br"):
+		w = cbrotli.NewWriter(&buf, cbrotli.WriterOptions{Quality: 5})
+		outCE = "br"
 	default:
 		// Unknown compression type or compression not allowed.
 		return uncompressed, "identity", errors.New("unknown compression: " + ae)
