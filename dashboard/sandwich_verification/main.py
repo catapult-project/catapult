253a9c999e0576f1b901ce3c60337c883c73d8b3
Alex Sun <sunpeng@google.com>
2024-02-07 19:45:26 +0000

    Update the logic to handle "Infinity" CIs that may happen when test result changes from zero to none zero.
    Issue:
    Some of Sandwich verification workflows failed at the postRegressionDetection step.
    Sample failed verification workflow: https://pantheon.corp.google.com/workflows/workflow/us-central1/sandwich-verification-workflow-prod/execution/ecbc6d37-5b96-43b8-8593-2df0c9d2f9ae/logs?e=-13802955&mods=component_inspector&project=chromeperf
    Sample pinpoint job: https://pinpoint-dot-chromeperf.appspot.com/job/1408951d6e0000
    
    Root cause:
    CABE code returns lower ci and/or upper ci as “Infinity”, while the cloud function “regression_detection” code cannot handle “Infinity”, so the cloud function “regression_detection” will return an error “500 Internal Server Error”. That will fail the verification workflow.
    
    Fix:
    Update the cloud function code to handle the infinity CI values.
    
    Change-Id: I63c35225ef09b85d2e3751b65366ddb22b80740b
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/5273980
    Commit-Queue: Alex Sun <sunpeng@google.com>
    Reviewed-by: Leina Sun <sunxiaodi@google.com>

--- dashboard/sandwich_verification/main.py
+++ dashboard/sandwich_verification/main.py
@@ -201,7 +201,11 @@ def RegressionDetection(request):
   # TODO(crbug/1455502): Define regression threshold based on story
   # specific attributes such as the anomaly's magnitude or the
   # subscription thresholds.
-  if ci_lower*ci_upper > 0 and p_value < 0.05:
+  # CI values might be Infinity if the result changes from zero to none zero
+  if ci_lower == "Infinity" or ci_upper == "Infinity":
+    if p_value < 0.05:
+      decision = True
+  elif ci_lower * ci_upper > 0 and p_value < 0.05:
     decision = True
 
   return jsonify({'decision': decision})
