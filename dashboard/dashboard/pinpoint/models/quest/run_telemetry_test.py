3ea4639ac5703fc483bbb7117362006418ef8ee5
Farid <faridzad@google.com>
2025-04-08 10:25:42 -0700

    Run the fast version of Crossbench loadline benchmarks.
    Fixed: 409328939
    Bug: chromium:409328939
    Change-Id: Ife93c4071148e45e0972d04e9520cd2e9e9a7424
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/6441772
    Commit-Queue: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>

--- dashboard/dashboard/pinpoint/models/quest/run_telemetry_test.py
+++ dashboard/dashboard/pinpoint/models/quest/run_telemetry_test.py
@@ -68,8 +68,8 @@ _CROSSBENCH_NAME = {
     'speedometer3.crossbench': 'speedometer_3.0',
     'speedometer2.0.crossbench': 'speedometer_2.0',
     'speedometer2.1.crossbench': 'speedometer_2.1',
-    'loadline_phone.crossbench': 'loadline-phone',
-    'loadline_tablet.crossbench': 'loadline-tablet'
+    'loadline_phone.crossbench': 'loadline-phone-fast',
+    'loadline_tablet.crossbench': 'loadline-tablet-fast'
 }
 
 
