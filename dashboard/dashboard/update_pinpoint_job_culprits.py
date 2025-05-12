d428ca55cb2cfb486ab08ea980f8d55585cf61c5
Wenbin Zhang <wenbinzhang@google.com>
2025-05-12 10:36:44 -0700

    [chromeperf] backfill pinpoint jobs in smaller batches
    The current cron job causes timeouts when query jobs on two years.
    
    This CL breaks down the query into per month.
    
    Bug: 406405606
    Change-Id: I398b5d7a1974b24bf93c21db7a5a385f313932b7
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/6537076
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Leina Sun <sunxiaodi@google.com>

--- dashboard/dashboard/update_pinpoint_job_culprits.py
+++ dashboard/dashboard/update_pinpoint_job_culprits.py
@@ -16,6 +16,7 @@ from dashboard.common import datastore_hooks
 from dashboard.pinpoint.models import job as job_module
 from dashboard.pinpoint.models import job_state
 
+LOOK_BACK_MONTHS_TOTAL = 24
 
 def UpdatePinpointJobCulpritsPost():
   """Checks if alerts have recovered, and marks them if so.
@@ -25,29 +26,67 @@ def UpdatePinpointJobCulpritsPost():
   """
   datastore_hooks.SetPrivilegedRequest()
 
+  counter = 0
+  for i in range(LOOK_BACK_MONTHS_TOTAL):
+    jobs_updated = _LookBackAndUpdateJobsWithDiff(i)
+    counter += jobs_updated
+
+  logging.debug('[CULPRITS] In total %d jobs are updated.'.counter)
+
+  return make_response('Update finished.')
+
+
+# Load the monthly changes for Pinpoint jobs between (now - look_back_months)
+# and (now - look_back_months - 1). If the job has difference_count > 0, try
+# to load the difference info from job state, and populate the commit info
+# into a new column 'culprits'.
+def _LookBackAndUpdateJobsWithDiff(look_back_months):
+  current = datetime.now() - relativedelta(months=look_back_months)
+  one_month_ago = current - relativedelta(months=1)
+
+  # Fetch the jobs for the month, which are bisection jobs, with different
+  # count > 0, and no culprits are been populated yet.
   query = job_module.Job.query()
-  two_years_ago = datetime.now() - relativedelta(years=2)
-  query = query.filter(job_module.Job.created > two_years_ago)
+  query = query.filter(job_module.Job.created > one_month_ago)
+  query = query.filter(job_module.Job.created < current)
   query = query.filter(job_module.Job.comparison_mode == job_state.PERFORMANCE)
   jobs = query.order(-job_module.Job.created).fetch()
 
-  logging.debug('[CULPRITS] Loaded %d bisect jobs in the last 2 years.',
-                len(jobs))
+  logging.debug('[CULPRITS] Loaded %d bisect jobs between %s and %s.',
+                len(jobs), one_month_ago.date(), current.date())
   jobs = [j for j in jobs if j.difference_count and j.difference_count > 0]
   logging.debug('[CULPRITS] Loaded %d jobs with DIFF.', len(jobs))
   jobs = [j for j in jobs if j.culprits == []]
   logging.debug('[CULPRITS] Loaded %d jobs with no culprits.', len(jobs))
 
   culprits = []
+  jobs_updated = 0
   for job in jobs:
     state = job.state
     differences = state.Differences()
-    logging.debug('[CULPRITS] Processing job %s with %d differences',
-                  job.key.id, len(differences))
+    logging.debug(
+        '[CULPRITS] Processing job %s with %d differences and culprits: %s',
+        job.key.id, len(differences), job.culprits)
+
+    if len(differences) == 0:
+      logging.warning(
+          '[CULPRITS] Job %s has diff count %d but has no difference in state.',
+          job.key.id, job.difference_count)
+      continue
+
+    if job.culprits:
+      logging.debug('[CULPRITS] Job %s already has culprits set: %s.',
+                    job.key.id, job.culprits)
+      continue
+
     for commit_pair in differences:
+      # The second Commit from the 'difference' is one caused the change.
       culprit = commit_pair[1].AsDict()
+      # For those with drilldowns, we may have multiple commits from different
+      # repo. The last commit is the one we report.
       commit = culprit['commits'][-1]
       logging.debug('[CULPRITS] ID: %s, Culprit: %s', job.key.id, commit)
+
       repo = commit.get('repository')
       git_hash = commit.get('git_hash')
       if not repo or not git_hash:
@@ -56,12 +95,11 @@ def UpdatePinpointJobCulpritsPost():
             repo, git_hash)
         continue
       culprits.append('%s/%s' % (repo, git_hash))
+
     logging.debug('[CULPRITS] Adding culprits %s to Pinpoint job %s', culprits,
                   job.key.id)
     job.culprits = culprits
     job.put()
-    # TODO (b/416555830) Start with backfilling culprits on one job per call,
-    # for debuggning purposes.
-    break
+    jobs_updated += 1
 
-  return make_response('')
+  return jobs_updated
