5477c6dfde1132b685c73edc16e1bc71449a691d
Hiroki Nakagawa <nhiroki@chromium.org>
2025-05-26 03:17:33 +0000

    Gardener: Disable testEnsureHotAfterColdRun on Linux
    The test is flaky on the Linux Tests (dbg):
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/126399/overview
    
    Bug: chromium:414813537
    Change-Id: I147078cd9d1ea53f393b0a47b024f26dedc9d7f6
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/6579830
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>

--- telemetry/telemetry/page/cache_temperature_unittest.py
+++ telemetry/telemetry/page/cache_temperature_unittest.py
@@ -147,6 +147,7 @@ class CacheTemperatureTests(browser_test_case.BrowserTestCase):
   @decorators.Disabled('reference')
   @decorators.Disabled('chromeos')  # crbug.com/840033
   @decorators.Disabled('win')  # crbug.com/840033
+  @decorators.Disabled('linux')  # crbug.com/414813537
   def testEnsureHotAfterColdRun(self):
     with self.CaptureTraceMarkers():
       story_set = story.StorySet()
