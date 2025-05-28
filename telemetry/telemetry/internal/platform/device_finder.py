3af14c3b4e2496ad1b85e1f33823f006e30fe8e0
Jeff Yoon <jeffyoon@google.com>
2025-05-27 17:06:08 -0700

    [telemetry] add debug stmt to device finder
    Adding debug logs to help unpack Al testing
    
    Bug: 414619873
    Change-Id: Ic62048e91e605bc172dd2258d3c2f39c3505927f
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/6594256
    Auto-Submit: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>

--- telemetry/telemetry/internal/platform/device_finder.py
+++ telemetry/telemetry/internal/platform/device_finder.py
@@ -5,6 +5,9 @@
 """Finds devices that can be controlled by telemetry."""
 
 from __future__ import absolute_import
+
+import logging
+
 from telemetry.internal.platform import android_device
 from telemetry.internal.platform import cast_device
 from telemetry.internal.platform import cros_device
@@ -32,6 +35,8 @@ def _GetDeviceFinders(supported_platforms):
     device_finders.append(cros_device)
   if 'fuchsia' in supported_platforms:
     device_finders.append(fuchsia_device)
+
+  logging.debug('Device finders: %s', device_finders)
   return device_finders
 
 
@@ -48,15 +53,20 @@ def GetDevicesMatchingOptions(options):
   devices = []
   remote_platform_options = options.remote_platform_options
 
+  logging.debug('Remote Platform Options: %s', options.remote_platform_options)
   # Establish a connection to network devices so they show up in adb.
   if remote_platform_options.connect_to_device_over_network:
     android_device.ConnectToTcpDevice(remote_platform_options.device)
 
+  logging.debug('Remote platform options device: %s',
+                remote_platform_options.device)
   if (not remote_platform_options.device or
       remote_platform_options.device == 'list'):
     devices = _GetAllAvailableDevices(options)
   elif (remote_platform_options.device == 'android'
         or remote_platform_options.connect_to_device_over_network):
+    logging.debug('Connect to device over network: %s',
+                  remote_platform_options.connect_to_device_over_network)
     devices = android_device.FindAllAvailableDevices(options)
   else:
     devices = _GetAllAvailableDevices(options)
@@ -64,4 +74,5 @@ def GetDevicesMatchingOptions(options):
                options.remote_platform_options.device]
 
   devices.sort(key=lambda device: device.name)
+  logging.debug('Devices: %s', devices)
   return devices
