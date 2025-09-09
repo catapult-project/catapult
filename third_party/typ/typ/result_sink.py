e42fa6beeb240437887d7e5c201b79e265b8eb8d
Ben Joyce <bjoyce@google.com>
2025-09-09 13:06:19 -0700

    Adjust name for when parsing as a webgpu test.
    Some test suites on dawn-linux-x64-deps-rel only have webgpu in their
    name and are getting parsed as pyunit in this case.
    
    This passed in: https://ci.chromium.org/ui/inv/u-bjoyce-2025-09-09-19-55-00-681e29ecb2d3d4fc/test-results
    
    Bug: chromium:418015486
    Change-Id: Iba7744657cbc6e62843392af09dabd26a66fb42f
    Reviewed-on: https://chromium-review.googlesource.com/c/catapult/+/6932803
    Commit-Queue: Benjamin Joyce (Ben) <bjoyce@google.com>
    Auto-Submit: Benjamin Joyce (Ben) <bjoyce@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>

--- third_party/typ/typ/result_sink.py
+++ third_party/typ/typ/result_sink.py
@@ -529,7 +529,7 @@ def _create_test_id_struct_dict(test_id, module_scheme):
       case_name = test_split[1] if len(test_split) > 1 else test_split[0]
       struct_test_dict['fineName'] =  fine_name
       struct_test_dict['caseNameComponents'] = [case_name]
-    elif module_scheme == ModuleScheme.WEBGPUCTS or 'webgpu_cts' in test_id:
+    elif module_scheme == ModuleScheme.WEBGPUCTS or 'webgpu' in test_id:
       # gpu_tests take the form of:
       # gpu_tests.WebGpu.{suite}:{path,to,file}:{test,test}:{param=}
       # the parameters can also have ':' so cannot use rsplit.
